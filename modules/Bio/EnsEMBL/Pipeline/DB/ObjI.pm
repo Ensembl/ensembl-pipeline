a9972b7d598e117ac2c342095bfccdb2357c1921
Michele Clamp <michele@sanger.ac.uk>
2000-03-10 16:12:57 +0000

    Made interfaces for database object

---
+++ modules/Bio/EnsEMBL/Pipeline/DB/ObjI.pm
@@ -0,0 +1,126 @@
+#
+# Object for storing the connection to the analysis database
+#
+# Cared for by Michele Clamp  <michele@sanger.ac.uk>
+#
+# Copyright Michele Clamp
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::DB::ObjI
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Interface for the connection to the analysis database
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+package Bio::EnsEMBL::Pipeline::ObjI;
+
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Object;
+
+use Bio::Root::Object;
+
+# Inherits from the base bioperl object
+@ISA = qw(Bio::Root::Object);
+
+
+=head2 get_Job {
+
+  Title   : get_Job
+  Usage   : my $job = $db->get_Job($id)
+  Function: Retrieves a job from the database
+            when given its id
+  Returns : Bio::EnsEMBL::Pipeline::DB::JobI
+  Args    : int
+
+=cut
+
+
+sub get_Job {
+    my ($self) = @_;
+
+    $self->throw("Method get_Job not implemented");
+}
+
+
+=head2 get_JobsByInputId {
+
+  Title   : get_JobsByInputId
+  Usage   : my @jobs = $db->get_JobsByInputId($id)
+  Function: Retrieves all jobs in the database
+            that have a certain input id.
+	    Input id will usually be associated with
+            a sequence in the main ensembl database.
+  Returns : @Bio::EnsEMBL::Pipeline::DB::JobI
+  Args    : int
+
+=cut
+
+
+sub get_JobsByInputId {
+    my ($self) = @_;
+
+    $self->throw("Method get_JobsByInputId not implemented");
+}
+
+
+=head2 write_Analysis {
+
+  Title   : write_Analysis
+  Usage   : $db->write_Analysis($analysis)
+  Function: Write an analysis object into the database
+            with a check as to whether it exists.
+  Returns : nothing
+  Args    : int
+
+=cut
+
+
+sub write_Analysis {
+    my ($self) = @_;
+
+    $self->throw("Method write_Analysis not implemented");
+}
+
+
+=head2 get_Analysis {
+
+  Title   : get_Analysis
+  Usage   : my $analyis = $db->get_Analysis($id)
+  Function: Retrieves an analysis object with
+            id $id
+  Returns : Bio::EnsEMBL::Pipeline::Analysis
+  Args    : int
+
+=cut
+
+
+sub get_Analysis {
+    my ($self) = @_;
+
+    $self->throw("Method get_Analysis not implemented");
+}
+
+
