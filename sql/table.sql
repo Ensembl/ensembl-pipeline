7f67803f78ebb4d171d8108cca2e2c62e259cc77
Michele Clamp <michele@sanger.ac.uk>
2000-03-23 16:44:55 +0000

    Added indices to tables. Added filenames in job table

--- sql/table.sql
+++ sql/table.sql
@@ -42,61 +42,6 @@ CREATE TABLE analysis_set (
 );
 
 #
-# Table structure for table 'clone'
-#
-CREATE TABLE clone (
-       disk_id varchar(40) NOT NULL,
-       clone_group set("SU","EU","SF","EF") NOT NULL,
-       chromosome varchar(10) DEFAULT 'unk' NOT NULL,
-       last_check datetime NOT NULL,
-       created datetime NOT NULL,
-       dna_update_state int(10) unsigned DEFAULT '0' NOT NULL,
-       update_state int(10) unsigned DEFAULT '2' NOT NULL,
-       update_label varchar(40) DEFAULT 'dna_read' NOT NULL,
-       update_date datetime DEFAULT 'now()' NOT NULL,
-       modified datetime NOT NULL,
-       internal_lock int(10) DEFAULT '1' NOT NULL,
-       external_lock int(10) DEFAULT '1' NOT NULL,
-       PRIMARY KEY (disk_id)
-);
-
-#
-# Table structure for table 'exon_pair'
-#
-CREATE TABLE exon_pair (
-  exon1_id varchar(40) DEFAULT '' NOT NULL,
-  exon2_id varchar(40) DEFAULT '' NOT NULL,
-  created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-  type varchar(40) DEFAULT '' NOT NULL,
-  exon1_version varchar(40) DEFAULT '0' NOT NULL,
-  exon2_version varchar(40) DEFAULT '0' NOT NULL,
-  PRIMARY KEY (exon1_id,exon2_id,exon1_version,exon2_version,type)
-);
-
-#
-# Table structure for table 'job'
-#
-CREATE TABLE job (
-  id int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
-  input_id int(10) unsigned DEFAULT '0' NOT NULL,
-  analysis int(10) unsigned DEFAULT '0' NOT NULL,
-  LSF_id int(10) unsigned DEFAULT '0',
-  machine varchar(40) DEFAULT '',
-  object mediumtext,
-  queue varchar(40) DEFAULT '',
-  PRIMARY KEY (id)
-);
-
-#
-# Table structure for table 'jobstatus'
-#
-CREATE TABLE jobstatus (
-  id int(10) unsigned DEFAULT '0' NOT NULL,
-  status varchar(40) DEFAULT 'CREATED' NOT NULL,
-  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL
-);
-
-#
 # Table structure for table 'exon'
 #
 CREATE TABLE exon (
@@ -117,4 +62,66 @@ CREATE TABLE exon (
 );
 
 #
+# Table structure for table 'exon_pair'
+#
+CREATE TABLE exon_pair (
+  exon1_id varchar(40) DEFAULT '' NOT NULL,
+  exon2_id varchar(40) DEFAULT '' NOT NULL,
+  created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  type varchar(40) DEFAULT '' NOT NULL,
+  exon1_version varchar(40) DEFAULT '0' NOT NULL,
+  exon2_version varchar(40) DEFAULT '0' NOT NULL,
+  PRIMARY KEY (exon1_id,exon2_id,exon1_version,exon2_version,type),
+  KEY exon1_index (exon1_id),
+  KEY exon2_index (exon2_id)
+);
+
+#
+# Table structure for table 'job'
+#
+CREATE TABLE job (
+  id int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+  input_id varchar(40) DEFAULT '' NOT NULL,
+  analysis int(10) unsigned DEFAULT '0' NOT NULL,
+  LSF_id int(10) unsigned DEFAULT '0',
+  machine varchar(40) DEFAULT '',
+  object mediumtext,
+  queue varchar(40) DEFAULT '',
+  stdout_file varchar(100) DEFAULT '' NOT NULL,
+  stderr_file varchar(100) DEFAULT '' NOT NULL,
+  input_object_file varchar(100) DEFAULT '' NOT NULL,
+  output_file varchar(100) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id),
+  KEY input_index (input_id),
+  KEY analysis_index (analysis)
+);
+
+#
+# Table structure for table 'jobstatus'
+#
+CREATE TABLE jobstatus (
+  id int(10) unsigned DEFAULT '0' NOT NULL,
+  status varchar(40) DEFAULT 'CREATED' NOT NULL,
+  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL
+);
+
+#
+# Table structure for table 'clone'
+#
+CREATE TABLE clone (
+       disk_id varchar(40) NOT NULL,
+       clone_group set("SU","EU","SF","EF") NOT NULL,
+       chromosome varchar(10) DEFAULT 'unk' NOT NULL,
+       last_check datetime NOT NULL,
+       created datetime NOT NULL,
+       dna_update_state int(10) unsigned DEFAULT '0' NOT NULL,
+       update_state int(10) unsigned DEFAULT '2' NOT NULL,
+       update_label varchar(40) DEFAULT 'dna_read' NOT NULL,
+       update_date datetime DEFAULT 'now()' NOT NULL,
+       modified datetime NOT NULL,
+       internal_lock int(10) DEFAULT '1' NOT NULL,
+       external_lock int(10) DEFAULT '1' NOT NULL,
+       PRIMARY KEY (disk_id)
+);
+
 
