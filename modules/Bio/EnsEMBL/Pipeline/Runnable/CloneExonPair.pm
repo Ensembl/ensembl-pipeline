528e261b0d64df092674c980664102e450c10fea
Michele Clamp <michele@sanger.ac.uk>
2000-03-24 17:29:18 +0000

    Added LSF modules to submit and query the LSF queue Added stdout/stderr file output to SimpleJob Added simple test Runnable implementation ExonPair Modified JobI to have stdout/stderr files Job now contains an LSFJob. Contig added - v. simple implementation of the interface but it has ability to make ExonPairs from features. Obj can query the pipeline queue as to which jobs are running. CloneExonPair and TransferClone are implementations of RunnableI

---
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/CloneExonPair.pm
@@ -0,0 +1,145 @@
+#
+# Mickeymouse implementation of RunnableI
+#
+# Cared for by Michele Clamp  <michele@sanger.ac.uk>
+#
+# Copyright Michele Clamp
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::Runnable::CloneExonPair
+
+=head1 SYNOPSIS
+
+    my $runnable = new Bio::EnsEMBL::Pipeline::Runnable::CloneExonPair(-clone => $clone);
+    my $status   = $runnable->run;
+    my @pairs    = $runnable->output;
+
+=head1 DESCRIPTION
+
+Mickeymouse implementation of RunnableI that returns the process list
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+package Bio::EnsEMBL::Pipeline::Runnable::CloneExonPair;
+
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Object;
+
+use Bio::EnsEMBL::Pipeline::RunnableI;
+
+@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableI Bio::Root::Object);
+
+sub _initialize {
+    my ($self,@args) = @_;
+
+    my $make = $self->SUPER::_initialize;
+
+    my ($clone) = $self->_rearrange([qw(CLONE
+					)],@args);
+
+    $clone                                  || $self->throw("No input clone object");
+    $clone->isa("Bio::EnsEMBL::DB::CloneI") || $self->throw("Clone isn't a Bio::EnsEMBL::DB::CloneI");
+
+    $self->clone($clone);
+    return $make; # success - we hope!
+}
+
+
+=head2 run
+  Title   : run
+  Usage   : $self->run
+  Function: Runs a ps command and stores the
+            output in an array
+  Returns : nothing
+  Args    : none
+
+=cut
+
+sub run {
+    my ($self) = @_;
+
+    my $clone = $self->clone;
+
+    foreach my $contig ($clone->get_all_Contigs) {
+	my @genes    = $contig->get_all_Genes;
+	my @features = $contig->get_all_SimilarityFeatures;
+
+	foreach my $gene (@genes) {
+	    foreach my $exon ($gene->each_unique_Exon) {
+		$exon->find_supporting_evidence(\@features);
+	    }
+	    $contig->make_ExonPairs($gene);
+	}
+
+	
+	my @pairs = $contig->get_all_ExonPairs;
+	
+	foreach my $p (@pairs) {
+	    $self->add_ExonPair($p);
+	}
+    }
+
+    $self->{_clone} = undef;
+}
+
+
+=head2 output
+  Title   : output
+  Usage   : my @out = $self->output
+  Function: Returns the output from the ps
+            command in an array of hashes
+            Each element of the array contains
+            details of one process
+  Returns : @HASH
+  Args    : none
+
+=cut
+
+sub output {
+    my ($self) = @_;
+
+    return @{$self->{_pairs}};
+
+}
+
+sub add_ExonPair {
+    my ($self,$arg) = @_;
+
+    push(@{$self->{_pairs}},$arg);
+}
+
+sub clone {
+    my ($self,$arg) = @_;
+
+    if (defined($arg)) {
+	if ($arg->isa("Bio::EnsEMBL::DB::CloneI")) {
+	    $self->{_clone} = $arg;
+	} else {
+
+	    $self->throw("[$arg] is not a Bio::EnsEMBL::DB::CloneI");
+	}
+    }
+
+    return $self->{_clone};
+}
+
+1;
