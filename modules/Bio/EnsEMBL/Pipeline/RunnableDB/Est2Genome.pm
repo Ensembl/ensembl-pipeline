e8269270cfcff0378781e6d0659dd31f7e44bfa9
Michele Clamp <michele@sanger.ac.uk>
2000-04-26 12:26:50 +0000

    Added files

---
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/Est2Genome.pm
@@ -0,0 +1,153 @@
+#!/usr/local/bin/perl
+
+#
+#
+# Cared for by Michele Clamp  <michele@sanger.ac.uk>
+#
+# Copyright Michele Clamp
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::RunnableDB::AlignFeature
+
+=head1 SYNOPSIS
+
+    my $obj = Bio::EnsEMBL::Pipeline::RunnableDB::Est2Genome->new(
+					     -dbobj     => $db,
+					     -input_id  => $id
+                                             );
+    $obj->fetch_input
+    $obj->run
+
+    my @newfeatures = $obj->output;
+
+
+=head1 DESCRIPTION
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+package Bio::EnsEMBL::Pipeline::RunnableDB::Est2Genome
+
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Object;
+use Bio::EnsEMBL::Pipeline::Runnable::AlignFeature;
+
+
+@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDBI Bio::Root::Object );
+
+sub _initialize {
+    my ($self,@args) = @_;
+    my $make = $self->SUPER::_initialize(@_);    
+           
+    $self->{'_fplist'} = []; #create key to an array of feature pairs
+    
+    my( $dbobj, $input_id ) = $self->_rearrange(['DBOBJ'
+						 'INPUT_ID'], @args);
+       
+    $self->throw("No database handle input")           unless defined($dbobj);
+    $self->throw("[$dbobj] is not a Bio::EnsEMBL::Pipeline::DB::ObjI") unless $dbobj->isa("Bio::EnsEMBL::Pipeline::DB::ObjI");
+    $self->dbobj($dbobj);
+
+    $self->throw("No input id input") unless defined($input_id);
+    $self->input_id($input_id);
+    
+    return $self; # success - we hope!
+}
+
+=head2 dbobj
+
+    Title   :   dbobj
+    Usage   :   $self->dbobj($db)
+    Function:   Get/set method for database handle
+    Returns :   Bio::EnsEMBL::Pipeline::DB::ObjI
+    Args    :   
+
+=cut 
+
+sub dbobj {
+    my( $self, $value ) = @_;    
+    if ($value) {
+
+        $value->isa("Bio::EnsEMBL::Pipeline::DB::ObjI") || $self->throw("Input [$value] isn't a Bio::EnsEMBL::Pipeline::DB::ObjI");
+        $self->{'_dbobj'} = $value;
+    }
+    return $self->{'_dbobj'};
+}
+
+=head2 fetch_input
+
+    Title   :   fetch_input
+    Usage   :   $self->fetch_input
+    Function:   Fetchs input data for est2genome from the database
+    Returns :   nothing
+    Args    :   none
+
+=cut
+
+sub fetch_input {
+    my( $self) = @_;
+    
+
+    $self->throw("No input id") unless defined($self->input_id);
+
+    my $cloneid  = $self->input_id;
+    my $clone    = $self->dbobj->get_Clone($cloneid);
+    my $genseq   = $clone->primary_seq;
+    my @features = $clone->get_all_SimilarityFeatures;
+    my $runnable = new Bio::EnsEMBL::Pipeline::Runnable::AlignFeature(-genomic  => $genseq,
+							    -features => \@features);
+
+    $self->runnable($runnable);
+}
+
+
+sub runnable {
+    my ($self,$arg) = @_;
+
+    if (defined($arg)) {
+	$self->throw("[$arg] is not a Bio::EnsEMBL::Pipeline::RunnableI") unless $arg->isa("Bio::EnsEMBL::Pipeline::RunnableI");
+	
+	$self->{_runnable} = $arg;
+    }
+
+    return $self->{_runnable};
+}
+
+sub run {
+    my ($self) = @_;
+
+    $self->throw("Can't run - no runnable object") unless defined($self->runnable);
+
+    $self->runnable->minirun;
+}
+
+sub output {
+    my ($self) = @_;
+
+    $self->throw("Can't return output  - no runnable object") unless defined($self->runnable);
+
+    return $self->runnable->output;
+}
+
+1;
+
+
---
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDBI.pm
@@ -0,0 +1,59 @@
+#
+# Interface for running programs
+#
+# Cared for by Michele Clamp  <michele@sanger.ac.uk>
+#
+# Copyright Michele Clamp
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::RunnableDBI
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Interface for running external programs
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+package Bio::EnsEMBL::Pipeline::RunnableDBI;
+
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Object;
+
+use Bio::Root::Object;
+
+@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableI Bio::Root::Object);
+
+sub dbobj {
+    my ($self) = @_;
+
+    $self->throw("dbobj not implemented in Bio::EnsEMBL::Pipeline::RunnableDBI");
+}
+
+sub fetch_input {
+    my ($self) = @_;
+
+    $self->throw("fetch_input not implemented in Bio::EnsEMBL::Pipeline::RunnableDBI");
+
+}
+
