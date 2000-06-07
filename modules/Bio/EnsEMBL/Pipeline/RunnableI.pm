6dd728e89f097b0abab2f562216192149e86a23b
Matloob Qureshi <mq2@sanger.ac.uk>
2000-06-07 12:46:06 +0000

    Added functions used by runnable modules

--- modules/Bio/EnsEMBL/Pipeline/RunnableI.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableI.pm
@@ -86,14 +86,178 @@ sub output {
 #########################
 # Added by MAQ 
 # functions used by Runnable modules replacing hp.pl functions
+# These aren't really interfaces more to do with background jobs.
 #########################
-
 
 sub locate_runnable {
     my ($self, $runnable) = @_;
     if ($runnable)
     {
-        Bio::EnsEMBL::Programs->import($runnable);
-        return Bio::EnsEMBL::Programs::Program_Paths{$runnable};
+        Bio::EnsEMBL::Analysis::Programs->import($runnable);
+        return $Bio::EnsEMBL::Analysis::Programs::Program_Paths { $runnable };
     }
+}
+
+#disk io methods
+
+#parsefile is a public method
+sub parsefile {
+    my ($self, $filename) = @_;
+    $self->results($filename) if ($filename);
+    $self->parse_results();
+}
+
+sub filename {
+    my ($self, $filename) = @_;
+    $self->{_filename} = $filename if ($filename);
+    return $self->{_filename};
+}
+
+sub results {
+    my ($self, $results) = @_;
+    $self->{_results} = $results if ($results);
+    return $self->{_results};
+}
+
+=head2 protect
+
+    Title   :   protect
+    Usage   :   $obj->protect('.masked', '.p');
+    Function:   Protects files with suffix from deletion when execution ends
+    Args    :   File suffixes
+
+=cut
+
+sub protect {
+    my ($self, @filename) =@_;
+    push (@{$self->{_protected}}, @filename) if (@filename);
+    return @{$self->{_protected}};
+}
+
+=head2 workdir
+
+    Title   :   workdir
+    Usage   :   $obj->wordir('~humpub/temp');
+    Function:   Get/set method for the location of a directory to contain temp files
+    Args    :   File path (optional)
+
+=cut
+
+sub workdir {
+    my ($self, $directory) = @_;
+    if ($directory)
+    {
+        mkdir ($directory, '777') unless (-d $directory);
+        $self->throw ("$directory doesn't exist\n") unless (-d $directory);
+        $self->{_workdir} = $directory;
+    }
+    return $self->{_workdir};
+}
+
+sub writefile {
+    my ($self) = @_;
+    print "Writing sequence to ".$self->filename."\n";
+    #create Bio::SeqIO object and save to file
+    my $clone_out = Bio::SeqIO->new(-file => ">".$self->filename , '-format' => 'Fasta')
+            or $self->throw("Can't create new Bio::SeqIO from ".$self->filename.":$!\n");
+    $clone_out->write_seq($self->clone) 
+            or $self->throw("Couldn't write to file ".$self->filename.":$!");
+}
+
+sub deletefiles {
+    my ($self) = @_;
+    #delete all analysis files 
+    my @list = glob($self->filename."*");
+    foreach my $result (@list)
+    {
+        my $protected = undef; #flag for match found in $protected
+        foreach my $suffix ($self->protect)
+        {        
+            $protected = 'true' if ($result eq $self->filename.$suffix);
+        }
+        unless ($protected)
+        {
+            unlink ($result) or $self->throw ("Couldn't delete $result :$!");    
+        }
+    }
+}
+
+sub checkdir {
+    my ($self) = @_;
+    #check for disk space
+    my $spacelimit = 0.01;
+    $self->throw("Not enough disk space ($spacelimit required):$!\n") 
+                        unless ($self->diskspace('./', $spacelimit));
+    my $dir = $self->workdir();
+    chdir ($dir) or $self->throw("Cannot change to directory $dir ($!)\n");
+}
+
+sub diskspace {
+    my ($self, $dir, $limit) =@_;
+    my $block_size; #could be used where block size != 512 ?
+    my $Gb = 1024 ** 3;
+    
+    open DF, "df $dir |" or $self->throw ("Can't open 'du' pipe ($!)\n");
+    while (<DF>) 
+    {
+        if ($block_size) 
+        {
+            my @L = split;
+            my $space_in_Gb = $L[3] * 512 / $Gb;
+            return 0 if ($space_in_Gb < $limit);
+            return 1;
+        } 
+        else 
+        {
+            ($block_size) = /(\d+).+blocks/i
+                || $self->throw ("Can't determine block size from:\n$_");
+        }
+    }
+    close DF || $self->throw("Error from 'df' : $!\n");
+}
+
+# FeaturePair methods:
+sub createfeaturepair {
+    my ($self, $feat1, $feat2) = @_;
+    #some contant strings
+    
+    #create analysis object
+    my $analysis_obj = new Bio::EnsEMBL::Analysis
+                        (   -db              => $feat2->{db},
+                            -db_version      => $feat2->{db_version},
+                            -program         => $feat2->{program},
+                            -program_version => $feat2->{p_version},
+                            -gff_source      => $feat2->{source},
+                            -gff_feature     => $feat2->{primary});
+    
+    #create and fill Bio::EnsEMBL::Seqfeature objects
+    my $seqfeature1 = new Bio::EnsEMBL::SeqFeature
+                        (   -seqname => $feat1->{name},
+                            -start   => $feat1->{start},
+                            -end     => $feat1->{end},
+                            -strand  => $feat1->{strand},
+                            -score   => $feat1->{score},
+                            -source_tag  => $feat1->{source},
+                            -primary_tag => $feat1->{primary},
+                            -analysis => $analysis_obj);
+    
+    my $seqfeature2 = new Bio::EnsEMBL::SeqFeature
+                        (   -seqname => $feat2->{name},
+                            -start   => $feat2->{start},
+                            -end     => $feat2->{end},
+                            -strand  => $feat2->{strand},
+                            -score   => $feat2->{score},
+                            -source_tag  => $feat2->{source},
+                            -primary_tag => $feat2->{primary},
+                            -analysis => $analysis_obj);
+    #create featurepair
+    my $fp = new Bio::EnsEMBL::FeaturePair  (-feature1 => $seqfeature1,
+                                             -feature2 => $seqfeature2) ;
+    $self->growfplist($fp);                             
+}
+
+sub growfplist {
+    my ($self, $fp) =@_;    
+    #load fp onto array using command _grow_fplist
+    push(@{$self->{'_fplist'}}, $fp);
 }
