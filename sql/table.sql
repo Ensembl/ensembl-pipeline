f03daa437f59b6b9e28f4ff8f87456f786276099
Arne Stabenau <stabenau@sanger.ac.uk>
2000-09-19 09:43:46 +0000

    inserted tables for rules and state infos on objects.

--- sql/table.sql
+++ sql/table.sql
@@ -2,8 +2,9 @@
 # Table structure for table 'analysisprocess'
 #
 CREATE TABLE analysisprocess (
-  id int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+  analysisId int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
   created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  logic_name varchar(40) not null,
   db varchar(40),
   db_version varchar(40),
   db_file varchar(80),
@@ -14,17 +15,17 @@ CREATE TABLE analysisprocess (
   module varchar(80),
   module_version varchar(40),
   gff_source varchar(40),
-  gff_feature varchar(40) DEFAULT '' NOT NULL,
-  PRIMARY KEY (id)
+  gff_feature varchar(40),
+  PRIMARY KEY (analysisId)
 );
 
 #
 # Table structure for table 'current_status'
 #
 CREATE TABLE current_status (
-  id int(10) unsigned DEFAULT '0' NOT NULL,
+  jobId int(10) unsigned DEFAULT '0' NOT NULL,
   status varchar(40) DEFAULT '' NOT NULL,
-  PRIMARY KEY (id),
+  PRIMARY KEY (jobId),
   KEY status_index (status)
 );
 
@@ -47,28 +48,25 @@ CREATE TABLE exon_pair (
 # Table structure for table 'job'
 #
 CREATE TABLE job (
-  id int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+  jobId int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
   input_id varchar(40) DEFAULT '' NOT NULL,
-  analysis int(10) unsigned DEFAULT '0' NOT NULL,
+  analysisId int(10) unsigned DEFAULT '0' NOT NULL,
   LSF_id int(10) unsigned DEFAULT '0',
-  machine varchar(40) DEFAULT '',
-  object mediumtext,
-  queue varchar(40) DEFAULT '',
   stdout_file varchar(100) DEFAULT '' NOT NULL,
   stderr_file varchar(100) DEFAULT '' NOT NULL,
-  input_object_file varchar(100) DEFAULT '' NOT NULL,
-  output_file varchar(100) DEFAULT '' NOT NULL,
-  status_file varchar(100) DEFAULT '' NOT NULL,
-  PRIMARY KEY (id),
+  object_file varchar(100) DEFAULT '' NOT NULL,
+  retry_count int default 0,
+
+  PRIMARY KEY (jobId),
   KEY input_index (input_id),
-  KEY analysis_index (analysis)
+  KEY analysis_index (analysisId)
 );
 
 #
 # Table structure for table 'jobstatus'
 #
 CREATE TABLE jobstatus (
-  id int(10) unsigned DEFAULT '0' NOT NULL,
+  jobId int(10) unsigned DEFAULT '0' NOT NULL,
   status varchar(40) DEFAULT 'CREATED' NOT NULL,
   time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL
 );
@@ -91,4 +89,27 @@ CREATE TABLE timclone (
   external_lock int(10) DEFAULT '1' NOT NULL,
   PRIMARY KEY (disk_id)
 );
+
+CREATE TABLE RuleGoal (
+  ruleId int unsigned default '0' not null auto_increment,
+  goalAnalysisId int unsigned,
+ 
+  PRIMARY KEY (	ruleID )
+);
+
+CREATE TABLE RuleConditions (
+  ruleId int not null,
+  conditionLiteral varchar(20)
+);
+
+CREATE TABLE InputIdAnalysis (
+  inputId varchar(20) not null,
+  class enum( "clone","contig","vc","gene" ),
+  analysisId int not null,
+  created datetime not null,
+
+  PRIMARY KEY ( analysisId, inputId, class ),
+  KEY inputIdx( inputId, created )
+);
+
 
