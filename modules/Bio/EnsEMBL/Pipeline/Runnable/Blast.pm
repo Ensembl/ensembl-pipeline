786b8a5bdc9a46975f27f859a0b6f13d67928905
Michele Clamp <michele@sanger.ac.uk>
2001-01-04 17:55:46 +0000

    Refactored Blast to be tidy. Mucho documentation added as I suspect I'll be back down here before too long.

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Blast.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Blast.pm
@@ -18,24 +18,41 @@ Bio::EnsEMBL::Pipeline::Runnable::Blast
 
 =head1 SYNOPSIS
 
-  #create and fill Bio::Seq object
-  my $dnafile   = '/nfs/disk65/mq2/temp/bA151E14.seq';
-  my $seqstream = Bio::SeqIO->new(-file => $clonefile, -fmt => 'Fasta');
-  my $seq       = $seqstream->next_seq();
+  # To run a blast job from scratch do the following.
 
-  #create Bio::EnsEMBL::Pipeline::Runnable::Blast object
-  my $blast = Bio::EnsEMBL::Pipeline::Runnable::Blast->new (-query     => $seq,
-							    -program   => 'blastn',
-							    -database  => 'dbest',
-							    -threshold => 100);
+  my $query = new Bio::Seq(-file   => 'somefile.fa',
+			   -format => 'fasta');
+
+  my $blast =  Bio::EnsEMBL::Pipeline::Runnable::Blast->new (-query    => $query,
+							     -program  => 'blastp',
+							     -database => 'swir',
+							     -threshold => 1e-6,
+							     -options   => 'V=1000000');
+
   $blast->run();
+
   @featurepairs = $blast->output();
+
+  foreach my $fp (@featurepairs) {
+      print $fp->gffstring . "\n";
+  }
+
+  # Additionally if you have blast runs lying around that need parsing
+  # you can do
+
+  open(BLAST,"<blast.out");
+
+  my @featurepairs = Bio::EnsEMBL::Pipeline::Runnable::Blast->parse_results(\*BLAST);
+
+  close(BLAST);
 
 =head1 DESCRIPTION
 
 Blast takes a Bio::Seq (or Bio::PrimarySeq) object and runs blast with, the
 output is parsed by BPLite and stored as Bio::EnsEMBL::FeaturePairs. 
 
+The output features can be filtered by probability using the -threshold option.
+Other options can be passed to the blast program using the -options method
 
 =head1 CONTACT
 
@@ -43,8 +60,6 @@ Describe contact details here
 
 =head1 APPENDIX
 
-The rest of the documentation details each of the object methods. 
-Internal methods are usually preceded with a _
 
 =cut
 
@@ -64,8 +79,6 @@ use Bio::Seq;
 use Bio::SeqIO;
 use Bio::Root::Object;
 use BPlite;
-
-use Data::Dumper;
 
 @ISA = qw(Bio::EnsEMBL::Pipeline::RunnableI Bio::Root::Object);
 
@@ -91,7 +104,7 @@ sub _initialize {
 
     $self->{_query}     = undef;     # location of Bio::Seq object
     $self->{_program}   = undef;     # location of Blast
-    $self->{_database}  = undef;     # name and location of database
+    $self->{_database}  = undef;     # name of database
     $self->{_threshold} = undef;     # Threshold for hit filterting
     $self->{_options}   = undef;     # arguments for blast
 
@@ -102,8 +115,10 @@ sub _initialize {
     $self->{_results}   = undef;     # file to store results of analysis
 
       
+    # Now parse the input options and store them in the object
+
     my( $query, $program, $database, $threshold, $options) = 
-      $self->_rearrange(['QUERY', 'PROGRAM', 'DATABASE','THRESHOLD','OPTIONS'], @args);
+	$self->_rearrange(['QUERY', 'PROGRAM', 'DATABASE','THRESHOLD','OPTIONS'], @args);
    
     if ($query) {
       $self->clone($query);
@@ -158,7 +173,6 @@ sub run {
 
     my $seq = $self->clone || $self->throw("Query seq required for Blast\n");
 
-    #set directory if provided (MC this should be in a config file somewhere)
     $self->workdir('/tmp') unless ($self->workdir($dir));
     $self->checkdir();
 
@@ -206,6 +220,19 @@ sub run_analysis {
     }
 }
 
+=head2 fetch_databases
+
+    Title   :   fetch_databases
+    Usage   :   $obj->fetch_databases
+    Function:   For split databases this
+                method checks whether the database in $self->database
+                exists.  If it doesn\'t it checks whether the
+                database has been split into $database-1, $database-2 etc.
+    Returns :   Array of database names
+    Args    :   none
+
+=cut
+
 sub fetch_databases {
     my ($self) = @_;
     
@@ -213,11 +240,20 @@ sub fetch_databases {
     
     my $fulldbname;
 
+    # If we have passed a full path name don't append the $BLASTDB
+    # environment variable.
+
     if ($self->database =~ /\//) {
 	$fulldbname = $self->database;
     } else {
 	$fulldbname = $ENV{BLASTDB} . "/" .$self->database;
     }
+
+    # If the expanded database name exists put this in
+    # the database array.
+    #
+    # If it doesn't exist then see if $database-1,$database-2 exist
+    # and put them in the database array
 
     if (-e $fulldbname) {
 	push(@databases,$self->database);
@@ -242,9 +278,16 @@ sub fetch_databases {
 
     Title   :   parse_results
     Usage   :   $obj->parse_results
-    Function:   Runs the blast query
+    Function:   Parses the blast results and stores the output in
+                an array of feature pairs.  The output will be filtered
+                by the probability threshold.
+                If we input a filehandle to this method the results 
+                will be read from this file rather than the filename 
+                stored in the object.  This is handy for processing
+                blast runs that have been run previously.
     Returns :   @Bio::EnsEMBL::FeaturePair
-    Args    :   optional filename
+    Args    :   Optional filehandle. 
+                 
 
 =cut
 
@@ -252,6 +295,8 @@ sub fetch_databases {
 sub parse_results {
   my ($self,$fh) = @_;
 
+  # If we have input a filehandle use that. Otherwise use
+  # the results file stored in the object
   my $filehandle;
 
   if (defined($fh)) {
@@ -269,18 +314,24 @@ sub parse_results {
     return;
   }
 
+  # BPlite does most of the work
+
   my $parser = new BPlite ($filehandle);
 
-  $parser->query;
-  $parser->database;
-
-  my $threshold = $self->threshold;
-
+  # Loop over each blast hit
   while(my $sbjct = $parser->nextSbjct)  {
+
+      my $name = $sbjct->name ;
+      $name =~ s/^>(\S+).*/$1/;
+
     HSP: while (my $hsp = $sbjct->nextHSP) {
 	next HSP if ($hsp->P > $self->threshold);
 	
-	$self->split_HSP($sbjct,$hsp);
+	# Each HSP is a gapped alignment.
+	# This method splits the gapped alignment into
+	# ungapped pieces
+
+	$self->split_HSP($hsp,$name);
 
     }
   } 
@@ -288,20 +339,253 @@ sub parse_results {
 }
 
 
+=head2 split_HSP
+
+    Title   :   split_HSP
+    Usage   :   $obj->split_HSP
+    Function:   Takes a gapped blast HSP 
+                and turns it into an array of ungapped feature pairs.
+    Returns :   Nothing
+    Args    :   BPlite::HSP,name string
+                 
+
+=cut
+
 sub split_HSP {
-    my ($self,$sbjct,$hsp) = @_;
+    my ($self,$hsp,$name) = @_;
 
-    my $name = $sbjct->name ;
-    $name =~ s/^>(\S+).*/$1/;
+    # First of all some jiggery pokery to find out what sort of alignment
+    # we have - (dna-dna, dna-pep, pep-dna etc).
+    # We also work out which strand each sequence is on.
+    # 
+    # Both these variables are needed to work out the increment
+    # values for the query and the hit sequences.  As we split
+    # up the gapped alignment we need to increment the coordinates.
+    #
+    # We have to take care with dna-pep alignments to increment the dna
+    # sequences by 3 as there are 3 bases in a codon.
+    #
+    # For dna-dna alignments this will be 1 or -1 depending on
+    # the strand.  
+    #
+    # For pep-dna alignments and vice-versa the increments will be +-3 
+    # for the dna sequence and +- 1 for the peptide sequence. 
 
-    my $type1;
-    my $type2;
+    my ($qtype,  $htype)   = $self->_findTypes     ($hsp);
+    my ($qstrand,$hstrand) = $self->_findStrands   ($hsp);
+    my ($qinc,   $hinc)    = $self->_findIncrements($hsp,$qstrand,$hstrand,$qtype,$htype);
 
-    my $len1 = abs($hsp->queryEnd - $hsp->queryBegin) + 1;
-    my $len2 = abs($hsp->sbjctEnd - $hsp->sbjctBegin) + 1;
+    print STDERR "Alignment q : " . $hsp->queryBegin . "\t" . $hsp->queryEnd . "\t" . $hsp->queryAlignment . "\n";
+    print STDERR "Alignment s : " . $hsp->sbjctBegin . "\t" . $hsp->sbjctEnd . "\t" . $hsp->sbjctAlignment . "\n";
 
+    print STDERR "types (increments) $qtype ($qinc) : $htype ($hinc)\n";
 
-    # Find the strands first
+    # We split the alignment strings into arrays of one char each.  
+    # We then loop over this array and when we come to a gap
+    # in either the query sequence or the hit sequence we make 
+    # a new feature pair. 
+    #
+    # Before making a new feature pair we check whether we have something to make
+    # a feature pair out of - i.e. we aren't just on the 2nd or greater base in a gapped
+    # region.
+    #
+    # As we loop over the array we need to keep track of both the query coordinates
+    # and the hit coordinates.  We track the last start of a feature pair and
+    # also the current end of a feature pair.  The feature pair start is reset every time 
+    # we hit a gapped position and the feature pair end is reset every time we hit 
+    # an aligned region.
+
+    my @gap;
+
+    my @qchars = split(//,$hsp->queryAlignment);  # split alignment into array of char
+    my @hchars = split(//,$hsp->sbjctAlignment);  # ditto for hit sequence
+    
+    my $qstart = $hsp->queryBegin;                # Start off the feature pair start
+    my $hstart = $hsp->sbjctBegin;                # ditto
+    
+    my $qend   = $hsp->queryBegin;                # Set the feature pair end also
+    my $hend   = $hsp->sbjctBegin;                # ditto
+    
+    my $count = 0;                                # counter for the bases in the alignment
+    my $found = 0;                                # flag saying whether we have a feature pair
+    
+    my $source = $self->program;             
+    $source =~ s/\/.*\/(.*)/$1/;
+
+    my $analysis = new Bio::EnsEMBL::Analysis(-db              => $self->database,
+					      -db_version      => 1,
+					      -program         => $source,
+					      -program_version => $1,
+					      -gff_source      => $source,
+					      -gff_feature     => 'similarity');
+    
+    # Here goes...
+
+    while ($count <= $#qchars) {
+	# We have hit an ungapped region.  Increase the query and hit counters.
+	# and flag that we have a feature pair.
+
+	if ($qchars[$count] ne '-' &&
+	    $hchars[$count] ne '-') {
+
+	    $qend += $qinc;
+	    $hend += $hinc;
+	    
+	    $found = 1;
+	} else {
+
+	    # We have hit a gapped region.  If the feature pair flag is set ($found)
+	    # then make a feature pair, store it and reset the start and end variables.
+
+	    if ($found == 1) {
+		my $fp = $self->_convert2FeaturePair($qstart,$qend,$qstrand,$hstart,$hend,$hstrand,$qinc,$hinc,$hsp,$name,$analysis);
+		
+		$self->growfplist($fp);                             	    
+	    }
+	
+	    # We're in a gapped region.  We need to increment the sequence that
+	    # doesn't have the gap in it to keep the coordinates correct.
+	    # We also need to reset the current end coordinates.
+
+	    if ($qchars[$count] ne '-') {
+		$qstart = $qend   + $qinc;
+	    } else {
+		$qstart = $qend;
+	    }
+	    if ($hchars[$count] ne '-') {
+		$hstart = $hend   + $hinc;
+	    } else {
+		$hstart = $hend;
+	    }
+	    
+	    $qend = $qstart;
+	    $hend = $hstart;
+
+	    $found = 0;
+	}
+	$count++;
+    }			     
+
+    # Remember the last feature
+    if ($found == 1) {
+	my $fp = $self->_convert2FeaturePair($qstart,$qend,$qstrand,$hstart,$hend,$hstrand,$qinc,$hinc,$hsp,$name,$analysis);
+	$self->growfplist($fp);                             	    
+    }
+
+}
+
+=head2 _convert2FeaturePair
+
+    Title   :   convert2FeaturePair
+    Usage   :   $obj->convert2FeaturePair
+    Function:   Internal function taking a set of coords and
+                converts them into a feature pair
+    Returns :   Bio::EnsEMBL::FeaturePair
+    Args    :   int,int,int,int,int,int,int,int,BPlite::HSP
+                 
+
+=cut
+
+sub _convert2FeaturePair {
+    my ($self,$qstart,$qend,$qstrand,$hstart,$hend,$hstrand,$qinc,$hinc,$hsp,$name,$analysis) = @_;
+
+    # The actual end of the alignment is the previous character.
+    
+    my $tmpqend = $qend; $tmpqend -= $qinc;
+    my $tmphend = $hend; $tmphend -= $hinc;
+    
+    my $tmpqstart = $qstart;
+    my $tmphstart = $hstart;
+    
+    # This is for dna-pep alignments.  The actual end base
+    # will be +- 2 bases further on.
+    if (abs($qinc) > 1) {
+	$tmpqend += $qstrand * 2;
+    }
+    if (abs($hinc) > 1) {
+	$tmphend += $hstrand * 2;
+    }
+    
+    # Make sure start is always < end
+    if ($tmpqstart > $tmpqend) {
+	my $tmp    = $tmpqstart;
+	$tmpqstart = $tmpqend;
+	$tmpqend   = $tmp;
+    }
+    if ($tmphstart > $tmphend) {
+	my $tmp    = $tmphstart;
+	$tmphstart = $tmphend;
+	$tmphend   = $tmp;
+    }
+    
+    print "Creating feature pair " . $tmpqstart . "\t" . $tmpqend . "\t" . $qstrand . "\t" . $tmphstart . "\t" . $tmphend . "\t" . $hstrand . "\n";
+
+    my $fp = $self->_makeFeaturePair($tmpqstart,$tmpqend,$qstrand,$tmphstart,$tmphend,$hstrand,$hsp->score,
+				     $hsp->percent,$hsp->P,$name,$analysis);
+
+    return $fp;
+}
+
+=head2 _makeFeaturePair
+
+    Title   :   _makeFeaturePair
+    Usage   :   $obj->_makeFeaturePair
+    Function:   Internal function that makes feature pairs
+    Returns :   Bio::EnsEMBL::FeaturePair
+    Args    :   int,int,int,int,int,int,int,int,BPlite::HSP
+                 
+
+=cut
+
+sub _makeFeaturePair {
+    my ($self,$qstart,$qend,$qstrand,$hstart,$hend,$hstrand,$score,$pid,$evalue,$name,$analysis)  = @_;
+
+    my $source = $self->program;             
+    $source =~ s/\/.*\/(.*)/$1/;
+
+    my $feature1 = new Bio::EnsEMBL::SeqFeature(-seqname     => $self->clone->id,
+						-start       => $qstart,
+						-end         => $qend,
+						-strand      => $qstrand * $hstrand,
+						-source_tag  => $source,
+						-primary_tag => 'similarity',
+						-analysis    => $analysis,
+						-score       => $score);
+		
+    my $feature2 = new Bio::EnsEMBL::SeqFeature(-seqname => $name,
+						-start   => $hstart,
+						-end     => $hend,
+						-strand  => $hstrand * $qstrand,
+						-source_tag  => $source,
+						-primary_tag => 'similarity',
+						-analysis => $analysis,
+						-score    => $score);
+    
+    
+    my $fp = new Bio::EnsEMBL::FeaturePair(-feature1 => $feature1,
+					   -feature2 => $feature2);
+    
+    return $fp;
+}
+
+sub _findIncrements {
+    my ($self,$hsp,$qstrand,$hstrand,$qtype,$htype) = @_;
+
+    my $qinc   = 1 * $qstrand;
+    my $hinc   = 1 * $hstrand;
+    
+    if ($qtype eq 'dna' && $htype eq 'pep') {
+	$qinc = 3 * $qinc;
+    } 
+    if ($qtype eq 'pep' && $htype eq 'dna') {
+	$hinc = 3 * $hinc;
+    }
+
+    return ($qinc,$hinc);
+}
+
+sub _findStrands {
+    my ($self,$hsp) = @_;
 
     my $qstrand;
     my $hstrand;
@@ -317,6 +601,19 @@ sub split_HSP {
 	$hstrand = -1;
     }
 
+    return ($qstrand,$hstrand);
+}
+
+sub _findTypes {
+    my ($self,$hsp) = @_;
+
+    my $type1;
+    my $type2;
+
+    my $len1 = abs($hsp->queryEnd - $hsp->queryBegin) + 1;
+    my $len2 = abs($hsp->sbjctEnd - $hsp->sbjctBegin) + 1;
+
+
     if ($len1/$len2 > 2) {
 	$type1 = 'dna';
 	$type2 = 'pep';
@@ -328,186 +625,8 @@ sub split_HSP {
 	$type2 = 'dna';
     }
 
-    print STDERR "Alignment q : " . $hsp->queryBegin . "\t" . $hsp->queryEnd . "\t" . $hsp->queryAlignment . "\n";
-    print STDERR "Alignment s : " . $hsp->sbjctBegin . "\t" . $hsp->sbjctEnd . "\t" . $hsp->sbjctAlignment . "\n";
-
-    my @features;
-    
-    my $qinc   = 1 * $qstrand;
-    my $sinc   = 1 * $hstrand;
-    
-    if ($type1 eq 'dna' && $type2 eq 'pep') {
-	$qinc = 3 * $qinc;
-    } 
-    if ($type1 eq 'pep' && $type2 eq 'dna') {
-	$sinc = 3 * $sinc;
-    }
-
-    print STDERR "types $type1 ($qinc) : $type2 ($sinc)\n";
-    my @gap;
-    
-    my @qchars = split(//,$hsp->queryAlignment);
-    my @schars = split(//,$hsp->sbjctAlignment);
-    
-    my $qstart = $hsp->queryBegin;
-    my $sstart = $hsp->sbjctBegin;
-    
-    my $qend   = $hsp->queryBegin;
-    my $send   = $hsp->sbjctBegin;
-    
-    my $count = 0;
-    my $found = 0;
-    
-    my $source = $self->program;
-    $source =~ s/\/.*\/(.*)/$1/;
-
-    my $analysis = new Bio::EnsEMBL::Analysis(-db              => $self->database,
-					      -db_version      => 1,
-					      -program         => $source,
-					      -program_version => $1,
-					      -gff_source      => $source,
-					      -gff_feature     => 'similarity');
-    
-    while ($count <= $#qchars) {
-	if ($qchars[$count] ne '-' &&
-	    $schars[$count] ne '-') {
-
-	    $qend += $qinc;
-	    $send += $sinc;
-	    
-	    $found = 1;
-	} else {
-	    if ($found == 1) {
-
-		my $tmpqend = $qend; $tmpqend -= $qinc;
-		my $tmpsend = $send; $tmpsend -= $sinc;
-
-		my $tmpqstart = $qstart;
-		my $tmpsstart = $sstart;
-
-		if (abs($qinc) > 1) {
-		    $tmpqend += $qstrand * 2;
-		}
-		if (abs($sinc) > 1) {
-		    $tmpsend += $hstrand * 2;
-		}
-
-		if ($tmpqstart > $tmpqend) {
-		    my $tmp    = $tmpqstart;
-		    $tmpqstart = $tmpqend;
-		    $tmpqend   = $tmp;
-		}
-		if ($tmpsstart > $tmpsend) {
-		    my $tmp    = $tmpsstart;
-		    $tmpsstart = $tmpsend;
-		    $tmpsend   = $tmp;
-		}
-
-		print "Creating feature pair " . $tmpqstart . "\t" . $tmpqend . "\t" . $qstrand . "\t" . $tmpsstart . "\t" . $tmpsend . "\t" . $hstrand . "\n";
-
-		my $feature1 = new Bio::EnsEMBL::SeqFeature(-seqname     => $self->clone->id,
-							    -start       => $tmpqstart,
-							    -end         => $tmpqend,
-							    -strand      => $qstrand * $hstrand,
-							    -source_tag  => $source,
-							    -primary_tag => 'similarity',
-							    -analysis    => $analysis,
-							    -score       => $hsp->score);
-		
-		my $feature2 = new Bio::EnsEMBL::SeqFeature(-seqname => $name,
-							    -start   => $tmpsstart,
-							    -end     => $tmpsend,
-							    -strand  => $hstrand * $qstrand,
-							    -source_tag  => $source,
-							    -primary_tag => 'similarity',
-							    -analysis => $analysis,
-							    -score    => $hsp->score);
-		
-		
-		my $fp = new Bio::EnsEMBL::FeaturePair(-feature1 => $feature1,
-						       -feature2 => $feature2);
-		
-		print $fp->source_tag . "\t" . $fp->primary_tag . "\t" . $fp->score . "\n";
-		push(@features,$fp);
-		$self->growfplist($fp);                             
-		
-	    }
-	    if ($qchars[$count] ne '-') {
-		$qstart = $qend   + $qinc;
-	    } else {
-		$qstart = $qend;
-	    }
-	    if ($schars[$count] ne '-') {
-		$sstart = $send   + $sinc;
-	    } else {
-		$sstart = $send;
-	    }
-	    
-	    $qend = $qstart;
-	    $send = $sstart;
-
-	    $found = 0;
-	}
-	$count++;
-    }			     
-
-    # Remember the last feature
-    if ($found == 1) {
-	my $tmpqend = $qend; $tmpqend -= $qinc;
-	my $tmpsend = $send; $tmpsend -= $sinc;
-	
-	my $tmpqstart = $qstart;
-	my $tmpsstart = $sstart;
-	
-	if (abs($qinc) > 1) {
-	    $tmpqend += $qstrand * 2;
-	}
-	if (abs($sinc) > 1) {
-	    $tmpsend += $hstrand * 2;
-	}
-	
-	if ($tmpqstart > $tmpqend) {
-	    my $tmp = $tmpqstart;
-	    $tmpqstart = $tmpqend;
-	    $tmpqend   = $tmp;
-	}
-	if ($tmpsstart > $tmpsend) {
-	    my $tmp = $tmpsstart;
-	    $tmpsstart = $tmpsend;
-	    $tmpsend   = $tmp;
-	}
-	
-	print "Creating feature pair " . $tmpqstart . "\t" . $tmpqend . "\t" . $qstrand . "\t" . $tmpsstart . "\t" . $tmpsend . "\t" . $hstrand . "\n";
-	
-	my $feature1 = new Bio::EnsEMBL::SeqFeature(-seqname     => $self->clone->id,
-						    -start       => $tmpqstart,
-						    -end         => $tmpqend,
-						    -strand      => $qstrand,
-						    -source_tag  => $source,
-						    -primary_tag => 'similarity',
-						    -analysis    => $analysis,
-						    -score       => $hsp->score,
-						    );
-	
-	my $feature2 = new Bio::EnsEMBL::SeqFeature(-seqname => $name,
-						    -start   => $tmpsstart,
-						    -end     => $tmpsend,
-						    -strand  => $hstrand,
-						    -source_tag  => $source,
-						    -primary_tag => 'similarity',
-						    -analysis => $analysis,
-						    -score    => $hsp->score);
-	
-	my $fp = new Bio::EnsEMBL::FeaturePair(-feature1 => $feature1,
-					       -feature2 => $feature2);
-	
-	push(@features,$fp);
-	$self->growfplist($fp);                             
-	
-    }
-
+    return ($type1,$type2);
 }
-	    
 
 ##############
 # input/output methods
