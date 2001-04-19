bd641995ff8a207c7a0de516d5109e34d72f0d52
Elia Stupka <elia@sanger.ac.uk>
2001-04-19 14:46:33 +0000

    *** empty log message ***

--- scripts/print_rules.pl
+++ scripts/print_rules.pl
@@ -27,7 +27,7 @@ my $db  = new Bio::EnsEMBL::Pipeline::DBSQL::Obj(-host   => $host,
 						 -user   => $dbuser,
 						 -pass   => $pass);
 
-my $sth=$db->prepare("select rg.ruleId,a.program,rc.conditionLiteral from RuleGoal rg, RuleConditions rc, analysisprocess a where rc.ruleId = rg.ruleId and a.id = rg.goalAnalysisId");
+my $sth=$db->prepare("select rg.ruleId,a.program,rc.conditionLiteral from RuleGoal rg, RuleConditions rc, analysisprocess a where rc.ruleId = rg.ruleId and a.analysisId = rg.goalAnalysisId");
 $sth->execute();
 while (my ($rule,$goal,$condition)= $sth->fetchrow_array) {
     print STDERR "Rule $rule: Do $goal only if $condition\n";
