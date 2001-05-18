bf333a07d3fd16a55dda91a6eedb3c8b64136781
cvs2git <cvs2git>
2001-05-18 15:52:22 +0000

    This commit was manufactured by cvs2svn to create branch 'branch-1_0'.
    Sprout from master 2001-04-04 16:58:38 UTC Ensembl Pipeline <enspipe@sanger.ac.uk> 'changed contig id to clone.version.offset.end'
    Cherrypick from master 2001-05-18 15:52:21 UTC Val Curwen <vac@sanger.ac.uk> 'Various sequence fetching modules to be used by Runnables and RunnableDBs.':
        modules/Bio/EnsEMBL/Pipeline/SeqFetcher/BPIndex.pm
        modules/Bio/EnsEMBL/Pipeline/SeqFetcher/Efetch.pm

---
+++ modules/Bio/EnsEMBL/Pipeline/SeqFetcher/BPIndex.pm
@@ -0,0 +1,175 @@
+#
+# Cared for by EnsEMBL  <ensembl-dev@ebi.ac.uk>
+#
+# Copyright GRL & EBI
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+  Bio::EnsEMBL::Pipeline::SeqFetcher::BPIndex
+
+=head1 SYNOPSIS
+
+    my $obj = Bio::EnsEMBL::Pipeline::SeqFetcher::BPIndex->new(
+							       '-index'  => $index,
+							       '-format' => 'Fasta',
+							      );
+    my $seq = $obj->get_Seq_by_acc($acc);
+
+=head1 DESCRIPTION
+
+    Object to retrieve sequences as Bio::Seq, from a bioperl index. 
+    The index is not made by this module; instead, the absolute path 
+    to the bioperl index must be set using $obj->bp_index. The format 
+    of the database must be set using bp_format.
+
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+package Bio::EnsEMBL::Pipeline::SeqFetcher::BPIndex;
+
+use strict;
+use Bio::Root::RootI;
+use Bio::DB::RandomAccessI;
+use Bio::Seq;
+# change these uses as new indices are released in bioperl - these are for 0.6.2
+use Bio::Index::Fasta;
+use Bio::Index::EMBL;
+use Bio::Index::SwissPfam;
+
+use vars qw(@ISA);
+
+@ISA = qw(Bio::Root::RootI Bio::DB::RandomAccessI);
+
+sub new {
+  my ($class, @args) = @_;
+  my $self = bless {}, $class;
+  
+  my($index, $format) = $self->_rearrange(['INDEX',
+					   'FORMAT'], @args);
+  
+  if (!defined $index) {
+    $self->throw("No bioperl indexfile provided to BPIndex\n");
+  }
+  $self->bp_index($index);
+  
+  
+  if (!defined $format) {
+    $self->throw("No bioperl index format provided to BPIndex\n");
+  }
+  $self->bp_format($format);
+
+
+  return $self; # success - we hope!
+}
+
+=head2 bp_index
+
+    Title   :   bp_index
+    Usage   :   $self->bp_index('/usr/local/ensembl/data/bp.inx')
+    Function:   Get/set for a bioperl index
+    Returns :   path to bioperl index
+    Args    :   path to bioperl index
+
+=cut
+
+sub bp_index {
+  
+  my ($self, $inx) = @_;
+  if ($inx)
+    {
+      $self->{'_inx'} = $inx;
+    }
+  return $self->{'_inx'};  
+  
+}
+
+=head2 bp_format
+
+    Title   :   bp_format
+    Usage   :   $self->bp_format('Fasta')
+    Function:   Get/set for a bioperl index format
+    Returns :   String representing format. NOTE - bp_format is used in run to identify the type of Bio::Index module to make - so case is crucial. eg Fasta, EMBL, SwissPfam
+    Args    :   String representing format
+
+=cut
+
+sub bp_format {
+  
+  my ($self, $format) = @_;
+  if ($format)
+    {
+      $self->{'_format'} = $format;
+    }
+  return $self->{'_format'};  
+  
+}
+
+=head2 get_Seq_by_acc
+
+  Title   : get_Seq_by_acc
+  Usage   : $self->get_Seq_by_acc($accession);
+  Function: Does the sequence retrieval
+  Returns : Bio::Seq
+  Args    : 
+
+=cut
+
+sub  get_Seq_by_acc {
+  my ($self, $acc) = @_;
+  
+  my $inx    = $self->bp_index;
+  my $format = $self->bp_format;
+  
+
+  if (!defined($acc)) {
+    $self->throw("No accession input");
+  }  
+
+  if (!defined($inx)) {
+    $self->throw("No search index specified; cannot run");
+  }  
+
+  if (!defined($format)) {
+    $self->throw("No index format specified");
+  }  
+  
+  my $type = 'Bio::Index::' . $format;
+  my $index;
+
+  eval {
+    $index = $type->new($inx);
+  };
+
+  if ($@) {
+    my $tmp = $@; # for some reason, warn empties out $@ ...
+    $self->warn("Problem opening the index [$inx] - check you have supplied the right format!");
+    $self->throw ("[$tmp]!");
+  }
+  
+  # get the sequence
+  my $seq;
+  eval{
+    $seq = $index->fetch($acc); # Returns Bio::Seq object
+  };
+
+  $self->throw("Could not fetch sequence for [$acc]") unless defined $seq;
+  return $seq; 
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/Pipeline/SeqFetcher/Efetch.pm
@@ -0,0 +1,151 @@
+#
+# Cared for by EnsEMBL  <ensembl-dev@ebi.ac.uk>
+#
+# Copyright GRL & EBI
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+  Bio::EnsEMBL::Pipeline::SeqFetcher::Efetch
+
+=head1 SYNOPSIS
+
+    my $obj = Bio::EnsEMBL::Pipeline::SeqFetcher::Efetch->new(
+							      '-executable' => $exe,
+							      '-lib'        => $lib,
+							     );
+    my $seq = $obj->get_Seq_by_acc($acc);
+
+=head1 DESCRIPTION
+
+  Object to retrieve sequences as Bio::Seq, using efetch.
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+package Bio::EnsEMBL::Pipeline::SeqFetcher::Efetch;
+
+use strict;
+use Bio::Root::RootI;
+use Bio::DB::RandomAccessI;
+use Bio::Seq;
+
+use vars qw(@ISA);
+
+@ISA = qw(Bio::Root::RootI Bio::DB::RandomAccessI);
+
+sub new {
+  my ($class, @args) = @_;
+  my $self = bless {}, $class;
+
+  my ($exe, $lib) = $self->_rearrange([
+				       'EXECUTABLE', 
+				       'LIBRARY'], @args);
+
+  if (!defined $exe) {
+    $exe = 'efetch';
+  }
+  $self->executable($exe);
+  
+  
+  if (defined $lib) {
+    $self->library($lib);
+  }  
+  return $self; # success - we hope!
+}
+
+=head2 executable
+
+  Title   : executable
+  Usage   : $self->executable('/path/to/executable');
+  Function: Get/set for the path to the executable being used by the module. If not set, the executable is looked for in $PATH.
+  Returns : string
+  Args    : string
+
+=cut
+
+sub executable {
+  my ($self, $exe) = @_;
+  if ($exe)
+    {
+      $self->{'_exe'} = $exe;
+    }
+  return $self->{'_exe'};  
+}
+
+=head2 library
+
+  Title   : library
+  Usage   : $self->library('sw');
+  Function: Get/set for a library to search in - eg efetch wil not search swall without the prefix sw:
+  Returns : string
+  Args    : string
+
+=cut
+
+sub library {
+  my ($self, $lib) = @_;
+  if ($lib) {
+      $self->{'_lib'} = $lib;
+    }
+  return $self->{'_lib'};  
+}
+
+
+=head2 get_Seq_by_acc
+
+  Title   : get_Seq_by_acc
+  Usage   : $self->get_eq_by_acc($accession);
+  Function: Does the sequence retrieval via efetch
+  Returns : Bio::Seq
+  Args    : 
+
+=cut
+
+sub  get_Seq_by_acc {
+  my ($self, $acc) = @_;
+
+  if (!defined($acc)) {
+    $self->throw("No accession input");
+  }  
+
+  my $lib = $self->library;
+  if (defined $lib && $lib ne ''){
+    $acc = $lib . ":" . $acc;
+  }
+
+  my $seqstr;
+  my $seq;
+  my $efetch = $self->executable;
+  
+  open(IN,"$efetch -q $acc |") or $self->throw("Error running efetch for acc [$acc]: $efetch");
+  $seqstr = <IN>;
+  close IN;
+  
+  if(defined $seqstr && $seqstr ne "no match") {
+    chomp($seqstr);
+    $seq = new Bio::Seq('-seq'               => $seqstr,
+			'-accession_number'  => $acc,
+			'-display_id'        => $acc);
+  }
+
+  $self->throw("Could not efetch sequence for [$acc]\n") unless defined $seq;
+  return $seq;
+
+}
+
+1;
