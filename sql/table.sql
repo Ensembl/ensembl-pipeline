7ead36b94e8f006b230717d16f702c0bb0368cd0
Ensembl Pipeline <enspipe@sanger.ac.uk>
2001-06-18 14:21:43 +0000

    "tidyied" and added blah_external tables so as to prevent a public lynching by Val ;)

--- sql/table.sql
+++ sql/table.sql
@@ -2,20 +2,20 @@
 # Table structure for table 'analysisprocess'
 #
 CREATE TABLE analysisprocess (
-  analysisId int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
-  created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-  logic_name varchar(40) not null,
-  db varchar(40),
-  db_version varchar(40),
-  db_file varchar(80),
-  program varchar(80),
-  program_version varchar(40),
-  program_file varchar(40),
-  parameters varchar(80),
-  module varchar(80),
-  module_version varchar(40),
-  gff_source varchar(40),
-  gff_feature varchar(40),
+  analysisId        int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+  created           datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  logic_name        varchar(40) not null,
+  db                varchar(40),
+  db_version        varchar(40),
+  db_file           varchar(80),
+  program           varchar(80),
+  program_version   varchar(40),
+  program_file      varchar(40),
+  parameters        varchar(80),
+  module            varchar(80),
+  module_version    varchar(40),
+  gff_source        varchar(40),
+  gff_feature       varchar(40),
 
   PRIMARY KEY (analysisId)
 );
@@ -39,8 +39,9 @@ CREATE TABLE analysis (
 # Table structure for table 'current_status'
 #
 CREATE TABLE current_status (
-  jobId int(10) unsigned DEFAULT '0' NOT NULL,
-  status varchar(40) DEFAULT '' NOT NULL,
+  jobId             int(10) unsigned DEFAULT '0' NOT NULL,
+  status            varchar(40) DEFAULT '' NOT NULL,
+
   PRIMARY KEY (jobId),
   KEY status_index (status)
 );
@@ -49,14 +50,14 @@ CREATE TABLE current_status (
 # Table structure for table 'job'
 #
 CREATE TABLE job (
-  jobId int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
-  input_id varchar(40) DEFAULT '' NOT NULL,
-  analysisId int(10) unsigned DEFAULT '0' NOT NULL,
-  LSF_id int(10) unsigned DEFAULT '0',
-  stdout_file varchar(100) DEFAULT '' NOT NULL,
-  stderr_file varchar(100) DEFAULT '' NOT NULL,
-  object_file varchar(100) DEFAULT '' NOT NULL,
-  retry_count int default 0,
+  jobId             int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+  input_id          varchar(40) DEFAULT '' NOT NULL,
+  analysisId        int(10) unsigned DEFAULT '0' NOT NULL,
+  LSF_id            int(10) unsigned DEFAULT '0',
+  stdout_file       varchar(100) DEFAULT '' NOT NULL,
+  stderr_file       varchar(100) DEFAULT '' NOT NULL,
+  object_file       varchar(100) DEFAULT '' NOT NULL,
+  retry_count       int default 0,
 
   PRIMARY KEY (jobId),
   KEY input_index (input_id),
@@ -67,40 +68,81 @@ CREATE TABLE job (
 # Table structure for table 'jobstatus'
 #
 CREATE TABLE jobstatus (
-  jobId int(10) unsigned DEFAULT '0' NOT NULL,
-  status varchar(40) DEFAULT 'CREATED' NOT NULL,
-  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  jobId             int(10) unsigned DEFAULT '0' NOT NULL,
+  status            varchar(40) DEFAULT 'CREATED' NOT NULL,
+  time              datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
 
   KEY (jobId),
   KEY status_index (status)
 );
 
 CREATE TABLE RuleGoal (
-  ruleId int unsigned default '0' not null auto_increment,
-  goalAnalysisId int unsigned,
+  ruleId            int unsigned default '0' not null auto_increment,
+  goalAnalysisId    int unsigned,
  
-  PRIMARY KEY (	ruleID )
+  PRIMARY KEY (ruleID)
 );
 
 CREATE TABLE RuleConditions (
-  ruleId int not null,
-  conditionLiteral varchar(20)
+  ruleId            int not null,
+  conditionLiteral  varchar(20)
 );
 
 CREATE TABLE InputIdAnalysis (
-  inputId varchar(40) not null,
-  class enum( "clone","contig","vc","gene" ) not null,
-  analysisId int not null,
-  created datetime not null,
+  inputId           varchar(40) not null,
+  class             enum ("clone","contig","vc","gene") not null,
+  analysisId        int not null,
+  created           datetime not null,
 
-  PRIMARY KEY ( analysisId, inputId, class ),
-  KEY inputIdx( inputId, created )
+  PRIMARY KEY (analysisId, inputId, class),
+  KEY inputIdx (inputId, created)
 );
 
 CREATE TABLE VoidInputIdAnalysis (
-  inputId varchar(40) DEFAULT '' NOT NULL,
-  analysisId int(11) DEFAULT '0' NOT NULL,
-  exception varchar(40),
+  inputId           varchar(40) DEFAULT '' NOT NULL,
+  analysisId        int(11) DEFAULT '0' NOT NULL,
+  exception         varchar(40),
+
   PRIMARY KEY (inputId,analysisId),
   KEY exc (exception)
+);
+
+#
+# Table structure for table 'exon_external'
+#
+CREATE TABLE exon_external (
+  internal_id       int(10) unsigned default NULL auto_increment,
+  external_id       varchar(40) NOT NULL default '',
+
+  PRIMARY KEY (internal_id)
+);
+
+#
+# Table structure for table 'gene_external'
+#
+CREATE TABLE gene_external (
+  internal_id       int(10) unsigned default NULL auto_increment,
+  external_id       varchar(40) NOT NULL default '',
+
+  PRIMARY KEY (internal_id)
+);
+
+#
+# Table structure for table 'transcript_external'
+#
+CREATE TABLE transcript_external (
+  internal_id       int(10) unsigned default NULL auto_increment,
+  external_id       varchar(40) NOT NULL default '',
+
+  PRIMARY KEY (internal_id)
+);
+
+#
+# Table structure for table 'translation_external'
+#
+CREATE TABLE translation_external (
+  internal_id       int(10) unsigned default NULL auto_increment,
+  external_id       varchar(40) NOT NULL default '',
+
+  PRIMARY KEY (internal_id)
 );
