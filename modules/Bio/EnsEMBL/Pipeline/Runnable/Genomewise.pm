8fe6d71b906803ce3ab6e0c465d0f0e520b91e08
Val Curwen <vac@sanger.ac.uk>
2001-06-20 15:06:51 +0000

    exons need a phase ...now they have one

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Genomewise.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Genomewise.pm
@@ -71,60 +71,78 @@ sub new {
 =cut
 
 sub run{
-   my ($self) = @_;
+  my ($self) = @_;
+  
+  if( !defined $self->workdir ) {
+    $self->workdir('/tmp');
+  }
+  
+  my $dir = $self->workdir();
+  my $genome_file = "$dir/gowise_seq.$$";
+  my $evi_file    = "$dir/gowise_evi.$$";
+  
+  open(F,">$genome_file") || $self->throw("Could not open $genome_file $!");
+  open(E,">$evi_file") || $self->throw("Could not open $evi_file $!");
+  
+  
+  my $seqout = Bio::SeqIO->new('-format' => 'fasta','-fh' => \*F);
+  $seqout->write_seq($self->seq);
+  
+  $seqout = undef;
+  close(F);
+  
+  foreach my $t ( $self->each_Transcript ) {
+    foreach my $e ( $t->each_Exon ) {
+      if( $e->strand == -1 ) {
+	$self->throw("Genomewise cannot handle reverse strand exons. Must flip outside");
+      }
+      print E "exon ",$e->start," ",$e->end,"\n";
+    }
+    print E "//\n";
+  }
+  close(E);
+  
+  #   open(GW,"genomewise $genome_file $evi_file |");
+  open(GW,"genomewise -gff -notrans -nogenes $genome_file $evi_file |");
+  
 
-   if( !defined $self->workdir ) {
-       $self->workdir('/tmp');
-   }
+  
+  # parse gff output for start, stop, strand, phase
+  my $genename = '';
+  my $t;
+  while( <GW> ) {
+    /cds\s+(\d+)\s+(\d+)\s+\S+\s+(\S+)\s+(\d+)\s+(\S+)/ || next;
+    if ($genename ne $5){
+      $genename = $5;
+      $t = Bio::EnsEMBL::Transcript->new();
+      push(@{$self->{'_output_array'}},$t);
+    }
 
-   my $dir = $self->workdir();
-   my $genome_file = "$dir/gowise_seq.$$";
-   my $evi_file    = "$dir/gowise_evi.$$";
+    my $start = $1;
+    my $end   = $2;
+    my $strand = 1;
+    my $phase = $4;
+    if ($phase == 2) { 
+      $phase = 1;
+    }	elsif ($phase == 1) { 
+      $phase = 2;
+    }
+    
+    my $exon = Bio::EnsEMBL::Exon->new();
+    $exon->start($start);
+    $exon->end  ($end);
+    $exon->strand($strand);
+    $exon->phase($phase);
+    $t->add_Exon($exon);
+  }
 
-   open(F,">$genome_file") || $self->throw("Could not open $genome_file $!");
-   open(E,">$evi_file") || $self->throw("Could not open $evi_file $!");
-
-   
-   my $seqout = Bio::SeqIO->new('-format' => 'fasta','-fh' => \*F);
-   $seqout->write_seq($self->seq);
-   
-   $seqout = undef;
-   close(F);
-   
-   foreach my $t ( $self->each_Transcript ) {
-       foreach my $e ( $t->each_Exon ) {
-	   if( $e->strand == -1 ) {
-	       $self->throw("Genomewise cannot handle reverse strand exons. Must flip outside");
-	   }
-	   print E "exon ",$e->start," ",$e->end,"\n";
-       }
-       print E "//\n";
-   }
-   close(E);
-       
-   open(GW,"genomewise $genome_file $evi_file |");
-   
-   while( <GW> ) {
-       /Gene\s+\d+\s+\d+/ && do {
-	   my $t = Bio::EnsEMBL::Transcript->new();
-	   push(@{$self->{'_output_array'}},$t);
-	   while( <GW> ) {
-	       /Gene/ && last;
-	       /Exon\s+(\d+)\s+(\d+)/ || next;
-	       my $start = $1;
-	       my $end   = $2;
-	       my $strand = 1;
-
-	       my $exon = Bio::EnsEMBL::Exon->new();
-	       $exon->start($start);
-	       $exon->end  ($end);
-	       $exon->strand($strand);
-	       $t->add_Exon($exon);
-	   }
-       };
-   }
+  # tidy up output files.
+  unlink $genome_file;
+  unlink $evi_file;
 
 }
+
+
 
 
 =head2 output
@@ -183,9 +201,11 @@ sub add_Transcript{
    my ($self,@args) = @_;
 
    foreach my $val ( @args ) {
-       if( !ref $val || !$val->isa('Bio::EnsEMBL::Transcript') ) {
-	   $self->throw("[$val] is not a Transcript...");
-       }
+     $self->throw("[$val] is not a Transcript...") unless $val->isa("Bio::EnsEMBL::Transcript");
+#     if (!defined $val || !$val->isa('Bio::EnsEMBL::Transcript') ) {
+#       if( !ref $val || !$val->isa('Bio::EnsEMBL::Transcript') ) {
+
+ #      }
        push(@{$self->{'_transcript_array'}},$val);
    }
 }
