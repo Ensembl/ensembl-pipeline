116eb9ec7eb94b7de17304a6f64b084962298f6d
Ensembl Pipeline <enspipe@sanger.ac.uk>
2001-07-12 15:26:18 +0000

    removed hardwiring

--- modules/Bio/EnsEMBL/Pipeline/Runnable/RepeatMasker.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/RepeatMasker.pm
@@ -64,6 +64,10 @@ Internal methods are usually preceded with a _
 
 package Bio::EnsEMBL::Pipeline::Runnable::RepeatMasker;
 
+BEGIN {
+    require "Bio/EnsEMBL/Pipeline/pipeConf.pl";
+}
+
 use vars qw(@ISA);
 use strict;
 # Object preamble - inherits from Bio::Root::RootI;
@@ -109,18 +113,35 @@ sub new {
 							  @args);
     
     $self->clone($clone) if ($clone);       
-    if ($repmask)
-    {   $self->repeatmasker($repmask);  }
-    eval {
-	$self->repeatmasker($self->locate_executable('RepeatMasker'));
-    };
-    if ($@) {
-	$self->repeatmasker('/usr/local/ensembl/bin/RepeatMasker');
+
+    my $bindir = $::pipeConf{'bindir'} || undef;
+
+    if (-x $repmask) {
+        # passed from RunnableDB (full path assumed)
+        $self->repeatmasker($repmask);
     }
-    if ($arguments) 
-    {   $self->arguments($arguments) ;}
-    else
-    { $self->arguments('-low') ;      }
+    elsif ($::pipeConf{'bin_RepeatMasker'} && -x ($repmask = "$::pipeConf{'bin_RepeatMasker'}")) {
+        $self->repeatmasker($repmask);
+    }
+    elsif ($bindir && -x ($repmask = "$bindir/RepeatMasker")) {
+        $self->repeatmasker($repmask);
+    }
+    else {
+        # search shell $PATH
+        eval {
+            $self->repmask($self->locate_executable('RepeatMasker'));
+        };
+        if ($@) {
+            $self->throw("Can't find executable RepeatMasker");
+        }
+    }
+
+    if ($arguments) {
+	$self->arguments($arguments);
+    }
+    else {
+	$self->arguments('');
+    }
     return $self;
 }
 
