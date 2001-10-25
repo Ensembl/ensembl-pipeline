cf8e94d7004249f2a846b80da944ad2808910d52
Emmanuel Mongin <mongin@sanger.ac.uk>
2001-10-25 20:20:46 +0000

    Not finished but need to commit it or I am gonna forget about it...

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/ParacelHMM.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/ParacelHMM.pm
@@ -41,7 +41,6 @@ package Bio::EnsEMBL::Pipeline::Runnable::Protein::ParacelHMM;
 
 use vars qw(@ISA);
 use strict;
-
 use Bio::Root::RootI;
 use Bio::EnsEMBL::Pipeline::RunnableI;
 use Bio::EnsEMBL::SeqFeature;
@@ -55,10 +54,9 @@ use Bio::Tools::BPlite;
 
  Title    : new
  Usage    : my $seg =  Bio::EnsEMBL::Pipeline::Runnable::Protein::ParacelHMM->new
-                       ( -program    => '/usr/local/pubseq/bin/seg',
-                         -clone      => $clone,
-                         -analysisid => 4,
-                       );
+                       ( -program    => '',
+                         -clone      => $clone
+			 );
  Function : initialises ParacelHMM object
  Returns  : a ParacelHMM object
  Args     : a Bio::Seq object, the path to the program binaries and an analysisId
@@ -110,16 +108,23 @@ sub clone {
     if ($seq) {
 	eval {
 	    $seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-	};
+	    };
+	
+	print STDERR $@;
 
 	if (!$@) {
-	    $self->{'_sequence'} = $seq ;
-	    $self->clonename ($self->clone->id);
-	    $self->filename ($self->clone->id.".$$.seq");
-	    $self->results ($self->filename.".out");
+	    #Actually should allow to run one peptide at time...should switch to a normal alpha hmm run
+	    $self->throw("You shouldn't use the paracel box to run only one peptide, use hmmpfam instead");
+
+	    #$self->{'_sequence'} = $seq ;
+	    #$self->clonename ($self->clone->id);
+	    #$self->filename ($self->clone->id.".$$.seq");
+	    #$self->results ($self->filename.".out");
 	}
 	else {
-	    print STDERR "WARNING: The input_id is not a Seq object but if its a peptide fasta file, it should go fine\n";
+
+	    
+	    print STDERR "\nWARNING:The input_id is not a Seq object but if its a peptide fasta file, it should go fine\n\n";
 	    $self->{'_sequence'} = $seq ;
 	    $self->filename ("$$.tmp.seq");
 	    
@@ -136,7 +141,7 @@ sub clone {
  Title    : analysis
  Usage    : $self->analysis ($analysis);
  Function : get/set method for the analysis
- Example  :
+    Example  :
  Returns  : analysis
  Args     : analysis (optional)
  Throws   :
@@ -189,14 +194,14 @@ sub output {
 
 sub run {
     my ($self, $dir) = @_;
-
+    
     # check clone
     my $seq = $self->clone || $self->throw("Clone required for Program");
-
+    
     # set directory if provided
     $self->workdir ('/tmp') unless ($self->workdir($dir));
     $self->checkdir;
-
+    
     # reset filename and results as necessary (adding the directory path)
     my $tmp = $self->workdir;
     my $input = $tmp."/".$self->filename;
@@ -204,21 +209,21 @@ sub run {
     $tmp .= "/".$self->results;
     $self->results ($tmp);
 
-
- eval {
+    
+    eval {
 	$seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
 	};
-	
-
+    
+    
     if (!$@) {
 	#The inputId is a sequence file...got the normal way...
-
+	
 	# write sequence to file
 	$self->writefile;        
-
+	
 	# run program
 	$self->run_program;
-
+	
 	# parse output
 	$self->parse_results;
 	$self->deletefiles;
@@ -227,16 +232,16 @@ sub run {
 	#The clone object is not a seq object but a file.
 	#Perhaps should check here or before if this file is fasta format...if not die
 	#Here the file does not need to be created or deleted. Its already written and may be used by other runnables.
-
+	
 	$self->filename($self->clone);
-
+	
 	# run program
 	$self->run_program;
-
+	
 	# parse output
 	$self->parse_results;
     }
-    
+   
 }
 
 
@@ -256,15 +261,33 @@ sub run_program {
     my ($self) = @_;
     # run program
     #Should set up the environment to use the paracel box here
+    
+    #These variables correspond to internal values, this will be removed soon
+    my $fdf_dir = "/fdf/build109/gm0/0/protein_pipeline";
+    my ($filename) = $self->filename =~ /(\w+)$/;
+    
+    my $btkload = "btkload src=".$self->filename." dst=".$fdf_dir." seqtype=protein";
+
+    #$self->throw ("Error running $btkload\n")
+	#unless ((system("$btkload")) == 0);
+
+#btkload src=test_tmp1.in dst=/fdf/build109/gm0/0/mongin seqtype=protein"
+
+#"cd mongin; btk hmm database=/fdf/build109/gm0/0/mongin/test_tmp1.prot mhsp=data query=pfam_query_1.tbl -invert format=blast "
+
 
     my $paracelfile;
 #Get it from the conf file
     my $pfamquery;
 
+    my $run = "btk ".$self->analysis->program." database=".$filename.".prot mhsp=data query=".$self->analysis->db." -invert format=blast > ".$self->results;
+    
+    
 
-    #print STDERR "Running ".$self->analysis->program." ".$self->filename." -l > ".$self->results."\n";
-    $self->throw ("Error running ".$self->analysis->program." on ".$self->filename) 
-        unless ((system ($self->analysis->program." database=".$paracelfile." mhsp=data query=".$pfamquery." -invert format=blast > ".$self->results)) == 0); 
+    print STDERR "Running $run\n";
+    
+$self->throw ("Error running ".$self->analysis->program." on ".$self->filename) 
+        unless ((system ("btk ".$self->analysis->program." database=".$paracelfile." mhsp=data query=".$pfamquery." -invert format=blast > ".$self->results)) == 0); 
 }
 
 
@@ -294,7 +317,7 @@ sub parse_results {
         else {
             open (OUT, "<$resfile") or $self->throw ("Error opening $resfile");
             $filehandle = \*OUT;
-      }
+	}
     }
     else {
         # it'a a filehandle
@@ -303,12 +326,12 @@ sub parse_results {
     
     # parse
     #while (<$filehandle>) {
-     #   chomp;
-	#Write the parser here :)
+    #   chomp;
+    #Write the parser here :)
     my $report = new Bio::Tools::BPlite(-fh=>\$filehandle);
-
+    
     #Use protein feature adaptor to write the protein features in the database  
-            
+    
     while(my $sbjct = $report->nextSbjct) {
 	
 	while (my $hsp = $sbjct->nextHSP) {
@@ -334,8 +357,9 @@ sub parse_results {
 	    if ($evalue <= 0.01) {
 		$self->create_feature();
 	    }
+	}
+	close $filehandle;   
     }
-    close $filehandle;   
 }
 
 
@@ -356,8 +380,8 @@ sub create_feature {
     my ($self, $feat) = @_;
 
     my $analysis = $self->analysis;
-
-
+    
+    
     # create feature object
     my $feat1 = Bio::EnsEMBL::SeqFeature->new ( -seqname     => $feat->{name},
 						-start       => $feat->{start},
