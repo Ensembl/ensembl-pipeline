e92d5cd8b7e9ef5248d49d9bcb8d332de7975518
cvs2git <cvs2git>
2001-11-14 13:38:44 +0000

    This commit was manufactured by cvs2svn to create branch 'branch- ensembl-120'.
    Cherrypick from master 2001-11-14 13:38:43 UTC Val Curwen <vac@sanger.ac.uk> 'Start of an adaptor for retrieving inforamtion about EST (id, length) from':
        modules/Bio/EnsEMBL/Pipeline/DBSQL/ESTFeatureAdaptor.pm

---
+++ modules/Bio/EnsEMBL/Pipeline/DBSQL/ESTFeatureAdaptor.pm
@@ -0,0 +1,80 @@
+=head1 NAME
+
+=head1 SYNOPSIS
+
+  
+=head1 DESCRIPTION
+
+
+=head1 CONTACT
+
+ensembl-dev@ebi.ac.uk
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are 
+usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+package Bio::EnsEMBL::Pipeline::DBSQL::ESTFeatureAdaptor;
+
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inheriets from Bio::EnsEMBL::DBSQL::BaseAdaptor
+use Bio::EnsEMBL::DBSQL::BaseAdaptor;
+
+
+@ISA = qw(Bio::EnsEMBL::DBSQL::BaseAdaptor);
+
+sub get_est_length {
+  my ($self,$est_id) = @_;
+
+  my $query = "select est_length from est where est_id = '$est_id'";
+  my $sth = $self->prepare($query);
+  my $res = $sth->execute;
+
+  if ($sth->rows > 0) {
+    my $row = $sth->fetchrow_hashref;
+    my $id = $row->{'est_length'};
+    return $id;
+  }
+}
+
+sub exists_est {
+  my ($self,$est_id) = @_;
+
+  my $query = "select * from est where est_id = '$est_id'";
+  
+  my $sth = $self->prepare($query);
+  my $res = $sth->execute;
+
+  if ($sth->rows > 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+sub create_sql {
+  my($self,$dbname) = @_;
+
+  my $create = "create database $dbname;";
+
+  my $est = "create table est ( est_internal_id int(10) unsigned NOT NULL auto_increment,
+					  est_id          varchar(40) NOT NULL, 
+					  est_length int(10) unsigned NOT NULL,
+					  
+					  PRIMARY KEY (est_internal_id),
+					  UNIQUE  est_id(est_id)
+					);";
+  
+  
+  return $create . "\n" . $est . "\n";
+}
+
+1;      
