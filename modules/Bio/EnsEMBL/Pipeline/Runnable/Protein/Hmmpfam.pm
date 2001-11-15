11524f90504c005d16b5320351659cd5664b1e25
Worm Pipeline <wormpipe@sanger.ac.uk>
2001-11-15 10:54:48 +0000

    corrected mix-up of end and hend

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Hmmpfam.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Hmmpfam.pm
@@ -325,7 +325,7 @@ sub parse_results {
             $feature{score} = $score;
             $feature{p_value} = sprintf ("%.3e", $evalue);
             $feature{start} = $start;
-            $feature{end} = $hend;
+            $feature{end} = $end;
             $feature{hname} = $hid;
             $feature{hstart} = $hstart;
             $feature{hend} = $hend;
