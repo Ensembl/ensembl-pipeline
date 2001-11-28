d911b8f6c62fa786632d0babd163727f6e554d55
Simon Potter <scp@sanger.ac.uk>
2001-11-28 15:21:31 +0000

    added pod

--- modules/Bio/EnsEMBL/Pipeline/DBSQL/StateInfoContainer.pm
+++ modules/Bio/EnsEMBL/Pipeline/DBSQL/StateInfoContainer.pm
@@ -17,25 +17,31 @@ Bio::EnsEMBL::Pipeline::DBSQL::StateInfoContainer
 
 =head1 SYNOPSIS
 
-  $infoContainer = $dbobj->get_StateInfoContainer;
-
+  $sic = $dbobj->get_StateInfoContainer;
 
 =head1 DESCRIPTION
 
-  Module which encapsulates state request for objects in the database.
-  Starts of with a table InputIdAnalysis, providing which analysis was done to
-  inputIds but every state information access should go via this object.
+Module which encapsulates state request for objects in the database.
+Starts of with a table input_id_analysis, providing which analysis was
+done to inputIds but every state information access should go via
+this object.
 
-  Deliberatly NOT called an adaptor, as it does not serve obejcts.
+Deliberatly NOT called an adaptor, as it does not serve objects.
 
 =head1 CONTACT
 
-    Contact Arne Stabenau on implemetation/design detail: stabenau@ebi.ac.uk
-    Contact Ewan Birney on EnsEMBL in general: birney@sanger.ac.uk
+=over 4
+
+=item Arne Stabenau on implemetation/design detail: stabenau@ebi.ac.uk
+
+=item Ewan Birney on EnsEMBL in general: birney@ebi.ac.uk
+
+=back
 
 =head1 APPENDIX
 
-The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+The rest of the documentation details each of the object methods.
+Internal methods are usually preceded with a _
 
 =cut
 
@@ -51,6 +57,18 @@ use strict;
 
 @ISA = qw( Bio::Root::RootI );
 
+
+=head2 new
+
+Object constructor. Takes a
+Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor as argument.
+
+Can also be constructed by calling
+get_StateInfoContainer() on a
+Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor.
+
+=cut
+
 sub new {
   my ($class, $dbobj) = @_;
   my $self = $class->SUPER::new();
@@ -58,6 +76,16 @@ sub new {
   $self->db( $dbobj );
   return $self;
 }
+
+
+=head2 fetch_analysis_by_inputId_class
+
+Fetches all analyses
+which have been completed on a specific input ID
+and class. Takes two strings - input and class
+and returns a list of Bio::EnsEMBL::Analysis.
+
+=cut
 
 sub fetch_analysis_by_inputId_class {
   my ($self,$inputId, $class) = @_;
@@ -84,20 +112,46 @@ sub fetch_analysis_by_inputId_class {
   return @result;
 }
 
+
+=head2 store_inputId_class_analysis
+
+Stores an input ID, class and analysis.
+Takes an input ID, class (as strings)
+and a Bio::EnsEMBL::Analysis object. Throws an
+exception if any of the inputs are invalid.
+
+=cut
+
 sub store_inputId_class_analysis {
   my ($self, $inputId, $class, $analysis ) = @_;
 
-  my $sth = $self->prepare( qq{
-    INSERT INTO input_id_analysis (
-          input_id, class, analysis_id, created)
-          values (
-          '$inputId',
-          '$class',
-          ?,
-          now() )} );
-  $sth->execute( $analysis->dbID );
+  $self->throw("[$arg] is not a Bio::EnsEMBL::Analysis object")
+   unless $arg->isa("Bio::EnsEMBL::Analysis");
 
+  $self->throw("Invalid inputId [$inputId]")
+   unless $inputId;
+
+  # do we want to use a default class here?
+
+  $self->throw("Invalid class [$class]")
+   unless $class;
+
+  my $sth = $self->prepare(qq{
+    INSERT INTO input_id_analysis
+                (input_id, class, analysis_id, created)
+                values (?, ?, ?, now())
+  });
+  $sth->execute($inputId, $class, $analysis->dbID);
 }
+
+
+=head2 list_inputId_by_analysis
+
+Takes a Bio::EnsEMBL::Analysis object and
+returns a list of input IDs which have had
+that analysis performed on them.
+
+=cut
 
 sub list_inputId_by_analysis {
   my $self = shift;
@@ -118,6 +172,21 @@ sub list_inputId_by_analysis {
   return @result;
 }
 
+
+=head2 list_inputId_created_by_analysis
+
+As list_inputId_by_analysis() but
+also returns the time the analysis was
+created. Returns an list of (anonymous)
+arrays.
+
+  foreach my $id_time ($sic->list_inputId_created_by_analysis) {
+    my $id      = $id_time->[0];
+    my $created = $id_time->[1];
+  }
+
+=cut
+
 sub list_inputId_created_by_analysis {
   my $self = shift;
   my $analysis = shift;
@@ -136,6 +205,22 @@ sub list_inputId_created_by_analysis {
 
   return @result;
 }
+
+
+=head2 list_inputId_class_by_start_count
+
+Returns a list of all inputId and class,
+with an optional start and end limit.
+
+  # get 1st 100 entries from list
+
+  foreach my $idlist ($sic->list_inputId_class_by_start_count(0, 100)) {
+    my $id    = $idlist->[0];
+    my $class = $idlist->[1];
+  }
+
+
+=cut
 
 sub list_inputId_class_by_start_count {
   my $self = shift;
@@ -161,6 +246,16 @@ sub list_inputId_class_by_start_count {
   return @result;
 }
 
+
+=head2 delete_inputId_class
+
+Takes an input ID and class (as strings) and
+removes all matching entries from the pipeline
+database. See also
+delete_inputId() and delete_inputId_analysis().
+
+=cut
+
 sub delete_inputId_class {
   my $self = shift;
   my ($inputId, $class) = @_;
@@ -172,6 +267,16 @@ sub delete_inputId_class {
   $sth->execute($inputId, $class);
 }
 
+
+=head2 delete_inputId
+
+Takes an input ID (as a string) and removes
+all matching entries from the pipeline
+database. See also delete_inputId_class()
+and delete_inputId_analysis().
+
+=cut
+
 sub delete_inputId {
   my $self = shift;
   my ($inputId) = shift;
@@ -181,6 +286,21 @@ sub delete_inputId {
     WHERE  input_id = ?} );
   $sth->execute($inputId);
 }
+
+
+=head2 delete_inputId_analysis
+
+Takes an input ID (as a string) and an
+analysis internal ID and removes
+all matching entries from the pipeline
+database. See also delete_inputId()
+and delete_inputId_class().
+
+=cut
+
+# FIXME: should these delete methods all take
+# Bio::EnsEMBL::Analysis rather than internal ID
+# Need to be consistent
 
 sub delete_inputId_analysis {
   my $self = shift;
@@ -193,12 +313,35 @@ sub delete_inputId_analysis {
   $sth->execute($inputId, $analysisId);
 }
 
+
+=head2 db
+
+Method to get/set the DB object. Takes
+and returns a Bio::EnsEMBL::DBSQL::DBAdaptor.
+
+  my $job_adaptor = $sic->db->get_JobAdaptor;
+
+=cut
+
 sub db {
   my ( $self, $arg )  = @_;
+
   ( defined $arg ) &&
     ($self->{'_db'} = $arg);
   $self->{'_db'};
 }
+
+
+=head2 prepare
+
+Convenience DBI prepare method.
+
+  my $sth = $sic->prepare(q{
+    SELECT distinct input_id from input_id_analysis
+  });
+  $sth->execute;
+
+=cut
 
 sub prepare {
   my ( $self, $query ) = @_;
@@ -217,6 +360,14 @@ sub deleteObj {
     }
   }
 }
+
+
+=head2 create_tables
+
+Create table needed by
+Bio::EnsEMBL::Pipeline::DBSQL::StateInfoContainer
+
+=cut
 
 sub create_tables {
   my $self = shift;
