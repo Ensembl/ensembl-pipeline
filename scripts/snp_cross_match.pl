4ee52d119853f10cebf3f2533f88fa8f5784c271
Yuan Chen <yuan@sanger.ac.uk>
2002-01-21 12:34:24 +0000

    few changes

--- scripts/snp_cross_match.pl
+++ scripts/snp_cross_match.pl
@@ -9,7 +9,7 @@
 
 use strict;
 use Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap;
-use Bio::EnsEMBL::DBSQL::CrossMatchDBAdaptor;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::ExternalData::SNPSQL::DBAdaptor; 
 use Bio::EnsEMBL::DBLoader;
 use Getopt::Long;
@@ -18,191 +18,124 @@ $| = 1;
 
 
 my $dbtype     = 'rdb';
-my $host_snp   = 'ecs1a';
+my $host       = 'ecs1d';
+my $host_snp   = 'ecs1d';
 my $port       = '';
-my $dbname_snp = 'snp120';
-my $module     = 'Bio::EnsEMBL::DBSQL::CrossMatchDBAdaptor';
+my $user       = 'ensro';
+my $dbname     = 'homo_sapiens_core_130';
+my $dbname_snp = 'homo_sapiens_snp_130';
 my $module_snp = 'Bio::EnsEMBL::ExternalData::SNPSQL::DBAdaptor';
 
-my $locator_cross = "$module/host=$host_snp;port=$port;dbname=$dbname_snp;user=ensro";
-my $locator_snp = "$module_snp/host=$host_snp;port=$port;dbname=$dbname_snp;user=ensro";
+my $locator_snp = "$module_snp/host=$host_snp;port=$port;dbname=$dbname_snp;user=$user";
 
-my $debug=2;
-my $score =45;
+my $debug=1;
+my $score =80;
 my $masklevel = 80;
 my $minmatch = 20;
 
 my $start_refnum = $ARGV[0];
 my $end_refnum = $ARGV[1];
 
-my $file="/work4/yuan/SNP_MAP2/SNP_EXIST_$start_refnum";
+my $file="/work4/yuan/SNP_MAP/SNP_EXIST_$start_refnum";
 open SAME, ">$file" || die "$file: $1";
-my $file1="/work4/yuan/SNP_MAP2/SNP_DIFF_$start_refnum";
+my $file1="/work4/yuan/SNP_MAP/SNP_DIFF_$start_refnum";
 open DIFF, ">$file1" || die "$file1: $1";
-my $file2="/work4/yuan/SNP_MAP2/NO_CLONE_$start_refnum";
+my $file2="/work4/yuan/SNP_MAP/NO_CLONE_$start_refnum";
 open NO_CLONE, ">$file2" || die "$file2: $1";
 
 
-print STDERR "Using $locator_snp for crossmatch db\n";
-my $crossdb =  Bio::EnsEMBL::DBLoader->new($locator_cross);
-my $snpdb =  Bio::EnsEMBL::DBLoader->new($locator_snp); 
+print STDERR "Using $locator_snp for snp db\n";
 
+my $snpdb =  Bio::EnsEMBL::DBLoader->new($locator_snp); 
+my $db = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
+					     -host => $host,
+					     -user => $user,
+					     -dbname => $dbname);
+
+my (%no_clone, %diff);
+my $first=1;
+my $crossmap = Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap->new(-snpdb=>$snpdb, 
+								    -db   =>$db,
+								    -score =>$score, 
+								    -minmatch =>$minmatch,
+								    -masklevel =>$masklevel,
+								    -debug => $debug,
+								    -first =>$first
+								   );
 print STDERR "Fetching input from snpdb\n";
 
-my @infos = $snpdb->get_snp_info_between_two_refsnpid($start_refnum,$end_refnum);
 
-my ($new_count, $old_count, $same_count, %id_acc, %acc_info, %done, @bases, %no_clone, %rec);
+$crossmap->fetch_input($start_refnum,$end_refnum);
 
-foreach my $info (@infos) {
+my (%all_version, %same_version, %diff_version, %diff_real);
+my $same_version = $crossmap->same_version;
+my $all_version = $crossmap->all_version;
+
+%same_version = %$same_version;
+%all_version = %$all_version;
+
+foreach my $snp (values %same_version) {
+  print SAME $snp->id,"\t",$snp->acc,"\t",$snp->version,"\t",$snp->start,"\t",$snp->end,"\t\tsnp_map\t",$snp->strand,"\n";
+}
+
+foreach my $refsnpid (keys %all_version) {
+  if (!$same_version{$refsnpid}) {
+    $diff_version{$refsnpid}=1;
+  }
+}
+
+foreach my $refsnpid (keys %diff_version) {
+  my $crossmap1 = Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap->new(-snpdb=>$snpdb,
+								    -db   =>$db,
+								    -score =>$score,
+								    -minmatch =>$minmatch,
+								    -masklevel =>$masklevel,
+								    -debug => $debug,
+								   );
+
   
-  #my ($refsnpid,$snpclass,$snptype,$observed,$seq5,$seq3,$acc,$version,$start,$end,$strand) = @$info;
-  if ($info) {
-    my $refsnpid = $$info[0];
-    my $mapweight = $$info[2];
-    my $acc      = $$info[6];
-    my $version  = $$info[7];
-    if ($mapweight <=2) {
-      push (@{$id_acc{$refsnpid}}, "$acc-$version"); 
-      my $key = "$acc-$refsnpid-$version";
-      $acc_info{$key} = $info;
+  my ($noclone) = $crossmap1->fetch_input($refsnpid);
+  if ($noclone) {
+    $no_clone{$refsnpid}=$noclone;
+  }
+  else {
+    print STDERR "Running mapping for diff clone $refsnpid\n";
+    $crossmap1->run;
+    print STDERR "Writing output for diff clone $refsnpid\n";
+    my ($last_line) = $crossmap1->write_output;
+    if ($last_line) {
+      my @all = split /\t/, $last_line;
+      print DIFF "$last_line\n";
+      $diff_real{$all[0]}=1;
     }
   }
 }
-
-REF : foreach my $refsnpid (sort numeric keys %id_acc) {
-  if ($refsnpid) {
-    print "refsnpid is $refsnpid and @{$id_acc{$refsnpid}}\n";
-    #next CLONE;
-    CLONE : foreach my $clone (@{$id_acc{$refsnpid}}) {
-      if (!$done{$refsnpid}) {
-	my ($acc, $version) = split /\-/, $clone;
-	my $key = "$acc-$refsnpid-$version";
-	if ($acc_info{$key}) {
-	  my $first=1;
-	  my $crossmap = Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap->new(-crossdb=>$crossdb, 
-									      -score =>$score, 
-									      -minmatch =>$minmatch,
-									      -masklevel =>$masklevel,
-									      -debug => $debug,
-									      -first =>$first
-									     );
-	  my ($done,$start,$end,$strand) = $crossmap->check_clone_in_GP($acc_info{$key});
-	  if ($done =~ /^done/) {
-	    $done{$refsnpid}= "same version";
-	    if ($debug>1) {
-	      print STDERR "$refsnpid $clone has same version in first round\n";
-	    }
-	    print SAME "$refsnpid\t$acc\t$version\t$start\t$end\tsnp_map\t$strand\n";
-	    next REF;
-	  }
-	  elsif ($done =~ /next/) {
-	    next CLONE;
-	  }
-	}
-      }
-    }
-  }
-}
-
-REF1 : foreach my $refsnpid (sort numeric keys %id_acc) {
-  if ($refsnpid) {
-    #print "refsnpid is $refsnpid and @{$id_acc{$refsnpid}}\n";
-    #next CLONE;
-    CLONE1 : foreach my $clone (@{$id_acc{$refsnpid}}) {
-      if (!$done{$refsnpid}) {
-	my ($acc, $version) = split /\-/, $clone;
-	my $key = "$acc-$refsnpid-$version";
-	if ($acc_info{$key}) {
-	  my $crossmap = Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap->new(-crossdb=>$crossdb,
-									      -score =>$score,
-									      -minmatch =>$minmatch,
-									      -masklevel =>$masklevel,
-									      -debug => $debug,
-									     );
-
-	  my ($done,$start,$end,$strand) = $crossmap->check_clone_in_GP($acc_info{$key});
-	  if ($done =~ /^done/) {
-	    $done{$refsnpid}= "same version";
-	    if ($debug>1) {
-	      print STDERR "$refsnpid $clone has same version\n";
-	    }
-	    print SAME "$refsnpid\t$acc\t$version\t$start\t$end\tsnp_map\t$strand\n";
-	    next REF1;
-	  }
-	  elsif ($done =~ /diff/) {
-	    print STDERR "Running mapping for clone $refsnpid $acc\n";
-	    $crossmap->run;
-	    print STDERR "Writing output for clone $refsnpid $acc\n";
-	    my ($last_line) = $crossmap->write_output;
-	    if ($last_line) {
-	      print DIFF "$last_line\n";
-	      $done{$refsnpid}="diff";
-	      next REF1;
-	    }
-	    else {
-	      next CLONE1;
-	    }
-	  }
-	  elsif ($done =~ /clone/) {
-	    if ($debug>1) {
-	      print STDERR "$refsnpid $acc is not in our database\n";
-	    }
-	    push (@{$no_clone{$refsnpid}}, "no clone $acc\n");
-	    next CLONE1;
-	  }
-	  elsif ($done =~ /flank/) {
-	    if ($debug>1) {
-	      print STDERR "no flank seqs for $refsnpid $acc in our database\n";
-	    }
-	    push (@{$no_clone{$refsnpid}}, "no flank $acc-$version\n");
-	    next CLONE1;
-	  }
-	  elsif ($done == "") {
-	    if ($debug>1) {
-	      print STDERR "Do not know the reseason for $refsnpid $acc\n";
-	    }
-	    push (@{$no_clone{$refsnpid}}, "no reason $acc-$version\n");
-	    next CLONE1;
-	  }
-	}
-	else {
-	  print "There is no info for $acc-$refsnpid\n";
-	  next CLONE1;
-	}
-      }
-    }
-  }
-}
-
-sub numeric {$a<=>$b}
 
 my ($no_clone_count, $same_version_count, $diff_version_count, $no_flank_count, $tot_count, $no_cross_count);
-foreach my $refsnpid (keys %id_acc) {
+foreach my $refsnpid (keys %all_version) {
   $tot_count++;
-  if (!$done{$refsnpid}) {
-    if ($no_clone{$refsnpid}) {
-      print NO_CLONE "$refsnpid @{$no_clone{$refsnpid}}\n";
-      $no_clone_count++;
+  if ($no_clone{$refsnpid}) {
+    print NO_CLONE "$refsnpid ",$no_clone{$refsnpid}->acc,"\n";
+    $no_clone_count++;
+  }
+  elsif ($same_version{$refsnpid}) {
+    $same_version_count++;
+  }
+  elsif ($diff_version{$refsnpid}) {
+    if ($diff_real{$refsnpid}) {
+      $diff_version_count++;
     }
     else {
       $no_cross_count++;
-      print "no CROSS_MATCH results for $refsnpid\n";}
-    
+    }
   }
-  elsif ($done{$refsnpid} =~ /same/) {
-    $same_version_count++;
-  }
-  elsif ($done{$refsnpid} =~ /diff/) {
-    $diff_version_count++;
-  }
-  
 }
 
 close(SAME)     || die $!;
 close(DIFF)     || die $!;
 close(NO_CLONE) || die $!;
 
-print STDERR "total count is $tot_count, $same_version_count have same versions, $diff_version_count have diff clones and $no_clone_count have no clones and $no_cross_count have no_cross_match results\n";
+print STDERR "total count is $tot_count, $same_version_count have same versions, $diff_version_count have diff clones and $no_clone_count have no clones, $no_cross_count have no_cross_match results\n";
 
 
 
--- scripts/snp_ssaha_match.pl
+++ scripts/snp_ssaha_match.pl
@@ -7,32 +7,39 @@
 use strict;
 use IPC::Open2; 
 use Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap;
-use Bio::EnsEMBL::DBSQL::CrossMatchDBAdaptor;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::ExternalData::SNPSQL::DBAdaptor; 
 use Bio::EnsEMBL::DBLoader;
 #use Getopt::Long;
 
-my $host_snp   = 'ecs1a';
+my $host   = 'ecs1d';
+my $host_snp = 'ecs1d';
 my $port       = '';
-my $dbname_snp = 'snp120';
+my $user   = 'ensro';
+#my $dbname_snp = 'mouse_snp';
+my $dbname_snp = 'homo_sapiens_snp_120';
+my $dbname     = 'homo_sapiens_core_120';
 
-my $module_cross     = 'Bio::EnsEMBL::DBSQL::CrossMatchDBAdaptor';
 my $module_snp = 'Bio::EnsEMBL::ExternalData::SNPSQL::DBAdaptor';
+my $locator_snp = "$module_snp/host=$host_snp;port=$port;dbname=$dbname_snp;user=$user";
 
-my $locator_cross = "$module_cross/host=$host_snp;port=$port;dbname=$dbname_snp;user=ensro";
-my $locator_snp = "$module_snp/host=$host_snp;port=$port;dbname=$dbname_snp;user=ensro";
+print STDERR "Using $locator_snp for snp_db\n";
 
-print STDERR "Using $locator_cross for crossmatch db\n";
-my $crossdb =  Bio::EnsEMBL::DBLoader->new($locator_cross);
-my $db_snp =  Bio::EnsEMBL::DBLoader->new($locator_snp);
-my $db = $crossdb->new_dbobj;#dbhandle defined in snp120 database
+my $snpdb =  Bio::EnsEMBL::DBLoader->new($locator_snp);
+my $db = Bio::EnsEMBL::DBSQL::DBAdaptor->new(-host =>$host,
+					     -user  =>$user,
+					     -dbname=>$dbname);
 
-my $score =45;
+my $score =50;
 my $masklevel = 80;
 my $minmatch = 20;
 my $debug=2;
 
-my ($file_num,$input,$diff,$seq_file,$ssaha_out);
+my ($file_num,$input,$diff,$seq_file,$ssaha_out,$mouse);
+
+if ($dbname_snp =~ /mouse/i) {
+  $mouse=1;
+}
 
 if ($ARGV[0] and $ARGV[1]) {
   $input = $ARGV[0];
@@ -43,101 +50,127 @@ if ($ARGV[0] and $ARGV[1]) {
   $ssaha_out = "$input\_ssaha";
 }
 
-elsif ($ARGV[0] =~/^\d+/ and !$ARGV[1]) {
+elsif ($ARGV[0] =~/^\d+|\d+$/ and !$ARGV[1]) {
   $file_num = $ARGV[0];
-  open FILE, "/work4/yuan/SNP_MAP/NO_CLONE_$file_num" || die "Can not open file: $! \n";
+  open FILE, "/work4/yuan/MOUSE/$file_num" || die "Can not open file: $! \n";
   #open FILE, "/work4/yuan/OUT1/NO_CROSS_MATCH$file_num" || die "Can not open file: $! \n";
-  open DIFF, ">/work4/yuan/SNP_MAP/SNP_SSAHA_FROM_NO_CLONE_$file_num" || die "Can not open FINAL: $! \n";
+  open DIFF, ">/work4/yuan/MOUSE/DIFF\_$file_num" || die "Can not open FINAL: $! \n";
   #open DIFF, ">/work4/yuan/OUT1/SNP_SSAHA_FROM_NO_CROSS_MATCH$file_num" || die "Can not open FINAL: $! \n";
   
   #my $seq_file = "/nfs/acari/yuan/ensembl/test/SNP_SSAHA_FROM_NO_CLONE_seq_$file_num";
-  my $seq_file =  "/nfs/acari/yuan/ensembl/test/SNP_SSAHA_NO_CROSS_MATCH_seq_$file_num";
-  my $ssaha_out = "/nfs/acari/yuan/ensembl/test/NO_CROSS_MATCH_seq_$file_num";
+  $seq_file =  "/work4/yuan/MOUSE/SEQ\_$file_num";
+  $ssaha_out = "/work4/yuan/MOUSE/ssaha\_$file_num";
 }
 
 my (%done, %no_clone, %find, %rec ,%snppos);
 
-if (!(-e $ssaha_out)) {
-  while (<FILE>) {
-    if (/^\d+/ or /CROSS_MATCH/) { 
-      s/^\s+//;
-      chomp;
-      my @all; my $refsnpid;
-      @all = split;
-      if (/^\d+/) {
-	$refsnpid = $all[0];
-      }
-      elsif (/CROSS_MATCH/) { 
-	$refsnpid = $all[4];
-      }
-      $no_clone{$refsnpid}=1;
+while (<FILE>) {
+  if (/^\d+/ or /CROSS_MATCH/) { 
+    s/^\s+//;
+    chomp;
+    my @all; my $refsnpid;
+    @all = split;
+    if (/^\d+/) {
+      $refsnpid = $all[0];
     }
-  }
-  
-  
-  my $clone_count = keys %no_clone;
-  print "Total key is $clone_count\n";
-  
-  my $snp_seq_all; 
-  
-  foreach my $refsnpid (sort numeric keys %no_clone) {
-    my $info = $db_snp->get_snp_info_by_refsnpid($refsnpid);
-    if ($info) {
-      #print "refsnpid is $refsnpid and info is $info\n";
-      my ($refsnpid,$snpclass,$snptype,$observed,$seq5,$seq3,$acc,$version,$start,$end,$strand) = @$info;
-      my $snp_pos = (length $seq5) +1;
-      $observed =~ s/\/.*//;
-      my $snp_seq = $seq5.$observed.$seq3;
-      $snp_seq =~ s/\s+//g;
-      if (length $snp_seq <40 ) {
-	#my $crossmap = Bio::EnsEMBL::Pipeline::RunnableDB::CrossCloneMap->new(-crossdb=>$crossdb, -score =>80, -debug=>$debug);
-	#my $snp_clone_seq = $crossmap->get_seq_from_embl($acc,$version);
-	#undef $crossmap;
-	#my $snp_seq;
-	#if ($snp_clone_seq) {
-	#  $snp_seq = substr ($$snp_clone_seq, $start-100, 200);
-	#}
-	print "SHORT FLANK SEQ FOR $refsnpid\n";
-	#$snp_pos = 100;
-      }
-      $snppos{$refsnpid} = $snp_pos;
-      $rec{$refsnpid} = $info;
-      $snp_seq_all .=">$refsnpid\n$snp_seq\n";
+    elsif (/CROSS_MATCH/) { 
+      $refsnpid = $all[4];
     }
+    $no_clone{$refsnpid}=1;
   }
+}
   
-  open FASTA, ">$seq_file" || die "Can not open $seq_file: $! \n";
-  print FASTA $snp_seq_all;
-  close FASTA;
   
-  sub numeric {$a<=>$b}
-  
+my $clone_count = keys %no_clone;
+print "Total key is $clone_count\n";
+
+my $snp_seq_all; 
+foreach my $refsnpid (sort numeric keys %no_clone) {
   if ($debug>1) {
-    #print STDERR "The query seq is $snp_seq_all\n";
+    print "refsnpid is $refsnpid\n";
   }
-}				###end if -e $ssaha_out
+  my ($info) = $snpdb->get_snp_info_by_refsnpid($refsnpid,$mouse);
+  if ($info) {
+    if ($debug>1) {
+      print "refsnpid is $refsnpid and info is $info\n";
+    }
+    print "THIS IS $info\n";
+    my $observed = $info->alleles;
+    $observed =~ s/\/.*//;
+    my $seq5 = $info->upStreamSeq;
+    my $seq3 = $info->dnStreamSeq;
+    #print STDERR "seq5 is $seq5 and seq3 is $seq3\n";
+    my $snp_pos = (length $seq5) +1;
+    my $snp_seq = $seq5.$observed.$seq3;
+    $snp_seq =~ s/\s+//g;
+    #print "the length of query sequence is ", length($snp_seq), "\n";
+    if (length $snp_seq <40 ) {
+      #my $crossmap = Bio::EnsEMBL::Pipeline::RunnableDB::CrossCloneMap->new(-crossdb=>$crossdb, -score =>80, -debug=>$debug);
+      #my $snp_clone_seq = $crossmap->get_seq_from_embl($acc,$version);
+      #undef $crossmap;
+      #my $snp_seq;
+      #if ($snp_clone_seq) {
+      #  $snp_seq = substr ($$snp_clone_seq, $start-100, 200);
+      #}
+      print "SHORT FLANK SEQ FOR $refsnpid\n";
+      #$snp_pos = 100;
+    }
+    $snppos{$refsnpid} = $snp_pos;
+    $rec{$refsnpid} = $info;
+    $snp_seq_all .=">$refsnpid\n$snp_seq\n";
+    #print "this is $snp_seq_all\n";
+  }
+}
+
+open FASTA, ">$seq_file" || die "Can not open $seq_file: $! \n";
+print FASTA $snp_seq_all;
+close FASTA;
+
+sub numeric {$a<=>$b}
+
+if ($debug>1) {
+  #print STDERR "The query seq is $snp_seq_all\n";
+}
 
 if (!(-e $ssaha_out) and -e $seq_file) {
-  open IN, "cat $seq_file | /usr/local/badger/bin/ssahaClient tcs1a 50000 20 0 0 0 DNA 10000 3 size |" ||  die "OPEN SSAHA failed :$!\n";
+  #### note port 50000 for human and 50006 is for mouse
+  #open IN, "cat $seq_file | /usr/local/badger/bin/ssahaClient tcs1a 50006 20 0 0 0 DNA 10000 2 size |" ||  die "OPEN SSAHA failed :$!\n";
+  open IN, "cat $seq_file | /usr/local/ensembl/bin/ssahaClient tcs1a 50000 20 0 0 0 DNA 10000 2 size |" ||  die "OPEN SSAHA failed :$!\n";
 }
 elsif (-e $ssaha_out) {
   open IN, "$ssaha_out" || die "$ssaha_out can not open :$!\n";
 }
 
+#open TEST, ">$ARGV[0]\_test";
+
 while (<IN>) {
   chomp;
   my ($qname, $qstart, $qend, $sname, $sstart, $ssend, $dir, $num, $perc) = split (/\s+/, $_); 
-  #print "this is line $_ and snp_pos = $snppos{$qname}\n";
-  if ($snppos{$qname}>$qstart-20 and $snppos{$qname}<$qend+20) {
+  print "this is line $_ and snp_pos = $snppos{$qname}\n";
+  #if ($snppos{$qname}>$qstart-20 and $snppos{$qname}<$qend+20) {
     push (@{$find{$qname}}, $sname);
-  }
+    #if ($snppos{$qname}>$qstart and $snppos{$qname}<$qend) {#####added for testing
+    #my $diff_po = $snppos{$qname}-$qstart;
+    #my $snp_po = $sstart+$diff_po;
+    #my ($clone, $version) = split /\./, $sname;
+    #my $strand;
+    #if ($dir eq 'F') {
+    #  $strand=1;
+    #}
+    #else {
+    #  $strand=-1;
+    #}
+    #print TEST "TEST $qname\t$sname\t$version\t$snp_po\t\tsnp_map\t$strand\n";
+    #}
+  #}
 }
-
+my $count;
 foreach my $refsnpid (keys %find) {
-  my (@contigs, @raw_contigs, $count);
+  my (@contigs, @raw_contigs);
   my @contigs = @{$find{$refsnpid}};
   print "this is contigs @contigs\n";
-  my $crossmap = Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap->new(-crossdb=>$crossdb, 
+  my $crossmap = Bio::EnsEMBL::Pipeline::RunnableDB::CrossSNPMap->new(-snpdb=>$snpdb, 
+								      -db    =>$db,
 								      -score =>$score, 
 								      -minmatch =>$minmatch,
 								      -masklevel =>$masklevel,
@@ -146,11 +179,8 @@ foreach my $refsnpid (keys %find) {
   ##get rid off duplicated contigs
   my %uniq_contig;
   foreach my $contig (@contigs) {
-    if (!$uniq_contig{$contig}) {
+    if (!$uniq_contig{$contig} and $contig) {
       $uniq_contig{$contig}=1;
-      if (!$db) {
-	$db = $crossmap->dbobj->new_dbobj; 
-      }
       my $rc;
       eval {
 	$rc = $db->get_Contig($contig);
@@ -180,32 +210,24 @@ foreach my $refsnpid (keys %find) {
   }
 
   #print "this is raw_contigs @raw_contigs\n";
-  my $done;
-  if ($rec{$refsnpid}) {
-    ($done) = $crossmap->check_clone_in_GP($rec{$refsnpid},@raw_contigs);
+  
+  print STDERR "fetching input for clone $refsnpid\n";
+  $crossmap->fetch_input($refsnpid,"0",@raw_contigs);
+  print STDERR "Running for clone $refsnpid\n";
+  $crossmap->run;
+  print STDERR "Writing output for clone $refsnpid\n";
+  my ($last_line) = $crossmap->write_output ();
+  if ($last_line) {
+    print DIFF "$last_line\n";
+    $done{$refsnpid}=1;
+    $count++;
+    print STDERR "$count $refsnpid has done\n";
   }
   else {
-    my $info = $db_snp->get_snp_info_by_refsnpid($refsnpid);
-    if ($info) {
-      print "refsnpid is $refsnpid and info is $info\n";
-      ($done) = $crossmap->check_clone_in_GP($info,@raw_contigs);
-    }
-  }
-  if ($done =~ /diff/) {
-    $crossmap->run;
-    print STDERR "Writing output for clone $refsnpid\n";
-    my ($last_line) = $crossmap->write_output ();
-    if ($last_line) {
-      print DIFF "$last_line\n";
-      $done{$refsnpid}=1;
-      $count++;
-      print STDERR "$count $refsnpid has done\n";
-    }
-    else {
-      print "no match at current criterion for $refsnpid\n";
-    }
+    print "no match at current criterion for $refsnpid\n";
   }
 }
+
 
 #open OUT, ">/work4/yuan/SNP_MAP/final_no_clone_from_no_clone_$file_num";
 open OUT, ">/work4/yuan/SNP_MAP2/final_no_clone_from_cross_match$file_num";
