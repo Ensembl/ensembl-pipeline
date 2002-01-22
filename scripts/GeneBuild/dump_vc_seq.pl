8ad829685e1802e717afaa8cc3d1b5b55634cfd8
Val Curwen <vac@sanger.ac.uk>
2002-01-22 10:28:35 +0000

    Simple script to dump chromosomal sequences ready for pmatch - first stage of targetted build

---
+++ scripts/GeneBuild/dump_vc_seq.pl
@@ -0,0 +1,119 @@
+#!/usr/local/bin/perl -w
+
+=head1 NAME
+
+  dump_vc_seq.pl
+
+=head1 SYNOPSIS
+ 
+  chr_genedump.pl
+
+=head1 DESCRIPTION
+
+  Dumps the sequence of a virtual contig to file, masked or unmasked.
+  db and dnadb can be the same or different databases - just make sure all the relevant arguments are passed in.
+  The user for each db can (and probably should) be read-only.
+  Specify chrname, start and end.
+
+=head1 OPTIONS
+
+  -dbname
+  -dbhost
+  -dnadbname
+  -dnadbhost
+  -path
+  -start
+  -end
+  -chrname
+  -out
+  -path
+  -masked
+
+=cut
+
+use strict;
+use Getopt::Long;
+use Bio::SeqIO;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+
+my $dbname     = '';
+my $dbuser     = ''; # always use read only
+my $dbhost     = '';
+my $dnadbname  = '';
+my $dnadbuser  = ''; # always use ensro
+my $dnadbhost  = '';
+my $path       = '';
+my $start;
+my $end;
+my $outfile    = 'out.fa';
+my $chrname    = '';
+my $masked     = 0;
+
+&GetOptions( 
+	    'dbname:s'     => \$dbname,
+	    'dbhost:s'     => \$dbhost,
+	    'dnadbname:s'  => \$dnadbname,
+	    'dnadbhost:s'  => \$dnadbhost,
+	    'outfile:s'    => \$outfile,
+	    'chrname:s'    => \$chrname,
+	    'path:s'       => \$path,
+	    'start:i'      => \$start,
+	    'end:i'        => \$end,
+	    'masked'       => \$masked,
+	   );
+
+# usage
+if(!defined $dbname    ||
+   !defined $dnadbname ||
+   !defined $dbhost    ||
+   !defined $dnadbhost ||
+   !defined $chrname   ||
+   !defined $start     ||
+   !defined $end       ||
+   !defined $path    
+  ){
+  print  "USAGE: dump_vc_seq.pl -dbname dbname -host host -chrname chr -start start_pos -end end_pos -path path\n optional:\n -masked, for repmasked sequence;\n -outfile to specify a filename other than out.fa\n";
+  exit(1);
+}
+
+# global stuff
+my $dnadb =  new Bio::EnsEMBL::DBSQL::DBAdaptor(
+						-host   => $dnadbhost,
+						-dbname => $dnadbname,
+						-user   => $dnadbuser,
+					       );
+
+my $db =  new Bio::EnsEMBL::DBSQL::DBAdaptor(
+					     -host   => $dbhost,
+					     -dbname => $dbname,
+					     -user   => $dbuser,
+					     -dnadb  =>$dnadb
+					    );
+
+$db->static_golden_path_type($path);
+
+my $sgpa = $db->get_StaticGoldenPathAdaptor;
+
+print STDERR "about to fetch $chrname $start $end\n";
+
+my $vc = $sgpa->fetch_VirtualContig_by_chr_start_end($chrname,$start,$end);
+
+
+my $seqout = Bio::SeqIO->new( '-format' => 'fasta',
+                              '-file'   => ">>$outfile");
+my $did = $chrname . "." . $start . "-" . $end;
+if($masked){
+  print STDERR "about to get repeatmasked sequence\n";
+  my $rmseq = $vc->get_repeatmasked_seq;
+  $rmseq->display_id($did);
+  print STDERR "about to write sequence to $outfile\n";
+
+  $seqout->write_seq($rmseq);	       
+}
+
+else {
+#  my $did = $chrname . "." . $start . "-" . $end;
+  $vc->primary_seq->display_id($did);
+  print STDERR "about to write sequence to $outfile\n";
+  $seqout->write_seq($vc->primary_seq);
+}
