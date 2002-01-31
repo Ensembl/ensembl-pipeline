253d4004f02263be7c644694ff2fbf0c143db3ff
Val Curwen <vac@sanger.ac.uk>
2002-01-31 14:50:24 +0000

    bringing in sync with brach-ensembl-121

--- scripts/EST/make_bsubs.pl
+++ scripts/EST/make_bsubs.pl
@@ -30,15 +30,25 @@ require "Bio/EnsEMBL/Pipeline/EST_conf.pl";
 my %chrhash;
 
 # declare these here so we can refer to them later
-my $ex_resultsdir = "exonerate_est/results";
-my $ex_bsubdir    = "exonerate_est/bsub";
-my $filterdir     = "filter_and_e2g";
+my $ex_resultsdir       = "exonerate_est/results";
+my $ex_bsubdir          = "exonerate_est/bsub";
+my $filterdir           = "filter_and_e2g";
+my $est_genebuilder_dir = "est_genebuilder";
 
+# get chr info from the database where you have contig, clone and static_golden_path
 &get_chrlengths();
+
+# make output directories
 &make_directories();
+
+# create jobs file for Exonerate
 &make_exonerate_bsubs();
+
+# create jobs file for FilterESTs_and_E2G
 &make_filter_bsubs();
 
+# create jobs file for EST_GeneBuilder
+&make_EST_GeneBuilder_bsubs();
 
 =head2 make_directories
 
@@ -54,6 +64,7 @@ sub make_directories {
   my $scratchdir =  $::scripts_conf{'tmpdir'};
 
   my @resdirs = split /\//, $ex_resultsdir;
+
   # exonerate_ests
   my $exoneratedir = $scratchdir . "/" . $resdirs[0] . "/";
   makedir($exoneratedir);
@@ -80,6 +91,15 @@ sub make_directories {
   
   foreach my $chr(keys %chrhash){
     my $chrdir = $filter . $chr . "/";
+    makedir($chrdir);
+  }
+
+  # EST_GeneBuilder output directories
+  my $estbuilder_path = $scratchdir . "/" . $est_genebuilder_dir . "/";
+  makedir($estbuilder_path);
+  
+  foreach my $chr(keys %chrhash){
+    my $chrdir = $estbuilder_path . $chr . "/";
     makedir($chrdir);
   }
 
@@ -135,7 +155,7 @@ sub make_exonerate_bsubs {
   my $estfile = $::scripts_conf{'estfile'}; # may be a full path
   my @path = split /\//, $estfile;
   $estfile = $path[$#path];
-  $estfile .= "_reruns_chunk_";
+  $estfile .= "_chunk_";
 
   my $numchunks = $::scripts_conf{'estchunknumber'};
 
@@ -149,7 +169,7 @@ sub make_exonerate_bsubs {
     my $outfile   = $bsubout . $chunk;
     my $errfile   = $bsuberr . $chunk;
     
-    my $command = "bsub -q $queue -o $outfile -e $errfile -E \"$check $chunk\" $exonerate_est -chunkname $chunk";
+    my $command = "bsub -q $queue -C0 -o $outfile -e $errfile -E \"$check $chunk\" $exonerate_est -chunkname $chunk";
     print OUT "$command\n";
   }
   
@@ -182,7 +202,7 @@ sub make_filter_bsubs {
   foreach my $chr(keys %chrhash) {
     my $length = $chrhash{$chr};
     
-    my $chrdir = $filter . "/$chr";
+    my $chrdir = $filter . "$chr";
     my $count = 1;
 
     while($count < $length) {
@@ -196,7 +216,7 @@ sub make_filter_bsubs {
       my $input_id = $chr . "." . $start . "-" .  $end;
       my $outfile  = $chrdir . "/$input_id.out";
       my $errfile  = $chrdir . "/$input_id.err";
-      my $command = "bsub -q $queue -o $outfile -e $errfile -E \"$runner -check -runnable Bio::EnsEMBL::Pipeline::RunnableDB::FilterESTs_and_E2G\" $filter_e2g -input_id $input_id";
+      my $command = "bsub -q $queue -C0 -o $outfile -e $errfile -E \"$runner -check -runnable Bio::EnsEMBL::Pipeline::RunnableDB::FilterESTs_and_E2G\" $filter_e2g -input_id $input_id";
       print OUT "$command\n";
       
       $count = $count + $size;
@@ -206,6 +226,59 @@ sub make_filter_bsubs {
   close (OUT) or die (" Error closing $jobfile: $!");
 }
 
+
+
+=head2 make_EST_GeneBuilder_bsubs
+
+  Title   : make_EST_GeneBuilder_bsubs
+  Function: makes bsubs to run EST_GeneBuilder
+
+=cut
+
+sub make_EST_GeneBuilder_bsubs{
+  my $jobfile    = $::scripts_conf{'EST_GeneBuilder_bsubsfile'};
+  my $scratchdir = $::scripts_conf{'tmpdir'};
+  my $queue      = $::scripts_conf{'queue'};
+  my $scriptdir  = $::scripts_conf{'scriptdir'};
+
+  open (OUT, ">$jobfile") or die ("Can't open $jobfile for writing: $!");
+
+  # where out and err files go
+  my $filter = $scratchdir . "/" . $est_genebuilder_dir . "/";
+
+  # genomic size for each job
+  my $size   = $::scripts_conf{'est_genebuilder_chunksize'};
+  
+  my $runner = $::scripts_conf{'runner'};
+
+  foreach my $chr(keys %chrhash) {
+    my $length = $chrhash{$chr};
+    
+    my $chrdir = $filter . "$chr";
+    my $count = 1;
+
+    while($count < $length) {
+      my $start = $count;
+      my $end   = $count + $size -1;
+      
+      if ($end > $length) {
+	$end = $length;
+      }
+      
+      my $input_id = $chr . "." . $start . "-" .  $end;
+      my $outfile  = $chrdir . "/$input_id.out";
+      my $errfile  = $chrdir . "/$input_id.err";
+
+      # if you don't want it to write to the database, eliminate the -write option
+      my $command = "bsub -q $queue -C0 -o $outfile -e $errfile -E \"$runner -check -runnable Bio::EnsEMBL::Pipeline::RunnableDB::EST_GeneBuilder\" $runner -runnable Bio::EnsEMBL::Pipeline::RunnableDB::EST_GeneBuilder -input_id $input_id -write";
+      print OUT "$command\n";
+      
+      $count = $count + $size;
+    }
+  }
+
+  close (OUT) or die (" Error closing $jobfile: $!");
+}
 
 =head2 makedir
 
