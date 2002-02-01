6d02e9a9ad7e3333ccb45f13c4ff3e47a2850c88
Val Curwen <vac@sanger.ac.uk>
2002-02-01 10:33:19 +0000

    now uses ESTConf.pm instead of EST_conf.pl (last one, hurrah!)

--- modules/Bio/EnsEMBL/Pipeline/RunnableDB/EST_GeneBuilder.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/EST_GeneBuilder.pm
@@ -58,9 +58,19 @@ use Bio::EnsEMBL::Pipeline::SeqFetcher::Pfetch;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Utils::TranscriptCluster;
 
-# config file; parameters searched for here if not passed in as @args
-# do FILE == the file FILE gets executed as script
-do "Bio/EnsEMBL/Pipeline/EST_conf.pl";
+use Bio::EnsEMBL::Pipeline::ESTConf qw (
+					EST_REFDBHOST
+					EST_REFDBUSER
+					EST_REFDBNAME
+					EST_REFDBPASS
+					EST_GOLDEN_PATH	
+					EST_INDEX
+					EST_GENEBUILDER_INPUT_GENETYPE
+					EST_EVIDENCE_TAG
+					EST_MIN_EVIDENCE_SIMILARITY
+					EST_MAX_EVIDENCE_DISCONTINUITY
+					EST_GENOMEWISE_GENETYPE
+				       );
 
 # use new Adaptor to get some extra info from the ESTs
 use Bio::EnsEMBL::Pipeline::DBSQL::ESTFeatureAdaptor;
@@ -78,10 +88,10 @@ sub new {
     }
     # dbobj needs a reference dna database
     my $refdb = new Bio::EnsEMBL::DBSQL::DBAdaptor(
-						  -host             => $::db_conf{'refdbhost'},
-						  -user             => $::db_conf{'refdbuser'},
-						  -dbname           => $::db_conf{'refdbname'},
-						  -pass             => $::db_conf{'refdbpass'},
+						  -host             => $EST_REFDBHOST,
+						  -user             => $EST_REFDBUSER,
+						  -dbname           => $EST_REFDBNAME,
+						  -pass             => $EST_REFDBPASS,
 						  -perlonlyfeatures => 0,
 						 );
 
@@ -99,7 +109,7 @@ sub new {
 
 
 
-    my $path = $::db_conf{'golden_path'};
+    my $path = $EST_GOLDEN_PATH;
     $path    = 'UCSC' unless (defined $path && $path ne '');
     $self->dbobj->static_golden_path_type($path);
 
@@ -123,7 +133,7 @@ sub new {
 
 sub make_seqfetcher {
   my ( $self ) = @_;
-  my $index   = $::est_genome_conf{'est_index'};
+  my $index   = $EST_INDEX;
 
   my $seqfetcher;
 
@@ -223,7 +233,7 @@ sub fetch_input {
     print STDERR "IN THE 121 BRANCH\n";
 
     # the type of the genes being read is specified in Bio/EnsEMBL/Pipeline/EST_conf.pl 
-    my $genetype =  $::est_genome_conf{'genetype'};
+    my $genetype =  $EST_GENEBUILDER_INPUT_GENETYPE;
 
     #print STDERR "Fetching input: " . $self->input_id. " \n";
     $self->throw("No input id") unless defined($self->input_id);
@@ -444,13 +454,13 @@ sub _check_Transcripts {
   my ($self, $ref_transcripts, $strand) = @_;
 
   # the source_tag of the supporting evidence is specified in Bio/EnsEMBL/Pipeline/EST_conf.pl
-  my $evidence_tag = $::evidence_conf{'evidence_tag'};
+  my $evidence_tag    = $EST_EVIDENCE_TAG;
   
   # the minimum allowed perc. identity of the evidence per exon 
-  my $min_similarity  = $::evidence_conf{'min_evidence_similarity'};
+  my $min_similarity  = $EST_MIN_EVIDENCE_SIMILARITY;
 
   # the maximum allowed discontinuity in EST hits
-  my $max_est_gap  = $::evidence_conf{'max_evidence_discontinuity'};
+  my $max_est_gap     = $EST_MAX_EVIDENCE_DISCONTINUITY;
 
   print STDERR "EST_GeneBuilder: checking consistency of transcripts...\n";
 
@@ -614,7 +624,7 @@ sub _check_Transcripts {
       my $addition = int( $decimals / 50 );  # gives 1 ( or 0 ) if $decimals > ( or < ) 50
       $exon_score  = int( $exon_score ) + $addition;
       
-      # reject EXONS with perc_identity below $min_similarity = $::evidence_conf{'min_evidence_similarity'}
+      # reject EXONS with perc_identity below $min_similarity = $EST_MIN_EVIDENCE_SIMILARITY
 #      if ($min_similarity > $exon_score ){
 #	print STDERR "rejecting Exon due to low percentage identity = $exon_score\n";
 #	$rejected++;
@@ -1667,19 +1677,18 @@ CLUSTER:
       $new_end = $cluster->end;  
     }
     
-#    print STDERR "new_start: $new_start\n";
-#    print STDERR "start array:\n";
-#    foreach my $s ( sort{ $start{ $b } <=> $start{ $a } } keys( %start ) ){
-#      print STDERR "start: $s\tnum_times: ".$start{ $s }."\t";
-#    }
-#    print STDERR "\n";
-#    print STDERR "new_end: $new_end\n";
-#    print STDERR "end array:\n";
-#    foreach my $e ( sort{ $end{ $b } <=> $end{ $a } } keys( %end ) ){
-#      print STDERR "end: $e\tnum_times: ".$end{ $e }."\t";
-#    }
-#    print STDERR "\n";
-
+    print STDERR "new_start: $new_start\n";
+    print STDERR "start array:\n";
+    foreach my $s ( sort{ $start{ $b } <=> $start{ $a } } keys( %start ) ){
+      print STDERR "start: $s\tnum_times: ".$start{ $s }."\t";
+    }
+    print STDERR "\n";
+    print STDERR "new_end: $new_end\n";
+    print STDERR "end array:\n";
+    foreach my $e ( sort{ $end{ $b } <=> $end{ $a } } keys( %end ) ){
+      print STDERR "end: $e\tnum_times: ".$end{ $e }."\t";
+    }
+    print STDERR "\n";
 
     ######  if new_start> new_end change them in turns until we get start < end ######
     my $other_start = $new_start;
@@ -1687,7 +1696,7 @@ CLUSTER:
     my $trouble     = 0;
 
     while ( $other_start >= $other_end ){
- #     print STDERR "*** Trouble: $new_start >= $new_end ***\n";
+      print STDERR "*** Trouble: $new_start >= $new_end ***\n";
       $trouble = 1;
 
       # take the next one
@@ -1701,11 +1710,11 @@ CLUSTER:
       if ($other_end && $other_start){
 	$new_start = $other_start;
 	$new_end   = $other_end;
-#	print STDERR "Reset: new_start: $new_start\t new_end: $new_end\n";
+	print STDERR "Reset: new_start: $new_start\t new_end: $new_end\n";
       }
       else{
 	# ok, you tried to change, but you got nothing, what can we do about it?
-#	print STDERR "Could not reset the start and end coordinates\n";
+	print STDERR "Could not reset the start and end coordinates\n";
       }
     }
 
@@ -2013,7 +2022,7 @@ sub run {
   # run genomewise 
 
   # genes get written in the database with the type specified in Bio/EnsEMBL/Pipeline/EST_conf.pl
-  my $genetype = $::genomewise_conf{'genetype'};
+  my $genetype = $EST_GENOMEWISE_GENETYPE;
   
   # sort out analysis here or we will get into trouble with duplicate analyses
   my $anaAdaptor = $self->dbobj->get_AnalysisAdaptor;
