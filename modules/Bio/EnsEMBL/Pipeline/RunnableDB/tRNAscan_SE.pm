56271508137c1ee461f516ef68636d27ac770225
Simon Potter <scp@sanger.ac.uk>
2002-03-13 16:03:37 +0000

    using RawContigAdaptor

--- modules/Bio/EnsEMBL/Pipeline/RunnableDB/tRNAscan_SE.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/tRNAscan_SE.pm
@@ -106,7 +106,7 @@ sub fetch_input {
     $self->throw("No input id") unless defined($self->input_id);
     
     my $contigid  = $self->input_id;
-    my $contig    = $self->dbobj->get_Contig($contigid);
+    my $contig    = $self->dbobj->get_RawContigAdaptor->fetch_by_name($contigid);
     my $genseq    = $contig->primary_seq() or $self->throw("Unable to fetch contig");
     $self->genseq($genseq);
 }
