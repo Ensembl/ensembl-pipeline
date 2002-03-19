921babfae8371dab5c06400689ec1c6451d81478
Laura Clarke <lec@sanger.ac.uk>
2002-03-19 16:00:23 +0000

    LSF.pm inherits from BatchSubmission.pm and has two methods construct_command_line and open_command_line these will make and open a bsubline given the appropriate information
    this module hasn't been tested yet so may not work

---
+++ modules/Bio/EnsEMBL/Pipeline/BatchSubmission/LSF.pm
@@ -0,0 +1,93 @@
+package Bio::EnsEMBL::Pipeline::BatchSubmission::LSF;
+
+BEGIN {
+  require "Bio/EnsEMBL/Pipeline/pipeConf.pl";
+}
+
+use Bio::EnsEMBL::Pipeline::BatchSubmission;
+use vars qw(@ISA);
+use strict;
+
+@ISA = qw(Bio::EnsEMBL::Pipeline::BatchSubmission);
+
+
+sub new{
+  my ($class, @args) = @_;
+  my $self = $class->SUPER::new(@args);
+
+  $self->{'bsub'} = undef;
+ 
+}
+
+
+##################
+#accessor methods#
+##################
+
+sub bsub{
+  my($self, $arg) = @_;
+
+  if(defined($arg)){
+    $self->{'bsub'} = $arg;
+  }
+
+  return $self->{'bsub'};
+
+}
+
+##other accessor are in base class##
+
+######################
+#command line methods#
+######################
+
+sub construct_command_line{
+  my($self, $command, $stdout, $stderr) = @_; 
+
+#command must be the first argument then if stdout or stderr aren't definhed the objects own can be used
+  if(!$command){
+    $self->throw("cannot create bsub if nothing to submit to it : $!\n");
+  }
+  my $bsub_line;
+  $self->command($command);
+  if($stdout){
+    $bsub_line = "bsub -o ".$stdout;
+  }else{
+    $bsub_line = "bsub -o ".$self->stdout;
+  }
+  if($self->nodes){
+    # $nodes needs to be a space-delimited list
+    $self->nodes =~ s/,/ /;
+    $self->nodes =~ s/ +/ /;
+    # undef $nodes unless $nodes =~ m{(\w+\ )*\w};
+    $bsub_line .= " -m '".$self->nodes."' ";
+  }
+  $bsub_line .= " -q ".$self->queue    if defined $self->queue;
+  $bsub_line .= " -J ".$self->jobname  if defined $self->jobname;
+  $bsub_line .= $self->parameters  if defined $self->parameters;
+  if($stderr){
+    $bsub_line .= "bsub -e ".$stderr;
+  }else{
+    $bsub_line .= "bsub -e ".$self->stderr;
+  }
+  $bsub_line .= "-E \"".$self->pre_exec."\"" if defined $self->pre_exec; 
+  ## must ensure the prexec is in quotes ##
+  $bsub_line .= $command;
+  $self->bsub($bsub_line);
+}
+
+
+
+sub open_command_line{
+  my ($self)= @_;
+
+  print $self->bsub."\n";
+  
+  open(SUB, $self->bsub."2>&1 |");
+
+  while(<SUB>){
+    if (/Job <(\d+)>/) {
+      $self->id($1);
+    }
+  }
+}
