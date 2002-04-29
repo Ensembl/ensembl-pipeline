cd840a119e209f2828296132126a8dabf921657b
Eduardo Eyras <eae@sanger.ac.uk>
2002-04-29 16:20:15 +0000

    script to calculate the sensitivity and specificity of a gene prediction with respect to given gene annotation simply based on the length of genomic sequence covered by each annotation

---
+++ scripts/GeneComparison/exon_Coverage.pl
@@ -0,0 +1,124 @@
+#!/usr/local/ensembl/bin/perl
+
+
+=head1 NAME
+
+comapre_Exons
+ 
+=head1 DESCRIPTION
+
+reads the config options from Bi::Ensembl::Pipeline::GeneComparison::GeneCompConf
+and reads as input an input_id in the style of other Runnables, i.e. -input_id chr_name.chr_start-chr_end
+
+=head1 OPTIONS
+
+    -input_id  The input id: chrname.chrstart-chrend
+
+=cut
+
+use strict;  
+use diagnostics;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Pipeline::GeneComparison::GeneComparison;
+use Getopt::Long;
+
+## load all the parameters
+use Bio::EnsEMBL::Pipeline::GeneComparison::GeneCompConf;
+
+
+my $host1   = $DBHOST1;
+my $dbname1 = $DBNAME1;
+my $path1   = $PATH1;
+my $type1   = $GENETYPES1;
+my $user1   = $DBUSER1;
+
+my $host2   = $DBHOST2;
+my $dbname2 = $DBNAME2;
+my $path2   = $PATH2;
+my $type2   = $GENETYPES2;
+my $user2   = $DBUSER2;
+
+my $runnable;
+my $input_id;
+my $write  = 0;
+my $check  = 0;
+my $params;
+my $pepfile;
+
+# can override db options on command line
+&GetOptions( 
+	     'input_id:s'  => \$input_id,
+
+	     );
+	
+unless( $input_id){     
+  print STDERR "Usage: exon_Coverage.pl -input_id <chrname>.<chrstart>-<chrend>\n";
+  exit(0);
+}
+    
+# get genomic region 
+my $chr      = $input_id;
+$chr         =~ s/\.(.*)-(.*)//;
+my $chrstart = $1;
+my $chrend   = $2;
+
+unless ( $chr && $chrstart && $chrend ){
+       print STDERR "bad input_id option, try something like 20.1-5000000\n";
+}
+
+# connect to the databases 
+my $db1= new Bio::EnsEMBL::DBSQL::DBAdaptor(-host  => $host1,
+					    -user  => $user1,
+					    -dbname=> $dbname1);
+print STDERR "Connected to database $dbname1 : $host1 : $user1 \n";
+
+
+my $db2= new Bio::EnsEMBL::DBSQL::DBAdaptor(-host  => $host2,
+					    -user  => $user2,
+					    -dbname=> $dbname2);
+print STDERR "Connected to database $dbname2 : $host2 : $user2 \n";
+
+
+
+# use different golden paths
+$db1->static_golden_path_type($path1); 
+$db2->static_golden_path_type($path2); 
+
+my $sgp1 = $db1->get_StaticGoldenPathAdaptor;
+my $sgp2 = $db2->get_StaticGoldenPathAdaptor;
+
+# get a virtual contig with a piece-of chromosome #
+my ($vcontig1,$vcontig2);
+
+print STDERR "Fetching region $chr, $chrstart - $chrend\n";
+$vcontig1 = $sgp1->fetch_VirtualContig_by_chr_start_end($chr,$chrstart,$chrend);
+$vcontig2 = $sgp2->fetch_VirtualContig_by_chr_start_end($chr,$chrstart,$chrend);
+
+# get the genes of type @type1 and @type2 from $vcontig1 and $vcontig2, respectively #
+my (@genes1,@genes2);
+
+foreach my $type ( @{ $type1 } ){
+  print STDERR "Fetching genes of type $type\n";
+  my @more_genes = $vcontig1->get_Genes_by_Type($type);
+  push ( @genes1, @more_genes ); 
+  print STDERR scalar(@more_genes)." genes found\n";
+}
+
+foreach my $type ( @{ $type2 } ){
+  print STDERR "Fetching genes of type $type\n";
+  my @more_genes = $vcontig2->get_Genes_by_Type($type);
+  push ( @genes2, @more_genes ); 
+  print STDERR scalar(@more_genes)." genes found\n";
+}
+
+# get a GeneComparison object 
+my $gene_comparison = Bio::EnsEMBL::Pipeline::GeneComparison::GeneComparison->new(\@genes1, \@genes2);
+# as convention, we put first the annotated (or benchmark) genes and second the predicted genes
+# and the comparison methods refer to the second list with respect to the first one
+
+
+#########################################################
+
+$gene_comparison->exon_Coverage(\@genes1, \@genes2);
+
+
