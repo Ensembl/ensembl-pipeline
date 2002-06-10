f1adc9cb2897329dc55b548dae8f2846eeecfe24
Stephen Keenan <keenan@ebi.ac.uk>
2002-06-10 10:48:12 +0000

    now stores percent id

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Finished_MiniEst2Genome.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Finished_MiniEst2Genome.pm
@@ -6,6 +6,8 @@ package Bio::EnsEMBL::Pipeline::Runnable::Finished_MiniEst2Genome;
 use strict;
 use Bio::EnsEMBL::Pipeline::Runnable::Finished_Est2Genome;
 
+use Data::Dumper;
+
 # Inherit from MiniEst2Genome
 use Bio::EnsEMBL::Pipeline::Runnable::MiniEst2Genome;
 use vars '@ISA';
@@ -29,14 +31,19 @@ sub run_blaste2g {
         if (@converted > 1) {
             warn "feature converts into '" . scalar(@converted) . "' > 1 features - ignoring\n";
         } else {            
+                                    
             # convert_FeaturePair zaps strand and hseqname,0
             # so we put them back here.
             my $new = $converted[0];
+            
             $new->seqname($fp->hseqname);
             $new->strand($fp->strand);
             $new->hseqname($fp->hseqname);
             $new->source_tag('Est2Genome');
             $new->primary_tag('I_am_valid');
+            
+            $new->percent_id($fp->percent_id);
+            $new->feature2->percent_id($fp->percent_id);
             $self->add_output(@converted);
         }
     }
