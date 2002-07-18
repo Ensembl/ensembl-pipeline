087bb3640ef7234ad7f3dc6b1df7c2b3d1a57aea
Laura Clarke <lec@sanger.ac.uk>
2002-07-18 11:02:15 +0000

    transfering the geneconf file from the branch to the main trunk, currently contains my settings will create an empty one soon when stuff has been checked

--- modules/Bio/EnsEMBL/Pipeline/GeneConf.pm
+++ modules/Bio/EnsEMBL/Pipeline/GeneConf.pm
@@ -56,100 +56,110 @@ my $prefix='COB';
 # Hash containing config info
 %GeneConf = (
 	     # database specific variables
-	     GB_DBHOST                  => '',
-#	     GB_DBNAME                  => '',
-	     GB_DBNAME                  => '',
-	     GB_DBUSER                  => '',
-	     GB_DBPASS                  => '',
+	     GB_DBHOST                  => 'ecs1c',
+	     GB_DBNAME                  => 'briggsae_intermediate_newschema',
+	     GB_DBUSER                  => 'ensadmin',
+	     GB_DBPASS                  => 'ensembl',
 
 	     #db for writing final genewise genes to - to get round table locks
 	     # this db needs to have clone & contig & static_golden_path tables populated
-	     GB_FINALDBHOST             => '',
-	     GB_FINALDBNAME             => '',
-
-	     GB_GOLDEN_PATH             => '',
+	     GB_FINALDBHOST             => 'ecs1c',
+	     GB_FINALDBNAME             => 'briggsae_genebuild_newschema',
 
 	     # general variables
 	     # path to run_GeneBuild_RunnableDB
-	     GB_RUNNER      => '',
-	     GB_TMPDIR      => '',
+	     GB_RUNNER      => '/acari/work4a/lec/blade_code/main_trunk/ensembl-pipeline/scripts/run_GeneBuild_RunnableDB',
+	     GB_TMPDIR      => '/acari/scratch4/ensembl/lec/new_out',
 	     # LSF queue plus any options you want to use
-	     GB_QUEUE       => '',
+	     GB_QUEUE       => 'acarilong',
 	     GB_TBLASTN     => '',
 	     
 	     # pmatch related variables - for Targetted build
 	     # path to refseq fasta file
-	     GB_REFSEQ      => '',
+	     GB_REFSEQ      => '/acari/work4a/lec/cDNAs/cdnas.fa',
 	     # path to swissprot fasta file
 	     GB_SPTR        => '',
-	     # path to swissprot "evidence kill list" file
-	     GB_KILL_LIST   => '',
 	     # path to file where we'll write cleaned up  proteome data
-	     GB_PFASTA      => '',
+	     GB_PFASTA      => '/acari/work4a/lec/cDNAs/prepare_cdnas.fa',
+	     #GB_PFASTA      => '/acari/work4a/lec/briggsae_sequence/proteome79.fa',
 	     # path pmatch executable
-	     GB_PMATCH      => '',
+	     GB_PMATCH      => '/usr/local/ensembl/bin/pmatch',
 	     # path to directory where fpc/chromosoaml sequences are 
-	     GB_FPCDIR      => '',
+	     GB_FPCDIR      => '/acari/scratch4/ensembl/lec/new_contigs/',
 	     # directory to write pmatch results
-	     GB_PM_OUTPUT   => '',
+	     GB_PM_OUTPUT   => '/acari/scratch4/ensembl/lec/pm_out/',
 
 	     # eg TargettedGeneE2G
 	     GB_TARGETTED_RUNNABLES   => [''],
 	     # eg FPC_TargettedGeneE2G
-	     GB_LENGTH_RUNNABLES      => [''],
+	     GB_LENGTH_RUNNABLES      => ['FPC_TargettedGeneWise'],
+					  #, 'FPC_BlastMiniGenewise','Combine_Genewises_and_E2Gs', 'Gene_Builder'],
 	     # size of chunk to use in length based build
-	     GB_SIZE                  => '5000000',
+	     GB_SIZE                  => '1000000',
 
-	     # location of sequence indices
-	     GB_PROTEIN_INDEX           => '',
+	     # targetted genewise/geneE2G specific parameters
 	     # species specific protein index
-	     GB_TARGETTED_PROTEIN_INDEX => '',
+	     #GB_TARGETTED_PROTEIN_INDEX => '/usr/local/ensembl/data/blastdb/Worms/prepare_cdnas.fa',
+	     GB_TARGETTED_PROTEIN_INDEX => '/acari/work4a/lec/cDNAs/prepare_cdnas.fa',
 	     GB_TARGETTED_CDNA_INDEX    => '',
-
+	     # minimum required coverage for multiexon predictions
+	     GB_TARGETTED_MULTI_EXON_COVERAGE      => '25',
+	     # minimum required coverage for single predictions
+	     GB_TARGETTED_SINGLE_EXON_COVERAGE     => '80',
+	     # maximum allowed size of intron in Targetted gene
+	     GB_TARGETTED_MAX_INTRON               => '20000',
+	     # minimum coverage required to prevent splitting on long introns - keep it high!
+	     GB_TARGETTED_MIN_SPLIT_COVERAGE       => '95',
+	     # genetype for Targetted_GeneWise
+	     #GB_TARGETTED_GW_GENETYPE              => 'targetted_cdna',
+	     GB_TARGETTED_GW_GENETYPE              => 'TGE_gw',
 	     # similairity genewise specific parameters
-	 
-	     GB_EST_DATABASES => [
-				  # fill in one complete hash for each database from which blast 
-				  # features are to be retrieved
-				  {				  
-				   'type'       => '',
-				   'threshold'  => '',
-				   'index'      => ''
-				  },
-		
-				  #					 {
-				  #					  'type'       => 'dbEST', #logic name of analysis type
-				  #					  'threshold'  => '100',
-				  #					  'index'      => '/full/path/to/dbEST'
-				  #					 },
-					],
-	     GB_EST_GENETYPE         => 'est2genome',
-	     # GeneBuilder parameters
-	     GB_VCONTIG              => 1,
-	     GB_SKIP_BMG             => 0,
 	     GB_SIMILARITY_DATABASES => [
 					 # fill in one complete hash for each database from which blast 
 					 # features are to be retrieved
 					 {				  
-					  'type'       => '',
-					  'threshold'  => '',
-					  'index'      => ''
+					  'type'       => 'Ensembl/swall',
+					  'threshold'  => '200',
+					  'index'      => '/usr/local/ensembl/data/blastdb/Ensembl/swall_270602'
 					 },
 # example:
-#					 {
-#					  'type'       => 'swall', #logic_name of analysis type
-#					  'threshold'  => '100',
-#					  'index'      => '/full/path/to/swall'
-#					 },
+					 {
+					  'type'       => 'Worms/wormpep79.pep',
+					  'threshold'  => '200',
+					  'index'      => '/usr/local/ensembl/data/blastdb/Worms/wormpep'
+					 },
 					],
-	     GB_SIMILARITY_GENETYPE => 'contig_blastminigenewise',
+	     
+	     # minimum required parent protein coverage
+	     GB_SIMILARITY_COVERAGE           => 70,
+	     # maximum allowed size of intron 
+	     GB_SIMILARITY_MAX_INTRON         => 10000,
+	     # minimum coverage required to prevent splitting on long introns - keep it high!
+	     GB_SIMILARITY_MIN_SPLIT_COVERAGE => 90,
+	     # low complexity threshold - transcripts whose translations have low
+	     # complexity % higher than GB_MAX_LOW_COMPLEXITY will be discarded
+	     GB_SIMILARITY_MAX_LOW_COMPLEXITY => 60,
+	     # gene type for FPC_BlastMiniGenewise
+	     GB_SIMILARITY_GENETYPE           => 'similarity_genewise',
+
+	     # Combine Genewises_and_E2Gs specific parameters
+	     # gene type for Combine_Genewises_and_E2Gs
+	     GB_COMBINED_GENETYPE           => 'combined_gw_e2g',
+	     #GB_COMBINED_GENETYPE           => 'combined_cdnaorf_e2g',
+	     # GeneBuilder parameters
+	     GB_VCONTIG              => 1,
+	     GB_SKIP_BMG             => 0,
+	     GB_MIN_GENSCAN_EXONS    => 4,
+	     GB_GENSCAN_MAX_INTRON   => 15000,
+	     GB_FINAL_GENETYPE       => 'ensembl',
+
 	     # Post gene build integrity checking script parameters
 	     GB_MINSHORTINTRONLEN    => 7, 
 	     GB_MAXSHORTINTRONLEN    => 50, 
-	     GB_MINLONGINTRONLEN     => 100000, 
+	     GB_MINLONGINTRONLEN     => 10000, 
 	     GB_MINSHORTEXONLEN      => 3, 
 	     GB_MAXSHORTEXONLEN      => 10, 
-	     GB_MINLONGEXONLEN       => 50000, 
+	     GB_MINLONGEXONLEN       => 5000, 
 	     GB_MINTRANSLATIONLEN    => 10, 
 	     GB_MAX_EXONSTRANSCRIPT  => 150, 
 	     GB_MAXTRANSCRIPTS       => 10, 
