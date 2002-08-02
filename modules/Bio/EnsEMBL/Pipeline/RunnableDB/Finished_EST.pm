157db41c75078774906592c880bcd76aa24762a1
Stephen Keenan <keenan@ebi.ac.uk>
2002-08-02 13:30:34 +0000

    can now use indicate indexes

--- modules/Bio/EnsEMBL/Pipeline/RunnableDB/Finished_EST.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/Finished_EST.pm
@@ -11,33 +11,32 @@ use Bio::Root::RootI;
 use Bio::EnsEMBL::Pipeline::SeqFetcher::Pfetch;
 use Bio::EnsEMBL::Pipeline::SeqFetcher::Getseqs;
 use Bio::EnsEMBL::Pipeline::Runnable::Finished_EST;
-#use Bio::EnsEMBL::Pipeline::SeqFetcher::OBDAIndexSeqFetcher;
+use Bio::EnsEMBL::Pipeline::SeqFetcher::OBDAIndexSeqFetcher;
 use vars qw(@ISA);
 @ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
 
-
 sub new {
-    my ($new,@args) = @_;
-    my $self = $new->SUPER::new(@args);    
-           
+    my ( $new, @args ) = @_;
+    my $self = $new->SUPER::new(@args);
+
     # dbobj, input_id, seqfetcher, and analysis objects are all set in
     # in superclass constructor (RunnableDB.pm)
-  	
+
     return $self;
 }
 
 sub fetch_input {
-    my( $self) = @_;
+    my ($self) = @_;
 
     my @fps;
 
-    $self->throw("No input id") unless defined($self->input_id);
+    $self->throw("No input id") unless defined( $self->input_id );
 
-    my $contigid  = $self->input_id;
-    my $contig    = $self->dbobj->get_Contig($contigid);
-    my $genseq    = $contig->primary_seq;
-    my $masked    = $contig->get_repeatmasked_seq;
-        
+    my $contigid = $self->input_id;
+    my $contig   = $self->dbobj->get_Contig($contigid);
+    my $genseq   = $contig->primary_seq;
+    my $masked   = $contig->get_repeatmasked_seq;
+
     my $seq = $masked->seq;
     if ( $seq =~ /[CATG]{3}/ ) {
         $self->input_is_void(0);
@@ -46,28 +45,29 @@ sub fetch_input {
         $self->input_is_void(1);
         $self->warn("Need at least 3 nucleotides");
     }
-    
+
     # Make seqfetcher
     my $seqfetcher = $self->make_seqfetcher;
     $self->seqfetcher($seqfetcher);
 
     my $runnable = Bio::EnsEMBL::Pipeline::Runnable::Finished_EST->new(
-        '-query'        => $masked,
-        '-unmasked'     => $genseq,
-        '-seqfetcher'   => $seqfetcher,
-        '-analysis'     => $self->analysis,
-        );
+        '-query'      => $masked,
+        '-unmasked'   => $genseq,
+        '-seqfetcher' => $seqfetcher,
+        '-analysis'   => $self->analysis,
+    );
 
     $self->runnable($runnable);
 }
-   
-sub runnable {
-    my ($self,$arg) = @_;
 
-    if (defined($arg)) {
-	$self->throw("[$arg] is not a Bio::EnsEMBL::Pipeline::RunnableI") unless $arg->isa("Bio::EnsEMBL::Pipeline::RunnableI");
-	
-	$self->{_runnable} = $arg;
+sub runnable {
+    my ( $self, $arg ) = @_;
+
+    if ( defined($arg) ) {
+        $self->throw("[$arg] is not a Bio::EnsEMBL::Pipeline::RunnableI")
+          unless $arg->isa("Bio::EnsEMBL::Pipeline::RunnableI");
+
+        $self->{_runnable} = $arg;
     }
 
     return $self->{_runnable};
@@ -78,7 +78,7 @@ sub run {
 
     my $runnable = $self->runnable;
     $runnable || $self->throw("Can't run - no runnable object");
-   
+
     $runnable->run;
 }
 
@@ -87,36 +87,32 @@ sub output {
 
     my @runnable = $self->runnable;
     my @results;
-    
-    foreach my $runnable (@runnable){
-      print STDERR "runnable = ".$runnable[0]."\n";
-      push(@results, $runnable->output);
-    }
-    return @results;  
-}
 
+    foreach my $runnable (@runnable) {
+        print STDERR "runnable = " . $runnable[0] . "\n";
+        push ( @results, $runnable->output );
+    }
+    return @results;
+}
 
 sub make_seqfetcher {
     my ( $self, $index ) = @_;
 
-    my( $seqfetcher );
-    #if (my $dbf = $self->analysis->db_file) {
-#        
-#        my $db ='/data/blastdb/Ensembl/dbEST';
-#        my @dbs = $db;
-#        $seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::OBDAIndexSeqFetcher->new(
-#        -db     => \@dbs,
-#        );
-#     
-#        #my $index = "$ENV{BLASTDB}/$dbf";
-#        #$seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::Getseqs->new('-db' => [$index]);
-#    } else {
+    my $seqfetcher;
+    if ( my $dbf = $self->analysis->db_file ) {
+
+        my @dbs = $dbf;
+        $seqfetcher =
+          Bio::EnsEMBL::Pipeline::SeqFetcher::OBDAIndexSeqFetcher->new(
+            -db => \@dbs,
+        );
+
+    }
+    else {
         $seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::Pfetch->new;
-    #}
+    }
     return $seqfetcher;
 }
-
-
 
 1;
 
