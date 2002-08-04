083ccef8de96b3a84704f287b8888f9ac59b2cdc
Eduardo Eyras <eae@sanger.ac.uk>
2002-08-04 13:26:02 +0000

    added new database fields. I include a fully-filled version, which I find more informative than commiting an empty config file, it is also faster and time is money and I'm hungry and I have no time to waste moving files around. Anyway, if you find it confusing, contact me eae@sanger.ac.uk

--- modules/Bio/EnsEMBL/Pipeline/ESTConf.pm
+++ modules/Bio/EnsEMBL/Pipeline/ESTConf.pm
@@ -27,7 +27,7 @@ module.
 
 It imports and sets a number of standard global variables into the
 calling package, which are used in many scripts in the human sequence
-analysis system.  The variables are first decalared using "use vars",
+analysis system.  The variables are first declared using "use vars",
 so that it can be used when "use strict" is in use in the calling
 script.  Without arguments all the standard variables are set, and
 with a list, only those variables whose names are provided are set.
@@ -51,79 +51,129 @@ package Bio::EnsEMBL::Pipeline::ESTConf;
 use strict;
 use vars qw( %ESTConf );
 
-# Hash containing config info
+# Hash containing config info]
 %ESTConf = (
 	    # general options for scripts
 
-	    EST_RUNNER                  => '', 	    # path to run_ESTRunnableDB
-	    EST_SCRIPTDIR               => '', 	    # path to ensembl-pipeline/scripts/EST
-	    EST_TMPDIR                  => '',
-	    EST_QUEUE                   => '',
-
-	    # make_bsubs.pl options
-	    EST_EXONERATE_BSUBS         => '',
-	    EST_FILTER_BSUBS            => '',
-	    EST_GENEBUILDER_BSUBS       => '',
-
-	    # for prepare_ests.pl
-	    EST_FILESPLITTER            => '', 	    # path to executable for chunking seqeuncefiles eg fastasplit
-	    EST_FILE                    => '', 	    # path to file containign ALL ESTs/cDNAs
-	    EST_CHUNKDIR                => '', 	    # path to directory where EST chunks live
-	    EST_CHUNKNUMBER             => '', 	    # how many chunks?
-	    EST_MAKESEQINDEX            => '', 	    # path to makeseqindex executable
+	    # path to run_ESTRunnableDB
+	    EST_RUNNER                  => '/nfs/acari/eae/ensembl-branch-121/ensembl-pipeline/scripts/run_EST_RunnableDB', 	    
 	    
+	    # path to ensembl-pipeline/scripts/EST
+	    EST_SCRIPTDIR               => '/nfs/acari/eae/ensembl-branch-121/ensembl-pipeline/scripts/EST/', 	    
+	    # where the result-directories are going to go
+	    #EST_TMPDIR                  => '/ecs2/scratch1/ensembl/eae/NCBI_30/cdnas',
+	    EST_TMPDIR => '/ecs2/scratch1/ensembl/eae/NCBI_30/ESTs/',
+	    # job-queue in the farm
+	    EST_QUEUE                   => 'acari',
+	    
+	    # make_bsubs.pl options
+	    EST_EXONERATE_BSUBS         => '/ecs2/work1/eae/NCBI_30/ests/jobs_dir/exonerate.jobs',
+	    EST_FILTER_BSUBS            => '/ecs2/work1/eae/NCBI_30/ests/jobs_dir/filter_and_e2g.jobs',
+	    EST_GENEBUILDER_BSUBS       => '/ecs2/work1/eae/NCBI_30/ests/jobs_dir/EST_GeneBuilder.jobs',	
+	    
+	    # for prepare_ests.pl
+	    
+	    # path to executable for chunking seqeuncefiles eg fastasplit
+	    EST_FILESPLITTER            => '/acari/work2/gs2/gs2/bin/fastasplit', 	    
+	    
+	    # path to file containign ALL ESTs/cDNAs
+	    EST_FILE                    => '/ecs2/work1/eae/NCBI_30/ests/all_human_ests', 	   
+
+	    # path to directory where EST chunks live
+	    #EST_CHUNKDIR                => '/acari/work6a/eae.tmp/Mouse/Mouse_5.3.1/riken_mRNAs/second_rechunks',
+
+	    #EST_CHUNKDIR                => '/work6a/eae.tmp/Human/NCBI_28/ESTs/chunks',
+	    EST_CHUNKDIR                => '/ecs2/work1/eae/NCBI_30/ests/chunks/',
+
+ 	    # how many chunks?
+	    # for NCBI_28 we have 3690891 ests, at approx. 350 ests per chunk, we estimate
+	    EST_CHUNKNUMBER             => 20000, 	 
+	       
+	    # path to makeseqindex executable
+	    EST_MAKESEQINDEX            => '/usr/local/ensembl/bin/makeseqindex', 	    
+	    
+
 	    # for exonerate_ests.pl
-	    EST_GENOMIC                 => '',      # path to file with repeatmasked dusted genomic sequences
+	    #EST_GENOMIC                 => '/data/blastdb/Ensembl/NCBI_30_dusted_masked_contigs.fa',
+	    #EST_GENOMIC                 => '/ecs2/work1/eae/NCBI_30/genome/NCBI_30_dusted_masked_all_contigs.fa',
+	     EST_GENOMIC                 => '/data/blastdb/Ensembl/NCBI_30_dusted_masked_all_contigs.fa',
+
+	                                            # path to file with repeatmasked dusted genomic sequences
 	                                            # NB this file is huge - distribute it across the farm or 
                                                     # be prepared to face the wrath of systems when the network 
                                                     # seizes up!
 	    # for filter_and_e2g.pl
-	    EST_FILTERCHUNKSIZE         => '',      #  we use 1000000 (ie 1MB) chunks
+	    EST_FILTERCHUNKSIZE         => 1000000,      #  we use 1000000 (ie 1MB) chunks
 	    
 	    # ExonerateESTs options
-	    EST_EXONERATE               => '',      # path to executable
+	    EST_EXONERATE               => '/usr/local/ensembl/bin/exonerate',      # path to executable
 	    EST_EXONERATE_ARGS          => '',
 	    EST_EXONERATE_RUNNABLE     => 'Bio::EnsEMBL::Pipeline::RunnableDB::ExonerateESTs',      
 
 	    # FilterESTs_and_E2G options
 	    EST_FILTER_RUNNABLE         => 'Bio::EnsEMBL::Pipeline::RunnableDB::FilterESTs_and_E2G',
-	    EST_INDEX                   => '',      # name of EST file that has been indexed and is 
+
+	    # new index, path where the directory of the index is
+	    EST_INDEX                   => '/data/blastdb/Ensembl/human_ests_index',
+	    
+	                                            # name of EST file that has been indexed and is 
 	                                            # accessible across the farm
-	    EST_SOURCE                  => '',      # for analysis(process)
+	                                            # for analysis(process)
+      	    EST_SOURCE                  => 'dbEST',      
 	    
 
 	    # for EST_GeneBuilder
-	    EST_GENEBUILDER_CHUNKSIZE   => '',      #  we use 1000000 (ie 1MB) chunks
-	    EST_GENEBUILDER_INPUT_GENETYPE => 'exonerate_e2g',   #
+	    EST_GENEBUILDER_CHUNKSIZE   => 1000000,      #  we use 1000000 (ie 1MB) chunks
+	                                                 # it should be equal to EST_FILTERCHUNKSIZE
+	    EST_GENEBUILDER_INPUT_GENETYPE => 'exonerate_e2g',   
 	    EST_GENOMEWISE_RUNNABLE     => 'Bio::EnsEMBL::Pipeline::RunnableDB::Genomewise',
 	    EST_GENOMEWISE_GENETYPE     => 'genomewise',
-	    EST_STRICT_LOWER_BOUND      => '', # 1 for ESTs only, 0 for cDNAs/mRNAs only
 	    EST_EVIDENCE_TAG            => 'exonerate_e2g',
-	    EST_MIN_EVIDENCE_SIMILARITY => '',
-	    EST_MAX_EVIDENCE_DISCONTINUITY => '',
+
+	    # not used at this moment
+	    EST_STRICT_LOWER_BOUND      => '', # 1 for ESTs only, 0 for cDNAs/mRNAs only
+	    EST_MIN_EVIDENCE_SIMILARITY => 65,
+	    EST_MAX_EVIDENCE_DISCONTINUITY => 10,
 
 	    # database config
+	    # IMPORTANT: make sure that all databases involved in each analysis are
+	    # not on the same mysql instance
+	    
 	    # ref_db - holds the static golden path, contig and dna information
-	    # est_db = where we load up exonerate results into the feature table and build genes
-	    EST_REFDBHOST               => '',
-	    EST_REFDBNAME               => '',
-	    EST_REFDBUSER               => '',
+	    EST_REFDBNAME               => 'ens_NCBI_30',
+	    EST_REFDBHOST               => 'ecs1e',
+	    EST_REFDBUSER               => 'ensro',
 	    EST_REFDBPASS               => '',
 
-	    EST_DBNAME                  => '',
-	    EST_DBHOST                  => '',
-	    EST_DBUSER                  => '',
-	    EST_DBPASS                  => '',
+	    EST_GOLDEN_PATH             => 'NCBI_30',
 
-	    # you can also choose to use cdnas for est_genebuilder
-	    # this is to read them from a different database:
-	    USE_cDNA_DB                  => '',  # set it to a def/undef value if you do/don't want to use it
+	    # est_db = where we load up exonerate results into the feature table
+	    EST_DBNAME                  => 'ens_NCBI_30_est2',
+	    EST_DBHOST                  => 'ecs1a',
+	    EST_DBUSER                  => 'ensadmin',
+	    EST_DBPASS                  => 'ensembl',
+	    
+	    # est_e2g_db = where we write the genes we produce from the exonerate features
+	    EST_E2G_DBNAME                  => 'ens_NCBI_30_est2',
+	    EST_E2G_DBHOST                  => 'ecs1a',
+	    EST_E2G_DBUSER                  => 'ensadmin',
+	    EST_E2G_DBPASS                  => 'ensembl',
+	    
+	    # est_gene_db = where we write the genes we produce from e2g transcripts
+	    EST_GENE_DBNAME                  => 'ens_NCBI_30_estgenes',
+	    EST_GENE_DBHOST                  => 'ecs1f',
+	    EST_GENE_DBUSER                  => 'ensadmin',
+	    EST_GENE_DBPASS                  => 'ensembl',
+	    
+	    USE_cDNA_DB                  => 0,  # set it to a def/undef value if you do/don't want to use it
 	    
 	    cDNA_DBNAME                  => '',
 	    cDNA_DBHOST                  => '',
 	    cDNA_DBUSER                  => '',
 	    cDNA_DBPASS                  => '',
-	    cDNA_GENETYPE                => '',
+	    cDNA_GENETYPE                => '',	  
+
+	   	   
 	   );
 
 sub import {
@@ -131,7 +181,7 @@ sub import {
     my $pack = shift; # Need to move package off @_
 
     # Get list of variables supplied, or else
-    # all of GeneConf:
+    # all of ESTConf:
     my @vars = @_ ? @_ : keys( %ESTConf );
     return unless @vars;
 
