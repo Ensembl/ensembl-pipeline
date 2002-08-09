a77b1f8c5c240e1b73a6a2e0eb205e908bea57bf
Eduardo Eyras <eae@sanger.ac.uk>
2002-08-09 15:36:17 +0000

    sort the cdnas first on their genomic extent and then on their exonic extent

--- modules/Bio/EnsEMBL/Pipeline/RunnableDB/Combine_Genewises_and_E2Gs.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/Combine_Genewises_and_E2Gs.pm
@@ -322,14 +322,14 @@ sub run {
 	  $result =  ( $$b[1] <=> $$a[1] );
 	}
 	unless ($result){
-	  # else, by exon extent of the cdna
-	  $result = (  $self->_transcript_exonic_length_in_gene($$b[2]) <=>
-		       $self->_transcript_exonic_length_in_gene($$a[2]) );
-	}
-	unless ($result){
 	  # else, by genomic length of the cdna
 	  $result = (  $self->_transcript_length_in_gene($$b[2]) <=>
 		       $self->_transcript_length_in_gene($$a[2]) );
+	}
+	unless ($result){
+	  # else, by exonic extent of the cdna
+	  $result = (  $self->_transcript_exonic_length_in_gene($$b[2]) <=>
+		       $self->_transcript_exonic_length_in_gene($$a[2]) );
 	}
       } @list;
       
