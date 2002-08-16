7e92560f61939e8e36479971bf31637025da37fa
Eduardo Eyras <eae@sanger.ac.uk>
2002-08-16 16:00:27 +0000

    modified to creat also bsub lines for GeneCombiner

--- scripts/GeneBuild/make_bsubs.pl
+++ scripts/GeneBuild/make_bsubs.pl
@@ -7,13 +7,19 @@ use Bio::EnsEMBL::Pipeline::GeneConf qw (GB_RUNNER
 					 GB_DBUSER
 					 GB_DBPASS
 					 GB_QUEUE
-					 GB_TMPDIR
+					 GB_OUTPUT_DIR
 					 GB_LENGTH_RUNNABLES
 					 GB_TARGETTED_RUNNABLES
 					 GB_PM_OUTPUT
 					 GB_SIZE
 					);
 
+use Bio::EnsEMBL::Pipeline::GeneCombinerConf qw (RUNNER
+						 RUNNABLE
+						 SLICE_SIZE
+						 QUEUE
+						 OUTPUT_DIR
+						);
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 
 if($GB_DBUSER eq 'ensadmin' && $GB_DBPASS eq ''){
@@ -21,21 +27,21 @@ if($GB_DBUSER eq 'ensadmin' && $GB_DBPASS eq ''){
   exit(1);
 }
 
-foreach my $arg($GB_RUNNER, $GB_DBNAME, $GB_DBHOST, $GB_DBUSER, $GB_QUEUE, $GB_TMPDIR){
-    if ($arg eq '' ){
-      print "You need to set various parameters in GeneConf.pl\n" .  
-	"Here are your current values for required settings: \n" .
+foreach my $arg($GB_RUNNER, $GB_DBNAME, $GB_DBHOST, $GB_DBUSER, $GB_QUEUE, $GB_OUTPUT_DIR){
+  if ($arg eq '' ){
+    print "You need to set various parameters in GeneConf.pl\n" .  
+      "Here are your current values for required settings: \n" .
 	"runner      => $GB_RUNNER\n" .
-	"dbname      => $GB_DBNAME\n" .
-	"dbhost      => $GB_DBHOST\n" .
-	"dbuser      => $GB_DBUSER\n" .
-	"dbpass      => $GB_DBPASS\n" .
-	"queue       => $GB_QUEUE\n" .
-	"tmpdir      => $GB_TMPDIR\n" ;
-
-      exit(1);
-    }
+	  "dbname      => $GB_DBNAME\n" .
+	    "dbhost      => $GB_DBHOST\n" .
+	      "dbuser      => $GB_DBUSER\n" .
+		"dbpass      => $GB_DBPASS\n" .
+		  "queue       => $GB_QUEUE\n" .		  
+		    "output_dir      => $GB_OUTPUT_DIR\n" ;
+    
+    exit(1);
   }
+}
 
 my %chrhash;
 
@@ -49,6 +55,9 @@ foreach my $tr(@{$GB_TARGETTED_RUNNABLES}) {
   make_tbsubs($tr) unless $tr eq '';
 }
 
+make_genecombiner_bsubs( $RUNNABLE ) if ($RUNNABLE);
+
+
 
 ### SUBROUTINES ###
 
@@ -60,7 +69,10 @@ sub get_chrlengths{
 					      -dbname => $GB_DBNAME,
 					     );
 
-  my $q = "select c.name, max(a.chr_end) from chromosome c, assembly a where c.chromosome_id = a.chromosome_id group by c.name";
+  my $q = "SELECT c.name, max(a.end) 
+           FROM   chromosome c, assembly a
+           WHERE  c.chromosome_id = a.chromosome_id
+           GROUP BY c.name";
 
   my $sth = $db->prepare($q) || $db->throw("can't prepare: $q");
   my $res = $sth->execute || $db->throw("can't execute: $q");
@@ -80,7 +92,7 @@ sub make_tbsubs {
   my $dbuser      = $GB_DBUSER;
   my $dbpass      = $GB_DBPASS;
   my $queue       = $GB_QUEUE;
-  my $dir         = $GB_TMPDIR . "/$runnable";
+  my $output_dir  = $GB_OUTPUT_DIR . "/$runnable";
   my $pm_out      = $GB_PM_OUTPUT;
 
   $pm_out     .= "pm_best.out";
@@ -99,21 +111,21 @@ sub make_tbsubs {
   my %pm_ids;
   
   # set up jobfile & output dirs
-  system("mkdir $dir") unless opendir(DIR, $dir);
+  system("mkdir $output_dir") unless opendir(DIR, $output_dir);
   closedir(DIR);
-  my $outf  = "$runnable.jobs.dat";
+  my $outf  = "$GB_OUTPUT_DIR/$runnable.jobs.dat";
 
   open(OUTF, ">$outf") or die "Can't open outfile $outf\n";
 
   # generate bsubs, one per protein
   open(PM, "<$pm_out") or die "Can't open pmoutfile $pm_out\n";
   my $tracker = 0;
-  my $resdir = $dir . "/jobs0";
-   
+  my $resdir = $output_dir . "/jobs0";
+  
   while(<PM>){
     # do we need to start another output directory? Limit the files in each so we can parse them easily
     if($tracker%100 == 0){
-      $resdir = $dir . "/jobs" . $tracker/100;
+      $resdir = $output_dir . "/jobs" . $tracker/100;
       print STDERR "$resdir\n";
       system("mkdir $resdir") unless opendir(DIR, $resdir);
       closedir(DIR);   
@@ -135,8 +147,8 @@ sub make_tbsubs {
     my $errfile  = $resdir . "/$input_id.err";
     my $command = "bsub -q $queue -C0 -o $outfile -e $errfile -E \"$runner -check -runnable Bio::EnsEMBL::Pipeline::RunnableDB::$runnable\"";
 
-    $command .= "  $runner  ";
-    $command .= "  -runnable Bio::EnsEMBL::Pipeline::RunnableDB::$runnable ";
+    $command .= "  $runner ";
+    $command .= " -runnable Bio::EnsEMBL::Pipeline::RunnableDB::$runnable ";
     $command .= " -input_id $input_id -write";      
     print OUTF "$command\n";
     
@@ -158,27 +170,27 @@ sub make_lbsubs {
   my $dbpass      = $GB_DBPASS;
   my $queue       = $GB_QUEUE;
   my $size        = $GB_SIZE;
-  my $dir         = $GB_TMPDIR . "/$runnable";
+  my $dir         = $GB_OUTPUT_DIR . "/$runnable";
   
 
   # check them!
-  foreach my $arg($size, $GB_TMPDIR){
+  foreach my $arg($size, $GB_OUTPUT_DIR){
     if ($arg eq '' ){
       print "You need to set various parameters in GeneConf.pl\n" .  
 	"Here are your current values for required settings: \n" .
-	"size   => $size\n" .
-	"tmpdir => $GB_TMPDIR\n";
-
+	  "size   => $size\n" .
+	    "tmpdir => $GB_OUTPUT_DIR\n";
+      
       exit(1);
     }
   }
-
+  
 
   # ought to do some checking here - does it exist, are there files in it, etc
   system("mkdir $dir") unless opendir(DIR, $dir);
   closedir(DIR);
 
-  my $outf  = "$runnable.jobs.dat";
+  my $outf  = "$OUTPUT_DIR/$runnable.jobs.dat";
   open(OUTF, ">$outf") or die "Can't open $outf\n";
   
   foreach my $chr(keys %chrhash) {
@@ -216,3 +228,66 @@ sub make_lbsubs {
   close OUTF;
 }
 	     
+sub make_genecombiner_bsubs {
+  my ($runnable) = @_;
+  
+  my $runner      = $RUNNER;
+  my $queue       = $QUEUE;
+  my $size        = $SLICE_SIZE;
+  my $dir         = $OUTPUT_DIR . "/$runnable";
+  
+
+  # check them!
+  foreach my $arg ($size, $OUTPUT_DIR){
+    if ($arg eq '' ){
+      print "You need to set various parameters in GeneConf.pl\n" .  
+	"Here are your current values for required settings: \n" .
+	  "size   => $SLICE_SIZE\n" .
+	    "tmpdir => $OUTPUT_DIR\n";
+      
+      exit(1);
+    }
+  }
+  
+
+  # ought to do some checking here - does it exist, are there files in it, etc
+  system("mkdir $dir") unless opendir(DIR, $dir);
+  closedir(DIR);
+  
+  my $outf  = "$OUTPUT_DIR/$runnable.jobs.dat";
+  open(OUTF, ">$outf") or die "Can't open $outf\n";
+  
+  foreach my $chr(keys %chrhash) {
+    my $length = $chrhash{$chr};
+    
+    my $chrdir = $dir . "/$chr";
+    
+    # needs checks
+    system("mkdir $chrdir") unless opendir(DIR, $chrdir);
+    closedir(DIR);
+    
+    my $count = 1;
+    
+    while ($count < $length) {
+      my $start = $count;
+      my $end   = $count + $size -1;
+      
+      if ($end > $length) {
+	$end = $length;
+      }
+      
+      my $input_id = $chr . "." . $start . "-" .  $end;
+      my $outfile  = $chrdir . "/$input_id.out";
+      my $errfile  = $chrdir . "/$input_id.err";
+      my $command = "bsub -q $queue -C0 -o $outfile -e $errfile -E \"$runner -check -runnable  Bio::EnsEMBL::Pipeline::RunnableDB::$runnable \"";
+      $command .= "  $runner ";
+      $command .= " -runnable Bio::EnsEMBL::Pipeline::RunnableDB::$runnable ";
+      $command .= " -input_id $input_id ";
+      $command .= " -write";      
+      print OUTF "$command\n";
+      
+      $count = $count + $size;
+    }
+  }
+  close OUTF;
+}
