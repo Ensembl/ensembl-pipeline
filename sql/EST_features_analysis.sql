766519bf4c5f2e09fb555881b40ca2db0dd443b2
Eduardo Eyras <eae@sanger.ac.uk>
2002-10-17 16:17:53 +0000

    analysis tables perfabs

---
+++ sql/EST_features_analysis.sql
@@ -0,0 +1,6 @@
+#
+# Dumping EST_feastures data for table 'analysisprocess'
+#
+
+INSERT INTO analysisprocess VALUES (1,'2002-06-19 17:00:00','ex_e2g_feat','dbEST','1','NULL','exonerate_e2g','3','NULL','NULL','Filter_ESTs_and_E2G','1','est','similarity');
+
