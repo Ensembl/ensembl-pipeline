d12473cb0b0d2ff088410d60e9c2463994b3c306
Michele Clamp <michele@sanger.ac.uk>
2002-10-22 14:38:38 +0000

    Moved to new shiny RunnableDB

--- modules/Bio/EnsEMBL/Pipeline/RunnableDB/EPCR.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/EPCR.pm
@@ -1,9 +1,3 @@
-#
-#
-# Cared for by Michele Clamp  <michele@sanger.ac.uk>
-#
-# Copyright Michele Clamp
-#
 # You may distribute this module under the same terms as perl itself
 #
 # POD documentation - main docs before the code
@@ -19,7 +13,7 @@ Bio::EnsEMBL::Pipeline::RunnableDB::EPCR
 my $db      = Bio::EnsEMBL::DBLoader->new($locator);
 my $epcr   = Bio::EnsEMBL::Pipeline::RunnableDB::EPCR->new ( 
                                                     -dbobj      => $db,
-			                                        -input_id   => $input_id
+			                            -input_id   => $input_id
                                                     -analysis   => $analysis );
 $epcr->fetch_input();
 $epcr->run();
@@ -48,41 +42,13 @@ Internal methods are usually preceded with a _
 package Bio::EnsEMBL::Pipeline::RunnableDB::EPCR;
 
 use strict;
+
 use Bio::EnsEMBL::Pipeline::RunnableDB;
 use Bio::EnsEMBL::Pipeline::Runnable::EPCR;
 
 use vars qw(@ISA);
+
 @ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
-
-=head2 new
-
-    Title   :   new
-    Usage   :   $self->new(-DBOBJ       => $db
-                           -INPUT_ID    => $id
-                           -ANALYSIS    => $analysis);
-                           
-    Function:   creates a Bio::EnsEMBL::Pipeline::RunnableDB::EPCR object
-    Returns :   A Bio::EnsEMBL::Pipeline::RunnableDB::EPCR object
-    Args    :   -dbobj:     A Bio::EnsEMBL::DBSQL::DBAdaptor, 
-                -input_id:   Contig input id , 
-                -analysis:  A Bio::EnsEMBL::Analysis 
-
-=cut
-
-sub new {
-    my ($class, @args) = @_;
-    my $self = $class->SUPER::new(@args);
-    
-    $self->{'_fplist'}      = [];
-    $self->{'_genseq'}      = undef;
-    $self->{'_runnable'}    = undef;
-    $self->{'_parameters'}  = undef;
-    
-    $self->throw("Analysis object required") unless ($self->analysis);
-    
-    $self->runnable('Bio::EnsEMBL::Pipeline::Runnable::EPCR');
-    return $self;
-}
 
 =head2 fetch_input
 
@@ -101,79 +67,19 @@ sub fetch_input {
    
     my $contigid  = $self->input_id;
     my $contig    = $self->db->get_RawContigAdaptor->fetch_by_name($contigid);
-   
-    $self->genseq($contig);
+
+    $self->query($contig);
+
+    my $runnable = new Bio::EnsEMBL::Pipeline::Runnable::EPCR(
+	    -query   => $self->query,
+            -sts     => $self->analysis->db_file,
+	    -pcr     => $self->analysis->program,
+	    -options => $self->arguments);
+
+    $self->runnable($runnable);
+
+    return 1;
 }
-
-#get/set for runnable and args
-sub runnable {
-    my ($self, $runnable) = @_;
-    my $arguments = "";
-
-    # $self->analysis->parameters is a comma-delimited list.
-    # Anything of the form a => b is passed to the runnable's new method.
-    # Other text is given to the runnable as "options"
-    
-    if ($runnable)
-    {
-        #extract parameters into a hash
-        my ($parameter_string) = $self->analysis->parameters() ;
-        my %parameters;
-        if ($parameter_string)
-        {
-            my @pairs = split (/,/, $parameter_string);
-            foreach my $pair (@pairs)
-            {
-		my ($key, $value) = split (/=>/, $pair);
-		if ($key && $value) {
-                    $key =~ s/^\s+//g;
-                    $key =~ s/\s+$//g;
-                    $value =~ s/^\s+//g;
-                    $value =~ s/\s+$//g;
-		    $parameters{$key} = $value;
-		}
-		else
-		{
-		    $arguments .= " $key ";
-		}
-            }
-        }
-        $parameters {'-sts'}     = $self->analysis->db_file();  
-        $parameters {'-options'} = $arguments;
-        $parameters {'-pcr'}     = $self->analysis->program_file();  
-        #creates empty Bio::EnsEMBL::Runnable::EPCR object
-        $self->{'_runnable'} = $runnable->new(%parameters);
-    }
-    return $self->{'_runnable'};
-}
-
-
-
-sub write_output{
-  my ($self) = @_;
-
-  my @features = $self->output();
-  my $simple_f_a = $self->db->get_SimpleFeatureAdaptor();
-  my $contig;
-
-  eval {
-    $contig = $self->db->get_RawContigAdaptor->fetch_by_name($self->input_id);
-  };
-
-  if ($@) {
-      print STDERR "Contig not found, skipping writing output to db: $@\n";
-      return;
-  }
-
-  foreach my $f(@features){
-    $f->analysis($self->analysis);
-    $f->attach_seq($contig);
-    $simple_f_a->store($f);
-  }
-
-
-}
-
 
 
 1;
