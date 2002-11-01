5aff5955a0ddb4b8c89bdb501d8f89d1f9231a62
Eduardo Eyras <eae@sanger.ac.uk>
2002-11-01 11:41:08 +0000

    corrected the insert statement

--- sql/EST_genes_analysis.sql
+++ sql/EST_genes_analysis.sql
@@ -2,6 +2,6 @@
 # Dumping EST_genes data for table 'analysisprocess'
 #
 
-INSERT INTO analysisprocess VALUES (4,'2002-02-28 00:00:00','genomewise','NULL','1','NULL','genomewise','1','NULL','NULL','EST_GeneBuilder','        NULL','genomewise','gene');
-INSERT INTO analysisprocess VALUES (2,'2002-02-28 00:00:00','exonerate_e2g','dbEST','1','NULL','exonerate_e2g','1','NULL','NULL','FilterESTs_and_E2G','NULL','exonerate_e2g','similarity');
+INSERT INTO analysis VALUES (\N,now(),'genomewise','NULL','1','NULL','genomewise','1','NULL','NULL','EST_GeneBuilder','        NULL','genomewise','gene');
+INSERT INTO analysisprocess VALUES (\N,'2002-02-28 00:00:00','exonerate_e2g','dbEST','1','NULL','exonerate_e2g','1','NULL','NULL','FilterESTs_and_E2G','NULL','exonerate_e2g','similarity');
 
