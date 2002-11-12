65b8767003fc484b34bffbbbc7674df36afa3fd7
Simon Potter <scp@sanger.ac.uk>
2002-11-12 14:57:57 +0000

    brought in line with new 'RunnableDB'

--- modules/Bio/EnsEMBL/Pipeline/RunnableDB/Genscan.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/Genscan.pm
@@ -1,9 +1,4 @@
 #
-#
-# Cared for by Michele Clamp  <michele@sanger.ac.uk>
-#
-# Copyright Michele Clamp
-#
 # You may distribute this module under the same terms as perl itself
 #
 # POD documentation - main docs before the code
@@ -16,23 +11,23 @@ Bio::EnsEMBL::Pipeline::RunnableDB::Genscan
 
 =head1 SYNOPSIS
 
-my $db          = Bio::EnsEMBL::DBLoader->new($locator);
-my $genscan     = Bio::EnsEMBL::Pipeline::RunnableDB::Genscan->new ( 
+my $db      = Bio::EnsEMBL::DBLoader->new($locator);
+my $repmask = Bio::EnsEMBL::Pipeline::RunnableDB::Genscan->new ( 
                                                     -dbobj      => $db,
 			                                        -input_id   => $input_id
                                                     -analysis   => $analysis );
-$genscan->fetch_input();
-$genscan->run();
-$genscan->output();
-$genscan->write_output(); #writes to DB
+$repmask->fetch_input();
+$repmask->run();
+$repmask->output();
+$repmask->write_output(); #writes to DB
 
 =head1 DESCRIPTION
 
 This object wraps Bio::EnsEMBL::Pipeline::Runnable::Genscan to add
-functionality for reading and writing to databases. The appropriate
-Bio::EnsEMBL::Analysis object must be passed for extraction
-of appropriate parameters. A Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor
-is required for database access.
+functionality to read and write to databases.
+The appropriate Bio::EnsEMBL::Analysis object must be passed for
+extraction of appropriate parameters. A Bio::EnsEMBL::Pipeline::DBSQL::Obj is
+required for databse access.
 
 =head1 CONTACT
 
@@ -48,46 +43,13 @@ Internal methods are usually preceded with a _
 package Bio::EnsEMBL::Pipeline::RunnableDB::Genscan;
 
 use strict;
+
 use Bio::EnsEMBL::Pipeline::RunnableDB;
 use Bio::EnsEMBL::Pipeline::Runnable::Genscan;
-use Data::Dumper;
 
 use vars qw(@ISA);
+
 @ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
-
-=head2 new
-
-    Title   :   new
-    Usage   :   $self->new(-DBOBJ       => $db
-                           -INPUT_ID    => $id
-                           -ANALYSIS    => $analysis);
-                           
-    Function:   creates a Bio::EnsEMBL::Pipeline::RunnableDB::Genscan object
-    Returns :   A Bio::EnsEMBL::Pipeline::RunnableDB::Genscan object
-    Args    :   -dbobj:     A Bio::EnsEMBL::DBSQL::DBAdaptor, 
-                input_id:   Contig input id , 
-                -analysis:  A Bio::EnsEMBL::Analysis 
-
-=cut
-
-sub new {
-    my ($class, @args) = @_;
-    my $self = $class->SUPER::new(@args);
-    
-    $self->{'_fplist'}      = [];
-    $self->{'_genseq'}      = undef;
-    $self->{'_runnable'}    = undef;
-    $self->{'_parameters'}  = undef;
-    
-    # dbobj input_id mandatory and read in by BlastableDB
-
-    # anlaysis not mandatory for BlastableDB, so we check here 
-    $self->throw("Analysis object required") unless ($self->analysis);
-    $self->init('Bio::EnsEMBL::Pipeline::Runnable::Genscan');
-    
-    return $self;
-}
-
 
 =head2 fetch_input
 
@@ -100,112 +62,27 @@ sub new {
 =cut
 
 sub fetch_input {
-    my($self) = @_;
+    my( $self) = @_;
     
-    $self->throw("No input id") unless defined($self->input_id);
 
+    $self->throw("No input id") unless defined($self->input_id);
+    
     my $contigid  = $self->input_id;
     my $contig    = $self->db->get_RawContigAdaptor->fetch_by_name($contigid);
-    my $genseq    = $contig->get_repeatmasked_seq() or $self->throw("Unable to fetch contig");
-    $self->genseq($genseq);
-    $self->{'contig'} = $contig;
- 
+
+    $self->query($contig->get_repeatmasked_seq(['RepeatMask']));
+
+    my $runnable = new Bio::EnsEMBL::Pipeline::Runnable::Genscan(
+	      -query   => $self->query,
+              -genscan => $self->analysis->program_file,
+              -matrix  => $self->analysis->db_file,
+	      -args    => $self->arguments
+    );
+
+    $self->runnable($runnable);
+
+    return 1;
 }
 
-#get/set for runnable and args
-sub init {
-    my ($self, $runnable) = @_;
-    
-    if ($runnable) {
-      #extract parameters into a hash
-      my %parameters;
-      my ($parameter_string) = $self->parameters();
-      if ($parameter_string) {
-	my @pairs = split (/,/, $parameter_string);
-	foreach my $pair (@pairs) {
-	  
-	  my ($key, $value) = split (/=>/, $pair);
-	  $key =~ s/\s+//g;
-	  $parameters{$key} = $value;
-	}
-	
-      }
-      $parameters {'-genscan'}  = $self->analysis->program_file;
-      $parameters {'-matrix'}   = $self->analysis->db_file;
-      #creates empty Bio::EnsEMBL::Runnable::Genscan object
-      my $runnable = $runnable->new(%parameters);
-      $self->runnable($runnable);
-    }
-}
-
-
-=head2 write_output
-
-  Args      : none
-  Function  : Standard RunnableDB write_output. Writes PredictionTranscripts
-              into the database.
-  Returntype: none
-  Exceptions: none
-  Caller    : Job module in Pipeline
-  Example   :  ( optional )
-
-=cut
-
-sub write_output {
-   my $self = shift;
- 
-   my $genscan_runnable = ($self->runnable())[0];
-   my @transcripts = @{$genscan_runnable->get_all_Transcripts()};
-   if( ! @transcripts ) { return; }
-
-   my $ptransAdaptor = $self->db()->get_PredictionTranscriptAdaptor();
-
-   for my $trans ( @transcripts ) {
-     my $ptrans = Bio::EnsEMBL::PredictionTranscript->new();
-     my @exons = @{$trans->get_all_Exons()};
-
-     if ($exons[0]->strand == 1) {
-       @exons = sort {$a->start <=> $b->start } @exons;
-     } else {
-       @exons = sort {$b->start <=> $a->start } @exons;
-     }
-     print "ANALYSIS: ",$self->analysis()->dbID,"\n";
-
-     $ptrans->analysis( $self->analysis() );
-     for my $exon ( @exons ) {
-       $exon->contig( $self->{'contig'} );
-       $ptrans->add_Exon( $exon );
-     }
-     $ptransAdaptor->store( $ptrans );
-   }
-  
-}
-
-
-
-=head2 result_quality_tag
-
-    Title   :   result_quality_tag
-    Usage   :   $self->result_quality_tag
-    Function:   Returns an indication of whether the data is suitable for 
-                further analyses. Allows distinction between failed run and 
-                no hits on a sequence.
-    Returns :   'VALID' or 'INVALID'
-    Args    :   none
-
-=cut
-#a method of writing back result quality
-sub result_quality_tag {
-    my ($self) = @_;
-    
-    if ($self->output)
-    {
-        return 'VALID';
-    }
-    else
-    {
-        return 'INVALID';
-    }
-}
 
 1;
