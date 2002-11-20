58739e1b214951446905087ba0b8cbed009624b3
Eduardo Eyras <eae@sanger.ac.uk>
2002-11-20 16:44:26 +0000

    extra options added to run Blat

--- modules/Bio/EnsEMBL/Pipeline/ESTConf.pm
+++ modules/Bio/EnsEMBL/Pipeline/ESTConf.pm
@@ -71,42 +71,43 @@ use vars qw( %ESTConf );
 	    
 	    # path to ensembl-pipeline/scripts/EST
 	    EST_SCRIPTDIR     => '/nfs/acari/eae/ensembl/ensembl-pipeline/scripts/EST/',
-
-	    # where the result-directories are going to go
 	    
+	    # where the result-directories are going to go	
+    
 	    #EST_TMPDIR                  => '/ecs2/scratch1/ensembl/eae/NCBI_30/cdnas',
-	    EST_TMPDIR        => '/ecs2/scratch1/ensembl/eae/blat_test',
+	    EST_TMPDIR        => '/ecs2/scratch1/ensembl/eae/NCBI_31/cdnas',
 	    
 	    # job-queue in the farm
 	    EST_QUEUE         => 'acari',
 	    
 	    # make_bsubs.pl options
-	    EST_EXONERATE_BSUBS   => '/ecs2/scratch1/ensembl/eae/main_trunk_tests/est_jobs/exonerate.jobs',
-	    EST_FILTER_BSUBS      => '/ecs2/scratch1/ensembl/eae/main_trunk_tests/est_jobs/filter_and_e2g.jobs',
-	    EST_GENEBUILDER_BSUBS => '/ecs2/scratch1/ensembl/eae/blat_test/EST_GeneBuilder.jobs',
-	    EST_EXPRESSION_BSUBS  => '/ecs2/scratch1/ensembl/eae/blat_test/genes2ests.jobs', 
+	    EST_EXONERATE_BSUBS   => '/ecs2/scratch1/ensembl/eae/new_exonerate_test/exonerate.jobs',
+	    EST_FILTER_BSUBS      => '/ecs2/scratch1/ensembl/eae/NCBI_31/cdnas/filter_and_e2g.jobs',
+	    EST_GENEBUILDER_BSUBS => '/ecs2/scratch1/ensembl/eae/NCBI_31/cdnas/EST_GeneBuilder.jobs',
+	    EST_EXPRESSION_BSUBS  => '/ecs2/scratch1/ensembl/eae/NCBI_31/cdnas/genes2ests.jobs', 
 	    
 	    # make_blat_bsubs.pl options
-	    EST_BLAT_BSUBS   => '/ecs2/scratch1/ensembl/eae/blat_test/blat.jobs',
-	    EST_BLAT_OPTIONS     => " -mask=lower ",
+	    EST_BLAT_BSUBS   => '/ecs2/scratch1/ensembl/eae/NCBI_31/cdnas/blat.jobs',
 	    
+
+	    	    
 	    # for prepare_ests.pl
 	    
 	    # path to executable for chunking seqeuncefiles eg fastasplit
 	    EST_FILESPLITTER            => '/acari/work2/gs2/gs2/bin/fastasplit', 	    
 	    
 	    # path to file containing ALL ESTs/cDNAs
-	    EST_FILE                    => '/ecs2/work1/eae/NCBI_30/ests/all_human_ests',
+	    EST_FILE                    => '/ecs2/work1/eae/NCBI_31/cdnas/clipped_refseqs',
 	    
 	    # path to directory where EST chunks live
 	    #EST_CHUNKDIR                => '/acari/work6a/eae.tmp/Mouse/Mouse_5.3.1/riken_mRNAs/second_rechunks',
 	    
-	    EST_CHUNKDIR                => '/ecs2/work1/eae/NCBI_30/ests/chunks/',
+	    EST_CHUNKDIR                => '/ecs2/work1/eae/NCBI_31/cdnas/chunks',
 	    #EST_CHUNKDIR                => '/acari/work4a/lec/briggsae_sequence/mRNA_chunks/',
-
+	    
  	    # how many chunks?
 	    # for NCBI_28 we have 3690891 ests, at approx. 350 ests per chunk, we estimate
-	    EST_CHUNKNUMBER             => 20000, 	 
+	    EST_CHUNKNUMBER             => 800, 	 
 	       
 	    # path to makeseqindex executable
 	    EST_MAKESEQINDEX            => '/usr/local/ensembl/bin/makeseqindex', 	    
@@ -116,27 +117,62 @@ use vars qw( %ESTConf );
 	    #EST_GENOMIC                 => '/acari/work4a/lec/briggsae_sequence/briggsae_dusted_masked_12_08_02.fa',
 	    #EST_BLAT_GENOMIC            => '/ecs2/work1/eae/NCBI_30/pmatches/human_genome_NCBI_30.fa',
 	    #EST_BLAT_GENOMIC            => '/data/blastdb/Ensembl/NCBI_30_dusted_masked_all_contigs.fa',
-
-	    EST_BLAT_GENOMIC             => '/ecs2/work1/eae/NCBI_30/genome/masked_seq/softmasked_chrs',
 	    
+	    ################
+	    # BLAT OPTIONS #
+	    ################
+	    # binary script
+	    EST_BLAT_BINARY  => '/usr/local/ensembl/bin/blat',    
+	    #EST_BLAT_BINARY  => '/nfs/acari/tjrc/blat',
+	    
+
+	    # full path fo the dir where we have the masked-dusted chromosomes
+	    EST_BLAT_GENOMIC             => '/data/blastdb/Ensembl/softmasked_NCBI_30_chromosomes/',
+	    #EST_BLAT_GENOMIC             => '/ecs2/work1/eae/NCBI_30/genome/masked_seq/softmasked_chrs/chr14/',
 	    #EST_BLAT_GENOMIC             => '/acari/work4a/lec/briggsae_sequence/briggsae_dusted_masked_12_08_02.fa',
-            EST_GENOMIC                 => '/data/blastdb/Ensembl/NCBI_30_dusted_masked_all_contigs.fa',
+	    # for cDNAs (long sequences) these options seem fine:
+	    EST_BLAT_OPTIONS => " -mask=lower -minMatch=3",
+	    
+	    
+	    EST_MIN_PERCENT_ID => 97,
+	    EST_MIN_COVERAGE => 90,
+	    	    
+	    #####################
+	    # EXONERATE OPTIONS #
+	    #####################
+	    # full path fo the dir where we have the masked-dusted chromosomes
+	    EST_EXONERATE_GENOMIC        => '/ecs2/work1/eae/NCBI_30/genome/masked_seq',
+	    EST_GENOMIC                 => '/data/blastdb/Ensembl/NCBI_30_dusted_masked_all_contigs.fa',
 	    
 	                                            # path to file with repeatmasked dusted genomic sequences
 	                                            # NB this file is huge - distribute it across the farm or 
                                                     # be prepared to face the wrath of systems when the network 
                                                     # seizes up!
+	    
+	    EST_EXONERATE               => '/acari/work2/gs2/gs2/bin/arch/OSF1/exonerate-0.6.4',
+            #EST_EXONERATE              => '/usr/local/ensembl/bin/exonerate', 
+	    EST_EXONERATE_ARGS          => '',
+	    
 	    # for filter_and_e2g.pl
 	    EST_FILTERCHUNKSIZE         => 1000000,      #  we use 1000000 (ie 1MB) chunks
 	    
-	    # ExonerateESTs options
-	    EST_EXONERATE               => '/usr/local/ensembl/bin/exonerate',      # path to executable
-	    EST_EXONERATE_ARGS          => '',
-	    
-	    # each runnable has an analysis
-	    EST_EXONERATE_RUNNABLE     => 'Bio::EnsEMBL::Pipeline::RunnableDB::ExonerateESTs',      
-	    EST_EXONERATE_ANALYSIS     => 'est_exonerate',
+	    ### new exonerate options ####
+	    #
+	    # score: min scores to report. 
+	    # Score is here the raw score for the alignment: +5 for every match and -4 for every mismatch 
+	    #
+	    # fsmmemory: memory given for the target sequence ( max memory required for holding the chromosomes )
+	    # In human this could be around 256
+	    #
+	    EST_EXONERATE_OPTIONS       => ' --score 500 --fsmmemory 256',
 
+	    
+	    ############################################################
+	    # each runnable has an analysis
+	    ############################################################
+	    EST_EXONERATE_RUNNABLE     => 'Bio::EnsEMBL::Pipeline::RunnableDB::ExonerateToGenes',      
+	    EST_EXONERATE_ANALYSIS     => 'exonerate',
+	    
 	    EST_FILTER_RUNNABLE        => 'Bio::EnsEMBL::Pipeline::RunnableDB::FilterESTs_and_E2G',
 	    EST_FILTER_ANALYSIS        => 'exonerate_e2g',
 
@@ -147,7 +183,7 @@ use vars qw( %ESTConf );
 	    EST_EXPRESSION_ANALYSIS    => 'expression',
 
 	    EST_BLAT_RUNNABLE          => 'Bio::EnsEMBL::Pipeline::RunnableDB::BlatToGenes',
-	    EST_BLAT_ANALYSIS          => 'blat',
+	    EST_BLAT_ANALYSIS          => 'blat_oneOFF',
 
 	    # Coverage setting for FeatureFilter
 	    EST_FEATFILT_COVERAGE       => 10,    
@@ -162,24 +198,36 @@ use vars qw( %ESTConf );
 	                                            # name of EST file that has been indexed and is 
 	                                            # accessible across the farm
 	                                            # for analysis(process)
-      	    EST_SOURCE                  => 'worm_mRNA',      
+      	    EST_SOURCE                  => 'refseq',      
 	    
-
-	    # for EST_GeneBuilder
+	    ############################################################
+	    # EST_GeneBuilder
+	    ############################################################
+	    
 	    EST_GENEBUILDER_CHUNKSIZE   => 1000000,      #  we use 1000000 (ie 1MB) chunks
 	                                                 # it should be equal to EST_FILTERCHUNKSIZE
-	    EST_GENEBUILDER_INPUT_GENETYPE => 'exonerate_e2g',   
+	    EST_GENEBUILDER_INPUT_GENETYPE => 'blat_minmatch3',   
 	    EST_GENOMEWISE_GENETYPE        => 'genomewise',
-	    EST_EVIDENCE_TAG               => 'exonerate_e2g',
+	    EST_EVIDENCE_TAG               => 'blat_minmatch3',
+	    
+
+	    # how strict you want the merging of cdnas/ests to be.
+	    # one of these values = 'exact', 'fuzzy', 'loose'
+	    EST_GENEBUILDER_MERGE          => 'fuzzy',
+	    # the recommended thing is to use 'fuzzy' if you want to allow edge-mismatches in the
+	    # exon comparisons, for instance in ests that are likely to contain errors
+	    # 'exact' would be for good quality data (e.g. cdnas) as it requires exact exon-edge matching
+	    # and it could allow alternative UTRs to be resolved
+	    # there is also the option 'loose', which merges also introns that falls in exons,
+	    # this is when you really don't trust those ests.
 
 	    EST_MAX_EVIDENCE_DISCONTINUITY => 10,
-	    EST_MAX_INTRON_SIZE => 10000,
+	    EST_MAX_INTRON_SIZE => 300000,
 	    
 	    # not used at this moment
 	    EST_STRICT_LOWER_BOUND      => '', # 1 for ESTs only, 0 for cDNAs/mRNAs only
 	    EST_MIN_EVIDENCE_SIMILARITY => 65,
-	    EST_MIN_PERCENT_ID => 70,
-	    EST_MIN_COVERAGE => 90,
+
 	    # database config
 	    # IMPORTANT: make sure that all databases involved in each analysis are
 	    # not on the same mysql instance 
@@ -201,28 +249,28 @@ use vars qw( %ESTConf );
 	    # est_db = where we load up exonerate results into the feature table
 	    ############################################################
 
-	    EST_DBNAME                  => 'briggsae_cdna_newschema',
- 	    EST_DBHOST                  => 'ecs1d',
-	    EST_DBUSER                  => 'ensro',
+	    EST_DBNAME                  => '',
+ 	    EST_DBHOST                  => '',
+	    EST_DBUSER                  => '',
 	    EST_DBPASS                  => '',
 	    
 	    # est_e2g_db = where we write the genes we produce from the exonerate features
-	         # this should be in a different place from the one above
+	    # this should be in a different place from the one above
 	    #EST_E2G_DBNAME                  => 'briggsae_cdna_newschema', 
 	    #EST_E2G_DBHOST                  => 'ecs1d',
 	    #EST_E2G_DBUSER                  => 'ecs1dadmin',
 	    #EST_E2G_DBPASS                  => 'TyhRv',
-	    EST_E2G_DBNAME                  => 'human_est_test',
+	    EST_E2G_DBNAME                  => 'human_cdna_test',
 	    EST_E2G_DBHOST                  => 'ecs2a',
 	    EST_E2G_DBUSER                  => 'ensadmin',
 	    EST_E2G_DBPASS                  => 'ensembl',
 
 	    
 	    # est_gene_db = where we write the genes we produce from e2g transcripts
-	    EST_GENE_DBNAME                  => 'briggsae_cdna_newschema',
-	    EST_GENE_DBHOST                  => 'ecs1d',
-	    EST_GENE_DBUSER                  => 'ecs1dadmin',
-	    EST_GENE_DBPASS                  => 'TyhRv',
+	    EST_GENE_DBNAME                  => 'human_cdna_test',
+	    EST_GENE_DBHOST                  => 'ecs2a',
+	    EST_GENE_DBUSER                  => 'ensadmin',
+	    EST_GENE_DBPASS                  => 'ensembl',
 	    
 	    # if you want to use ests together with cdnas in EST_GeneBuilder
 	    # and your cdnas are in a SEPARATE DATABASE, you can specify it here:
