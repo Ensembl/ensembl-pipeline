68f2af366f2c0ec1155e96383c1d6fae732a6067
Laura Clarke <lec@sanger.ac.uk>
2002-11-22 08:48:00 +0000

    typos

--- scripts/DataConversion/wormbase/WormBaseConf.pm
+++ scripts/DataConversion/wormbase/WormBaseConf.pm
@@ -1,4 +1,4 @@
-package WormBaseConf.pm
+package WormBaseConf;
 
 use strict;
 use vars qw( %WormBaseConf );
@@ -9,50 +9,50 @@ use vars qw( %WormBaseConf );
 		 WB_CHR_INFO => [
 				 { 
 				  chr_name => 'I',
-				  agp_file => '',
+				  agp_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_I.agp',
 				  length => '15080471',
-				  gff_file => '',
-				 }
+				  gff_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_I.gff',
+				 },
 				 { 
 				  chr_name => 'II',
-				  agp_file => '',
+				  agp_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_II.agp',
 				  length => '15279300',
-				  gff_file => '',
-				 }
+				  gff_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_II.gff',
+				 },
 				 { 
 				  chr_name => 'III',
-				  agp_file => '',
+				  agp_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_III.agp',
 				  length => '13783262',
-				  gff_file => '',
-				 }
+				  gff_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_III.gff',
+				 },
 				 { 
 				  chr_name => 'IV',
-				  agp_file => '',
+				  agp_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_IV.agp',
 				  length => '17493790',
-				  gff_file => '',
-				 }
+				  gff_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_IV.gff',
+				 },
 				 { 
 				  chr_name => 'V',
-				  agp_file => '',
+				  agp_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_V.agp',
 				  length => '20916335',
-				  gff_file => '',
-				 }
+				  gff_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_V.gff',
+				 },
 				 { 
 				  chr_name => 'X',
-				  agp_file => '',
+				  agp_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_X.agp',
 				  length => '17705013',
-				  gff_file => '',
+				  gff_file => '/ecs2/work1/lec/code/elegans_conversion/CHROMOSOME_X.gff',
 				 }
 			       ],
-		 WB_AGP_TYPE => '',
+		 WB_AGP_TYPE => 'elegans_90',
 		 
 		 # database to put sequnece and genes into
-		 WB_DBNAME => '',
-		 WB_DBHOST => '',
-		 WB_DBUSER => '',
-		 WB_DBPASS => '',
+		 WB_DBNAME => 'elegans_maintrunk',
+		 WB_DBHOST => 'ecs1d',
+		 WB_DBUSER => 'ecs1dadmin',
+		 WB_DBPASS => 'TyhRv',
 		 #path to index based on clone name
-		 WB_CLONE_INDEX => [''];
+		 WB_CLONE_INDEX => ['/ecs2/work1/lec/code/elegans_conversion/wormbase_clones'],
 		 
 		);
 
@@ -72,14 +72,14 @@ sub import {
 
 
     foreach (@vars) {
-	if ( defined $GeneConf{ $_ } ) {
+	if ( defined $WormBaseConf{ $_ } ) {
             no strict 'refs';
 	    # Exporter does a similar job to the following
 	    # statement, but for function names, not
 	    # scalar variables:
-	    *{"${callpack}::$_"} = \$GeneConf{ $_ };
+	    *{"${callpack}::$_"} = \$WormBaseConf{ $_ };
 	} else {
-	    die "Error: GeneConf: $_ not known\n";
+	    die "Error: WormBaseConf: $_ not known\n";
 	}
     }
 }
