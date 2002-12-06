84bd8dfd48e7484e2e4b75c8fe14ce787a0fdf31
Eduardo Eyras <eae@sanger.ac.uk>
2002-12-06 01:14:44 +0000

    class to generate a map between two transcripts according to a coordinate based comparison. An example is to map ests to transcripts according to compatible splicing structure. This class will be the one used in the RunnableDB MapGeneToExpression, but it has been made as generic as possible to make it useful for other things

---
+++ modules/Bio/EnsEMBL/Pipeline/GeneComparison/GenericTranscriptMatcher.pm
@@ -0,0 +1,426 @@
+#
+# Written by Eduardo Eyras
+#
+# Cared for by EnsEMBL  <ensembl-dev@ebi.ac.uk>
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::Runnable::GenericTranscriptMatcher
+
+=head1 SYNOPSIS
+
+    my $obj = Bio::EnsEMBL::Pipeline::Runnable::GenericTranscriptMatcher->new(
+									      -reference_set => \@transcript_set1,
+									      -match_set => \@transcript_set2,
+									      );
+    
+    $obj->run;
+    my %expression_map = %{ $obj->output };
+    where @{ $expression_map{$transcript_id} } is an array of ests mapped to this transcript
+    ests are here Bio::EnsEMBL::Transcript objects   
+    
+    one can do:
+    my @transcript_ids = keys %expression_map;
+    foreach my $transcript_id ( @transcripts_ids ){
+	@mapped_ests = $expression_map{ $transcript_id };
+    }
+
+    
+=head1 DESCRIPTION
+
+Class to map one set of transcripts (match_set) to a second set (reference_set) 
+according to certain coordinate-based comparison rules
+
+=head1 CONTACT
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+package Bio::EnsEMBL::Pipeline::Runnable::GenericTranscriptMatcher;
+
+use diagnostics;
+use vars qw(@ISA);
+use strict;
+
+use Bio::EnsEMBL::Root;
+use Bio::EnsEMBL::Pipeline::RunnableDB;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Pipeline::GeneComparison::TranscriptCluster;
+use Bio::EnsEMBL::Pipeline::GeneComparison::GeneCluster;
+use Bio::EnsEMBL::Pipeline::GeneComparison::GeneComparison;
+use Bio::EnsEMBL::Pipeline::GeneComparison::ObjectMap;
+use Bio::EnsEMBL::Pipeline::GeneComparison::TranscriptComparator;
+use Bio::EnsEMBL::Pipeline::DBSQL::ExpressionAdaptor;
+
+
+
+use Bio::EnsEMBL::Pipeline::ESTConf qw(
+				       EST_INPUTID_REGEX
+				       EST_REFDBHOST
+				       EST_REFDBUSER
+				       EST_REFDBNAME
+				       EST_REFDBPASS
+				       EST_E2G_DBHOST
+				       EST_E2G_DBUSER
+				       EST_E2G_DBNAME
+				       EST_E2G_DBPASS
+				       EST_TARGET_DBNAME
+				       EST_TARGET_DBHOST
+				       EST_TARGET_DBUSER
+				       EST_TARGET_DBPASS      
+				       EST_TARGET_GENETYPE
+				       EST_GENEBUILDER_INPUT_GENETYPE
+				       EST_EXPRESSION_DBHOST
+				       EST_EXPRESSION_DBNAME
+				       EST_EXPRESSION_DBUSER
+				       EST_EXPRESSION_DBPASS
+				      );
+
+@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
+
+######################################################################
+
+sub new{
+  my ($class,@args) = @_;
+  my $self = $class->SUPER::new(@args);
+  my( $reference_set, $match_set ) = 
+      $self->_rearrange([qw(REFERENCE_SET MATCH_SET)], @args);
+  
+  unless( $reference_set && $match_set ){
+      $self->warn("lacking of the inputs. Nothing to match");
+      exit(0);
+  }
+  unless ( $reference_set->[0]->isa('Bio::EnsEMBL::Transcript') &&
+	   $match_set->[0]->isa('Bio::EnsEMBL::Transcript') ){
+      $self->throw("wrong input type, needs two sets of transcripts");
+  }
+  
+  my $matching_map = Bio::EnsEMBL::Pipeline::GeneComparison::ObjectMap->new();
+  
+  $self->matching_map( $matching_map );	
+  $self->_reference_set($reference_set);
+  $self->_match_set($match_set);
+  return $self;
+  
+}
+
+#########################################################################
+#
+# GET/SET METHODS 
+#
+#########################################################################
+
+sub reference_set{
+  my ( $self, $listref ) = @_;
+  if ( $listref ){
+      $self->{_reference_set} = $listref;
+  }
+  return $self->{_reference_set};
+}
+
+############################################################
+
+sub match_set{
+  my ( $self, $listref ) = @_;
+  if ( $listref ){
+      $self->{_matach_set} = $listref;
+  }
+  return $self->{_match_set};
+}
+
+############################################################
+
+sub gene_Clusters {
+  my ($self, @clusters) = @_;
+  if (@clusters){
+    push ( @{$self->{'_gene_clusters'} }, @clusters);
+  }
+  return @{ $self->{'_gene_clusters'} };
+}
+
+############################################################
+
+sub matching_map{
+    my ($self,$object_map) = @_;
+    if ( $object_map ){
+	$self->{_matching_map} = $object_map;
+    }
+    return 	$self->{_matching_map};
+} 
+
+#########################################################################
+
+sub output{
+  my ($self)= @_;
+  
+  return $self->matching_Map
+}
+
+#########################################################################
+
+############################################################
+#
+# RUN METHOD
+#
+############################################################
+
+sub run{
+  my ($self,@args) = @_;
+
+  my @reference_trans = @{$self->reference_set};
+  my @match_trans     = @{$self->match_set};
+
+  print STDERR "Trying to match ".scalar( @match_trans )." transcripts to ".
+      scalar(@reference_trans)." reference transcripts\n";
+  
+  foreach my $transcript ( @reference_trans ){
+      $self->_map_Transcripts( $transcript, \@match_trans );
+  }
+  
+  # before returning, check that we have written anything
+  unless( $self->matching_Map ){
+      print STDERR "not matches found\n";
+      exit(0);
+  }
+  return;
+}
+
+############################################################
+#
+# METHODS CALLED FROM RUN METHOD... DOING ALL THE MAGIC
+#
+############################################################
+  
+############################################################
+#
+#  Method for mapping ESTs or any transcripts to genes:
+#
+#  For a given transcript,
+#
+# link to it all the ests that have exons which
+# are consecutively included in the transcript
+# (this will avoid linking to alternative variants which may in fact be expressed differently)
+# allow the 5' and 3' exons of the ests to extend beyond the transcript
+# (they may contain UTRs that we failed to annotate)
+
+# ests           (1)###------###
+#                          (2)#####--------#######
+#           (3)########------###
+#                (4)#####----#####
+#             (5)####--------#####
+
+#transcript      ######------######--------######
+
+# (1),(2) and (3) would be linked. I'm not sure yet about cases like (4) and
+# (5).
+
+# Also, case (2) could be a hint for alternative polyA site if we have
+# already annotated an UTR for that transcript. We could check for this case
+# and only add (2) if there is no 3'UTR, to be sure.
+
+# Case (3) could be also related to an alternative start of transcription,
+# we could add it only for cases that a 5'UTR is not annotated.
+
+# Part of the alternative polyA sites and start of transcription is
+# correlated with alternative splicing so maybe this 'ambiguity' cases will
+# not cause too many problems.
+
+# We can keep the conditions tight for a start. As ests have been filtered,
+# we could accept the splicing of (4) and (5) with some confidence, and
+# we can include the check of UTRs for (2) and (3). ESTs that are not linked
+# would be rejected in principle, I cannot predict yet how much data wil
+# remain unused by doing this.
+
+sub _map_Transcripts{
+  my ($self,$transcript,$ests) = @_;
+
+  # a map from transcript to est/transcript
+  my %expression_map;
+  
+  # a comparison tool
+  my $transcript_comparator = Bio::EnsEMBL::Pipeline::GeneComparison::TranscriptComparator->new();
+  
+ EST:
+  foreach my $est (@$ests){
+      
+      # compare this est
+      my $merge = $transcript_comparator->_test_for_semiexact_Merge($transcript,$est);
+      
+      # (this method checks exact exon boundary matches but
+      # allows mismatches at outer end of the 5' and 3' exons)
+      # check 5' and 3' ends in case ESTs give an alternative transcription 
+      
+      # start or alternative polyA site, respectively
+      if ($merge){
+	  my $alt_start = $self->_check_5prime($transcript,$est);
+	  my $alt_polyA = $self->_check_3prime($transcript,$est);
+	  
+      }
+      
+      
+      # if match, put est in $expression_map{ $transcript }
+      if ($merge){
+	  $self->matching_map->match($transcript,$est);
+      }
+  }
+}
+
+#########################################################################
+
+sub _check_5prime{
+  my ($self,$transcript,$est) = @_;
+  my $alt_start = 0;
+  
+  # first find out whether the transcript has 5' UTR
+  my $utr5;
+  eval{
+    $utr5 = $transcript->five_prime_utr;
+  };
+  unless( $utr5 ){
+    return 0;
+  }
+  
+  $transcript->sort;
+  $est->sort;
+  
+  my $start_exon = $transcript->start_exon;
+  #my $start_exon = $transcript->translation->start_exon;
+  my $strand     = $start_exon->strand;
+  foreach my $exon ( @{$transcript->get_all_Exons} ){
+    my $est_exon_count = 0;
+    
+    foreach my $est_exon ( @{$est->get_all_Exons} ){
+      $est_exon_count++;
+      if ( $exon == $start_exon ){
+	if ( $exon->overlaps( $est_exon ) ){
+	  if ($strand == 1){
+	    if ( $est_exon->start < $exon->start ){
+	      print STDERR "potential alternative transcription start in forward strand\n";
+	      $alt_start = 1;
+	    }
+	  }
+	  if ($strand == -1){
+	    if ( $est_exon->end > $exon->end ){
+	      print STDERR "potential alternative transcription start in reverse strand\n";
+	      $alt_start = 1;
+	    }
+	  }
+	  if ($est_exon_count > 1){
+	    print STDERR "There are more est exons upstream\n";
+	    if ( $alt_start == 1){
+	      return 1;
+	    }
+	  }
+	}
+      }
+    }
+  }
+  return 0;
+}
+
+
+#########################################################################
+
+sub _check_3prime{
+  my ($self,$transcript,$est) = @_;
+  my $alt_polyA = 0;
+  
+  # first find out whether the transcript has 5' UTR
+  my $utr3;
+  eval{
+    $utr3 = $transcript->three_prime_utr;
+  };
+  unless( $utr3 ){
+    return 0;
+  }
+  
+  $transcript->sort;
+  $est->sort;
+
+  my $end_exon = $transcript->end_exon;
+  #my $end_exon = $transcript->translation->end_exon;
+  my $strand   = $end_exon->strand;
+  
+  foreach my $exon ( @{$transcript->get_all_Exons} ){
+    my $est_exon_count = 0;
+    my @est_exons = @{$est->get_all_Exons};
+    
+    foreach my $est_exon ( @est_exons ){
+      $est_exon_count++;
+      if ( $exon == $end_exon ){
+	if ( $exon->overlaps( $est_exon ) ){
+	  if ($strand == 1){
+	    if ( $est_exon->end > $exon->end ){
+	      print STDERR "potential alternative polyA site in forward strand\n";
+	      $alt_polyA = 1;
+	    }
+	  }
+	  if ($strand == -1){
+	    if ( $est_exon->start < $exon->start ){
+	      print STDERR "potential alternative polyA site in reverse strand\n";
+	      print STDERR "looking at : exon:".$exon->start."-".$exon->end." and est_exon:".$est_exon->start."-".$est_exon->end."\n";
+	      $alt_polyA = 1;
+	    }
+	  }
+	  if ($est_exon_count != scalar(@est_exons) ){
+	    print STDERR "There are more est exons downstream\n";
+	    print STDERR "est exon count = $est_exon_count, exons = ".scalar(@est_exons)."\n";
+	    
+	    if ( $alt_polyA ==1 ){
+	      return 1;
+	    }
+	  }
+	}
+      }
+    }
+  }
+  return 0;
+}
+
+#########################################################################
+
+# method to calculate the exonic length of a transcript which is inside a gene
+
+sub _transcript_exonic_length{
+  my ($self,$tran) = @_;
+  my $exonic_length = 0;
+  foreach my $exon (@{$tran->get_all_Exons}){
+    $exonic_length += ($exon->end - $exon->start + 1);
+  }
+  return $exonic_length;
+}
+
+#########################################################################
+
+# method to calculate the length of a transcript in genomic extent, 
+
+sub _transcript_length{
+    my ($self,$tran) = @_;
+    my @exons= @{$tran->get_all_Exons};
+    my $genomic_extent = 0;
+    if ( $exons[0]->strand == -1 ){
+      @exons = sort{ $b->start <=> $a->start } @exons;
+      $genomic_extent = $exons[0]->end - $exons[$#exons]->start + 1;
+    }
+    elsif( $exons[0]->strand == 1 ){
+      @exons = sort{ $a->start <=> $b->start } @exons;
+      $genomic_extent = $exons[$#exons]->end - $exons[0]->start + 1;
+    }
+    return $genomic_extent;
+}
+
+############################################################
+ 
+1;
