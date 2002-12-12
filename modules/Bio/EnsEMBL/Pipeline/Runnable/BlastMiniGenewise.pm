a631bed40b33d32ec9f991d30df6cd44fdd10e6e
Dan T Andrews <dta@sanger.ac.uk>
2002-12-12 12:58:15 +0000

    The code that checks whether multiple homologous genes exist in a single minigenomic sequence has been removed to a module of its own - Runnable::BlastMiniBuilder - from which this module now inherits.

--- modules/Bio/EnsEMBL/Pipeline/Runnable/BlastMiniGenewise.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/BlastMiniGenewise.pm
@@ -48,6 +48,7 @@ package Bio::EnsEMBL::Pipeline::Runnable::BlastMiniGenewise;
 use vars qw(@ISA);
 use strict;
 
+use Bio::EnsEMBL::Pipeline::Runnable::BlastMiniBuilder;
 use Bio::EnsEMBL::Pipeline::Runnable::MultiMiniGenewise;
 use Bio::EnsEMBL::Pipeline::Runnable::Blast;
 use Bio::EnsEMBL::Pipeline::Runnable::BlastDB;
@@ -60,7 +61,7 @@ use Bio::EnsEMBL::Pipeline::GeneConf qw (
 					);
 use Bio::EnsEMBL::Pipeline::Config::Blast;
 
-@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableI);
+@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableI Bio::EnsEMBL::Pipeline::Runnable::BlastMiniBuilder);
 
 sub new {
   my ($class,@args) = @_;
@@ -207,20 +208,27 @@ sub check_repeated {
 sub run {
   my ($self) = @_;
   
-  my @features = $self->run_blast;
+  my @blast_features = $self->run_blast;
 
   #print STDERR "There are ".scalar @features." features remaining after re-blasting.\n";
-  unless (@features) {
+  unless (@blast_features) {
     print STDERR "Contig has no associated features.  Finishing run.\n";
     return;
   }
 
   my $mg_runnables;
 
+  my @feature_pairs;
+  foreach my $f (@blast_features){
+    my $feature_pair = new Bio::EnsEMBL::FeaturePair(-feature1 => $f->feature2,
+						     -feature2 => $f->feature1);
+    push(@feature_pairs, $feature_pair);
+  }
+
   if ($self->check_repeated > 0){ 
-    $mg_runnables = $self->build_runnables(@features);
+    $mg_runnables = $self->build_runnables(@feature_pairs);
   } else {
-    my $runnable = $self->make_mmgw($self->genomic_sequence, \@features);
+    my $runnable = $self->make_object($self->genomic_sequence, \@feature_pairs);
     push (@$mg_runnables, $runnable); 
   }
 
@@ -285,360 +293,17 @@ sub run_blast {
   return @features;
 }
 
-
-=head2 build_runnables
-
-  Args [1]   : @features - list of features generated from a 
-               blast run.
-  Example    : $self->build_runnables(@features);
-  Description: Builds the runnable objects for conducting
-               minigenewise runs.  In the simplest case this
-               method creates a single runnable object.  The
-               method itself was written to cope with
-               situations where the presence of repeated genes
-               means that more than one whole gene of high 
-               homology is present in a minigenomic sequence.
-               This module separates possible repeated genes
-               into clusters of exons such that minigenomic
-               sequence fragments can be generated for passing
-               to genewise - when this happens this method
-               returns multiple minigenewise runnables that
-               examine smaller portions of genomic DNA.
-  Returntype : A list of Bio::EnsEMBL::Pipeline::Runnable::MultiMiniGenewise
-  Exceptions : None.
-  Caller     : $self->run
-
-=cut
-
-sub build_runnables {
-  my ($self, @features) = @_;
-
-  my @mg_runnables;
-
-  my %unfiltered_partitioned_features;
-
-  foreach my $raw_feat (@features){
-      push (@{$unfiltered_partitioned_features{$raw_feat->seqname}}, $raw_feat);
-  }
-
-  # We partition the features by protein id such that we can
-  # iterate for each protein id.  Most ids  will fall through
-  # to the simplest case where we are running genewise on one 
-  # unique gene per minigenomic sequence fragment.  In cases
-  # where multiple similar genes are present we switch to the
-  # special case algorithm.  When this happens we cluster the 
-  # likely exons of each individual gene and make small 
-  # minigenomic sequences than span just one cluster.
-
-  my %partitioned_features;
-
-  foreach my $raw_feat (@features){
-
-    # Imporantly, the features are filtered for low identity matches.
-    # This is just for the clustering process.  The full set of features
-    # are still passed to genewise.
-    if($raw_feat->percent_id >= 80){
-      push (@{$partitioned_features{$raw_feat->seqname}}, $raw_feat);
-    }
-  }
-
-  my %runnable_featids;
-
-  foreach my $seqname (keys %partitioned_features){
-
-    # Preliminary check to see whether our filtered features
-    # give enough coverage to make it worth continuing.
-
-    my $coverage = $self->check_coverage($seqname, $partitioned_features{$seqname});
-
-    unless ($coverage > 0.75) {
-      my $mmgw = $self->make_mmgw($self->genomic_sequence, $unfiltered_partitioned_features{$seqname});
-      push (@mg_runnables, $mmgw);
-
-      $runnable_featids{$seqname}++;
-      next
-    }
-
-     # Now, we attempt to cluster our features into groups of
-     # similar exons.
-
-     my $clustered_features = $self->cluster_features(\@{$partitioned_features{$seqname}});
-
-    # We have to do a little test to see if the clusters
-    # represent the bulk of the features present in the 
-    # genomic region.  This test helps avoid the situation
-    # where a non-repeated gene has exons that are very 
-    # similar to one another.  If the flag $clusters_seem_real
-    # is not set to one then the analysis will default to the  
-    # non-repeated gene way of doing things.
-
-    my $clusters_seem_real = 0;
-
-    if (@$clustered_features) {
-      my $number_of_clustered_features = 0;
-      foreach my $cluster (@$clustered_features) {
-	foreach my $clust_feat (@$cluster){
-	  $number_of_clustered_features++;
-	}
-      }
-
-      if ($number_of_clustered_features  >= (0.75 * (scalar @{$partitioned_features{$seqname}}))) {
-	$clusters_seem_real = 1;
-      }
-    }
-
-    # If we have clusters is such numbers that it looks like
-    # there might be repeated genes, break the mini genomic
-    # sequence into fragments and run multiple minigenewises.
-    # Otherwise, default to the normal way of running
-    # multiminigenewise.
-    
-    if ((@$clustered_features)&&($clusters_seem_real > 0)) {
-
-      print STDERR "Minigenomic sequence could contain a number "
-	. "of highly similar genes.  Fragmenting the minigenomic "
-	  . "sequence to try to resolve these genes individually.\n";
-
-      my $gene_clusters = $self->form_gene_clusters($clustered_features);
-
-      # Determine the extreme start and ends of each gene
-      # cluster.  Decide on positions to split the genomic
-      # sequence for genomewise - mid-way between 
-      # ends and starts of neighboring clusters.  The
-      # fragments from clusters that don't have the maximum
-      # number of members are a problem - these presumably 
-      # will fall at the ends of the genomic fragment and 
-      # should just be discarded.
-
-    GENE:      
-      foreach my $gene_cluster (@$gene_clusters){
-
-	my @sorted_gene_cluster = sort {$a->{_hstart} <=> $b->{_hstart};} @$gene_cluster;
-       	my $cluster_start = $sorted_gene_cluster[0]->{_hstart} - 1000;
-	my $cluster_end = $sorted_gene_cluster[-1]->{_hend} + 1000;
-        unless ($cluster_start > 0) {
-	  if ($sorted_gene_cluster[0]->{_hend} > 0) {
-	    $cluster_start = 1;
-	  } else {
-	    next GENE;
-	  }
-	}
-        unless ($cluster_end <= $self->genomic_sequence->length) {
-	  if ($sorted_gene_cluster[0]->{_hstart} <= $self->genomic_sequence->length) {
-	    $cluster_end = $self->genomic_sequence->length;
-	  } else {
-	    next GENE;
-	  }
-	}        
-
-	# Here we create our genomic fragment for passing to 
-	# genewise.  This is the fragment of genomic sequence
-	# that we have calculated should only contain the 
-	# exons of one single gene.  It is padded with Ns to
-        # maintain the coordinate system of the features that
-	# may be returned.  Another way of looking at this is
-	# that we make a genomic sequence where the regions 
-	# flanking our gene are masked.
-	my $string_seq = ('N' x ($cluster_start - 1)) . 
-	         $self->genomic_sequence->subseq($cluster_start, $cluster_end)
-		 . ('N' x ($self->genomic_sequence->length - ($cluster_end + 1)));
-
-	my $genomic_subseq = Bio::Seq->new(-seq => $string_seq,
-					   -id  => $self->genomic_sequence->id );
-
-	my $mmgw = $self->make_mmgw($genomic_subseq, $unfiltered_partitioned_features{$seqname});
-	push (@mg_runnables, $mmgw);
-
-	$runnable_featids{$seqname}++;
-      }
-    
-    }else{
-      # This is what we do when we dont have multiple genes
-      # in our genomic fragment.  This is "Normal mode".
-
-      my $mmgw = $self->make_mmgw($self->genomic_sequence, $unfiltered_partitioned_features{$seqname});
-      push (@mg_runnables, $mmgw);
-
-      $runnable_featids{$seqname}++;
-    }
-
-  }
-
-  # Gah, what if we havent constructed a runnable for each
-  # feature id by this late stage?
-  # It means that the homology of the blast features for
-  # an id was so low that we threw all the features 
-  # away.  Had better just make a default runnable...
-
-  my %feature_ids;
-  foreach my $feature (@features){
-    $feature_ids{$feature->seqname}++;
-  }
-
-  foreach my $feature_id (keys %feature_ids){
-    unless ($runnable_featids{$feature_id} > 0){
-      my $mmgw = $self->make_mmgw($self->genomic_sequence, $unfiltered_partitioned_features{$feature_id});
-      push (@mg_runnables, $mmgw);
-    }
-  }
-
-  return \@mg_runnables;
-}
-
-=head2 cluster_features
-
-  Args [1]   : $features - reference to a list of 
-               PepDnaAlignFeatures generated by a blast run.
-               Ideally, these features should have been
-               filtered to remove garbage - say, filter by
-               percent_id > 80.
-  Example    : $self->cluster_features($features);
-  Description: Takes a list of features from a blast run
-               and attempts to cluster these into groups 
-               of high homology.  If the blast features
-               result from matches to a region with several
-               highly similar copies of the same gene this
-               clustering should produce a series of exon
-               clusters.  If this is not the case only a
-               couple of grubby clusters will be produced.
-  Returntype : A reference to an array of clusters.  Each
-               cluster in the array is an array of similar
-               features.
-  Exceptions : none
-  Caller     : $self->build_runnables
-
-=cut
-
-sub cluster_features {
-
-  my ($self, $features) = @_;
-
-  # Sort the list of features according to their percent id.
-  my @sorted_features = sort { $b->percent_id <=> $a->percent_id;} @$features;
-
-  # With the sorted features, we iteratively check each     
-  # highest scoring hit against each lesser scoring
-  # sequence.  Features that correspond to the same part of 
-  # the hit sequence (ie, are overlapping) are clustered
-  # in an array of 'exons' (and removed from the 
-  # sorted_features array). 
-  
-  my @all_feature_clusters;
-  
-  while (@sorted_features) {
-
-    my $top_feature = shift @sorted_features;
-    
-    my @similar_features;
-    push (@similar_features, $top_feature);
-
-    my @subtracted_sorted_features;
-    foreach my $lesser_feature (@sorted_features) {
-
-      if ($self->check_overlap($top_feature,$lesser_feature)){
-	push (@similar_features, $lesser_feature);
-
-      } else {
-	push (@subtracted_sorted_features, $lesser_feature);
-      }
-    }
-    @sorted_features = @subtracted_sorted_features;
-    unless ((scalar @similar_features) == 1){
-      push (@all_feature_clusters, \@similar_features);
-    }
-  }
-
-  return \@all_feature_clusters;
-}
-
-=head2 form_gene_clusters
-
-  Args [1]   : Array of exon clusters generated by
-               $self->cluster_features (an array of arrays,
-               each sub-array containing similar blast 
-               features)
-  Example    : $self->for_gene_clusters($exon_clusters);
-  Description: Using an array of exons clusters (similar
-               blast features) to build genes with a feature
-               from each cluster.  Genes are formed from
-               exons that lie closest together.
-  Returntype : A reference to an array of arrays, each sub-
-               array containing features that should 
-               correspond to exons. 
-  Exceptions : none
-  Caller     : $self->build_runnables
-
-=cut
-
-
-
-sub form_gene_clusters {
-
-  my ($self, $exon_clusters) = @_;
-
-  # Choose a cluster with the maximum number of members.
-  # Bump the other clusters onto a new array.
-
-
-  my @sorted_by_start = sort {$a->[0]->{_gsf_start} <=> $b->[0]->{_gsf_start}} @$exon_clusters;  
-
-  my $big_cluster = shift @sorted_by_start;
-  my @other_clusters = @sorted_by_start;
-
-  my @final_gene_clusters;      
-  while (@$big_cluster){
-
-    my $seed_exon = shift @$big_cluster;
-	
-    my @gene_cluster;
-    push (@gene_cluster, $seed_exon);
-    
-    foreach my $other_cluster (@other_clusters) {
-      if (@$other_cluster){
-	
-	my @subtracted_other_clusters;
-	my $closest = 1000000;
-	my $closest_feature;
-	foreach my $candidate_exon (@$other_cluster){
-	  
-	  my $distance = abs($candidate_exon->{_hstart} 
-			     - $seed_exon->{_hstart});
-	  
-	  if(($distance < $closest)&&($candidate_exon->{_gsf_start} > $seed_exon->{_gsf_end})){
-	    if (defined $closest_feature){
-	      push (@subtracted_other_clusters, $closest_feature);
-	    }
-	    $closest = $distance;
-	    $closest_feature = $candidate_exon;
-	  } else {
-	    push (@subtracted_other_clusters, $candidate_exon);
-	  }
-	}
-	@$other_cluster = @subtracted_other_clusters;
-
-	push (@gene_cluster, $closest_feature) if (defined $closest_feature);
-	undef $closest_feature;
-
-      }
-    }
-    push (@final_gene_clusters, \@gene_cluster);
-  }	
-
-  return \@final_gene_clusters;
-}
-
-
-
-=head2 make_mmgw
+#=head2 make_mmgw
+=head2 make_object
 
   Args [1]   : $miniseq - a Bio::Seq object representing the
                target sequence for the genewise run.
   Args [2]   : $features - reference to a list of 
-               PepDnaAlignFeatures generated by a blast run.
-  Example    : $self->makemmgw($miniseq, $features);
+               FeaturePairs generated by a blast run.
+  Example    : $self->make_object($miniseq, $features);
   Description: Takes a genomic sequence and builds a
                MultiMiniGenewise runnable object using the 
-               list of PepDnaAlignFeatures.
+               list of FeaturePairs.
   Returntype : A list of 
                Bio::EnsEMBL::Pipeline::Runnable::MultiMiniGenewise
   Exceptions : none
@@ -647,131 +312,32 @@ sub form_gene_clusters {
 =cut
 
 
-sub make_mmgw {
+#sub make_mmgw {
+sub make_object {
   my ($self, $miniseq, $features) = @_;
 
-  # Before we pass our blast generated features to 
-  # MultiMiniGenewise we must first convert them from 
-  # PepDnaAlignFeatures to FeaturePairs.
-
-  my @newf;
-  foreach my $f (@$features){
-    my $newf = new Bio::EnsEMBL::FeaturePair(-feature1 => $f->feature2,
-					     -feature2 => $f->feature1);
-    push(@newf,$newf);
-  }
+#  # Before we pass our blast generated features to 
+#  # MultiMiniGenewise we must first convert them from 
+#  # PepDnaAlignFeatures to FeaturePairs.
+#
+#  my @newf;
+#  foreach my $f (@$features){
+#    my $newf = new Bio::EnsEMBL::FeaturePair(-feature1 => $f->feature2,
+#					     -feature2 => $f->feature1);
+#    push(@newf,$newf);
+#  }
 
   # Create a MultiMiniGenewise object with the features weve
   # just converted.
   my $mg      = new Bio::EnsEMBL::Pipeline::Runnable::MultiMiniGenewise(
 				       '-genomic'    => $miniseq,
-				       '-features'   => \@newf,
+				       '-features'   => $features,
 				       '-seqfetcher' => $self->seqfetcher,
 				       '-endbias'    => $self->endbias
 				      );
 
   return $mg;
 }
-
-
-
-
-=head2 check_overlap
-
-  Args [1]   : query_feature - any kind of align feature.
-  Args [2]   : other feature - another align feature that is
-               to be checked for substantial overlap with the
-               query feature.
-  Example    : $self->check_overlap($query_feature, $other_feature);
-  Description: Checks two features for greater than 90% hit overlap.
-               Features must be blast-generated align features 
-               resultant from a blast run against a common hit
-               sequence.
-  Returntype : 0 or 1
-  Exceptions : none
-  Caller     : $self->cluster_features
-
-=cut
-
-
-sub check_overlap {
-  my ($self, $query_feature, $other_feature) = @_;
-
-  my $query_start = $query_feature->{_gsf_start};
-  my $query_end   = $query_feature->{_gsf_end};
-
-  if ($query_start > $query_end){($query_start, $query_end) = ($query_end, $query_start)}
-  
-  my $other_start = $other_feature->{_gsf_start};
-  my $other_end   = $other_feature->{_gsf_end};
-  
-  if ($other_start > $other_end) {($other_start, $other_end) = ($other_end, $other_start)}
-  
-  unless ($query_end < $other_start || $query_start > $other_end){
-    # We have overlapping features
-    
-    my $query_length = $query_end - $query_start;
-    my $other_length = $other_end - $other_start;
-    
-    my ($start1, $start2, $end1, $end2) = sort {$a <=> $b} ($query_start, $other_start, $query_end, $other_end);
-    
-    my $overlap = $end1 - $start2;
-    
-    if (($overlap >= (0.9 * $query_length))&&($overlap >= (0.9 * $other_length))){
-      return 1
-    }
-  }
-    
-  return 0;    
-}
-  
-=head2 check_coverage
-
-  Args [1]   : protein id - id used by seqfetcher to retrieve
-               protein object.
-  Args [2]   : feature listref - reference to list of align features
-               that align to parts of the protein specified in Arg [1]
-  Example    : $self->check_coverage($seqname, $features);
-  Description: Using a protein and a list of corresponding align
-               features, calculates the percentage of the protein
-               that the features cover.
-  Returntype : A real (e.g. a percentage like 0.80)
-  Exceptions : none
-  Caller     : $self->make_runnables
-
-=cut
-
-
-sub check_coverage {
-  my ($self, $seqname, $features) = @_;
-
-  my $protein = $self->get_Sequence($seqname);  
-  my $protein_length = $protein->length;
-
-  my @voider;
-  my @score_keeper;
-
-  foreach my $feature (@$features){
-
-    my $start = $feature->{_gsf_start};
-    my $end = $feature->{_gsf_end};
-
-    ($start, $end) = sort {$a <=> $b} ($start, $end);
-
-    for (my $j = $start; $j <= $end; $j++){
-      $score_keeper[$j] = 1;
-    }
-  }
-
-  my $tally = 0;
-
-  foreach my $score (@score_keeper){
-    $tally++ if $score;
-  }
-
-  return ($tally/$protein_length);
-}
-
 
 sub get_Sequences {
     my ($self) = @_;
