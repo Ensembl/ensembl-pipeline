1c47ab859911365f1b8e14b278e718a413b56404
Simon Potter <scp@sanger.ac.uk>
2003-01-06 18:21:19 +0000

    cleans up

--- modules/Bio/EnsEMBL/Pipeline/Runnable/EponineTSS.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/EponineTSS.pm
@@ -150,6 +150,7 @@ sub query {
 
       $self->filename($self->query->id.".$$.seq");
       $self->results($self->filename.".out");
+      $self->file($self->results);
   }
   return $self->{'_sequence'};
 }
