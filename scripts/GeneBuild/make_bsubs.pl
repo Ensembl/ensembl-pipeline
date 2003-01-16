839473ef137686073810cefa05b00486ca8f3d25
Val Curwen <vac@sanger.ac.uk>
2003-01-16 09:36:12 +0000

    Removing TARGETTED_RUNNABLES as we never use it any more.

--- scripts/GeneBuild/make_bsubs.pl
+++ scripts/GeneBuild/make_bsubs.pl
@@ -24,7 +24,6 @@
      GeneBuild::Scripts::GB_QUEUE
      GeneBuild::Scripts::GB_OUTPUT_DIR
      GeneBuild::Scripts::GB_LENGTH_RUNNABLES
-     GeneBuild::Scripts::GB_TARGETTED_RUNNABLES
      GeneBuild::Scripts::GB_PM_OUTPUT
      GeneBuild::Scripts::GB_SIZE
 							   
@@ -48,7 +47,6 @@ use Bio::EnsEMBL::Pipeline::Config::GeneBuild::Scripts qw (GB_RUNNER
 							   GB_QUEUE
 							   GB_OUTPUT_DIR
 							   GB_LENGTH_RUNNABLES
-							   GB_TARGETTED_RUNNABLES
 							   GB_PM_OUTPUT
 							   GB_SIZE
 							  );
@@ -94,12 +92,6 @@ foreach my $length_runnable_list (@{$GB_LENGTH_RUNNABLES}) {
   make_lbsubs($runnable, $analysis) unless $runnable eq '';
 }
 
-foreach my $other_runnable_list (@{$GB_TARGETTED_RUNNABLES}) {
-  my $analysis = $other_runnable_list->{analysis};
-  my $runnable = $other_runnable_list->{runnable};
-  make_tbsubs($runnable, $analysis) unless $runnable eq '';
-}
-
 foreach my $genecombiner_runnable ( @{$GENECOMBINER_RUNNABLES} ){
   my $analysis =  $genecombiner_runnable->{analysis};
   my $runnable = $genecombiner_runnable->{runnable};
@@ -131,84 +123,6 @@ sub get_chrlengths{
   }
   
 }
-
-sub make_tbsubs {
-  my ($runnable,$analysis_logic_name) = @_;
-  
-  my $runner      = $GB_RUNNER;
-  my $dbname      = $GB_DBNAME;
-  my $dbhost      = $GB_DBHOST;
-  my $dbuser      = $GB_DBUSER;
-  my $dbpass      = $GB_DBPASS;
-  my $queue       = $GB_QUEUE;
-  my $output_dir  = $GB_OUTPUT_DIR . "/$runnable";
-  
-  my $pm_out      = $GB_PM_OUTPUT;
-
-  $pm_out     .= "pm_best.out";
-  # check them!
-  foreach my $arg($pm_out){
-    if ($arg eq '' ){
-      print "You need to set various parameters in GeneBuild config files\n" .  
-	"Here are your current values for required settings: \n" .
-	"GB_PM_OUTPUT  => $GB_PM_OUTPUT\n" ;
-
-      exit(1);
-    }
-  }
-
-  # parse pmatch results
-  my %pm_ids;
-  
-  # set up jobfile & output dirs
-  system("mkdir $output_dir") unless opendir(DIR, $output_dir);
-  closedir(DIR);
-  my $outf  = "$GB_OUTPUT_DIR/$runnable.jobs.dat";
-
-  open(OUTF, ">$outf") or die "Can't open outfile $outf $!\n";
-
-  # generate bsubs, one per protein
-  open(PM, "<$pm_out") or die "Can't open pmoutfile $pm_out\n";
-  my $tracker = 0;
-  my $resdir = $output_dir . "/jobs0";
-  
-  while(<PM>){
-    # do we need to start another output directory? Limit the files in each so we can parse them easily
-    if($tracker%100 == 0){
-      $resdir = $output_dir . "/jobs" . $tracker/100;
-      #print STDERR "$resdir\n";
-      system("mkdir $resdir") unless opendir(DIR, $resdir);
-      closedir(DIR);   
-    }
-    $tracker++;
-    # exact format of input_id varies depending on which runnable we're using ...
-    chomp;
-    my $input_id;
-    if($runnable eq "TargettedGeneWise"){
-      $input_id = $_;
-    }
-    else {
-      #  ctg15907:34857075,34858997:NP_005336.2:1,641
-      my @cols = split /:/;
-      $input_id =  $cols[0] . ":" . $cols[1] . ":" . $cols[2] . ":";
-    }
-
-    my $outfile  = $resdir . "/$input_id.out";
-    my $errfile  = $resdir . "/$input_id.err";
-    my $command = "bsub -q $queue -C0 -o $outfile -e $errfile -E \"$runner -check -runnable Bio::EnsEMBL::Pipeline::RunnableDB::$runnable -analysis $analysis_logic_name\"";
-
-    $command .= "  $runner ";
-    $command .= " -runnable Bio::EnsEMBL::Pipeline::RunnableDB::$runnable -analysis $analysis_logic_name";
-    $command .= " -input_id $input_id -write";      
-    print OUTF "$command\n";
-    
-  }
-
-  close PM;
-  close OUTF;
-  
-}
-
 
 sub make_lbsubs {
   my ($runnable,$analysis_logic_name) = @_;
