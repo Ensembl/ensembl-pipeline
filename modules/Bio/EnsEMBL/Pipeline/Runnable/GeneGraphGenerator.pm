bb4a6e71dc2f18ed2185d69ee818195ddc056ede
Eduardo Eyras <eae@sanger.ac.uk>
2003-01-23 17:53:00 +0000

    Even though we cannot create non-trivial connected sub-graphs we want to accept at least one of them, but which one? So far, the one with the best score(coverage)/percentage_identity. Every gene counts

--- modules/Bio/EnsEMBL/Pipeline/Runnable/GeneGraphGenerator.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/GeneGraphGenerator.pm
@@ -70,9 +70,9 @@ sub new{
 
 
 ############################################################
-# this function takes est_transcripts that have been clustered together
-# but not with any ensembl transcript and tries to figure out whether they
-# make an acceptable set of alt-forms
+# this function takes transcripts 
+# and tries to figure out whether they
+# make an acceptable set of isoforms
 
 sub _check_est_Cluster{
   my ($self,@est_transcripts) = @_;
@@ -98,53 +98,82 @@ sub _check_est_Cluster{
       # we only check on coincident exon:
       if ( $self->_check_exact_intron_Match( $est_transcripts[$i], $est_transcripts[$j]) 
 	   && $self->_check_exact_exon_Match( $est_transcripts[$i], $est_transcripts[$j]) 
-	 ){
-	print STDERR "they are linked\n";
-	push ( @{ $adj{$est_transcripts[$i]} } , $est_transcripts[$j] );
-	unless ( defined $seen{ $est_transcripts[$i] } &&  $seen{ $est_transcripts[$i] } ){
-	  push ( @linked, $est_transcripts[$i] );
-	  $seen{ $est_transcripts[$i] } =1 ;
-	}
-	unless ( defined $seen{ $est_transcripts[$j] } &&  $seen{ $est_transcripts[$j] } ){
-	  push ( @linked, $est_transcripts[$j] );
-	  $seen{ $est_transcripts[$j] } = 1;
-	}
-      }
+	   ){
+	  print STDERR "they are linked\n";
+	  push ( @{ $adj{$est_transcripts[$i]} } , $est_transcripts[$j] );
+	  unless ( defined $seen{ $est_transcripts[$i] } &&  $seen{ $est_transcripts[$i] } ){
+	      push ( @linked, $est_transcripts[$i] );
+	      $seen{ $est_transcripts[$i] } =1 ;
+	  }
+	  unless ( defined $seen{ $est_transcripts[$j] } &&  $seen{ $est_transcripts[$j] } ){
+	      push ( @linked, $est_transcripts[$j] );
+	      $seen{ $est_transcripts[$j] } = 1;
+	   }
+       }
     }
   }
   
-  print STDERR scalar(@linked). " linked transcripts\n";
-  
-  print STDERR "adjacency lists:\n";
-  foreach my $tran (@linked){
-    print STDERR $tran->dbID." -> ";
-    foreach my $link ( @{ $adj{ $tran } } ){
-      print STDERR $link->dbID.",";
-    }
-    print STDERR "\n";
-  }
-  
-  foreach my $tran ( @linked ){
-    $color{$tran} = "white";
-  }
-  
-  my @potential_genes;
-  
-  # find the connected components doing a depth-first search
-  foreach my $tran ( @linked ){
-    if ( $color{$tran} eq 'white' ){
-      my @potential_gene;
-      $self->_visit( $tran, \%color, \%adj, \@potential_gene);
-      push ( @potential_genes, \@potential_gene );
-    }
-  }
-  print STDERR scalar(@potential_genes)." potential genes created\n";
   my @accepted_transcripts;
-  foreach my $gene (@potential_genes){
-    push ( @accepted_transcripts, @$gene );
+  ############################################################
+  # find the connected components doing a depth-first search
+  # only with the transcripts that have been linked
+  ############################################################
+  if ( @linked ){
+      print STDERR scalar(@linked). " linked transcripts\n";
+      
+      print STDERR "adjacency lists:\n";
+      foreach my $tran (@linked){
+	  print STDERR $tran->dbID." -> ";
+	  foreach my $link ( @{ $adj{ $tran } } ){
+	      print STDERR $link->dbID.",";
+	  }
+	  print STDERR "\n";
+      }
+      
+      foreach my $tran ( @linked ){
+	  $color{$tran} = "white";
+      }
+      
+      my @potential_genes;
+      
+      
+      foreach my $tran ( @linked ){
+	  if ( $color{$tran} eq 'white' ){
+	      my @potential_gene;
+	      $self->_visit( $tran, \%color, \%adj, \@potential_gene);
+	      push ( @potential_genes, \@potential_gene );
+	  }
+      }
+      print STDERR scalar(@potential_genes)." potential genes created\n";
+      my @accepted_transcripts;
+      
+      # we accept 
+      foreach my $gene (@potential_genes){
+	  push ( @accepted_transcripts, @$gene );
+      }
   }
+  elsif ( @est_transcripts ){
+      ############################################################
+      # if there are no linked transcripts, but we have some transcripts to work with,
+      # it is worthwhile to try to keep at least one, every gene counts
+      ############################################################
+      
+      print STDERR "no linked est-transcripts, taking the one with the best score/perc_id\n";
+      my @sorted_transcripts = sort { my $result = ( $self->_get_score($b) <=> 
+						     $self->_get_score($a) );
+				      unless($result){
+					  return ( $self->_get_precent_id($b) <=>
+						   $self->_get_percent_id($a) );
+				      }
+				      return $result;
+				  } @est_transcripts;
+      
+      push( @accepted_transcripts, shift @sorted_transcripts );
+  }
+  
   print STDERR "returning ".scalar( @accepted_transcripts)." transcripts\n";
   return @accepted_transcripts;
+      
 }
 
 #########################################################################
@@ -270,5 +299,40 @@ sub _check_protein_Match{
 }
 
 ############################################################
+
+# we average over the number of exons with supporting evidence
+# we take the largest value from all the evidences in each exon
+
+sub _get_score{
+    my ( $self, $tran ) = @_;
+    my $score = 0;
+    my $count = 0;
+    foreach my $exon ( @{$tran->get_all_Exons} ){
+	$count++;
+	my @evidence = sort { $b->score <=> $a->score } @{$exon->get_all_supporting_features};
+	$score += $evidence[0];
+    }
+    my $final_score = $score/$count;
+    return $final_score;
+}
+
+############################################################
+
+# we average over the number of exons with supporting evidence
+# we take the largest value from all the evidences in each exon
+
+sub _get_percent_id{
+    my ( $self, $tran ) = @_;
+    my $percent_id = 0;
+    my $count = 0;
+    foreach my $exon ( @{$tran->get_all_Exons} ){
+	$count++;
+	my @evidence = sort { $b->percent_id <=> $a->percent_id } @{$exon->get_all_supporting_features};
+	$percent_id += $evidence[0];
+    }
+    my $final_percent_id = $percent_id/$count;
+    return $final_percent_id;
+}
+
 
 1;
