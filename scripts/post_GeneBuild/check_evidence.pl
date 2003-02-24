80ceccdea91717d966b59397714d4610775bac21
Eduardo Eyras <eae@sanger.ac.uk>
2003-02-24 13:39:42 +0000

    now also reads stable_id as input

--- scripts/post_GeneBuild/check_evidence.pl
+++ scripts/post_GeneBuild/check_evidence.pl
@@ -41,10 +41,9 @@ my $t_id;
 &GetOptions(
 	    'tstable_id:s' => \$tstable_id,
 	    't_id:s' => \$t_id,
+	    'tstable_id:s' => \$tstable_id,
 	    'dbhost:s'        => \$dbhost,
 	    'dbname:s'        => \$dbname,
-	    'dnadbhost:s'     => \$dnadbhost,
-	    'dnadbname:s'     => \$dnadbname,
 	    'genetype:s'      => \$genetype,
             'path:s'          => \$path,
 );
@@ -52,25 +51,15 @@ my $t_id;
 unless ( $t_id || $tstable_id){
   print STDERR "script to print out all the evidence info from a transcript. useful for sanity checks.\n";
  
-  print STDERR "Usage: $0 [-dbname -dbhost -dnadbname -dnadbhost -genetype] -t_id -tstable_id\n";
+  print STDERR "Usage: $0 -dbname -dbhost [ -genetype] -t_id -tstable_id\n";
   exit(0);
 }
-
-
-my $dnadb = new Bio::EnsEMBL::DBSQL::DBAdaptor(
-					       '-host'   => $dnadbhost,
-					       '-user'   => $dnadbuser,
-					       '-dbname' => $dnadbname,
-					       '-pass'   => $dnadbpass,
-					      );
-
 
 my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
 					    '-host'   => $dbhost,
 					    '-user'   => $dbuser,
 					    '-dbname' => $dbname,
 					    '-pass'   => $dbpass,
-					    '-dnadb'  => $dnadb,
 					   );
 
 
@@ -89,21 +78,6 @@ my $seqio = Bio::SeqIO->new('-format' => 'Fasta' , -fh => \*STDOUT ) ;
 
 
 if ( $t_id){
-
-  print "\nFrom database: $t_id\n";
-  &check_transcript_without_translation($db,$t_id);
-  print "\n";
-
-
-  #my $slice = $sa->fetch_by_transcript_id($t_id);
-  
-
-  #my @new_exons;
-  #foreach my $exon ( @exons ){
-  #  my $new_exon = $exon->transform($slice);
-  #  push (@new_exons, $exon);
-  #}
-  #@new_exons = sort { $a->start <=> $b->start } @new_exons;
 
   print "From TranscriptAdaptor: $t_id\n";
   my $tran = $db->get_TranscriptAdaptor->fetch_by_dbID($t_id);
@@ -156,6 +130,45 @@ if ( $t_id){
 #  }
 
 
+}
+elsif ( $tstable_id){
+  #print "\nFrom database: $tstable_id\n";
+  #&check_transcript_without_translation_from_stable_id($db,$tstable_id);
+  #print "\n";
+
+  print "From TranscriptAdaptor: $t_id\n";
+  my $tran = $db->get_TranscriptAdaptor->fetch_by_stable_id($tstable_id);
+  my @exons = @{$tran->get_all_Exons};
+  print "contig_id\tcontig_name\texon_id\tstart\tend\tphase\tend_phase\strand\tlength\n";
+  foreach my $exon (@exons){
+    
+    # if exon is sticky print each component
+    if ( $exon->isa('Bio::EnsEMBL::StickyExon') ){
+      
+      foreach my $exon_c ( @{$exon->get_all_component_Exons} ){
+	my $length = $exon_c->end - $exon_c->start +1;
+	print $exon_c->contig->dbID."\t".$exon_c->contig->name."\t".
+	  $exon_c->dbID."\t".$exon_c->start."\t".$exon_c->end."\t".$exon_c->phase."\t".
+	    $exon_c->end_phase."\t".$exon_c->strand."\t".$length."\n";
+	&print_evidence($exon_c);
+	print "\n";
+      }
+    }
+    else{
+      my $length = $exon->end - $exon->start +1;
+      print $exon->contig->dbID."\t".$exon->contig->name."\t".
+	$exon->dbID."\t".$exon->start."\t".$exon->end."\t".$exon->phase."\t".
+	  $exon->end_phase."\t".$exon->strand."\t".$length."\n";
+      &print_evidence($exon);
+      print "\n";
+    }
+  }
+
+
+
+}
+else{
+  print STDERR "No id entered\n";
 }
   
 ############################################################
@@ -246,6 +259,50 @@ sub check_transcript_without_translation{
     print STDERR "$exon_id\t$start\t$end\t$phase\t$end_phase\t$strand\t$chr\t$length\t$rank\t$transl\t$sticky\n";
   }
 }
+
+############################################################
+
+sub check_transcript_without_translation_from_stable_id{
+  my $db = shift;
+  my $stable_id = shift;
+
+  
+  my $q = qq( SELECT es.stable_id, 
+	      if(ass.contig_ori=1,(e.contig_start-ass.contig_start+ass.chr_start), 
+		 (ass.chr_start+ass.contig_end-e.contig_end)) as start, 
+	      if(ass.contig_ori=1,(e.contig_end-ass.contig_start+ass.chr_start), 
+		 (ass.chr_start+ass.contig_end-e.contig_start)) as end, 
+	      if (ass.contig_ori=1,e.contig_strand,(-e.contig_strand)) as strand,
+	      c.name,        
+	      abs(e.contig_end-e.contig_start)+1 as length,
+	      e.phase,
+	      e.end_phase,
+	      et.rank, 
+	      if(e.sticky_rank>1,(concat("sticky (rank = ",e.sticky_rank,")")),
+		 ("")) as sticky
+	      FROM  exon e, transcript tr, exon_transcript et, 
+	      assembly ass, chromosome c, transcript_stable_id ts, exon_stable_id es
+	      WHERE e.exon_id        = et.exon_id       AND 
+	            es.exon_id       = e.exon_id        AND
+	            et.transcript_id = tr.transcript_id AND
+	            ts.transcript_id = tr.transcript_id AND
+	            ts.stable_id     = "$stable_id"     AND
+	            ass.chromosome_id= c.chromosome_id  AND                              
+	            ass.contig_id    = e.contig_id      AND 
+	            ass.type         = "$path"          
+	      ORDER BY et.rank
+	    );
+
+  #print STDERR $q."\n";
+  my $sth = $db->prepare($q) || $db->throw("can't prepare: $q");
+  my $res = $sth->execute || $db->throw("can't execute: $q");
+  
+  print STDERR "exon_id\tstart\tend\tphase\tend_phase\tstrand\tchr\tlength\trank\tsticky\n";
+  while( my ($exon_id,$start,$end,$strand,$chr,$length,$phase,$end_phase,$rank,$transl,$sticky) = $sth->fetchrow_array) {
+    print STDERR "$exon_id\t$start\t$end\t$phase\t$end_phase\t$strand\t$chr\t$length\t$rank\t$transl\t$sticky\n";
+  }
+}
+
 
 ############################################################
 
