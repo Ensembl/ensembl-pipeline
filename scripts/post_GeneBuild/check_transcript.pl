b6e93cac0acfaac43d8b8b6e420bbf7af6ac86a0
Eduardo Eyras <eae@sanger.ac.uk>
2003-02-24 13:27:23 +0000

    can read also stable_ids

--- scripts/post_GeneBuild/check_transcript.pl
+++ scripts/post_GeneBuild/check_transcript.pl
@@ -1,4 +1,4 @@
-#!/usr/local/ensembl/bin/perl
+#!/usr/local/ensembl/bin/perl -w
 
 use strict;
 
@@ -13,24 +13,8 @@ my $dbuser = 'ensro';
 my $dbname = 'homo_sapiens_core_9_30';
 my $dbpass = undef;
 
-my $dnadbhost = 'ecs2d';
-my $dnadbuser = 'ensro';
-my $dnadbname = 'homo_sapiens_core_9_30';
-my $dnadbpass = undef;
-
-
-my $path = 'NCBI_30';
 my $genetype = 'ensembl';
-
-#my $dbhost = ecs2d;
-#my $dbuser = 'ensro';
-#my $dbname;
-#my $dbpass    = undef;
-
-#my $dnadbhost;
-#my $dnadbuser = 'ensro';
-#my $dnadbname;
-#my $dnadbpass = undef;
+my $path;
 
 my $id_file;
 my $gstable_id;
@@ -43,8 +27,6 @@ my $t_id;
 	    't_id:s' => \$t_id,
 	    'dbhost:s'        => \$dbhost,
 	    'dbname:s'        => \$dbname,
-	    'dnadbhost:s'     => \$dnadbhost,
-	    'dnadbname:s'     => \$dnadbname,
 	    'genetype:s'      => \$genetype,
             'path:s'          => \$path,
 );
@@ -52,17 +34,10 @@ my $t_id;
 unless ( $t_id || $tstable_id){
   print STDERR "script to print out all the info from a transcript. useful for sanity checks.\n";
  
-  print STDERR "Usage: $0 [-dbname -dbhost -dnadbname -dnadbhost -genetype] -t_id -tstable_id\n";
+  print STDERR "Usage: $0 -dbname -dbhost [-genetype -path] -t_id -tstable_id\n";
   exit(0);
 }
 
-
-my $dnadb = new Bio::EnsEMBL::DBSQL::DBAdaptor(
-					       '-host'   => $dnadbhost,
-					       '-user'   => $dnadbuser,
-					       '-dbname' => $dnadbname,
-					       '-pass'   => $dnadbpass,
-					      );
 
 
 my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
@@ -70,18 +45,16 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
 					    '-user'   => $dbuser,
 					    '-dbname' => $dbname,
 					    '-pass'   => $dbpass,
-					    '-dnadb'  => $dnadb,
 					   );
 
 
 
 print STDERR "connected to $dbname : $dbhost\n";
-my $path = $db->assembly_type;
 
-#my $sa = $db->get_SliceAdaptor();
-
-
-#open (OUT,">$file") or die("unable to open file $file");
+unless( $path ){
+  $path = $db->assembly_type;
+}
+print STDERR "path = $path\n";
 
 my $seqio = Bio::SeqIO->new('-format' => 'Fasta' , -fh => \*STDOUT ) ;
 
@@ -91,87 +64,38 @@ if ( $t_id){
   print STDERR "\nFrom database: $t_id\n";
   &check_transcript($db,$t_id);
   print STDERR "\n";
-
-
-  #my $slice = $sa->fetch_by_transcript_id($t_id);
   
-
-  #my @new_exons;
-  #foreach my $exon ( @exons ){
-  #  my $new_exon = $exon->transform($slice);
-  #  push (@new_exons, $exon);
-  #}
-  #@new_exons = sort { $a->start <=> $b->start } @new_exons;
-
   print STDERR "From TranscriptAdaptor: $t_id\n";
   my $tran = $db->get_TranscriptAdaptor->fetch_by_dbID($t_id);
   my @exons = @{$tran->get_all_Exons};
-  print "contig_id\tcontig_name\texon_id\tstart\tend\tphase\tend_phase\strand\tlength\n";
+  print "contig_id\tcontig_name\texon_id\tstart\tend\tphase\tend_phase\tstrand\tlength\n";
   foreach my $exon (@exons){
-    my $length = $exon->end - $exon->start +1;
-    print $exon->contig->dbID."\t".$exon->contig->name."\t".
-      $exon->dbID."\t".$exon->start."\t".$exon->end."\t".$exon->phase."\t".
-	$exon->end_phase."\t".$exon->strand."\t".$length."\n";
-  }
-  
- # print STDERR "Transcript from slice:\n";
-#  my @genes = $slice->get_all_Genes_by_Type($genetype,'evidence');
-#  my $this_transcript;
-# GENE:
-#  foreach my $gene (@genes){
-#    foreach my $transcript ( $gene->each_Transcript ){
-#      if ( $transcript->dbID == $t_id ){
-#	$this_transcript = $transcript;
-#	last GENE;
-#      }
-#    }
-#  }
-#  unless ($this_transcript){
-#    print STDERR "cannot find transcript $t_id\n";
-#    exit(0);
-#  }
-#  my @exons = $this_transcript->get_all_Exons;
-#  @exons = sort { $a->start <=> $b->start } @exons;
-#  foreach my $exon (@exons){
-#    print "Exon ".$exon->start."-".$exon->end." phase: ".$exon->phase." end_phase: ".$exon->end_phase." strand: ".$exon->strand."\n";
-#  }
-
-
-}
-
-
-sub translate_from_gene_stable_id{
-  my $g_id = shift;
-  eval {
-    my $gene = $db->get_GeneAdaptor->fetch_by_dbID($g_id);
-    print STDERR "gene id $g_id, type: ".$gene->type."\n";
-    foreach my $trans ( $gene->each_Transcript ) {
-      # get out first exon. Tag it to clone and gene on this basis
-           
-      print STDERR "trying to get ->translate from transcript ".$trans->dbID."\n";
-      my $tseq = $trans->translate();
-
-      print STDERR "trying to get ->translation from transcript ".$trans->dbID."\n";
-      my $translation = $trans->translation();
-
-      #print STDERR "translation->translate is a : $tseq\n";
-      #print STDERR "translation: ".$tseq->seq()."\n";
-      if ( $tseq->seq =~ /\*/ ) {
-	print STDERR "translation of transcript: ".$trans->dbID." in chr 20 has stop codons. Skipping! (in clone)\n";
+    if ( $exon->isa('Bio::EnsEMBL::StickyExon') ){
+      foreach my $exon_c ( @{$exon->get_all_component_Exons} ){
+	my $length = $exon_c->end - $exon_c->start +1;
+	print $exon_c->contig->dbID."\t".$exon_c->contig->name."\t".
+	  $exon_c->dbID."\t".$exon_c->start."\t".$exon_c->end."\t".$exon_c->phase."\t".
+	    $exon_c->end_phase."\t".$exon_c->strand."\t".$length."\n";
       }
-      $tseq->desc("Gene:$g_id transcript: ".$trans->dbID);
-      my $result = $seqio->write_seq($tseq);
     }
-  };
-  
-   
-  if( $@ ) {
-    print STDERR "unable to process $g_id, due to \n$@\n";
+    else{
+      my $length = $exon->end - $exon->start +1;
+      print $exon->contig->dbID."\t".$exon->contig->name."\t".
+	$exon->dbID."\t".$exon->start."\t".$exon->end."\t".$exon->phase."\t".
+	  $exon->end_phase."\t".$exon->strand."\t".$length."\n";
+    }
   }
 }
+elsif( $tstable_id ){
+  
+  print STDERR "\nFrom database: $tstable_id\n";
+  &check_transcript_from_stable_id($db,$tstable_id);
+  print STDERR "\n";
+}
+else{
+  print STDERR "No id entered\n";
+}
 
-
-close (OUT);
 
 
 sub check_transcript{
@@ -184,7 +108,7 @@ sub check_transcript{
 		 (ass.chr_start+ass.contig_end-e.contig_end)) as start, 
 	      if(ass.contig_ori=1,(e.contig_end-ass.contig_start+ass.chr_start), 
 		 (ass.chr_start+ass.contig_end-e.contig_start)) as end, 
-	       if (ass.contig_ori=1,e.contig_strand,(-e.contig_strand)) as strand,
+	      if (ass.contig_ori=1,e.contig_strand,(-e.contig_strand)) as strand,
 	      c.name,        
 	      abs(e.contig_end-e.contig_start)+1 as length,
 	      e.phase,
@@ -206,6 +130,53 @@ sub check_transcript{
 	      ass.type = "$path" AND
 	      tr.transcript_id = $t_id AND  
 	      tr.translation_id=tl.translation_id
+	      ORDER BY et.rank
+	    );
+
+  my $sth = $db->prepare($q) || $db->throw("can't prepare: $q");
+  my $res = $sth->execute || $db->throw("can't execute: $q");
+  
+  print STDERR "exon_id\tstart\tend\tphase\tend_phase\tstrand\tchr\tlength\trank\ttransl\tsticky\n";
+  while( my ($exon_id,$start,$end,$strand,$chr,$length,$phase,$end_phase,$rank,$transl,$sticky) = $sth->fetchrow_array) {
+    print STDERR "$exon_id\t$start\t$end\t$phase\t$end_phase\t$strand\t$chr\t$length\t$rank\t$transl\t$sticky\n";
+  }
+}
+  
+
+sub check_transcript_from_stable_id{
+  my $db = shift;
+  my $stable_id = shift;
+
+  
+  my $q = qq( SELECT es.stable_id, 
+	      if(ass.contig_ori=1,(e.contig_start-ass.contig_start+ass.chr_start), 
+		 (ass.chr_start+ass.contig_end-e.contig_end)) as start, 
+	      if(ass.contig_ori=1,(e.contig_end-ass.contig_start+ass.chr_start), 
+		 (ass.chr_start+ass.contig_end-e.contig_start)) as end, 
+	      if (ass.contig_ori=1,e.contig_strand,(-e.contig_strand)) as strand,
+	      c.name,        
+	      abs(e.contig_end-e.contig_start)+1 as length,
+	      e.phase,
+	      e.end_phase,
+	      et.rank, 
+	      if(e.exon_id=tl.start_exon_id,
+		 (concat(tl.seq_start," (start)",
+			 if(e.exon_id=tl.end_exon_id,(concat(" ",tl.seq_end," (end)")),("")))),
+		 if (e.exon_id=tl.end_exon_id,(concat(tl.seq_end," (end)")),("")))   
+	      as transcoord,
+	      if(e.sticky_rank>1,(concat("sticky (rank = ",e.sticky_rank,")")),
+		 ("")) as sticky
+	      FROM  translation tl, exon e, transcript tr, exon_transcript et, 
+	      assembly ass, chromosome c, transcript_stable_id ts, exon_stable_id es
+	      WHERE e.exon_id         = et.exon_id       AND 
+	            es.exon_id        = e.exon_id        AND
+	            et.transcript_id  = tr.transcript_id AND 
+	            ts.transcript_id  = tr.transcript_id AND
+	            ts.stable_id      = "$stable_id"     AND
+	            ass.chromosome_id = c.chromosome_id  AND                              
+	            ass.contig_id     = e.contig_id      AND 
+	            ass.type          = "$path"          AND
+	            tr.translation_id = tl.translation_id
 	      ORDER BY et.rank
 	    );
 
