82d6860e6e84ed11bca1af523b1c2def0a77e996
Laura Clarke <lec@sanger.ac.uk>
2003-04-04 08:23:03 +0000

    additions to allow pseudogenes to be stored

---
+++ scripts/DataConversion/wormbase/pseudo_store.pl
@@ -0,0 +1,33 @@
+#!/usr/local/ensembl/bin/perl  -w
+
+use strict;
+use WormBase;
+use WormBaseConf;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+
+
+$| = 1;
+
+my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(-host => $WB_DBHOST,
+					    -user => $WB_DBUSER,
+					    -dbname => $WB_DBNAME,
+					    -pass  => $WB_DBPASS,
+					   );
+my $analysis_adaptor = $db->get_AnalysisAdaptor;
+my $analysis = $analysis_adaptor->fetch_by_logic_name($WB_PSEUDO_LOGIC_NAME);
+
+foreach my $chromosome_info(@{$WB_CHR_INFO}) {
+
+  print "handling ".$chromosome_info->{'chr_name'}." with files ".$chromosome_info->{'agp_file'}." and ".$chromosome_info->{'gff_file'}."\n" if($WB_DEBUG);
+  
+
+ my $chr = $db->get_SliceAdaptor->fetch_by_chr_start_end($chromosome_info->{'chr_name'}, 1, ($chromosome_info->{'length'}+1));
+
+  my $genes = &parse_pseudo_gff($chromosome_info->{'gff_file'}, $chr, $analysis);
+  my $non_transforming =  &write_genes($genes, $db, 1);
+
+  my $slice = $db->get_SliceAdaptor->fetch_by_chr_start_end($chromosome_info->{chr_name}, 1, ($chromosome_info->{length} - 1));
+
+  my @genes = @{$slice->get_all_Genes_by_type($analysis->logic_name)};
+  print STDERR "have ".@genes." pseudogenes\n";
+}
