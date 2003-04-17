5e8a40476e27f659edf6df41d3be16d79e5e8adb
Dan T Andrews <dta@sanger.ac.uk>
2003-04-17 13:52:19 +0000

    Added method to allow batch retrieval of sequences.

--- modules/Bio/EnsEMBL/Pipeline/SeqFetcher/Pfetch.pm
+++ modules/Bio/EnsEMBL/Pipeline/SeqFetcher/Pfetch.pm
@@ -159,4 +159,89 @@ sub  get_Seq_by_acc {
   return $seq;
 }
 
+
+=head2 batch_fetch
+
+  Title   : batch_fetch
+  Usage   : $self->batch_retrieval(@accession_list);
+  Function: Retrieves multiple sequences via pfetch
+  Returns : reference to a list of Bio::Seq objects
+  Args    : array of accession strings
+
+=cut
+
+sub  batch_fetch {
+  my $self = shift @_;
+
+  my @sequence_list;
+  
+  unless (scalar @_) {
+    $self->throw("No accession input");
+  }  
+
+  my $accession_concatenation;
+  my @accession_list;
+
+  while (my $acc = shift @_) {
+    push (@accession_list, $acc);
+    $accession_concatenation .= $acc . ' ';
+  }
+  
+  my $pfetch = $self->executable;
+  my $options = $self->options;
+  if (defined($options)) { $options = '-' . $options  unless $options =~ /^-/; }
+
+  my $command = "$pfetch -q ";
+  if (defined $options){
+    $command .= "$options ";
+  }
+
+  $command .= $accession_concatenation;
+
+  print STDERR "$command\n";
+
+  open(IN,"$command |") or $self->throw("Error opening pipe to pfetch : $pfetch");
+
+  my $seq_placemarker = -1;
+
+ SEQ:
+  while (my $seqstr = <IN>) {
+    
+    $seq_placemarker++;
+
+    chomp($seqstr);
+    
+    my $seq;
+    
+    unless (defined $seqstr && $seqstr eq 'no match') {
+
+      eval{
+	if(defined $seqstr && $seqstr ne "no match") {
+	  $seq = new Bio::Seq('-seq'               => $seqstr,
+			      '-accession_number'  => $accession_list[$seq_placemarker],
+			      '-display_id'        => $accession_list[$seq_placemarker]);
+	}
+      };
+      
+      if($@){
+	print STDERR "$@\n";
+      }
+    }
+
+    unless (defined $seq){
+      $self->warn("PFetch Error : Could not pfetch sequence for " . 
+		  $accession_list[$seq_placemarker] . "\n");
+      next SEQ;
+    }
+
+    push (@sequence_list, $seq);
+  }
+  
+  close IN or $self->throw("Error running pfetch : $pfetch");
+  
+  return \@sequence_list;
+}
+
+
+
 1;
