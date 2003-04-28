593bcc06f5de479d036054377b3a568eb7346baa
Glenn Proctor <gp1@sanger.ac.uk>
2003-04-28 13:29:10 +0000

    First implementation

---
+++ modules/Bio/EnsEMBL/Pipeline/StartPipeline.pl
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+use Bio::EnsEMBL::Pipeline::Config;
+use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
+
+# set a sgnal handler 
+$SIG{INT} = \&_shut_down;
+
+# Parse command line
+my @files  = ();
+# should there be defauts for these?
+my $host = '';
+my $user = '';
+my $pass = '';
+my $port = '';
+my $dbname = '';
+GetOptions ('file=s'   => \@files,
+	    'host=s'   => \$host,
+	    'port=s'   => \$port,
+	    'user=s'   => \$user,
+	    'pass=s'   => \$pass,
+	    'dbname=s' => \$dbname);
+
+# create config with file or db
+my $config;
+
+if (@files > 0) {
+
+  $config = new Bio::EnsEMBL::Pipeline::Config(-FILES => @files);
+
+} else {
+
+  # create DBAdaptor, then Config
+  my $dbobj = new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor('-host'   => $host,
+							   '-port'   => $port,
+							   '-dbname' => $dbname,
+							   '-user'   => $user,
+							   '-pass'   => $pass);
+
+  $config = new Bio::EnsEMBL::Pipeline::Config(-DB => $dbobj);
+
+}
+
+# instantiate PipelineManager
+my $pipeline_manager = new Bio::EnsEMBL::Pipeline::PipelineManager($config);
+
+# call run
+$pipeline_manager->run();
+
+# Called when signal is recieved
+sub _shut_down {
+
+  $pipeline_manager->{'stop'};
+
+}
