6c44a178c8bd6edce5b2ef3164af967eefb564bd
Graham McVicker <mcvicker@sanger.ac.uk>
2003-05-15 15:37:22 +0000

    forked jobs get their own database connection; less chatty

--- modules/Bio/EnsEMBL/Pipeline/SubmissionSystem/Local.pm
+++ modules/Bio/EnsEMBL/Pipeline/SubmissionSystem/Local.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Bio::EnsEMBL::Pipeline::SubmissionSystem;
 use Bio::EnsEMBL::Pipeline::Job;
+use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
 use POSIX;
 
 @ISA = qw(Bio::EnsEMBL::Pipeline::SubmissionSystem);
@@ -54,7 +55,7 @@ sub submit {
 
   my $self = shift;
   my $job  = shift;
-  print STDERR "have ".$self." and job ".$job."\n";
+
   # Check params
   unless(ref($job) && $job->isa('Bio::EnsEMBL::Pipeline::Job')) {
     $self->throw('expected Bio::EnsEMBL::Pipeline::Job argument');
@@ -62,8 +63,6 @@ sub submit {
 
   my $config = $self->get_Config();
   my $max_jobs = $config->get_parameter("LOCAL", "maxjobs"); # default???
-
-  print "submit: max_jobs = $max_jobs\n";
 
   #put the just received job onto the end of the queue
   push @{$self->{'_queue'}}, $job;
@@ -81,7 +80,7 @@ sub submit {
 
 sub sig_chld {
   $children--;
-  print "Child died; children now $children\n";
+  print "Child finished; children now $children\n";
 }
 
 =head2 create_job
@@ -185,7 +184,8 @@ sub _generate_filename_prefix {
   # get temp dir from config
   my $config = $self->get_Config();
   my $temp_dir = $config->get_parameter('LOCAL', 'output_dir');
-  $temp_dir || $self->throw('Could not determine output dir for job ' . $job->taskname() . ' ID ' . $job->dbID() . '\n');
+  $temp_dir || $self->throw('Could not determine output dir for job ' . 
+			    $job->taskname() . ' ID ' . $job->dbID() . '\n');
 
   # have a subdirectory for each type of task
   $temp_dir .= "/" .$job->taskname;
@@ -221,9 +221,14 @@ sub _start_job {
   my $self = shift;
   my $job = shift;
 
-  print "in _start_job\n";
+  my $db = $job->adaptor()->db();
+  my $dbname = $db->dbname();
+  my $host   = $db->host();
+  my $pass   = $db->password();
+  my $user   = $db->username();
+  my $port   = $db->port();
 
-  if (my $pid = fork) {		# fork returns PID of child to parent, 0 to child
+  if (my $pid = fork) {	 # fork returns PID of child to parent, 0 to child
     # PARENT
     $children++;
     print "Size of children is now " . $children . "; " . 
@@ -232,21 +237,36 @@ sub _start_job {
   } else {
     #CHILD
 
-    my $file_prefix = $self->_generate_filename_prefix($job);
+    #The child needs its own connection to the database. We don't want the
+    #parent's database handle cleaned up when the child exits and vice-versa
 
+    $db = Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor->new
+      (-dbname   => $dbname,
+       -host     => $host,
+       -user     => $user,
+       -pass     => $pass,
+       -port     => $port);
+
+    #re-retrive the job so it is using the new dbhandle
+
+    $job = $db->get_JobAdaptor->fetch_by_dbID($job->dbID);
+
+    my $file_prefix = $self->_generate_filename_prefix($job);
 
     $job->stdout_file("${file_prefix}.out");
     $job->stderr_file("${file_prefix}.err");
 
-    POSIX::setsid();		# make session leader, and effectively a daemon
+    POSIX::setsid();  # make session leader, and effectively a daemon
 
     # redirect stdout/stderr to files
     # read stdin from /dev/null
     close(STDERR);
     close(STDOUT);
     close(STDIN);
-    open(STDERR, "+>" . $job->stderr_file()) || warn "Error redirecting STDERR to " .  $job->stderr_file();
-    open(STDOUT, "+>" . $job->stdout_file()) || warn "Error redirecting STDOUT to " .  $job->stdout_file();
+    open(STDERR, "+>" . $job->stderr_file()) 
+      || warn "Error redirecting STDERR to " .  $job->stderr_file();
+    open(STDOUT, "+>" . $job->stdout_file()) 
+      || warn "Error redirecting STDOUT to " .  $job->stdout_file();
     open(STDIN,  "+>/dev/null");
 
     #print "Executing $job with PID $$\n";
