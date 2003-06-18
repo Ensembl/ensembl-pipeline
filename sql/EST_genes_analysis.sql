10ed09993bc710f074e1e18368a97496ab0ac72c
Paul John Gane <pjg@sanger.ac.uk>
2003-06-18 14:18:30 +0000

    Updated to use ExonerateToGenes

--- sql/EST_genes_analysis.sql
+++ sql/EST_genes_analysis.sql
@@ -3,5 +3,5 @@
 #
 
 INSERT INTO analysis VALUES (\N,now(),'genomewise','NULL','1','NULL','genomewise','1','NULL','NULL','EST_GeneBuilder','        NULL','genomewise','gene');
-INSERT INTO analysis VALUES (\N,'2002-02-28 00:00:00','exonerate_e2g','dbEST','1','NULL','exonerate_e2g','1','NULL','NULL','FilterESTs_and_E2G','NULL','exonerate_e2g','similarity');
+INSERT INTO analysis VALUES (\N,'2002-02-28 00:00:00','exonerate','HGMP','1','NULL','exonerate','1','NULL','NULL','ExonerateToGenes','NULL','exonerate','similarity');
 
