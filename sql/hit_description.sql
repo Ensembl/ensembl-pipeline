88cfa68d46b2bdc1a8c0f3f547f019a4409dfff8
Roy Storey <rds@sanger.ac.uk>
2003-06-20 15:43:26 +0000

    sql for hit_description table

---
+++ sql/hit_description.sql
@@ -0,0 +1,19 @@
+# Table structure for table 'hit_description'
+# Table for the Finished Analysis Runnables to store hit information in.
+# hit_name         joins to hit_name of dna_align_feature & protein_align_feature tables
+# hit_length       sequence length.
+# hit_description  DE line from EMBL/TrEMBL/Swissprot format file
+# hit_taxon        Taxon ID for sequence
+# hit_db column open to change
+#
+
+CREATE TABLE hit_description (
+  hit_name varchar(20) DEFAULT '' NOT NULL,
+  hit_length int(10) unsigned,
+  hit_description text,
+  hit_taxon int(10) unsigned,
+  hit_db enum('EMBL','Swissprot','TrEMBL') DEFAULT 'EMBL' NOT NULL,
+  PRIMARY KEY (hit_name),
+  KEY hit_db (hit_db)
+);
+
