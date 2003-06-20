abcf7297b5f1c56c42ed4600e8351f194736ded5
Roy Storey <rds@sanger.ac.uk>
2003-06-20 16:15:11 +0000

    *** empty log message ***

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Finished_EST.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Finished_EST.pm
@@ -10,6 +10,7 @@ use vars qw(@ISA);
 use Bio::EnsEMBL::Pipeline::RunnableI;
 use Bio::EnsEMBL::Pipeline::Runnable::Finished_MiniEst2Genome;
 use Bio::EnsEMBL::Pipeline::Runnable::Finished_Blast;
+use Bio::EnsEMBL::Pipeline::Config::Blast;
 use Data::Dumper;
 
 use vars qw(@ISA $verbose);
@@ -19,35 +20,38 @@ $verbose = 0;
 
 sub new {
     my ( $class, @args ) = @_;
-
+    
     #my $self = $class->SUPER::new(@args);
     my $self = bless {}, $class;
     my ( $query, $unmasked, $analysis, $seqfetcher, ) = $self->_rearrange(
         [
-            qw{
+        qw{
             QUERY
             UNMASKED
             ANALYSIS
             SEQFETCHER
-            }
+        }
         ],
         @args
     );
-
+    
     die "No QUERY (masked genomic sequence) given" unless $query;
     die "No UNMASKED (genomic sequence) given"     unless $unmasked;
-
+    
     $self->query($query);
     $self->unmasked($unmasked);
     $self->analysis($analysis);
+    # this should make a OBDAIndexSeqFetcher, but will default to Pfetch
+    my $indices = [ split(",", $self->analysis->db) ];
+    my $seqfetcher = $self->_make_seqfetcher($indices);
     $self->seqfetcher($seqfetcher);
-
+    
     return $self;
 }
 
 sub query {
     my ( $self, $seq ) = @_;
-
+    
     if ($seq) {
         unless ( $seq->isa("Bio::PrimarySeqI") || $seq->isa("Bio::Seq") ) {
             $self->throw("Input isn't a Bio::Seq or Bio::PrimarySeq");
@@ -59,10 +63,10 @@ sub query {
 
 sub unmasked {
     my ( $self, $seq ) = @_;
-
+    
     if ($seq) {
         unless ( $seq->isa("Bio::PrimarySeqI") || $seq->isa("Bio::Seq") ) {
-	    die Dumper($seq);
+            die Dumper($seq);
             $self->throw("Input isn't a Bio::Seq or Bio::PrimarySeq");
         }
         $self->{'_unmasked'} = $seq;
@@ -72,7 +76,7 @@ sub unmasked {
 
 sub seqfetcher {
     my ( $self, $seqfetcher ) = @_;
-
+    
     if ($seqfetcher) {
         $self->{'_seqfetcher'} = $seqfetcher;
     }
@@ -81,7 +85,7 @@ sub seqfetcher {
 
 sub analysis {
     my ( $self, $analysis ) = @_;
-
+    
     if ($analysis) {
         $self->{'_analysis'} = $analysis;
     }
@@ -90,10 +94,10 @@ sub analysis {
 
 sub default_blast_parameters {
     my ( $self, $ana ) = @_;
-
+    
     return (
         '-query'          => $self->query,
-        '-database'       => $ana->db,
+        '-database'       => $ana->db_file,
         '-program'        => $ana->program || 'wublastn',
         '-threshold_type' => 'PVALUE',
         '-threshold'      => 1e-4,
@@ -103,19 +107,21 @@ sub default_blast_parameters {
 
 sub _make_blast_parameters {
     my ($self) = @_;
-
+    
     # Set parameters from analysis object, or use defaults
     my $ana = $self->analysis or $self->throw('analysis not set');
     my %param = $self->default_blast_parameters($ana);
-
+    
     my ($arguments);
+    
     foreach my $ele ( split /\s*,\s*/, $ana->parameters ) {
         if ( my ( $key, $value ) = split /\s*=>\s*/, $ele ) {
             if ( defined $value ) {
-                $param{$key} = $value;
+                #                $param{$key} = $value;
+                print STDERR "Need to use Blast Config to set module parameters\n";
+                print STDERR "The analysis.parameters field [varchar(40)] should only be for program params\n";
             }
             else {
-
                 # remaining arguments not of '=>' form
                 # are simple flags (like -p1)
                 $arguments .= " $key";
@@ -123,48 +129,52 @@ sub _make_blast_parameters {
         }
     }
     $param{'-options'} = $arguments if $arguments;
-#    $param{'-ungapped'} = 1;
-
+    
     return %param;
 }
 
 sub run {
     my ($self) = @_;
-
+    
     my $seq   = $self->query;
-    my $blast =
-      Bio::EnsEMBL::Pipeline::Runnable::Finished_Blast->new( $self->_make_blast_parameters );
-    $blast->run;
+    my $blast = Bio::EnsEMBL::Pipeline::Runnable::Finished_Blast->new( $self->_make_blast_parameters );
+    # run the blast
+    $blast->run();
+    # keep the features
     my $features = [ $blast->output ];
-
-#    print STDERR Dumper($features);
-
+    # set the db_version_searched
+    my $dbv = $blast->db_version_searched;
+    print "using $dbv\n";
+    $self->db_version_searched($dbv);
+    
+    # print STDERR Dumper($features);
+    
     print STDERR "\nPlus strand est_genome\n" if $verbose;
     $self->run_est_genome_on_strand( 1, $features );
-
-    print STDERR "\nMinus strand est_genome\n" if $verbose; 
+    
+    print STDERR "\nMinus strand est_genome\n" if $verbose;
     $self->run_est_genome_on_strand( -1, $features );
-
+    
 }
 
 sub run_est_genome_on_strand {
     my ( $self, $strand, $feat ) = @_;
-
+    
     my $hit_features = {};
     for ( my $i = 0 ; $i < @$feat ; $i++ ) {
         my $f   = $feat->[$i];
         my $hid = $f->hseqname or $self->throw("Missing hid");
-
+        
         next unless $f->strand == $strand;
         $hit_features->{$hid} ||= [];
         push ( @{ $hit_features->{$hid} }, $f );
     }
-
+    
     my ($is_linear);
     if ( $strand == 1 ) {
         $is_linear = sub {
             my ( $x, $y ) = @_;
-
+            
             return $x->hstart <= $y->hstart;
         };
     }
@@ -174,31 +184,31 @@ sub run_est_genome_on_strand {
             return $x->hend >= $y->hend;
         };
     }
-
+    
     while ( my ( $hid, $flist ) = each %$hit_features ) {
-
-	print STDERR "$hid\n" if $verbose;
+        
+        print STDERR "$hid\n" if $verbose;
         print STDERR "PRESORTED\n" if $verbose; #
         foreach my $f (@$flist) {
             print STDERR "hit:  " . $f->gffstring . "\n" if $verbose;
         }
-
+        
         # Sort features by start/end in genomic
         @$flist = sort { $a->start <=> $b->start or $a->end <=> $b->end } @$flist;
-
+        
         print STDERR "SORTED\n" if $verbose;
         foreach my $f (@$flist) {
             print STDERR "hit:  " . $f->gffstring . "\n" if $verbose;
         }
-
+        
         # Group into linear matches
         my @sets = ( [ $flist->[0] ] );
         my $curr = $sets[0];
         for ( my $i  = 1 ; $i < @$flist ; $i++ ) {
-                        
+            
             my $prev = $flist->[ $i - 1 ];
             my $this = $flist->[$i];
-
+            
             if ( &$is_linear( $prev, $this ) ) {
                 push ( @$curr, $this );
             }
@@ -207,18 +217,18 @@ sub run_est_genome_on_strand {
                 push ( @sets, $curr );
             }
         }
-
+        
         print STDERR "MADE ",scalar(@sets)," LINEAR MATCHES SET(S)\n" if $verbose;
         foreach my $lin (@sets) {
             $self->do_mini_est_genome($lin);
         }
-#        $self->do_mini_est_genome($flist);
+        #        $self->do_mini_est_genome($flist);
     }
 }
 
 sub do_mini_est_genome {
     my ( $self, $linear ) = @_;
-
+    
     print STDERR "\nLinear Matches\n" if $verbose;
     foreach my $lin (@$linear) {
         print STDERR "before:  " . $lin->gffstring . "\n" if $verbose;
@@ -230,15 +240,15 @@ sub do_mini_est_genome {
         '-seqfetcher' => $self->seqfetcher,
     );
     $e2g->run;
-  
-
+    
+    
     foreach my $fp ( $e2g->output ) {
-        print STDERR "after:  " . $fp->gffstring . "\n" if $verbose;       
+        print STDERR "after:  " . $fp->gffstring . "\n" if $verbose;
         # source_tag and primary_tag have to be set to
         # something, or validate method in FeaturePair
         # (callled by RunnableDB) throws!
-#        $fp->feature2->source_tag('I_am_valid');
-#        $fp->feature2->primary_tag('I_am_valid');
+        #        $fp->feature2->source_tag('I_am_valid');
+        #        $fp->feature2->primary_tag('I_am_valid');
         $self->add_output($fp);
     }
 }
@@ -246,7 +256,7 @@ sub do_mini_est_genome {
 
 sub add_output {
     my ( $self, @feat ) = @_;
-
+    
     my $ana = $self->analysis;
     foreach my $f (@feat) {
         $f->analysis($ana);
@@ -260,7 +270,7 @@ sub add_output {
 
 sub output {
     my ($self) = @_;
-
+    
     if ( my $out = $self->{'_output'} ) {
         return @$out;
     }
@@ -268,8 +278,25 @@ sub output {
         return;
     }
 }
-
-
+sub _make_seqfetcher {
+    my ( $self, $indices ) = @_;
+    
+    my $seqfetcher;
+    if ( ref($indices) eq "ARRAY"){
+        $seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::OBDAIndexSeqFetcher->new(-db => $indices);
+    }
+    else{
+        $seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::Pfetch->new();
+    }
+    return $seqfetcher;
+}
+sub db_version_searched{
+    my $self = shift;
+    
+    $self->{'_db_version_searched'} = shift if @_;
+    print "I've decided to search db version: " . $self->{'_db_version_searched'} . "\n";
+    return $self->{'_db_version_searched'};
+}
 1;
 
 __END__
