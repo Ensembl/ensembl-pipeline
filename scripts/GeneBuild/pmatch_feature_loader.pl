301dc6a605eae14185117092137bc5e61f465f2d
Val Curwen <vac@sanger.ac.uk>
2003-06-23 09:55:23 +0000

    Correcting path to Pmatch modules

--- scripts/GeneBuild/pmatch_feature_loader.pl
+++ scripts/GeneBuild/pmatch_feature_loader.pl
@@ -141,7 +141,7 @@ sub process_proteins {
     # not used anymore, we deal with cDNAs separately
     my $cdna_id = $pmfa->get_cdna_id($protein);
 
-    my $pmf = new Bio::EnsEMBL::Pipeline::PmatchFeature(-protein_id  => $protein,
+    my $pmf = new Bio::EnsEMBL::Pipeline::Tools::Pmatch::PmatchFeature(-protein_id  => $protein,
 							-start       => $start,
 							-end         => $end,
 							-chr_name    => $chr,
