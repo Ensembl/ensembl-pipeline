14029bee596927130a8db081df790759948db5f9
Roy Storey <rds@sanger.ac.uk>
2003-09-11 14:12:50 +0000

    addition of access to meta information

--- modules/Bio/EnsEMBL/Pipeline/DBSQL/DBAdaptor.pm
+++ modules/Bio/EnsEMBL/Pipeline/DBSQL/DBAdaptor.pm
@@ -176,7 +176,8 @@ sub _db_handle {
 }
 
 
-=head2
+=head2 DESTROY
+
  Title   : DESTROY
  Usage   :
  Function:
@@ -195,53 +196,96 @@ sub DESTROY {
    }
 }
 
+=head2 Some Utility Stuff
+
+ Access to the meta table of the schema including
+ 
+ pipeline_lock             - write a lock to the meta table
+ pipeline_unlock           - remove the lock
+ get_meta_value_by_key     - retrieve value by key
+ store_meta_key_value      - write key, value pair
+ remove_meta_key           - delete by key name
+ make_meta_value_from_hash - flatten a hash to a string (uses dbi->quote to escape)
+ make_hash_from_meta_value - returns a hash from a previously flattened string
+ 
+    $lock_string = 'whatever to lock';
+    $dbA->pipeline_lock($lock_string);
+    my $lock_string = $dbA->pipeline_lock();
+    
+    $dbA->pipeline_unlock();
+ 
+    $dbA->store_meta_key_value('my_key', 'the value');
+
+    my $value = $dbA->get_meta_value_by_key('my_key');
+    
+    $dbA->remove_meta_key('my_key');
+
+    my %hash = ('-host' => 'pfam', '-port' => '3306'...);
+    my $flat = $dbA->make_meta_value_from_hash(\%hash);
+    my %retrieved = %{$dbA->make_hash_from_meta_value($flat)};
+
+=cut
 
 sub pipeline_lock {
     my ($self, $string) = @_;
+    my $lock = 'pipeline.lock';
 
-    my $sth;
-
-    if ($string) {
-	$sth = $self->prepare(qq{
-	    INSERT into meta (meta_key, meta_value)
-	    VALUES ('pipeline.lock', ?)
-	});
-	$sth->execute($string);
-    }
-    else {
-	$sth = $self->prepare(qq{
-	    SELECT meta_value
-	    FROM   meta
-	    WHERE  meta_key = 'pipeline.lock'
-	});
-
-        $sth->execute;
-        my $row = $sth->fetchrow_arrayref;
-
-        if ($row) {
-	    return $row->[0];
-        }
-        else {
-	    return undef;
-        }
-    }
-    $sth->finish;
+    return $string ? $self->store_meta_key_value($lock, $string) : $self->get_meta_value_by_key($lock);
 }
-
 
 sub pipeline_unlock {
     my ($self) = @_;
-
-    my $sth;
-
-    $sth = $self->prepare(qq{
-	DELETE
-	FROM   meta
-	WHERE  meta_key = 'pipeline.lock'
-    });
-
-    $sth->execute;
-    $sth->finish;
+    $self->remove_meta_key('pipeline.lock');
 }
 
+sub get_meta_value_by_key{
+    my ($self, $key, $value) = @_;
+    $self->throw("No key to get supplied") unless $key;
+    my $sth = $self->prepare(qq{
+        SELECT meta_value
+        FROM   meta
+        WHERE  meta_key = ? LIMIT 1
+    }); # ONLY RETRIEVES FIRST ENTRY,
+    # SHOULD THERE BE A UNIQUE KEY ON meta_key??
+    $sth->execute($key);
+    my $row = $sth->fetchrow_arrayref();
+    $value = $row->[0] if $row;
+    $sth->finish();
+    return $value;
+}
+sub store_meta_key_value{
+    my ($self, $key, $value) = @_;
+    $self->throw("No key|value to insert supplied") unless $key && $value;
+    my $sth = $self->prepare(qq{
+        INSERT INTO meta (meta_key, meta_value) VALUES (?, ?)
+    });
+    $sth->execute($key, $value);
+    $sth->finish();
+    return undef;
+}
+sub remove_meta_key{
+    my ($self, $key) = @_;
+    $self->throw("No key to remove supplied") unless $key;
+    my $sth = $self->prepare(qq{
+	DELETE
+	FROM   meta
+	WHERE  meta_key = ?
+    });
+    $sth->execute($key);
+    $sth->finish;
+    return undef;
+}
+sub make_meta_value_from_hash{
+    my ($self, $hash) = @_;
+    my $dbh = $self->_db_handle();
+    return join(",\n", map{ $dbh->quote($_)." => ".$dbh->quote($hash->{$_}) } keys(%$hash));
+}
+sub make_hash_from_meta_value{
+    my ($self,$string) = @_;
+    if($string){
+        my $hash = { eval $string };
+        $@ ? die "error evaluating $string" : return $hash || {};
+    }
+    return {};
+}
 1;
