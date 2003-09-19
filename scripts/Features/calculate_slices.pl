28b5e262686f74d1e90cbb20c92b3d440123fc11
Eduardo Eyras <eae@sanger.ac.uk>
2003-09-19 16:01:56 +0000

    now it places the cuts *before* it gets over the given thresholds, so that we are always sure that it will never go over a certain length or a certain number of exons/features

--- scripts/Features/calculate_slices.pl
+++ scripts/Features/calculate_slices.pl
@@ -13,7 +13,7 @@ my $dbname;
 my $chr_name;
 my $outfile;
 
-# 
+
 my $max_length = 200000;
 my $max_feature_count = 200;
 
@@ -43,10 +43,17 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
 
 
 my $slice = $db->get_SliceAdaptor->fetch_by_chr_name( $chr_name );
+#my $slice = $db->get_SliceAdaptor->fetch_by_chr_start_end( $chr_name, 20500000, 21000000 );
 my $chr_length = $slice->length;
 print STDERR "got slice $chr_name.1-$chr_length\n";
 
-my ($clusters,$feature_count) = &cluster_Genes($slice->get_all_Genes);
+my @all_genes = $slice->get_all_Genes;
+unless ( @all_genes ){
+  print STDERR "No genes found - exiting\n";
+  exit(0);
+}
+
+my ($clusters,$feature_count) = &cluster_Genes(@all_genes);
 print STDERR scalar(@$clusters)." clusters found\n";
 
 my @clusters = sort { $a->start <=> $b->start } @$clusters;
@@ -55,29 +62,60 @@ open ( OUTPUT, ">$outfile" ) or die("cannot open $outfile");
 
 my $start = 1;
 my $count = 0;
+my $end;
+my $this_end;
+my $order = 1;
 
- CLUSTER:
-    for (my $i=0; $i<=$#clusters; $i++ ){
-	my $end;
-	$count += $feature_count->{$clusters[$i]};
-	if ( $i==$#clusters ){
-	    $end = $chr_length;
-	}
-	else{
-	    $end = $clusters[$i]->end 
-		+ int( ( $clusters[$i+1]->start - $clusters[$i]->end )/2 );
-	}
-	
-	if ( ($end - $start + 1 ) > $max_length || $count > $max_feature_count ){
-	    print OUTPUT "$chr_name.$start-$end\tlength:".($end-$start+1)."\tfeature_count:$count\n";
-	    $start = $end + 1;
-	    $count = 0;
-	    next CLUSTER;
-	}
-	else{
-	    next CLUSTER;
-	}
+CLUSTER:
+for (my $i=0; $i<=$#clusters; $i++ ){
+  
+  my $extra_features = $feature_count->{$clusters[$i]};
+  
+  my $range_end = $chr_length;
+  if ( $i<$#clusters ){
+    $range_end = $clusters[$i]->end + int( ( $clusters[$i+1]->start - $clusters[$i]->end )/2 );
+  }
+  print OUTPUT "   this range:$chr_name.".($end+1)."-$range_end\tlength:".($range_end - $end)."\tfeature_count:$extra_features\n";
+  
+  if ( $i == 0 ){
+    $end = $clusters[$i]->end + int( ( $clusters[$i+1]->start - $clusters[$i]->end )/2 );
+  }
+  
+  if ( $i==$#clusters ){
+    $this_end = $chr_length;    
+  }
+  else{
+    $this_end = $clusters[$i]->end + int( ( $clusters[$i+1]->start - $clusters[$i]->end )/2 );
+  }
+  
+  if ( ($this_end - $start + 1 ) > $max_length || ( $count + $extra_features ) > $max_feature_count ){
+    if ( $order == 1 ){
+      my $features = $count + $extra_features;
+      print OUTPUT "$chr_name.$start-$this_end\tlength:".($this_end-$start+1)."\tfeature_count:$features (single range)\n";
+      $start = $this_end + 1;
+      $count = 0;
+      $order = 1;
+      next CLUSTER;
     }
+    else{
+      print OUTPUT "$chr_name.$start-$end\tlength:".($end-$start+1)."\tfeature_count:$count\n";
+      $start = $end + 1;
+      $count = $extra_features;
+      $order = 1;
+      next CLUSTER;
+    }
+  }
+  else{
+    $order++;
+    $count += $extra_features;
+    $end    = $this_end;
+    next CLUSTER;
+  }
+}
+
+if ( $end != $chr_length ){
+  print OUTPUT "$chr_name.$start-$chr_length\tlength:".( $chr_length - $start + 1 )."\tfeature_count:$count\n";
+}
 
 close(OUTPUT);
 
@@ -111,56 +149,58 @@ sub get_feature_count{
 #####################################################################
 
 sub cluster_Genes{
-    my ($genes) = @_;
+  my ($genes) = @_;
+  
+  my %features;
+  # first sort the genes
+  my @genes = sort by_lower_coordinate @$genes;
+  
+  # create a new cluster 
+  my $cluster = Bio::EnsEMBL::SeqFeature->new();
+  
+  my @clusters;
+  
+  my $first_gene = shift @genes;
+  # put the first gene into these cluster
+  my ($start,$end,$strand) = &get_gene_start_end_strand( $first_gene );
+  
+  $cluster->start( $start );
+  $cluster->end($end);
+  $features{$cluster} += &get_feature_count($genes[0]);
+  # store the list of clusters
+  push( @clusters, $cluster );
+  
+  # loop over the rest of the genes
+ LOOP1:
+  for (my $c=1; $c<=$#genes; $c++){
     
-    my %features;
-    # first sort the genes
-    my @genes = sort by_lower_coordinate @$genes;
-   
-    # create a new cluster 
-    my $cluster = Bio::EnsEMBL::SeqFeature->new();
-    
-    my @clusters;
-    
-    # put the first gene into these cluster
-    my ($start,$end,$strand) = &get_gene_start_end_strand( $genes[0] );
-    
-    $cluster->start( $start );
-    $cluster->end($end);
-    $features{$cluster} += &get_feature_count($genes[0]);
-    # store the list of clusters
-    push( @clusters, $cluster );
-    
-    # loop over the rest of the genes
-  LOOP1:
-    for (my $c=1; $c<=$#genes; $c++){
-	
-	my ( $tstart,$tend,$tstrand ) = &get_gene_start_end_strand( $genes[$c] );
-	if ( !( $tend   < $cluster->start 
-		||
-		$tstart > $cluster->end 
-		) 
-	     ){
-	    
-	    $features{$cluster} += &get_feature_count($genes[$c]);
-	    # re-adjust size of cluster
-	    if ( $tstart < $cluster->start ) {
-		$cluster->start( $tstart );
-	    }
-	    if ( $tend > $cluster->end ) {
-		$cluster->end($tend);
-	    }
-	}
-	else{
-	    # else, create a new cluster with this feature
-	    $cluster = Bio::EnsEMBL::SeqFeature->new();
-	    $cluster->start( $tstart );
-	    $cluster->end( $tend );
-	     $features{$cluster} += &get_feature_count($genes[$c]);
-	    push(@clusters,$cluster);
-	}
+    my $next_gene = shift @genes;
+    my ( $tstart,$tend,$tstrand ) = &get_gene_start_end_strand( $next_gene );
+    if ( !( $tend   < $cluster->start 
+	    ||
+	    $tstart > $cluster->end 
+	  ) 
+       ){
+      
+      $features{$cluster} += &get_feature_count($next_gene);
+      # re-adjust size of cluster
+      if ( $tstart < $cluster->start ) {
+	$cluster->start( $tstart );
+      }
+      if ( $tend > $cluster->end ) {
+	$cluster->end($tend);
+      }
     }
-    return (\@clusters,\%features);
+    else{
+      # else, create a new cluster with this feature
+      $cluster = Bio::EnsEMBL::SeqFeature->new();
+      $cluster->start( $tstart );
+      $cluster->end( $tend );
+      $features{$cluster} += &get_feature_count($next_gene);
+      push(@clusters,$cluster);
+    }
+  }
+  return (\@clusters,\%features);
 }
 
 
