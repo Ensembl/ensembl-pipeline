f0cf9959fd2c451fdea2de15bfae3372625bd593
Laura Clarke <lec@sanger.ac.uk>
2003-12-03 15:38:38 +0000

    Works with new schema and tidier

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Profile.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Profile.pm
@@ -42,203 +42,20 @@ use strict;
 
 # Object preamble - inheriets from Bio::Root::Object
 
-use Bio::Root::Object;
 
-
-@ISA = qw(Bio::Root::Object);
-
-use Bio::EnsEMBL::Pipeline::RunnableI;
-use Bio::EnsEMBL::SeqFeature;
-use Bio::EnsEMBL::FeaturePair;
-use Bio::EnsEMBL::Analysis;
+use Bio::EnsEMBL::Pipeline::Runnable::Protein_Annotation;
 use Bio::Seq;
 use Bio::SeqIO;
-use Bio::EnsEMBL::Root;
 
-@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableI);
-
+@ISA = qw(Bio::EnsEMBL::Pipeline::Runnable::Protein_Annotation);
 
 
-=head2 new
 
-    Title   :   new
-    Usage   :   my obj =  Bio::EnsEMBL::Pipeline::Runnable::CPG->new (-QUERY => $seq);
-    Function:   Initialises CPG object
-    Returns :   a CPG Object
-    Args    :   A Bio::Seq object (-QUERY), any arguments (-LENGTH, -GC, -OE) 
-
-=cut
-
-sub new {
-    my ($class,@args) = @_;
-    my $self = $class->SUPER::new(@args);    
-    
-    $self->{'_flist'}     = [];    # an array of Bio::SeqFeatures
-    $self->{'_sequence'}  = undef; # location of Bio::Seq object
-    $self->{'_workdir'}   = undef; # location of temp directory
-    $self->{'_filename'}  = undef; # file to store Bio::Seq object
-    $self->{'_results'}   = undef; # file to store results of Profile
-    $self->{'_threshold'} = undef; # Value of the threshod
-    $self->{'_protected'} = [];    # a list of files protected from deletion ???
-    
-  
-     my( $query, $analysis) = $self->_rearrange([qw(QUERY
-						    ANALYSIS
-						    )],
-						@args);
-    
-    
-    $self->query ($query) if ($query);       
-    $self->analysis ($analysis) if ($analysis);
-        	
-    return $self; # success - we hope!
+sub multiprotein{
+  my ($self) = @_;
+  return 0;
 }
 
-######
-#Get set methods
-######
-
-=head2 query
-
- Title   : query
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub query{
-    my ($self, $seq) = @_;
-    if ($seq) {
-	eval {
-	    $seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-	    };
-	
-	if (!$@) {
-	    $self->{'_sequence'} = $seq ;
-	    $self->filename ("query.$$.seq");
-	    $self->results ($self->filename.".out");
-	}
-	else {
-	    print STDERR "WARNING: The input_id is not a Seq object but if its a peptide fasta file, it should go fine\n";
-	    $self->{'_sequence'} = $seq ;
-	    $self->filename ("$$.tmp.seq");
-	    
-	    $self->results ("profile.$$.out");
-	    
-	}
-    }
-    return $self->{'_sequence'};
-}
-
-
-=head2 analysis
-
- Title   : analysis
- Usage   : $obj->analysis($newval)
- Function: 
- Returns : value of analysis
- Args    : newvalue (optional)
-
-
-=cut
-
-sub analysis{
-   my $obj = shift;
-   if( @_ ) {
-      my $value = shift;
-      $obj->{'analysis'} = $value;
-    }
-    return $obj->{'analysis'};
-
-}
-
-
-
-###########
-# Analysis methods
-##########
-
-=head2 run
-
-    Title   :  run
-    Usage   :   $obj->run()
-    Function:   Runs blast and BPLite and creates array of feature pairs
-    Returns :   none
-    Args    :   none
-
-=cut
-
-sub run {
- my ($self, $dir) = @_;
-
-    # check query
-    my $seq = $self->query || $self->throw("Query required for Program");
-
-    # set directory if provided
-    $self->workdir ('/tmp') unless ($self->workdir($dir));
-    $self->checkdir;
-
-    # reset filename and results as necessary (adding the directory path)
-    my $tmp = $self->workdir;
-    my $input = $tmp."/".$self->filename;
-    $self->filename ($input);
-    $tmp .= "/".$self->results;
-    $self->results ($tmp);
-
-
- eval {
-	$seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-	};
-	
-
-    if (!$@) {
-	#The inputId is a sequence file...got the normal way...
-
-	# write sequence to file
-	$self->writefile;        
-
-	# run program
-	$self->run_analysis;
-
-	# parse output
-	$self->parse_results;
-	$self->deletefiles;
-    }
-    else {
-	#The query object is not a seq object but a file.
-	#Perhaps should check here or before if this file is fasta format...if not die
-	#Here the file does not need to be created or deleted. Its already written and may be used by other runnables.
-
-	#pfscan runs over one protein at a time
-	
-	my $in  = Bio::SeqIO->new(-file => $seq, '-format' =>'Fasta');
-
-	while ( my $tmpseq = $in->next_seq() ) {
-	    print STDERR "SEQ: ".$self->workdir."/profile_tmp_seq\n";    
-	    open (OUT,">".$self->workdir."/profile_tmp_seq");
-	    print STDERR "AC: ".$tmpseq->display_id."\n";
-	    #print STDERR "AC: ".$tmpseq->seq."\n";
-	    
-	    print OUT ">".$tmpseq->display_id."\n".$tmpseq->seq."\n";
-
-	    close(OUT);
-	$self->filename($self->workdir."/profile_tmp_seq");
-
-	# run program
-	$self->run_analysis;
-
-	# parse output
-	$self->parse_results($tmpseq->display_id);
-	}
-	$self->throw("Can't remove tmp sequence $!\n")
-	    unless (system ("rm ".$self->workdir."/profile_tmp_seq") == 0);
-
-    }
-}
 
 =head2 run_analysis
 
@@ -251,16 +68,14 @@ sub run {
 =cut
 
 sub run_analysis {
-    my ($self) = @_;
-
-    print STDERR "RUNNING: ".$self->analysis->program . ' -fz ' .$self->filename. ' ' .$self->analysis->db_file . ' > ' .$self->results."\n";
-
-    $self->throw("Failed during Profile run $!\n")
-	    
-	unless (system ($self->analysis->program . ' -fz ' . 
-			$self->filename. ' ' .
-			$self->analysis->db_file . ' > ' .
-			$self->results) == 0) ;
+  my ($self) = @_;
+  
+  print STDERR "RUNNING: ".$self->program . ' -fz ' .$self->filename. ' ' .$self->database . ' > ' .$self->results."\n";
+  
+  $self->throw("Failed during Profile run $!\n") unless 
+    (system ($self->program . ' -fz ' . $self->filename. ' ' .
+             $self->database . ' > ' .$self->results) == 0) ;
+ 
 }
 
 
@@ -283,100 +98,27 @@ sub parse_results {
     
     if (-e $resfile) {
 	
-	if (-z $self->results) {  
-	    print STDERR "pfscan didn't find any hits\n";
-	    return; }       
-	else {
-	    open (CPGOUT, "<$resfile") or $self->throw("Error opening ", $resfile, " \n");#
+      if (-z $self->results) {  
+        print STDERR "pfscan didn't find any hits\n";
+        return; }       
+      else {
+        open (CPGOUT, "<$resfile") or $self->throw("Error opening ", $resfile, " \n");#
 	    }
     }
     my %printsac;
     my $line;
     
-    my @features;
     while (<CPGOUT>) {
-	$line = $_;
-	chomp $line;
-	#print STDERR "$line\n";
-	my ($nscore,$rawscore,$from,$to,$hfrom,$hto,$ac) = $line =~ /(\S+)\s+(\d+)\s*pos.\s+(\d*)\s*-\s+(\d*)\s*\[\s+(\d*),\s+(\S*)\]\s*(\w+)/;
-
-	my $feat = "$ac,$from,$to,$hfrom,$hto,$nscore";
-		
-		push (@features,$feat);
-	    }
-		
-    foreach my $feats (@features) {
-	$self->create_feature($feats,$sequenceId);
-	print STDERR "$feats\n";
-    }
-    @features = 0;
-}
-
-
-##############
-# input/output methods
-#############
-
-=head2 output
-
-    Title   :   output
-    Usage   :   obj->output()
-    Function:   Returns an array of features
-    Returns :   Returns an array of features
-    Args    :   none
-
-=cut
-
-sub output {
-    my ($self) = @_;
-    return @{$self->{'_flist'}};
-}
-
-=head2 create_feature
-
-    Title   :   create_feature
-    Usage   :   obj->create_feature($feature)
-    Function:   Returns an array of features
-    Returns :   Returns an array of features
-    Args    :   none
-
-=cut
-sub create_feature {
-    my ($self, $feat, $sequenceId) = @_;
-    
-#my $feat = "$print,$start,$end,$percentageIdentity,$profileScore,$pvalue";
-    my @f = split (/,/,$feat);
-    
-#$f[4] represents here the position of the match on the profile but if its values its -1, it means that the match is on the entire profile, thus we should calculate it.
-
-    my $hto = $f[4];
-
-    if ($f[4] =~ /-1/) {
-#Calculate the lenght of the match using the values given for the sequence.
-	$hto = $f[2] - $f[1] + 1;
-    }
-
-
-    my $feat1 = new Bio::EnsEMBL::SeqFeature ( -start => $f[1],                   
-					       -end => $f[2],        
-					       -score => $f[5],
-					       -analysis => $self->analysis,
-					       -seqname => $sequenceId,
-					       -percent_id => 0,
-					       -p_value => 0);
-    
-    my $feat2 = new Bio::EnsEMBL::SeqFeature (-start => 0,
-					      -end => 0,
-					      -analysis => $self->analysis,
-					      -seqname => $f[0]);
-    
-    
-    my $feature = new Bio::EnsEMBL::FeaturePair(-feature1 => $feat1,
-						-feature2 => $feat2);
-    
-    if ($feature)
-    {
-	push(@{$self->{'_flist'}}, $feature);
+      $line = $_;
+      chomp $line;
+      #print STDERR "$line\n";
+      my ($nscore,$rawscore,$from,$to,$hfrom,$hto,$ac) = $line =~ /(\S+)\s+(\d+)\s*pos.\s+(\d*)\s*-\s+(\d*)\s*\[\s+(\d*),\s+(\S*)\]\s*(\w+)/;
+      
+      my $fp = $self->create_protein_feature($from, $to, $nscore, $sequenceId,
+                                             0, 0, $ac, $self->analysis, 0, 
+                                             0);
+      $self->add_to_output($fp);
     }
 }
+
 
--- modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Tmhmm.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Tmhmm.pm
@@ -46,212 +46,13 @@ package Bio::EnsEMBL::Pipeline::Runnable::Protein::Tmhmm;
 use vars qw(@ISA);
 use strict;
 
-use Bio::EnsEMBL::Pipeline::RunnableI;
-use Bio::EnsEMBL::SeqFeature;
-use Bio::EnsEMBL::FeaturePair;
-use Bio::EnsEMBL::Analysis;
-
-@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableI);
+use Bio::EnsEMBL::Pipeline::Runnable::Protein_Annotation;
 
 
-=head2 new
-
- Title    : new
- Usage    : my $tmhmm =  Bio::EnsEMBL::Pipeline::Runnable::Protein::Tmhmm->new
-                         ( -program    => '/usr/local/pubseq/bin/tmhmm',
-                           -query      => $query,
-                           -analysis   => $analysis
-                         );
- Function : initialises Tmhmm object
- Returns  : a Tmhmm object
- Args     : a Bio::Seq object, the path to the program binaries and an analysisId
- Throws   :
-
-=cut
-
-sub new {
-    my ($class, @args) = @_;
- 
-    my $self = $class->SUPER::new (@_);    
-  
-    $self->{'_flist'}     = [];           # an array of Bio::SeqFeatures
-    $self->{'_sequence'}  = undef;        # location of Bio::Seq object
-    $self->{'_program'}   = undef;        # location of executable
-    $self->{'_workdir'}   = undef;        # location of tmp directory
-    $self->{'_filename'}  = undef;        # file to store Bio::Seq object
-    $self->{'_results'}   = undef;        # file to store results of program run
-    $self->{'_protected'} = [];           # a list of files protected from deletion
-  
-    my ($query, $analysis, $program) = $self->_rearrange([qw(QUERY 
-						             ANALYSIS
-                                                             PROGRAM)], 
-						          @args);
-  
-    $self->query ($query) if ($query);
-
-    if ($analysis) {
-        $self->analysis ($analysis);
-    }
-    else {
-        $self->throw("Tmhmm needs an analysis");
-    }
-
-    $self->program ($self->analysis->program_file);
-  
-    return $self;
-}
-
-###################
-# get/set methods 
-###################
-
-=head2 query
-
- Title    : query
- Usage    : $self->query ($query);
- Function : get/set method for the Sequence object; assigns query, filename and results
- Example  :
- Returns  : a Bio::Seq or Bio::PrimarySeq object
- Args     : a Bio::Seq or Bio::PrimarySeq object (optional)
- Throws   :
-
-=cut
-
-sub query {
-  my ($self, $seq) = @_;
-    if ($seq) {
-	eval {
-	    $seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-	};
-
-	if (!$@) {
-	    $self->{'_sequence'} = $seq ;
-	    $self->filename ("query.$$.seq");
-	    $self->results ($self->filename.".out");
-	}
-	else {
-	    print STDERR "WARNING: The input_id is not a Seq object but if its a peptide fasta file, it should go fine\n";
-	    $self->{'_sequence'} = $seq ;
-	    $self->filename ("$$.tmp.seq");
-	    
-	    $self->results ("tmhmm.$$.out");
-	    
-	}
-    }
-    return $self->{'_sequence'};
-}
+@ISA = qw(Bio::EnsEMBL::Pipeline::Runnable::Protein_Annotation);
 
 
-=head2 analysis
 
- Title    : analysis
- Usage    : $self->analysis ($analysis);
- Function : get/set method for the analysis
- Example  :
- Returns  : analysis
- Args     : analysis (optional)
- Throws   :
-
-=cut
-
-sub analysis {
-    my $self = shift;
-    if (@_) {
-        $self->{'_analysis'} = shift;
-    }
-    return $self->{'_analysis'};
-} 
-
-
-=head2 program
-
- Title    : program
- Usage    : $self->program ('/usr/local/pubseq/bin/hmmpfam');
- Function : get/set method for the path to the program binaries
- Example  :
- Returns  : File path
- Args     : File path (optional)
- Throws   :
-
-=cut
-
-sub program {
-    my ($self, $location) = @_;
-    if ($location) {
-        unless (-e $location) {
-            $self->throw ($self->program." not found at $location");
-        }
-        $self->{'_program'} = $location ;
-    }
-    return $self->{'_program'};
-}
-
-
-###################
-# analysis methods
-###################
-
-=head2 run
-
- Title    : run
- Usage    : $self->run ($workdir, $args)
- Function : runs program and populates @{$self->{'_flist'}} (array of features)
- Example  :
- Returns  :   
- Args     : workdir (optional)
- Throws   :
-
-=cut
-
-sub run {
-    my ($self, $dir) = @_;
-
-    # check query
-    my $seq = $self->query || $self->throw("Query required for Program\n");
-
-    # set directory if provided
-    $self->workdir ('/tmp') unless ($self->workdir($dir));
-    $self->checkdir;
-
-    # reset filename and results as necessary (adding the directory path)
-    my $tmp = $self->workdir;
-    my $input = $tmp."/".$self->filename;
-    $self->filename ($input);
-    $tmp .= "/".$self->results;
-    $self->results ($tmp);
-
- eval {
-	$seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-	};
-	
-
-    if (!$@) {
-	#The inputId is a sequence file...got the normal way...
-
-	# write sequence to file
-	$self->writefile;        
-
-	# run program
-	$self->run_program;
-
-	# parse output
-	$self->parse_results;
-	$self->deletefiles;
-    }
-    else {
-	#The query object is not a seq object but a file.
-	#Perhaps should check here or before if this file is fasta format...if not die
-	#Here the file does not need to be created or deleted. Its already written and may be used by other runnables.
-
-	$self->filename($self->query);
-
-	# run program
-	$self->run_program;
-
-	# parse output
-	$self->parse_results;
-    }
-}
 
 
 =head2 run_program
@@ -266,12 +67,13 @@ sub run {
 
 =cut
 
-sub run_program {
+sub run_analysis {
     my ($self) = @_;
     # run program
     print STDERR "running ".$self->program." ".$self->filename."\n";
     $self->throw ("Error running ".$self->program." on ".$self->filename) 
-        unless ((system ("perl ".$self->program." ".$self->filename." > ".$self->results)) == 0); 
+        unless ((system ("perl ".$self->program." ".$self->filename.
+                         " > ".$self->results)) == 0); 
 }
 
 
@@ -288,124 +90,58 @@ sub run_program {
 =cut
 
 sub parse_results {
-    my ($self) = @_;
-    my $filehandle;
-    my $resfile = $self->results;
-    
-    if (-e $resfile) {
-        # it's a filename
-        if (-z $self->results) {  
+  my ($self) = @_;
+  my $filehandle;
+  my $resfile = $self->results;
+  
+  if (-e $resfile) {
+    # it's a filename
+    if (-z $self->results) {  
 	    print STDERR $self->program." didn't find anything\n";
 	    return;
-        }       
-        else {
-            open (OUT, "<$resfile") or $self->throw ("Error opening $resfile");
-            $filehandle = \*OUT;
-      }
+    }else {
+      open (OUT, "<$resfile") or $self->throw ("Error opening $resfile");
+      $filehandle = \*OUT;
     }
-    else {
-        # it'a a filehandle
-        $filehandle = $resfile;
+  }else {
+    # it'a a filehandle
+    $filehandle = $resfile;
+  }
+  
+  # parse
+  my $id;
+  while (<$filehandle>) {
+    #print STDERR;
+    chomp;
+    next if /^$/;
+    if (/^\>(\S+)/) {
+      $id = $1;
+      print STDERR "have id ".$id."\n";
     }
-    
-    # parse
-    my $id;
-    while (<$filehandle>) {
-	chomp;
-        next if /^$/;
-        if (/^\>(\S+)/) {
-            $id = $1;
-	}
-        elsif (/^%pred/) {
-            my ($junk, $values) = split /:/;
-            my @tm = split (/,/, $values);
-            foreach (@tm) {
-                /(\w+)\s+(\d+)\s+(\d+)/;
-                my $orien = $1;
-                my $start = $2;
-                my $end = $3;
-                $orien = uc ($orien);
-                if ($orien eq "M") {
-                    my (%feature);
-	            $feature{name} = $id;
-	            $feature{start} = $start;
-	            $feature{end} = $end;
-	            ($feature{source}) = $self->program =~ /([^\/]+)$/;
-	            $feature{primary}= 'transmembrane';
-	            ($feature{program}) = $self->program =~ /([^\/]+)$/;
-                    $feature{logic_name} = 'transmembrane';
-		    $self->create_feature(\%feature);
-		}
+    elsif (/^%pred/) {
+      my ($junk, $values) = split /:/;
+      my @tm = split (/,/, $values);
+      foreach (@tm) {
+        /(\w+)\s+(\d+)\s+(\d+)/;
+        my $orien = $1;
+        my $start = $2;
+        my $end = $3;
+        $orien = uc ($orien);
+        if ($orien eq "M") {
+          my $fp = $self->create_protein_feature($start, $end, 0, $id, 0, 
+                                                 0, 'Tmhmm', 
+                                                 $self->analysis, 0, 0);
+          $self->add_to_output($fp);
+        }
 	    }
-	}
     }
-    close $filehandle;   
+  }
+  close $filehandle;   
 }
 
-
-=head2 create_feature
-
- Title    : create_feature
- Usage    : $self->create_feature ($feature)
- Function : creates a Bio::EnsEMBL::seqFeature object from %feature,
-            and pushes it onto @{$self->{'_flist'}}
- Example  :
- Returns  :
- Args     :
- Throws   :
-
-=cut
-
-sub create_feature {
-    my ($self, $feat) = @_;
-    
-    my $analysis = $self->analysis;
-
-    
-    # create feature object
-    my $feat1 = Bio::EnsEMBL::SeqFeature->new ( -seqname     => $feat->{name},
-						-start       => $feat->{start},
-						-end         => $feat->{end},
-						#-score       => $feat->{score},
-						-analysis    => $analysis,
-						-score => 0,
-						-percent_id => 0,
-						-p_value => 0,
-						); 
-
-
-
-    my $feat2 = new Bio::EnsEMBL::SeqFeature (-start => 0,
-					      -end => 0,
-					      -analysis => $analysis,
-					      -seqname => 'Tmhmm');
-    
-    
-    my $feature = new Bio::EnsEMBL::FeaturePair(-feature1 => $feat1,
-						-feature2 => $feat2);
-
-    if ($feature) {
-	push (@{$self->{'_flist'}}, $feature);
-    }
-}
-
-
-=head2 output
-
- Title    : output
- Usage    : $self->output
- Function : returns an array of feature objects
- Example  :
- Returns  : an array of Bio::EnsEMBL::SeqFeature objects
- Args     :
- Throws   :
-
-=cut
-
-sub output {
-    my ($self) = @_;
-    my @list = @{$self->{'_flist'}};
-    return @{$self->{'_flist'}};
+sub multiprotein{
+  my ($self) = @_;
+  return 1;
 }
 
 
