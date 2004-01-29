0b3d10c5864ec82c568851c00bd6b94b25a292a9
Simon Potter <scp@sanger.ac.uk>
2004-01-29 15:42:16 +0000

    file cleanup

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Waba.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Waba.pm
@@ -114,6 +114,7 @@ sub query {
 	$self->{'_sequence'} = $seq;
 	$self->filename ($self->query->id.".$$.seq");
 	$self->results ($self->filename.".out");
+	$self->file ($self->results);
     }
     return $self->{'_sequence'};
 }
