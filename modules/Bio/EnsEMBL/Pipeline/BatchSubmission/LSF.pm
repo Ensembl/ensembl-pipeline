85640746e55a04e1f6f4bb94e8c14e2ca4253a25
Laura Clarke <lec@sanger.ac.uk>
2004-02-10 10:01:15 +0000

    roling back last commit

--- modules/Bio/EnsEMBL/Pipeline/BatchSubmission/LSF.pm
+++ modules/Bio/EnsEMBL/Pipeline/BatchSubmission/LSF.pm
@@ -248,9 +248,9 @@ sub check_existance{
     }
     my @values = split;
     if($values[0] =~ /\d+/){
-      if($values[2] eq 'UNKWN'){
-        next LINE;
-      }
+      #if($values[2] eq 'UNKWN'){
+      #  next LINE;
+      #}
       $existing_ids{$values[0]} = 1;
     }
   }
