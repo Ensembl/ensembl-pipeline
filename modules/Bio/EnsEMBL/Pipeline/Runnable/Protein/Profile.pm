c596c01af24cd6b78929c339f2960ca982dc23d6
Kevin Howe <klh@ebi.ac.uk>
2004-04-13 10:42:13 +0000

    Fixed bug which caused generation of wrong results when 2 jobs end up on the same machine.

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Profile.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Protein/Profile.pm
@@ -81,16 +81,16 @@ sub new {
     $self->{'_threshold'} = undef; # Value of the threshod
     $self->{'_protected'} = [];    # a list of files protected from deletion ???
     
-  
-     my( $query, $analysis) = $self->_rearrange([qw(QUERY
-						    ANALYSIS
-						    )],
-						@args);
+    
+    my( $query, $analysis) = $self->_rearrange([qw(QUERY
+						   ANALYSIS
+						   )],
+					       @args);
     
     
     $self->query ($query) if ($query);       
     $self->analysis ($analysis) if ($analysis);
-        	
+    
     return $self; # success - we hope!
 }
 
@@ -112,25 +112,21 @@ sub new {
 
 sub query{
     my ($self, $seq) = @_;
+
     if ($seq) {
+	$self->{'_sequence'} = $seq ;
+	$self->filename ("profile_search.$$.seq");
+	$self->results ("profile_search.$$.out");
+
 	eval {
-	    $seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-	    };
-	
-	if (!$@) {
-	    $self->{'_sequence'} = $seq ;
-	    $self->filename ("query.$$.seq");
-	    $self->results ($self->filename.".out");
-	}
-	else {
+	    $seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI");
+	};
+	      
+	if ($@) {
 	    print STDERR "WARNING: The input_id is not a Seq object but if its a peptide fasta file, it should go fine\n";
-	    $self->{'_sequence'} = $seq ;
-	    $self->filename ("$$.tmp.seq");
-	    
-	    $self->results ("profile.$$.out");
-	    
 	}
     }
+
     return $self->{'_sequence'};
 }
 
@@ -173,69 +169,68 @@ sub analysis{
 =cut
 
 sub run {
- my ($self, $dir) = @_;
+    my ($self, $dir) = @_;
 
     # check query
     my $seq = $self->query || $self->throw("Query required for Program");
-
+    
     # set directory if provided
     $self->workdir ('/tmp') unless ($self->workdir($dir));
     $self->checkdir;
-
+    
     # reset filename and results as necessary (adding the directory path)
     my $tmp = $self->workdir;
     my $input = $tmp."/".$self->filename;
     $self->filename ($input);
     $tmp .= "/".$self->results;
     $self->results ($tmp);
-
-
- eval {
-	$seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-	};
-	
-
+    $self->file($self->results);
+    
+    eval {
+	$seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI");
+    };
+        
     if (!$@) {
 	#The inputId is a sequence file...got the normal way...
 
 	# write sequence to file
 	$self->writefile;        
 
+	print STDERR "AC: ", $seq->id, "\n";
 	# run program
 	$self->run_analysis;
 
 	# parse output
-	$self->parse_results;
+	$self->parse_results($seq->id);
 	$self->deletefiles;
     }
     else {
 	#The query object is not a seq object but a file.
 	#Perhaps should check here or before if this file is fasta format...if not die
-	#Here the file does not need to be created or deleted. Its already written and may be used by other runnables.
-
-	#pfscan runs over one protein at a time
+	#Here the file does not need to be created or deleted. Its already written and may 
+	#be used by other runnables. pfscan runs over one protein at a time
 	
 	my $in  = Bio::SeqIO->new(-file => $seq, '-format' =>'Fasta');
-
+	my $temp_file = $self->filename;
+	$self->file($temp_file);
+	
 	while ( my $tmpseq = $in->next_seq() ) {
-	    print STDERR "SEQ: ".$self->workdir."/profile_tmp_seq\n";    
-	    open (OUT,">".$self->workdir."/profile_tmp_seq");
-	    print STDERR "AC: ".$tmpseq->display_id."\n";
-	    #print STDERR "AC: ".$tmpseq->seq."\n";
+	    $tmpseq->desc("");
+	    my $out  = Bio::SeqIO->new(-file    => ">$temp_file",
+				       -format  =>'Fasta');
+	    	    
+	    $out->write_seq($tmpseq);
+	    $out->close();
 	    
-	    print OUT ">".$tmpseq->display_id."\n".$tmpseq->seq."\n";
-
-	    close(OUT);
-	$self->filename($self->workdir."/profile_tmp_seq");
-
-	# run program
-	$self->run_analysis;
-
-	# parse output
-	$self->parse_results($tmpseq->display_id);
+	    print STDERR "AC: ", $tmpseq->display_id, "\n";
+	    # run program
+	    $self->run_analysis;
+	    
+	    # parse output
+	    $self->parse_results($tmpseq->display_id);
 	}
-	$self->throw("Can't remove tmp sequence $!\n")
-	    unless (system ("rm ".$self->workdir."/profile_tmp_seq") == 0);
+
+	$self->deletefiles;
 
     }
 }
@@ -300,10 +295,9 @@ sub parse_results {
 	#print STDERR "$line\n";
 	my ($nscore,$rawscore,$from,$to,$hfrom,$hto,$ac) = $line =~ /(\S+)\s+(\d+)\s*pos.\s+(\d*)\s*-\s+(\d*)\s*\[\s+(\d*),\s+(\S*)\]\s*(\w+)/;
 
-	my $feat = "$ac,$from,$to,$hfrom,$hto,$nscore";
-		
-		push (@features,$feat);
-	    }
+	my $feat = "$ac,$from,$to,$hfrom,$hto,$nscore";	
+	push (@features,$feat);
+    }
 		
     foreach my $feats (@features) {
 	$self->create_feature($feats,$sequenceId);
