9f0d793b85334e356cbd43b9d5d29be5926e104e
Kevin Howe <klh@ebi.ac.uk>
2004-04-16 10:18:45 +0000

    Added command-line option to get all species rather than just human

--- scripts/mRNA/get_cdnas_from_EMBL.pl
+++ scripts/mRNA/get_cdnas_from_EMBL.pl
@@ -33,11 +33,13 @@ my $estfile;
 my $seqoutfile;
 my $clip;
 my $softmask;
+my $all_orgs = 0;
 
 &GetOptions( 
-	    'cdnafile:s'     => \$estfile,
-	    'clip'          => \$clip,
-	    'softmask'      => \$softmask,
+	     'cdnafile:s'     => \$estfile,
+	     'clip'          => \$clip,
+	     'softmask'      => \$softmask,
+	     'allorgs'       => \$all_orgs
 	   );
 
 # usage
@@ -79,14 +81,13 @@ while( my $cdna = $seqin->next_seq ){
   
   my $display_id    = $cdna->display_id;
   my $description   = $cdna->desc;
-  
   my $is_pseudogene = 0;
   my $is_noncoding  = 0;
   my $is_codinggene = 0;
   my $is_partial    = 0;
 
   # First select the species:
-  next SEQFETCH unless (   $description =~ /Homo sapiens/ || $description =~ /DNA.*coding.*human/ );
+  next SEQFETCH unless ($all_orgs or $description =~ /Homo sapiens/ or $description =~ /DNA.*coding.*human/ );
 
   next SEQFETCH if ( $description =~ /RIKEN/ 
 		     && ( $description =~/Mus musculus/
