1322073b417b1fdd6cf66aae9acb659fb1484e5f
Jan-hinnerk Vogel <jhv@sanger.ac.uk>
2004-06-15 13:03:06 +0000

    last version before  THE BIG CHANGE

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Avid.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Avid.pm
@@ -18,26 +18,26 @@ Bio::EnsEMBL::Pipeline::Runnable::Avid
 @sequences   = an array Bio::Seq objects,
 $avid        = an optional string which specifies the location of the used avid binary
 $options     = an optional string with  options (default: binary output (.mout-file))
-$workdir     = an optional string containing the location of the working-directory (default: /tmp/)
-$file_prefix = an optional array of strings used as file_prefixes for the written output-files
+$workdir     = an optional string containing the location of the working-directory (def: /tmp:)
+$file_prefix = an optional array of strings used as file_prefixes for the temporarily written output-files
 
-  my $obj = Bio::EnsEMBL::Pipeline::Runnable::Avid->new(
-							-query_seqs    => \@sequences,
-							-avid          => $avid,
-							-options       => $options,
-							-workdir       => $workdir,
-							-file_prefix   => \@file_prefix
-						       );
+my $obj = Bio::EnsEMBL::Pipeline::Runnable::Avid->new(
+                                                      -query_seqs    => \@sequences,
+                                                      -avid          => $avid,
+                                                      -options       => $options,
+                                                      -workdir       => $workdir,
+                                                      -file_prefix   => \@file_prefix
+                                                     );
 
 or
 
   my $obj = Bio::EnsEMBL::Pipeline::Runnable::Avid->new(
-							-query_seqs    => \@sequences,
-						       );
+                                                        -query_seqs    => \@sequences,
+                                                       );
 
 
-     $obj->run;
-  my $resultfile = $obj->results;
+$obj->run;
+my $resultfile = $obj->results;
 
 
 =head1 DESCRIPTION
@@ -59,7 +59,6 @@ Internal methods are usually preceded with a _
 =cut
 
 package Bio::EnsEMBL::Pipeline::Runnable::Avid;
-
 use vars qw(@ISA);
 use strict;
 
@@ -79,7 +78,7 @@ use Bio::EnsEMBL::Analysis;
 sub new {
 
   my ($class,@args) = @_;
-  my $self = {};  #construct empty hash
+  my $self = {};                #construct empty hash
   bless $self,$class;
 
   my (
@@ -88,66 +87,34 @@ sub new {
       $options,
       $workdir,
       $file_prefix ) = $self->_rearrange([qw(
-					QUERY_SEQUENCES
-					AVID
-					OPTIONS
-					WORKDIR
-					FILE_PREFIX
-				       )
-				    ], @args);
-  $self->{_output} = [];
-  $self->{_verbose} = 0 ;
-
-
+                                             QUERY_SEQUENCES
+                                             AVID
+                                             OPTIONS
+                                             WORKDIR
+                                             FILE_PREFIX
+                                            )
+                                         ], @args);
+  $self->{_verbose} = 1 ;
 
 
   ########### TESTING TYPES OF PASSED OBJECTS ###########
-
-  if(ref($query_seqs) ne "ARRAY") {
-    $self->throw("[$query_seqs] is not a reference to an Array\n"); 
-  }else{
-    foreach(@{$query_seqs}) {
-	    $self->throw("[$_] is not a Bio::PrimarySeqI") unless $_->isa("Bio::PrimarySeqI");
-    }
-  }
   $self->query_sequences($query_seqs);
 
 
-  ########### LOCATION OF USED BINARY ###################
-  # if not given, we use the  default:avid-2.1b0
-
-  if ($avid) {
-      $self->avid_binary($avid);
-  } else {
-      $self->avid_binary('/nfs/acari/jhv/bin/avid');
-  }
-
+  ########### LOCATION OF BINARY default av9d-2.1b0 #####
+  $self->avid_binary($avid);
 
 
   ########### PASSED OPTIONS ############################
+  $self->options( $options );
 
-  if ($options) {
-    $self->options( $options );
-  }else{
-    $self->options("-obin");
-  }
-  print "Passed options: ".$self->options."\n" if $self->verbose;
 
   ########## FILE_PREFIX ####################
+  $self->file_prefix ($file_prefix);
 
-  if (ref ($file_prefix) eq "ARRAY") {
-    $self->file_prefix ($file_prefix);
-  }else{
-    my @tmp = ("seq1","seq2");
-    $self->file_prefix (\@tmp);
-  }
 
   ########## SETTING WORKDIR ####################
-  if ($workdir) {
-    $self->workdir($workdir);
-  }else{
-    $self->workdir('/tmp/');
-  }
+  defined $workdir ? $self->workdir($workdir) : $self->workdir('/tmp/');
 
   return $self;
 }
@@ -174,8 +141,8 @@ objects and their according masked sequences and puts the results into the file 
 sub run {
   my ($self) = @_;
 
-   # Write masked and unmasked query sequences to 4 files and store their names
-   $self->write_sequences;
+  # Write masked and unmasked query sequences to 4 files and store their names
+  $self->write_sequences;
 
 
   # names of fasta files on which avid runs on (there must also be a .masked-file)
@@ -184,8 +151,8 @@ sub run {
 
 
   my $command  = $self->avid_binary  . " " .$self->options;
-     $command .=  " " . $fa_first    . " ";
-     $command .=  " " . $fa_secnd         ;
+  $command .=  " " . $fa_first    . " ";
+  $command .=  " " . $fa_secnd         ;
 
   print STDERR "Avid command : $command\n"  if $self->verbose;
 
@@ -195,7 +162,6 @@ sub run {
 
   # add the files written by avid to the @file-array for deletion
   $self->files_to_delete($fa_first,$fa_secnd);
-
 
   return 1
 }
@@ -209,20 +175,20 @@ sub files_to_delete {
 
   my $workdir        =  $self->workdir;
 
-     # substitution of workdir-prefix and .fasta-suffix to get filenames
-     $ff             =~s/$workdir(.+)\.fasta/$1/;
-     $sf             =~s/$workdir(.+)\.fasta/$1/;
+  # substitution of workdir-prefix and .fasta-suffix to get filenames
+  $ff             =~s/$workdir(.+)\.fasta/$1/;
+  $sf             =~s/$workdir(.+)\.fasta/$1/;
 
 
-    # option -opsl writes out: .info .out .psl
-    # option -obin writes out: .minfo .mout
+  # option -opsl writes out: .info .out .psl
+  # option -obin writes out: .minfo .mout
 
-  if ($self->options =~ /obin/){
-    @temp     = qw ( .minfo );                            # add .mout
+  if ($self->options =~ /obin/) {
+    #    @temp     = qw ( .minfo );                            # add .mout
     $self->results( $ff . "_" . $sf . ".mout" );
   }
-  if ($self->options =~ /opsl/){
-    @temp     = qw ( .minfo .info .out );                 # add .psl
+  if ($self->options =~ /opsl/) {
+    #    @temp     = qw ( .minfo .info .out );                 # add .psl
     $self->results( $ff . "_" . $sf . ".psl" );
   }
 
@@ -235,9 +201,18 @@ sub files_to_delete {
 }
 
 
+
 sub query_sequences {
   my ($self,$seq_ref) = @_;
+
   if ($seq_ref) {
+    if (ref($seq_ref) ne "ARRAY") {
+      $self->throw("[$seq_ref] is not a reference to an Array\n");
+    } else {
+      foreach (@{$seq_ref}) {
+        $self->throw("[$_] is not a Bio::PrimarySeqI") unless $_->isa("Bio::PrimarySeqI");
+      }
+    }
     my @seqs                 = @{$seq_ref};
     $self->{_query_sequences} = \@seqs;
   }
@@ -249,19 +224,25 @@ sub query_sequences {
 
 sub avid_binary {
   my ($self, $location) = @_;
-  if ($location) {                #if two values are submitted check if the submit. binary exists
-    $self->throw("Slam not found at $location: $!\n") unless (-e $location);
-    $self->{_slam} = $location ;
+
+  if (defined $location) {
+    print "Location of Avid-File is $location\n" if ($self->verbose);
+    $self->throw("Avid not found at $location: $!\n") unless (-e $location);
+    $self->{_avid} = $location ;
   }
-  return $self->{_slam};
+  $self->{_avid}='/nfs/acari/jhv/bin/avid' if ( !defined $location  && !defined $self->{_avid});
+
+  return $self->{_avid};
 }
 
 ############################################################
 
 sub options {
   my ($self,$opt) =  @_;
-      $self       -> {_options} = $opt if $opt;
-  return $self    -> {_options};
+
+  $self->{_options} = $opt if (defined $opt);
+  $self->{_options} = '-obin' if ( ( !defined $opt) && ( !defined  $self->{_options}));
+  return $self->{_options};
 }
 
 
@@ -269,13 +250,13 @@ sub options {
 # writing out 4 files for avid-input : 2 fasta-files and 2 repeatmasked fastas
 
 sub write_sequences {
-  my $self   = shift;
+  my $self = shift;
 
-  my $count      = 0;
-  foreach my $slice ( $self->query_sequences ){
+  my $count = 0;
+  foreach my $slice ( $self->query_sequences ) {
 
     # getting filename of (first) unmasked sequence
-    my $filename   =   $self->get_tmp_file($self->workdir , @{$self->file_prefix}[$count] , "fasta");
+    my $filename = $self->get_tmp_file($self->workdir , @{$self->file_prefix}[$count] , "fasta");
 
     # inherited method to store filenames to unlink in @array
     $self->file($filename);
@@ -284,21 +265,21 @@ sub write_sequences {
     $self->run_avid_on_files($filename);
 
     # writing unmasked fasta-sequence
-    my  $seqobj    =  Bio::SeqIO->new(-file => ">$filename" , '-format' => 'Fasta');
-        $seqobj    -> write_seq($slice);
-        print "Writing sequence $filename\n" if ($self->verbose);
+    my  $seqobj = Bio::SeqIO->new(-file => ">$filename" , '-format' => 'Fasta');
+    $seqobj->write_seq($slice);
+    print "Writing sequence $filename\n" if ($self->verbose);
 
     # getting filename of (first) masked sequence 
-    $filename   .= ".masked";
+    $filename .= ".masked";
 
     # inherited method to store filenames to unlink in @array
     $self->file($filename);
 
     # writing masked fasta-sequence
-    my $rm_slice   =  $slice->get_repeatmasked_seq();
-       $seqobj       =  Bio::SeqIO->new(-file => ">$filename" , '-format' => 'Fasta');
-       $seqobj       -> write_seq($rm_slice);
-       print "Writing sequence $filename\n" if ($self->verbose);
+    my $rm_slice = $slice->get_repeatmasked_seq();
+    $seqobj =  Bio::SeqIO->new(-file => ">$filename" , '-format' => 'Fasta');
+    $seqobj -> write_seq($rm_slice);
+    print "Writing sequence $filename\n" if ($self->verbose);
     $count++;
   }
 }
@@ -309,12 +290,13 @@ sub write_sequences {
 
 sub run_avid_on_files {
   my ($self,$file) = @_;
-    if (!defined  $self->{_run_avid_on_files} )  {
-	$self->{_run_avid_on_files} = [];
-    }
-    if (defined $file)  {
-	push  @{$self->{_run_avid_on_files}} , $file ;
-    }
+
+  if (!defined  $self->{_run_avid_on_files} ) {
+    $self->{_run_avid_on_files} = [];
+  }
+  if (defined $file) {
+    push  @{$self->{_run_avid_on_files}} , $file ;
+  }
   return $self->{_run_avid_on_files} ;
 }
 
@@ -322,17 +304,19 @@ sub run_avid_on_files {
 
 sub verbose {
   my $self = shift;
-    $self->{_verbose} = shift if @_ ;
-    return $self->{_verbose}
+
+  $self->{_verbose} = shift if @_ ;
+  return $self->{_verbose}
 }
 
 
 sub file_prefix{
-    my ($self,$prefix) = @_;
-    if (defined $prefix) {
-      $self->{_file_prefix} = \@$prefix;
-    }
-     return  $self->{_file_prefix};
+  my ($self,$prefix) = @_;
+
+  $self->{_file_prefix} = \@$prefix   if (defined $prefix && ref($prefix) eq "ARRAY");
+  $self->{_file_prefix} = ["seq1","seq2"]   if (!defined $prefix && !defined $self->{_file_prefix});
+
+  return $self->{_file_prefix};
 }
 
 
