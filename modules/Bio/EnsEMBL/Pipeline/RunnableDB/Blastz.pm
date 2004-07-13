4095fe7c3f7cdf131f69e4ebcf9a592d0eb79a9e
Kevin Howe <klh@ebi.ac.uk>
2004-07-13 13:45:28 +0000

    Added RunnableDB for Blastz and removed obsolete Runnable/Blastz_m.pm and RunnableDB/Blastz_m.pm

---
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/Blastz.pm
@@ -0,0 +1,219 @@
+# Cared for by Ensembl
+#
+# Copyright GRL & EBI
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::RunnableDB::Blastz
+
+=head1 SYNOPSIS
+
+  my $db      = Bio::EnsEMBL::DBAdaptor->new($locator);
+  my $genscan = Bio::EnsEMBL::Pipeline::RunnableDB::Blastz->new (
+                                                    -db      => $db,
+                                                    -input_id   => $input_id
+                                                    -analysis   => $analysis );
+  $genscan->fetch_input();
+  $genscan->run();
+  $genscan->write_output(); #writes to DB
+
+
+=head1 DESCRIPTION
+
+This is a RunnableDB wrapper for Bio::EnsEMBL::Pipeline::Runnable::Blastz,
+allowing query sequences to fetched from, and results to be written to,
+the given database handle. 
+
+
+=cut
+package Bio::EnsEMBL::Pipeline::RunnableDB::Blastz;
+
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inherits from Bio::Root::RootI;
+use Bio::EnsEMBL::Pipeline::RunnableDB;
+use Bio::EnsEMBL::Pipeline::Runnable::Blastz;
+use Bio::EnsEMBL::Analysis;
+use Bio::EnsEMBL::DnaDnaAlignFeature;
+use Bio::EnsEMBL::SeqFeature;
+use Bio::EnsEMBL::Pipeline::Config::General;
+
+@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
+
+sub new {
+  my ($class, @args) = @_;
+
+  my $self = $class->SUPER::new(@args);  
+
+  if(!defined $self->seqfetcher) {
+    # will look for pfetch in $PATH - change this once PipeConf up to date
+    my $seqfetcher = new Bio::EnsEMBL::Pipeline::SeqFetcher::Pfetch; 
+    $self->seqfetcher($seqfetcher);
+  }  
+  
+  return $self; 
+}
+
+=head2 fetch_input
+
+    Title   :   fetch_input
+    Usage   :   $self->fetch_input
+    Function:   Fetches input data for exonerate from the database
+    Returns :   nothing
+    Args    :   none
+
+=cut
+
+sub fetch_input {
+  my( $self) = @_; 
+  
+  $self->throw("No input id") unless defined($self->input_id);
+
+  my $input_id  = $self->input_id;
+  
+  my ($contig, $chr_name, $chr_start, $chr_end);
+ 
+  if ($input_id =~ /$SLICE_INPUT_ID_REGEX/) {
+    ($chr_name, $chr_start, $chr_end) = ($1, $2, $3);
+    $contig  = $self->db->get_SliceAdaptor->fetch_by_chr_start_end($chr_name,$chr_start,$chr_end);
+  } else {
+    $contig = $self->db->get_RawContigAdaptor->fetch_by_name($input_id);
+  }
+
+  $self->slice($contig);
+
+  # if repeatmasking required, always soft mask for blastz
+  my $genseq = (@$PIPELINE_REPEAT_MASKING) 
+      ? $contig->get_repeatmasked_seq($PIPELINE_REPEAT_MASKING, 1)
+      : $contig->get_repeatmasked_seq('', 1);
+
+  my @db;
+
+  if (!defined $contig || !defined($self->analysis->db)) {
+    $self->throw("Can't run blat if no sequence [$contig] or database [" . $self->analysis->db ."] exists");
+  }
+
+  my $executable =  $self->analysis->program_file();
+  my $database = $self->analysis->db;
+  
+  if ( -d $database) {
+    print "Found database dir $database\n";
+    
+    #my $files = `ls -1 $db/*.fa`;
+    @db = glob("$database/*");
+    
+    # if the files have standard names, try to sort them 
+    # for consistency
+    @db = sort { my ($o) = ($a =~ /\/([^\.\/]+)[^\/]*\.fa$/); 
+                 my ($t) = ($b =~ /\/([^\.\/]+)[^\/]*\.fa$/); 
+                 $o <=> $t } @db;
+    
+  }
+  else {
+    push(@db,$database);
+  }
+
+  foreach my $db (@db) {
+    my $blastz = new Bio::EnsEMBL::Pipeline::Runnable::Blastz(-query         => $genseq,
+                                                              -program       => $executable,
+                                                              -database      => $db,
+                                                              -options       => $self->analysis->parameters);    
+    $self->runnable($blastz);
+  }
+}
+
+
+=head2 run
+
+    Title   :   run
+    Usage   :   $self->run()
+    Function:   Runs the Blastz analysis, producing Bio::EnsEMBL::DnaDnaAlignFeature
+    Returns :   
+    Args    :   
+
+=cut
+
+sub run {
+  my ($self) = @_;
+  
+  $self->throw("Can't run - no runnable objects") unless defined($self->runnable);
+  
+  foreach my $run ($self->runnable) {
+    $run->run;
+  }
+
+}
+
+=head2 slice
+
+    Title   :   slice
+    Usage   :   $self->slice($slice)
+    Function:   Get/Set the underlying sequence for this analysis
+    Returns :   
+    Args    :   
+
+=cut
+
+sub slice {
+  my ($self,$slice) = @_;
+
+  if (defined($slice)) {
+    $self->{_slice} = $slice;
+  }
+  return $self->{_slice};
+}
+
+=head2 write_output
+
+    Title   :   write_output
+    Usage   :   $self->write_output()
+    Function:   Writes contents of $self->{_output} into $self->db
+    Returns :   1
+    Args    :   None
+
+=cut
+
+sub write_output {
+
+  my($self) = @_;
+  
+  my @features = $self->output();
+  my $db       = $self->db;
+  
+  my $fa = $db->get_DnaAlignFeatureAdaptor;
+  
+  foreach my $output (@features) {
+    $output->contig($self->slice);
+    $output->attach_seq($self->slice);
+    
+    $output->analysis($self->analysis);
+    
+    if ($self->slice->isa("Bio::EnsEMBL::Slice")) {
+      my @mapped = $output->transform;
+      
+      if (@mapped == 0) {
+        $self->warn("Couldn't map $output - skipping");
+        next;
+      }
+      if (@mapped == 1 && $mapped[0]->isa("Bio::EnsEMBL::Mapper::Gap")) {
+        $self->warn("$output seems to be on a gap - something bad has happened ...");
+        next;
+      }
+      
+      $fa->store(@mapped);
+    } 
+    else {
+      $fa->store($output);
+    }
+  }
+}
+
+
+1;
