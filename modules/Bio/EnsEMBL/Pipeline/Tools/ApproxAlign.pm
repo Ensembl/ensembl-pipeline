89dd7a9c7c73dd3246abb0ddfc4df0fd0acf8544
Jan-hinnerk Vogel <jhv@sanger.ac.uk>
2004-07-15 21:03:13 +0000

    - method to write used repeats for the first sequence into db-file for   comparision of db-repeats (and cutting results) with orignal Slam run.   Would be nice if both do the same, eeh ?

--- modules/Bio/EnsEMBL/Pipeline/Tools/ApproxAlign.pm
+++ modules/Bio/EnsEMBL/Pipeline/Tools/ApproxAlign.pm
@@ -27,9 +27,11 @@ my $obj = Bio::EnsEMBL::Pipeline::Runnable::Tools::ApproxAlign->new(
 
 =head1 DESCRIPTION
 
-ApproxAlign reads the output of an approximate alignment. It can be used to manipulate the alignment as well as 
-to write it to a file. ApproxAlign inherits it's methods from a module called aat.pm (Copyright by L.Pachter, 
-see http://baboon.math.berkeley.edu/~syntenic/slam.html or http://math.berkeley.edu/~lpachter/).
+ApproxAlign reads the output of an approximate alignment. It can be used to manipulate the
+alignment as well as to write it to a file. ApproxAlign inherits from a module called aat.pm
+(Copyright by L.Pachter,see http://baboon.math.berkeley.edu/~syntenic/slam.html or
+http://math.berkeley.edu/~lpachter/). To use this module, you have to include the path to
+the aat.pm-module in your PERL5LIB-variable.
 
 
 =head1 CONTACT
@@ -45,14 +47,12 @@ Internal methods are usually preceded with a _
 
 package Bio::EnsEMBL::Pipeline::Tools::ApproxAlign;
 
-use vars qw(@ISA);
-use strict;
 use aat;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::Pipeline::RunnableI;
 
- @ISA = qw (aat Bio::EnsEMBL::Pipeline::RunnableI);
-
-
-
+@ISA = qw (aat Bio::EnsEMBL::Pipeline::RunnableI);
 
 ### TODO:
 ### Constructing new ApproxAlign-Object (inherits aat.pm)
@@ -64,53 +64,170 @@ use aat;
 ###          -filename which returns name of written file for handling over to slam
 
 
-# Reads in approximate alignment from supplied filename
+# super the constructor for aat-object and set default-values for expanding the approximate Alignment
 
-sub read {
-  my($this, $file) = @_;
+sub new {
+  my ($class,@args) = @_;
+  my $self = $class->SUPER::new(@args);
 
-  my($i,@fields,$aatLenY,$aatLenZ);
-  $i = 0;
-  $this->{'aat'} = [];
+  my (
+      $fatten_value,
+      $fwdonly,
+      $exonbounds,
+      $workdir
+     ) = $self->_rearrange([qw(
+                               FATTEN_VALUE
+                               FWDONLY
+                               EXONBOUNDS
+                               WORKDIR
+                              )
+                           ], @args
+                          );
 
-  open(IN,"<$file") || die "Could not open file: $file\n";
-  while(<IN>) {
-    @fields = split;
-    die "aat->read(): bad format in line ".($i+1).", should have 3 fields\n" if(scalar(@fields) != 3);
-    die "aat->read(): expected first entry of line ".($i+1)." to be $i.\n" if($fields[0] != $i);
-    push(@{$this->{'aat'}},[$fields[1],$fields[2]]);
-    $this->{'weight'} += ($fields[2] - $fields[1] + 1);
-    $i++;
+  # setting defaults
+  $self->fwdonly('0');
+  $self->fatten_value(undef);
+
+  # overriding default if optional value is passed
+  $self->fwdonly($fwdonly);
+  $self->fatten_value($fatten_value);
+
+  $self->exonbounds($exonbounds);
+  $self->workdir($workdir);
+  $self->checkdir();
+
+  return $self;
+}
+
+sub exonbounds {
+  my ($self,$exonbounds) = @_;
+
+  if (!defined $exonbounds && !defined $self->{_exonbounds}) {
+
+    # default parameters for expanding the exon bounds 
+    # of approximate Aligement (aatFill.pl)
+
+      my $acc = 20;             # acc-slam-default: 20
+      my $don = 10;             # don-slam-default: 10
+      my $stp = 5;              # stp-slam-default: 5
+
+      # fwd-exon-boundaries
+      my $fwdStp1 = ['TAA', 0, 'zOnly', $stp];
+      my $fwdStp2 = ['TAG', 0, 'zOnly', $stp];
+      my $fwdStp3 = ['TGA', 0, 'zOnly', $stp];
+
+      # rev-exon-boundaries
+      my $revStp1 = ['TTA', 2, 'zOnly', $stp];
+      my $revStp2 = ['CTA', 2, 'zOnly', $stp];
+      my $revStp3 = ['TCA', 2, 'zOnly', $stp];
+
+      #my $fwdDon = ['GT',  2, 'points',  $don];
+      #my $revDon = ['AC', -1, 'points', -$don, -($don+1) ],
+      #my $fwdAcc = ['AG', -1, 'points', -$acc, -($acc+1) ];
+      #my $revAcc = ['CT',  2, 'points',  $acc];
+
+      my $fwdDon = ['GT',  2, 'line',   $don]; # 10
+      my $revDon = ['AC', -1, 'line', -($don + 1)]; # $don=10--> -11
+
+      my $fwdAcc = ['AG', -1, 'line', -($acc + 1)]; # $acc=20--> -21
+      my $revAcc = ['CT',  2, 'line',   $acc]; # 20
+
+
+      if (defined $self->fwdonly && $self->fwdonly=~m/(1|true|t)/) {
+        $exonbounds = [$fwdStp1, $fwdStp2, $fwdStp3, $fwdDon, $fwdAcc ];
+      } else {
+        $exonbounds = [$fwdStp1, $fwdStp2, $fwdStp3,$revStp1, $revStp2, $revStp3,$fwdDon, $revDon,$fwdAcc, $revAcc];
+      }
+
+      # not used because slam-default doesn't fatten
+      if ($self->fatten_value) {
+        push(@{$exonbounds}, ['fatten', undef, undef, $self->fatten_value]) if(defined($self->fatten_value));
+      }
+      $self->{_exonbounds}=$exonbounds;
+    }elsif ($exonbounds) {
+      $self->{_exonbounds}=$exonbounds;
+    }
+  return $self->{_exonbounds};
+}
+
+
+
+
+sub fatten_value {
+    my ($self, $fatten_value) = @_;
+    $self->{_fatten_value} = $fatten_value if ($fatten_value);
+    return $self->{_fatten_value};
+}
+
+
+sub fwdonly{
+  my ($self,$fwdonly) = @_;
+
+  $self->{_fwdonly} = $fwdonly if ($fwdonly);
+  return $self->{_fwdonly};
+}
+
+
+
+  # Reads in approximate alignment from supplied filename
+
+  sub read {
+    my($self, $file) = @_;
+
+    my($i,@fields,$aatLenY,$aatLenZ);
+    $i = 0;
+    $self->{'aat'} = [];
+
+    open(IN,"<$file") || die "Could not open file: $file\n";
+    while (<IN>) {
+      @fields = split;
+      die "aat->read(): bad format in line ".($i+1).", should have 3 fields\n" if(scalar(@fields) != 3);
+      die "aat->read(): expected first entry of line ".($i+1)." to be $i.\n" if($fields[0] != $i);
+      push(@{$self->{'aat'}},[$fields[1],$fields[2]]);
+      $self->{'weight'} += ($fields[2] - $fields[1] + 1);
+      $i++;
+    }
+    close(IN);
+    $aatLenY = scalar(@{$self->{'aat'}});
+    $aatLenZ = 1 + $self->{'aat'}->[$aatLenY-1][1];
+    if (defined($self->{seqY}) && ($aatLenY != scalar(@{$self->{seqY}}))) {
+      printf STDERR ("Unequal lengths for aatSeqY (%d) and seqY (%d).\n",$aatLenY,scalar(@{$self->{'seqY'}}));
+    }
+    if (defined($self->{seqZ}) && ($aatLenZ != scalar(@{$self->{seqZ}}))) {
+      printf STDERR ("Unequal lengths for aatSeqZ (%d) and seqZ (%d).\n",$aatLenZ,scalar(@{$self->{'seqZ'}}));
+    }
+    return;
   }
-  close(IN);
-  $aatLenY = scalar(@{$this->{'aat'}});
-  $aatLenZ = 1 + $this->{'aat'}->[$aatLenY-1][1];
-  if(defined($this->{seqY}) && ($aatLenY != scalar(@{$this->{seqY}}))) {
-    printf STDERR ("Unequal lengths for aatSeqY (%d) and seqY (%d).\n",$aatLenY,scalar(@{$this->{'seqY'}}));
+
+
+# Writes sequence to a file (filename will be created by Bio::EnsEMBL::Pipeline::RunnableI->get_tmp_filename
+# if $self->workdir is set, than $workdir will be used, otherwise /tmp/ will be used
+# Remember: Bio::EnsEMBL::Pipeline::RunnableI must be in your path
+# write returns the name and path of the written file
+
+sub write {
+  my ($self) = @_;
+
+  my $n = scalar(@{$self->{'aat'}});
+  my $aatfile = Bio::EnsEMBL::Pipeline::RunnableI->get_tmp_file($self->workdir,"approxAlignOutput","aat");
+  print "ApproxAlign: $aatfile written\n";
+
+  open(OUT,">$aatfile") || die "Could not write to file $aatfile\n";
+  for (my $i=0; $i < $n; $i++) {
+    print OUT $i . " " . ${$self->{'aat'}}[$i][0] . " " . ${$self->{'aat'}}[$i][1] . "\n";
   }
-  if(defined($this->{seqZ}) && ($aatLenZ != scalar(@{$this->{seqZ}}))) {
-    printf STDERR ("Unequal lengths for aatSeqZ (%d) and seqZ (%d).\n",$aatLenZ,scalar(@{$this->{'seqZ'}}));
-  }
+  close(OUT);
+  $self->aatfile($aatfile);
   return;
 }
 
 
-# Writes sequence to a file (filename will be created by Bio::EnsEMBL::Pipeline::RunnableI->get_tmp_filename
-# Bio::EnsEMBL::Pipeline::RunnableI must be in your path
-# and return the filename of the written file
 
-sub write {
-  my($this, $out) = @_;
-  my($n,$i);
+sub aatfile {
+  my $self = shift;
 
-  $n = scalar(@{$this->{'aat'}});
-  open(OUT,">$out") || die "Could not write to file $out\n";
-
-  for($i=0; $i < $n; $i++) {
-    print OUT $i . " " . ${$this->{'aat'}}[$i][0] . " " . ${$this->{'aat'}}[$i][1] . "\n";
-  }
- close(OUT);
-  return;
+  $self->{'aatfile'} = shift if @_ ;
+  return $self->{'aatfile'}
 }
 
 
