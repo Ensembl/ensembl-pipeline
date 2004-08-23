de7184659f4b311a2d9c7a45a0c45e229310c226
Val Curwen <vac@sanger.ac.uk>
2004-08-23 08:27:15 +0000

    Fix to make sure we don't send -init endbias twice on the command line

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Genewise.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Genewise.pm
@@ -140,7 +140,11 @@ sub align_protein {
 
 
   if ($self->endbias == 1) {
+    $command =~ s/-init endbias//;
+    $command =~ s/-splice flat//;
+
     $command .= " -init endbias -splice flat ";
+
   }
 
   if (($self->reverse) && $self->reverse == 1) {
