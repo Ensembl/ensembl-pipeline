0b7c625a24c98db76b778d55441efde65a2eb436
Laura Clarke <lec@sanger.ac.uk>
2004-11-15 16:38:55 +0000

    changes to make sure it sets all the stable ids

--- scripts/Pre-release-site/set_stable_ids.pl
+++ scripts/Pre-release-site/set_stable_ids.pl
@@ -6,8 +6,9 @@
 =head1 SYNOPSIS
  
   Script to create stable ids for the pre-release targetted gene set.
-  Sets stable ids to be the name of the evidence used to buikld the structure, 
-  plus a .version in case more than one gene is built from the same protein.
+  Sets stable ids to be the name of the evidence used to buikld the 
+  structure, plus a .version in case more than one gene is built from the 
+  same protein.
 
 =head1 DESCRIPTION
 
@@ -54,30 +55,62 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
 					    '-port'   => $port,
 					   );
 
-my $query = "insert into gene_stable_id values (?,?,?,now(),now())";
-my $sth = $db->prepare($query);
+my $gene_query = "insert into gene_stable_id values (?,?,?,".
+                 "FROM_UNIXTIME(?), FROM_UNIXTIME(?))";
+my $gene_sth = $db->dbc->prepare($gene_query);
+my $trans_query = "insert into transcript_stable_id values (?,?,?,".
+                 "FROM_UNIXTIME(?), FROM_UNIXTIME(?))";
+my $trans_sth = $db->prepare($trans_query);
+my $tln_query = "insert into translation_stable_id values (?,?,?,".
+                 "FROM_UNIXTIME(?), FROM_UNIXTIME(?))";
+my $tln_sth = $db->prepare($tln_query);
+my $exon_query = "insert into exon_stable_id values (?,?,?,".
+                 "FROM_UNIXTIME(?), FROM_UNIXTIME(?))";
+my $exon_sth = $db->prepare($exon_query);
+
+my $time = time;
 
 GENE:
+foreach my $gene_id(@{$db->get_GeneAdaptor->list_dbIDs}) {
+  print "gene id $gene_id\n";
 
-
-foreach my $gene_id(@{$db->get_GeneAdaptor->list_geneIds}) {
-  print STDERR "gene id $gene_id\n";
-  
   my $gene = $db->get_GeneAdaptor->fetch_by_dbID($gene_id);
   my @exons = @{$gene->get_all_Exons};
-  my @sf = @{$exons[0]->get_all_supporting_features};
-
   my $protein_id;
-  foreach my $f(@sf){
-    last if defined $protein_id;
-    eval{
-      $protein_id = $f->hseqname;
+ EXON:foreach my $e(@exons){
+    my @sf = @{$e->get_all_supporting_features};
+    foreach my $f(@sf){
+      last EXON if defined $protein_id;
+      eval{
+        $protein_id = $f->hseqname;
+      }
     }
   }
-  print "protein_id: $protein_id\n";
-  next GENE unless defined $protein_id;
+  if(!$protein_id){
+    print "Gene ".$gene->dbID." seems to have no supporting features\n";
+    next GENE;
+  }
+  if(!$proteins{$protein_id}){
+    $proteins{$protein_id} = 1;
+  }
+  my $stable_id = "Built_from_".$protein_id;
+  print "Stable_id ".$stable_id."\n";
+  $gene_sth->execute($gene_id,$stable_id, $proteins{$protein_id}, 
+                $time, $time);
+  my $exon_count = 1;
+  foreach my $exon(@exons){
+    $exon_sth->execute($exon->dbID, $stable_id."_".$exon_count, 
+                  $proteins{$protein_id}, $time, $time);
+    $exon_count++;
+  }
+  foreach my $transcript(@{$gene->get_all_Transcripts}){
+    $trans_sth->execute($transcript->dbID, $stable_id, 
+                        $proteins{$protein_id}, $time, $time);
+    if($transcript->translation){
+      $tln_sth->execute($transcript->translation->dbID, $stable_id,
+                        $proteins{$protein_id}, $time, $time);
+    }
+  }
   $proteins{$protein_id}++;
-
-  $sth->execute($gene_id,$protein_id, $proteins{$protein_id});
-
 }
+
