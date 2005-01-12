f89efd8556fddb1980f56a6eb2da63c27b2035bf
Dan T Andrews <dta@sanger.ac.uk>
2005-01-12 17:46:24 +0000

    Adding script to dump the longest transcript from every protein coding gene in a core database.  This is used to make the blast database for the *Paralogue Prediction* process.

---
+++ scripts/dump_genes_from_core.pl
@@ -0,0 +1,108 @@
+#! /usr/local/ensembl/bin/perl -w
+
+use strict;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+
+
+my $db = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
+                    -dbname => 'homo_sapiens_core_27_35a',
+#                    -dbname => 'mus_musculus_core_27_33c',
+#                    -dbname => 'rattus_norvegicus_core_27_3e',
+		    -host   => 'ecs2',
+		    -user   => 'ensro',
+		    -port   => 3365);
+
+my $slice_adaptor     = $db->get_SliceAdaptor;
+my $gene_adaptor      = $db->get_GeneAdaptor;
+
+# Fetch all genes from all chromosomes
+
+my @genes;
+
+foreach my $chr (@{$slice_adaptor->fetch_all('chromosome')}){
+
+  print STDERR "Chr " .  $chr->name . "\t";
+
+  my $chr_slice = $slice_adaptor->fetch_by_name($chr->name);
+  my $genes = $gene_adaptor->fetch_all_by_Slice($chr_slice);
+
+  print STDERR scalar @$genes . " genes\n";
+
+  push @genes, @$genes;
+}
+
+
+# Process each gene.
+
+foreach my $gene (@genes) {
+
+  next if (($gene->type =~ /pseudogene/i) ||
+	   ($gene->type =~ /putative/i) ||
+	   ($gene->type =~ /obsolete/i) ||
+	   ($gene->type =~ /Novel_Transcript/i));
+
+  my $transcript = &longest_transcript($gene);
+
+  next unless $transcript;
+
+  print STDERR join ("\t", ($gene->stable_id, 
+			    $transcript->stable_id,
+			    $transcript->translation->stable_id)
+		    ) . "\n";
+
+  print STDOUT '>' . $gene->stable_id . "\t" . 
+    $transcript->stable_id . "\t" . $transcript->translation->stable_id . "\n";
+
+#  my $seq = $transcript->spliced_seq;
+  my $seq = $transcript->translateable_seq;
+
+
+  # Trim any stop codons TAA, TGA, TGG from sequence ends.
+  $seq =~ s/TGA$//i;
+  $seq =~ s/TAA$//i;
+  $seq =~ s/TAG$//i;
+
+
+  # Add line breaks
+  $seq =~ s/(.{60})/$1\n/g;
+
+  print STDOUT uc($seq) . "\n";
+}
+
+sub longest_transcript ($) {
+  my $gene = shift;
+
+  my $longest = 0;
+  my $longest_transcript;
+
+  my $translatable = 0;
+
+  foreach my $transcript (@{$gene->get_all_Transcripts}){
+
+    eval {
+      $transcript->translate;
+    };
+
+    next if $@;
+
+    $translatable = 1;
+
+#     my $length = $transcript->spliced_seq =~ tr/[ATGCatgcNn]//;
+     my $length = $transcript->translateable_seq =~ tr/[ATGCatgcNn]//;
+
+     if ($length > $longest){
+       $longest = $length;
+       $longest_transcript = $transcript
+     }
+  }
+
+
+  unless ($longest_transcript){
+    warn("Gene [" . $gene->stable_id . 
+	 "] does not have a transcript with a translation.");
+    $longest_transcript = 0;
+  }
+
+
+  return $longest_transcript
+}
