4020774d89999f4dba70aedba242e297f69137ae
Laura Clarke <lec@sanger.ac.uk>
2005-03-30 14:41:39 +0000

    code for settingup stable ids for pre release site

--- scripts/Pre-release-site/set_stable_ids.pl
+++ scripts/Pre-release-site/set_stable_ids.pl
@@ -29,6 +29,7 @@ use strict;
 use Getopt::Long;
 
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Utils::Exception qw( deprecate throw warning );
 
 my $host;
 my $port;
@@ -55,29 +56,57 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
 					   );
 
 my $query = "insert into gene_stable_id values (?,?,?,now(),now())";
-my $sth = $db->prepare($query);
-
+my $sth = $db->dbc->prepare($query);
+my $trans_query = "insert into transcript_stable_id ".
+  "values (?,?,?,now(),now())";
+my $trans_sth = $db->dbc->prepare($trans_query);
+my $translation_query = "insert into translation_stable_id ".
+  "values (?,?,?,now(),now())";
+my $translation_sth = $db->dbc->prepare($translation_query);
+my $exon_query = "insert into exon_stable_id ".
+  "values (?,?,?,now(),now())";
+my $exon_sth = $db->dbc->prepare($exon_query);
 GENE:
-
-
-foreach my $gene_id(@{$db->get_GeneAdaptor->list_geneIds}) {
+foreach my $gene_id(@{$db->get_GeneAdaptor->list_dbIDs}) {
   print STDERR "gene id $gene_id\n";
-  
   my $gene = $db->get_GeneAdaptor->fetch_by_dbID($gene_id);
-  my @exons = @{$gene->get_all_Exons};
-  my @sf = @{$exons[0]->get_all_supporting_features};
-
-  my $protein_id;
-  foreach my $f(@sf){
-    last if defined $protein_id;
-    eval{
-      $protein_id = $f->hseqname;
-    }
-  }
-  print "protein_id: $protein_id\n";
+  
+  my $protein_id = get_protein_id($gene);
+  print "Have protein id ".$protein_id."\n";
   next GENE unless defined $protein_id;
   $proteins{$protein_id}++;
-
   $sth->execute($gene_id,$protein_id, $proteins{$protein_id});
-
+  foreach my $transcript(@{$gene->get_all_Transcripts}){
+    $trans_sth->execute($transcript->dbID, $protein_id, 
+                        $proteins{$protein_id});
+    my $translation = $transcript->translation;
+    $translation_sth->execute($translation->dbID, $protein_id,
+                       $proteins{$protein_id});
+    my $exon_count = 1;
+    foreach my $exon(@{$transcript->get_all_Exons}){
+      my $stable_id = $protein_id.".".$exon_count;
+      $exon_count++;
+      $exon_sth->execute($exon->dbID, $stable_id, 
+                         $proteins{$protein_id});
+    }
+  }
 }
+
+
+sub get_protein_id{
+  my ($gene) = @_;
+ 
+  my @exons = @{$gene->get_all_Exons};
+  my $protein_id;
+  foreach my $exon(@exons){
+    foreach my $sf(@{$exon->get_all_supporting_features}){
+      $protein_id = $sf->hseqname;
+      return $protein_id if($protein_id);
+    }
+  }
+  if(!$protein_id){
+    throw("Found no protein id for ".$gene->dbID);
+  }
+}
+
+
