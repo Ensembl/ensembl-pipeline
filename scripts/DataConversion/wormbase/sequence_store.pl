2814bec99747927e9e5e15ec9933fb92f6c2c3d6
Bronwen Aken <ba1@sanger.ac.uk>
2005-12-13 10:00:20 +0000

    Modified to skip MtDNA

--- scripts/DataConversion/wormbase/sequence_store.pl
+++ scripts/DataConversion/wormbase/sequence_store.pl
@@ -10,6 +10,7 @@ use Bio::EnsEMBL::DBSQL::DBAdaptor;
 
 $| = 1;
 
+
 my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(-host   => $WB_DBHOST,
 					    -user   => $WB_DBUSER,
 					    -dbname => $WB_DBNAME,
@@ -17,20 +18,21 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(-host   => $WB_DBHOST,
 					    -port   => $WB_DBPORT,
 					   );
 
-my $clone_cs = &store_coord_system($db, $WB_CLONE_SYSTEM_NAME, 
-                                   '', 0, 1, 1, 2);
-my $chromosome_cs = &store_coord_system($db, $WB_CHROMOSOME_SYSTEM_NAME, 
-                                        $WB_AGP_TYPE, 1, 0, 1, 1);
-
+#$db, $name, $version, $sequence_level, $default, $rank where $sequence_level and $default are binary (0=false, 1=true)
+my $clone_cs = &store_coord_system($db, $WB_CLONE_SYSTEM_NAME, $WB_NEW_COORD_SYSTEM_VERSION, 1, 1, 2);  #($db, $WB_CLONE_SYSTEM_NAME, '', 0, 1, 1, 2)
+my $chromosome_cs = &store_coord_system($db, $WB_CHROMOSOME_SYSTEM_NAME, $WB_NEW_COORD_SYSTEM_VERSION, 0, 1, 1);  #($db, $WB_CHROMOSOME_SYSTEM_NAME, $WB_AGP_TYPE, 1, 0, 1, 1)
+					
 foreach my $chromosome_info(@{$WB_CHR_INFO}) {
+  next if ($chromosome_info->{'chr_name'} eq 'MtDNA'); #see insert_MtDNA
 
-  if($chromosome_info->{'agp_file'} && $chromosome_info->{'gff_file'}){
-    print "handling ".$chromosome_info->{'chr_name'}." with files ".$chromosome_info->{'agp_file'}.
+  if($chromosome_info->{'agp_file'} && $chromosome_info->{'gff_file'} && $chromosome_info->{'dna_file'}){
+    print "Handling ".$chromosome_info->{'chr_name'}." with files ".$chromosome_info->{'agp_file'}.
       " and ".$chromosome_info->{'gff_file'}."\n" if($WB_DEBUG);
-
+    
     my $chromosome = &store_slice($db, $chromosome_info->{'chr_name'}, 1, $chromosome_info->{'length'}, 1, $chromosome_cs);
-
-    open(FH, $chromosome_info->{'agp_file'}) or die "couldn't open ".$chromosome_info->{'agp_file'}." $!";
+        
+    my $readfile = $WB_workDIR."".$chromosome_info->{'agp_file'};
+    open(FH, "<$readfile") or die "couldn't open ".$readfile." $!";
     my $fh = \*FH;
     my ($seq_ids, $non_ids) = &get_seq_ids($fh); # getting embl accs from agp
     open(NON, "+>>".$WB_SEQ_IDS) or die "couldn't open ".$WB_SEQ_IDS." $!";
@@ -44,7 +46,7 @@ foreach my $chromosome_info(@{$WB_CHR_INFO}) {
     my %seqs = %{&get_sequences_pfetch($seq_ids, $pfetch)};
     my %chr_hash = %{&agp_parse($fh, 
 				$chromosome->adaptor->get_seq_region_id($chromosome), 
-				$WB_AGP_TYPE)};
+				$WB_NEW_COORD_SYSTEM_VERSION)};
     my %contig_id;
     foreach my $id(keys(%seqs)){
       my $seq = $seqs{$id};
