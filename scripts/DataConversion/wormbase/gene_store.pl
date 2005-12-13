67f587ef5fedc225e6f043c63c2c6cb946e9cbeb
Bronwen Aken <ba1@sanger.ac.uk>
2005-12-13 09:35:29 +0000

    Use $WB_NEW_COORD_SYSTEM_VERSION instead of $WB_AGP_TYPE when fetching chromosome.

--- scripts/DataConversion/wormbase/gene_store.pl
+++ scripts/DataConversion/wormbase/gene_store.pl
@@ -34,7 +34,7 @@ foreach my $chromosome_info(@{$WB_CHR_INFO}) {
   print "handling ".$chromosome_info->{'chr_name'}." with files ".$chromosome_info->{'agp_file'}." and ".$chromosome_info->{'gff_file'}."\n" if($WB_DEBUG);
   
 
-  my $chr = $db->get_SliceAdaptor->fetch_by_region('Chromosome', $chromosome_info->{'chr_name'}, 1, ($chromosome_info->{'length'}, 1, $WB_AGP_TYPE));
+  my $chr = $db->get_SliceAdaptor->fetch_by_region('Chromosome', $chromosome_info->{'chr_name'}, 1, ($chromosome_info->{'length'}, 1, $WB_NEW_COORD_SYSTEM_VERSION));
  
   my $genes = &parse_gff($chromosome_info->{'gff_file'}, $chr, $analysis);
   &write_genes($genes, $db);
@@ -46,7 +46,7 @@ foreach my $chromosome_info(@{$WB_CHR_INFO}) {
   my @genes = @{$chr->get_all_Genes};
   open(TRANSLATE, "+>>".$WB_NON_TRANSLATE) or die "couldn't open ".$WB_NON_TRANSLATE." $!";
   TRANSLATION: foreach my $gene(@genes){
-      my $translation = &translation_check($gene);
+      my $translation = &translation_check($gene,$db);
       if($translation){
 	next TRANSLATION;
       }else{
