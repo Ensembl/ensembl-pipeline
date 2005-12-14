d7ca7ae344e7f40b933b3fe9b06abaf3f13b1984
Mustapha Larbaoui <ml6@sanger.ac.uk>
2005-12-14 11:19:26 +0000

    get port number from ~/.netrc file

--- scripts/Finished/make_input_ids.pl
+++ scripts/Finished/make_input_ids.pl
@@ -6,18 +6,18 @@ make_input_ids.pl
 
 =head1 SYNOPSIS
 
-make_input_ids.pl -p_host ecs1a -p_user user -p_pass **** -p_name pipe_mouse -seq_region chr12-03
+make_input_ids.pl -p_host ecs1a -p_user user -p_pass **** -p_name pipe_mouse -set chr12-03
 
 =head1 DESCRIPTION
 
 This script allows to prime the pipeline database with the input_ids of the seq_region name(s) provided.
-If no values are provided for the database connexion (login and password), the ~/.netrc file will be checked. 
+If no values are provided for the database connexion (login, password and port), the ~/.netrc file will be checked. 
 See Net::Netrc module for more details.
 
 =head1 OPTIONS
 
     -p_host    (default: otterpipe1) host name for database (gets put as host= in locator)
-    -p_port    (default: 3302) For RDBs, what port to connect to (port= in locator) 
+    -p_port    (check the ~/.netrc file) For RDBs, what port to connect to (port= in locator) 
     -p_name    For RDBs, what name to connect to (p_name= in locator)
     -p_user    (check the ~/.netrc file) For RDBs, what username to connect as (user= in locator)
     -p_pass    (check the ~/.netrc file) For RDBs, what password to use (pass= in locator)
@@ -25,11 +25,11 @@ See Net::Netrc module for more details.
     -logic_name (default: SubmitContig) the logic_name of the analysis object which needs to be 
                 associated with these entries 
 
-	-seqreg_name the seq_region name you want to prime (could be used several times)
+	-set|name the seq_region name you want to prime (could be used several times)
 	-cs (default: chromosome) the coordinate system associated with the seq_region name 
     -cs_version (default: Otter) the version of the coord system you want 
     -target_cs (default: contig) the target coordinate system you want slices in 
-    -target_cs_version the version of the target coord system you want
+    -target_cs_version (optional) the version of the target coord system you want
 
     -verbose if you want more information about what the script is doing
     -help      Displays script documentation with PERLDOC
@@ -52,7 +52,7 @@ use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 my $host = 'otterpipe1';
 my $user;
 my $pass;
-my $port = 3302;
+my $port;
 my $p_name;
 my $logic_name = 'SubmitContig';
 my $cs         = 'chromosome';
@@ -68,7 +68,7 @@ my $help = 0;
 	'p_user:s'            => \$user,
 	'p_pass:s'            => \$pass,
 	'p_name:s'            => \$p_name,
-	'seqreg_name:s'       => \@seqreg_name,
+	'set|name:s'       => \@seqreg_name,
 	'cs:s'                => \$cs,
 	'cs_version:s'        => \$cs_version,
 	'target_cs:s'         => \$target_cs,
@@ -82,20 +82,34 @@ if ($help) {
 	exec( 'perldoc', $0 );
 }
 
-if ( !$user || !$pass ) {
+if ( !$user || !$pass || !$port ) {
 	my $ref = Net::Netrc->lookup($host);
-	throw("Need to provide pipeline login ( -p_user ) and password ( -p_pass )")
+	throw(
+		"~/.netrc file unavailable; 
+			need to provide missing parameter: 
+			user [$user]; password [$pass]; port [$port]"
+	  )
 	  unless ($ref);
-	$user = $ref->login;
-	$pass = $ref->password;
+	$user = $ref->login    unless $user;
+	$pass = $ref->password unless $pass;
+	$port = $ref->account  unless $port;
+	throw(
+		"Missing parameter in the ~/.netrc file:\n
+			machine " .  ( $host || 'missing' ) . "\n
+			login " .    ( $user || 'missing' ) . "\n
+			password " . ( $pass || 'missing' ) . "\n
+			account "
+		  . ( $port || 'missing' ) . " (should be used to set the port number)"
+	  )
+	  unless ( $user && $pass && $port );
 }
 
 if ( !$p_name ) {
-	throw("You must specify a database name");
+	throw("You must specify a database name (-p_name option");
 }
 
 if ( !@seqreg_name ) {
-	throw("You must at least specify one seq_region name");
+	throw("You must at least specify one seq_region name (-set option)");
 }
 
 my $db = new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor(
