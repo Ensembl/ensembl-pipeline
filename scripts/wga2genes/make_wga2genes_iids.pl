a0383deee81d07f15e3be0224c8b625f9b51704f
Kevin Howe <klh@ebi.ac.uk>
2006-02-01 12:01:25 +0000

    Now makes use of compara database to make input ids that do not intersect relevant chains

--- scripts/wga2genes/make_wga2genes_iids.pl
+++ scripts/wga2genes/make_wga2genes_iids.pl
@@ -10,6 +10,7 @@ use strict;
 use Getopt::Long;
 
 use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
 my (
     $dbname,
@@ -17,37 +18,74 @@ my (
     $dbuser,
     $dbport,
     $dbpass,
-    $gene_dbname,
-    $gene_dbhost,
-    $gene_dbport,
-    $gene_dbuser,
-    $gene_dbpass,
+    $query_dbname,
+    $query_dbhost,
+    $query_dbport,
+    $query_dbuser,
+    $query_dbpass,
+    $target_dbname,
+    $target_dbhost,
+    $target_dbport,
+    $target_dbuser,
+    $target_dbpass,
+    $compara_dbname,
+    $compara_dbhost,
+    $compara_dbport,
+    $compara_dbuser,
+    $compara_dbpass,
+    $slice_name,
     $source_type,
-    $target_logic,
+    $source_align_type,
+    $logic_name,
     $write,
 );
 
 $dbuser = 'ensro';
 $dbport = 3306;
-$gene_dbuser = 'ensro';
-$gene_dbport = 3306;
+$query_dbuser = 'ensro';
+$query_dbport = 3306;
+$target_dbuser = 'ensro';
+$target_dbport = 3306;
+$compara_dbuser = "ensro";
+$compara_dbport = 3306;
 
 $source_type = 'protein_coding';
+$source_align_type = 'BLASTZ_NET';
 
 &GetOptions(
+            
             'dbname=s' => \$dbname,
             'dbuser=s' => \$dbuser,
             'dbhost=s' => \$dbhost,
             'dbport=s' => \$dbport,
             'dbpass=s' => \$dbpass,
-            'genedbname=s' => \$gene_dbname,
-            'genedbhost=s' => \$gene_dbhost,
-            'genedbport=s' => \$gene_dbport,
-            'genedbuser=s' => \$gene_dbuser,
-            'genedbpass=s' => \$gene_dbpass,
-            'targetlogic=s' => \$target_logic,
+            'querydbname=s' => \$query_dbname,
+            'querydbhost=s' => \$query_dbhost,
+            'querydbport=s' => \$query_dbport,
+            'querydbuser=s' => \$query_dbuser,
+            'querybpass=s' => \$query_dbpass,
+            'targetdbname=s' => \$target_dbname,
+            'targetdbhost=s' => \$target_dbhost,
+            'targetdbport=s' => \$target_dbport,
+            'targetdbuser=s' => \$target_dbuser,
+            'targetbpass=s'  => \$target_dbpass,
+            'comparadbname=s' => \$compara_dbname,
+            'comparadbhost=s' => \$compara_dbhost,
+            'comparadbport=s' => \$compara_dbport,
+            'comparadbuser=s' => \$compara_dbuser,
+            'comparadbpass=s' => \$compara_dbpass,
+            'logic=s' => \$logic_name,
+            'aligntype=s'   => \$source_align_type,
+            'genetype=s'    => \$source_type,
+            'slice=s'       => \$slice_name,
             'write' => \$write,
             );
+
+
+if ($slice_name !~ /^[^:]+:/) {
+  die "You must give an Ensembl slice identifier\n";
+}
+
 
 my $db = Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor->new(
 	'-dbname' => $dbname,
@@ -56,81 +94,212 @@ my $db = Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor->new(
 	'-port' => $dbport,
 );
 
-my $gene_db = Bio::EnsEMBL::DBSQL::DBAdaptor->
+my $ana = $db->get_AnalysisAdaptor->fetch_by_logic_name($logic_name);
+if (not defined $ana) {
+  die "Could not find analysis with logic name '$logic_name' in pipe db\n";
+}
+
+
+
+my ($query_db, $target_db, $compara_db);
+
+$query_db = Bio::EnsEMBL::DBSQL::DBAdaptor->
     new(
-	'-dbname' => $gene_dbname,
-	'-host' => $gene_dbhost,
-	'-port' => $gene_dbport,
-	'-user' => $gene_dbuser,
-        '-pass' => $gene_dbpass,
+	'-dbname' => $query_dbname,
+	'-host' => $query_dbhost,
+	'-port' => $query_dbport,
+	'-user' => $query_dbuser,
+        '-pass' => $query_dbpass,
         );
 
-my $ana = $db->get_AnalysisAdaptor->fetch_by_logic_name($target_logic);
-if (not defined $ana) {
-  die "Could not find analysis with logic name '$target_logic' in pipe db\n";
+if (not defined $target_dbname) {
+  $target_db = $db;
+} else {
+ $target_db = Bio::EnsEMBL::DBSQL::DBAdaptor->
+    new(
+	'-dbname' => $target_dbname,
+	'-host' => $target_dbhost,
+	'-port' => $target_dbport,
+	'-user' => $target_dbuser,
+        '-pass' => $target_dbpass,
+        );
+
 }
 
+if (defined $compara_dbname) {
+  $compara_db = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->
+      new(
+          '-dbname' => $compara_dbname,
+          '-host' => $compara_dbhost,
+          '-port' => $compara_dbport,
+          '-user' => $compara_dbuser,
+          '-pass' => $compara_dbpass,
+          );
+}
 
-my @gene_clusters;
+my $q_species =
+    $query_db->get_MetaContainerAdaptor->get_Species->binomial;
+my $t_species =
+    $target_db->get_MetaContainerAdaptor->get_Species->binomial;
 
-my @slices = @{$gene_db->get_SliceAdaptor->fetch_all('toplevel')};
+my ($coord_sys, $version, $seq_region_name) = split(/:/, $slice_name);
+
+my @slices;
+if (not $seq_region_name) {
+  @slices = @{$query_db->get_SliceAdaptor->fetch_all($coord_sys)}; 
+} else {
+  @slices = ($query_db->get_SliceAdaptor->fetch_by_name($slice_name));
+}
+
+my ($gdb_adap, $q_gdb, $t_gdb, $mlss, $gaba);
+
+if (defined $compara_db) {
+  $gdb_adap = $compara_db->get_GenomeDBAdaptor;
+  $q_gdb = $gdb_adap->fetch_by_name_assembly($q_species);
+  $t_gdb = $gdb_adap->fetch_by_name_assembly($t_species);
+
+  $mlss = $compara_db->get_MethodLinkSpeciesSetAdaptor
+      ->fetch_by_method_link_type_GenomeDBs($source_align_type,
+                                          [$q_gdb, $t_gdb]);
+  $gaba = $compara_db->get_GenomicAlignBlockAdaptor;
+}
+
 foreach my $sl (@slices) {
   next if $sl->seq_region_name =~ /^MT$/;
-
+ 
   my @genes = @{$sl->get_all_Genes};
   @genes = grep { $_->biotype eq $source_type } @genes;
-  @genes = sort { $a->start <=> $b->start } @genes;
-  
-  my $new_slice = 1;
+
+  my @gene_regions;
+
   foreach my $g (@genes) {
-    my $g_start = $g->start + $sl->start - 1;
-    my $g_end   = $g->end   + $sl->start - 1;
-
-    if ($new_slice or
-        $gene_clusters[-1]->{end} < $g_start) {
-      push @gene_clusters, {
-        coordsys => $sl->coord_system->name,
-        name  => $g->slice->seq_region_name,
-        start => $g_start,
-        end   => $g_end,
-        genes => [$g],
-      };
-      $new_slice = 0;
-    } else {
-      push @{$gene_clusters[-1]->{genes}}, $g;
-      if ($gene_clusters[-1]->{end} < $g_end) {
-        $gene_clusters[-1]->{end} = $g_end;
-      }
-    } 
+    push @gene_regions, {
+      qstart => $g->start + $sl->start - 1,
+      qend   => $g->end   + $sl->start - 1,
+    };
   }
-}
 
-foreach my $c (@gene_clusters) {
-  my $iid = sprintf("%s::%s:%d:%d:", 
-                    $c->{coordsys},
-                    $c->{name},
-                    $c->{start},
-                    $c->{end});
+  @gene_regions = sort { $a->{qstart} <=> $b->{qstart} } @gene_regions;
 
- if ($write) {
-   my $s_inf_cont = $db->get_StateInfoContainer;
-   
-   eval {
-     $db->get_StateInfoContainer->
-         store_input_id_analysis( $iid, $ana, '' );
-   };
-   if ($@) {
-     print STDERR "Input id $iid already present\n";
-   } else {
-     print STDERR "Stored input id $iid\n";
-   }       
- } else {
-    printf("INSERT into input_id_analysis(input_id, input_id_type, analysis_id) " . 
-           " values('%s', '%s', %d);\n", 
-           $iid, 
-           $ana->input_id_type,
-           $ana->dbID);
+
+  my (%chains, @chains, @ov_chains);
+
+  if (defined $compara_db) {
+    my $dnafrag = $compara_db->get_DnaFragAdaptor->
+        fetch_by_GenomeDB_and_name($q_gdb,
+                                   $sl->seq_region_name);
     
+    for(my $gen_start = $sl->start; $gen_start < $sl->end; $gen_start += 1000000) {
+      my $gen_end = $gen_start + 1000000 - 1;
+      $gen_end = $sl->end if $gen_end > $sl->end;
+      
+      #printf(STDERR "Fetching blocks for $source_align_type %s %d %d...\n", 
+      #       $dnafrag->name, 
+      #       $gen_start,
+      #       $gen_end);
+
+      my $gen_al_blocks =
+          $gaba->fetch_all_by_MethodLinkSpeciesSet_DnaFrag($mlss,
+                                                           $dnafrag,
+                                                           $gen_start,
+                                                           $gen_end);
+      
+      # printf(STDERR "Extracting chain information...\n");
+      
+      foreach my $block (@$gen_al_blocks) {
+        my $qga = $block->reference_genomic_align;
+        my ($tga) = @{$block->get_all_non_reference_genomic_aligns};
+        
+        my $chain_id =  $qga->genomic_align_group_id_by_type("chain");
+        
+        if ($block->reference_genomic_align->dnafrag_strand < 0) {
+          $block->reverse_complement;
+        }
+        
+        if (not exists $chains{$chain_id}) {
+          $chains{$chain_id} = {
+            qstart => $qga->dnafrag_start,
+            qend   => $qga->dnafrag_end,
+          };
+        } else {
+          if ($qga->dnafrag_start < $chains{$chain_id}->{qstart}) {
+            $chains{$chain_id}->{qstart} = $qga->dnafrag_start;
+          }
+          if ($qga->dnafrag_end > $chains{$chain_id}->{qend}) {
+            $chains{$chain_id}->{qend} = $qga->dnafrag_end;
+          }
+        }
+      }
+    }
+  }
+
+  map { push @chains, $chains{$_} } keys %chains;
+  @chains = sort { $a->{qstart} <=> $b->{qstart} } @chains;
+  
+  # remove chains that do not overlap with genes
+  my @gchains;
+  foreach my $ch (@chains) {
+    my $ov = 0;
+
+    foreach my $gr (@gene_regions) {
+      if ($gr->{qstart} > $ch->{qend}) {
+        last;
+      } elsif ($gr->{qend} >= $ch->{qstart}){ 
+        $ov = 1;
+        last;
+      }
+    }
+    
+    if ($ov) {
+      push @gchains, $ch;
+    }
+  }
+
+  my @sl_regs;
+
+  # merge ov_chains and gene clusters
+  foreach my $cm (sort { $a->{qstart} <=> $b->{qstart} } (@gchains, @gene_regions)) {
+    if (not @sl_regs or $sl_regs[-1]->{qend} < $cm->{qstart}) {
+      push @sl_regs, $cm;
+    } elsif ($cm->{qend} > $sl_regs[-1]->{qend}) {
+      $sl_regs[-1]->{qend} = $cm->{qend};
+    }
+  }
+  
+  foreach my $cl (@sl_regs) {
+    #printf("REGION %s %d %d %d\n",
+    #      $sl->seq_region_name,
+    #      $cl->{qstart},
+    #      $cl->{qend},
+    #      $cl->{qend} - $cl->{qstart} + 1);
+    #next;
+    
+    my $iid = sprintf("%s::%s:%d:%d:", 
+                      $sl->coord_system->name,
+                      $sl->seq_region_name,
+                      $cl->{qstart},
+                      $cl->{qend});
+    
+    if ($write) {
+      my $s_inf_cont = $db->get_StateInfoContainer;
+      
+      eval {
+        $db->get_StateInfoContainer->
+            store_input_id_analysis( $iid, $ana, '' );
+      };
+      if ($@) {
+        print STDERR "Input id $iid already present\n";
+      } else {
+        print STDERR "Stored input id $iid\n";
+      }       
+    } else {
+      printf("INSERT into input_id_analysis(input_id, input_id_type, analysis_id) " . 
+             " values('%s', '%s', %d);\n", 
+             $iid, 
+             $ana->input_id_type,
+             $ana->dbID);
+      
+    }
   }
 }
 
