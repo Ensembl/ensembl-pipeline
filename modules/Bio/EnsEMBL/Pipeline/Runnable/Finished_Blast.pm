e818a3303f17c8c94cd7d26a15ea05fc1efc722d
Leo Gordon <lg4@ebi.ac.uk>
2006-05-25 11:53:21 +0000

    no more diag output to STDOUT!

--- modules/Bio/EnsEMBL/Pipeline/Runnable/Finished_Blast.pm
+++ modules/Bio/EnsEMBL/Pipeline/Runnable/Finished_Blast.pm
@@ -14,7 +14,7 @@ use vars qw(@ISA);
 @ISA = qw(Bio::EnsEMBL::Pipeline::Runnable::Blast);
 
 BEGIN {
-    print "\nUSING " . __PACKAGE__ . "\n\n";
+    print STDERR "\nUSING " . __PACKAGE__ . "\n\n";
 }
 
 my %FASTA_HEADER;
