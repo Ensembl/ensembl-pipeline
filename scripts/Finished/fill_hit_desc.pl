f5bb09742e7c1338b9c8b79564099424e39567a9
Mustapha Larbaoui <ml6@sanger.ac.uk>
2006-10-05 09:49:02 +0000

    imported from ensembl-pipeline 19

---
+++ scripts/Finished/fill_hit_desc.pl
@@ -0,0 +1,114 @@
+#!/usr/local/bin/perl -w
+### fill_hit_desc ###
+
+use strict;
+## CREATED FROM TEMPLATE
+use Getopt::Long;
+use Bio::EnsEMBL::Pipeline::DBSQL::Finished::DBAdaptor;
+use Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch;
+use Data::Dumper;
+
+{
+    my $help   = 0;
+    my $dbhost = 'otterpipe1';
+    my $dbname = 'pipe_human';
+    my $dbuser = 'ottadmin';
+    my $dbpass = undef;
+    my $dbport = 3302;
+    my $table  = 'dna';
+    my $query  = undef;
+    my $left   = 0;
+    my $rev    = 0;
+    my $ana_id = 0;
+    GetOptions(
+	       'help|h'   => \$help,
+	       'dbhost=s' => \$dbhost,
+	       'dbname=s' => \$dbname,
+	       'dbuser=s' => \$dbuser,
+	       'dbpass=s' => \$dbpass,
+	       'dbport=s' => \$dbport,
+	       'table=s'  => \$table,
+	       'ana_id=s' => \$ana_id,
+	       'reverse'  => \$rev,
+	       'left'     => \$left,
+	       ) or useage();
+    useage() if $help;
+
+    my $db = Bio::EnsEMBL::Pipeline::DBSQL::Finished::DBAdaptor->new(
+        -host   => $dbhost,
+        -dbname => $dbname,
+        -user   => $dbuser,
+        -pass   => $dbpass,
+        -port   => $dbport,
+    );
+
+    my( $ids );
+    if (! -t STDIN) {
+        while (<>) {
+            push(@$ids, split);
+        }
+    }
+    elsif (@ARGV) {
+        @$ids = @ARGV;
+    } else {
+        if($left){
+            $query = "SELECT DISTINCT(f.hit_name)
+                        FROM ${table}_align_feature f LEFT JOIN hit_description h 
+                          ON f.hit_name = h.hit_name
+                       WHERE h.hit_name IS NULL 
+                    ORDER BY f.hit_name";
+        }
+        elsif($ana_id){
+            $query = "SELECT DISTINCT(f.hit_name)
+                        FROM ${table}_align_feature f LEFT JOIN hit_description h 
+                          ON f.hit_name = h.hit_name
+                       WHERE h.hit_name IS NULL 
+                         AND f.analysis_id = '${ana_id}'
+                    ORDER BY f.hit_name";
+        }else{
+            $query = "SELECT DISTINCT(f.hit_name) FROM ${table}_align_feature f ORDER BY f.hit_name";
+        }
+        $query .= " DESC" if $rev;
+
+        my $sth = $db->prepare($query);
+        $sth->execute();
+        while (my ($id) = $sth->fetchrow){
+            push(@$ids, $id);
+        }
+    }
+
+    my $seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch->new(
+        #-PFETCH_PORT    => 22101,
+        #-ARCHIVE_PORT   => 23101,
+        );
+    
+    my $chunk_size = 200;
+    for (my $i = 0; $i < @$ids; $i += $chunk_size) {
+        my $j = $i + $chunk_size - 1;
+        $j = $#$ids if $j > $#$ids;
+        my $chunk = [@$ids[$i..$j]];
+	    my $failed = $seqfetcher->write_descriptions($db, $chunk);
+        if (@$failed) {
+            warn "Failed to fetch:\n", map("  $_\n", @$failed);
+        }
+    }
+    print STDERR "FINISHED OK\n"
+}
+
+sub useage{
+    exec('perldoc', $0);
+    exit;
+}
+##END TEMPLATE##
+
+=pod
+
+=head1 NAME fill_hit_desc
+
+
+
+=head1 AUTHOR
+
+
+
+=cut
