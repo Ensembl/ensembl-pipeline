c53ba81faf57d223e7abf9744907f4be0c133141
Mustapha Larbaoui <ml6@sanger.ac.uk>
2007-04-02 10:43:09 +0000

    make the script more generic

--- scripts/Finished/create_agp_from_scaffold.pl
+++ scripts/Finished/create_agp_from_scaffold.pl
@@ -11,13 +11,13 @@ create_agp_from_scaffold.pl
 =head1 DESCRIPTION
 
 This script is used to refactor a scaffold which is a set of short sequences into clone sized pieces (150Kb). 
-It will generate an agp of the new assembly in the chromosome coordinates, the clones sequence files (fasta) and 
-GFF files for the internal gaps.
+( It can also be used to refactor any coord_system type ) It will generate an agp of the new assembly 
+into the chromosome coordinates, the clones sequence files (fasta) and GFF files for the internal gaps.
 
 here is an example commandline
 
 ./create_agp_from_scaffold.pl \
--scaffold_name chr1.47 \
+-seq_region_name chr1.47 \
 -host ens-livemirror \
 -port 3306 \
 -name bos_taurus_core_43_3 \
@@ -33,7 +33,10 @@ here is an example commandline
     -pass (check the ~/.netrc file)  For RDBs, what password to use (ppass= in locator)
     -port (check the ~/.netrc file)   For RDBs, what port to use (pport= in locator)
 
-    -scaffold_name	the scaffold name
+    -cs_name	(default:scaffold) coordinate system of the seq_region 
+    -seq_region_name	seq_region name
+    -start	The start of the slice on the sequence region
+    -end	The end of the slice on the sequence region
     -outdir		output directory
     -verbose 		display the chromosome -> scaffold -> contig coordinates mapping
     -help|h		displays this documentation with PERLDOC
@@ -61,7 +64,10 @@ my $pname = '';
 my $puser = '';
 my $ppass = '';
 
-my $scaffold_name;
+my $coord_sytem = 'scaffold';
+my $seq_region_name;
+my $start;
+my $end;
 my $output = '';
 my $verbose;
 
@@ -73,14 +79,17 @@ my $usage = sub { exec( 'perldoc', $0 ); };
 	'name:s'                => \$pname,
 	'user:s'                => \$puser,
 	'pass:s'                => \$ppass,
-	'scaffold_name=s'       => \$scaffold_name,
+	'cs_name:s'				=> \$coord_sytem,
+	'seq_region_name=s'     => \$seq_region_name,
+	'start:s'				=> \$start,
+	'end:s'					=> \$end,
 	'outdir=s'				=> \$output,
 	'verbose!'				=> \$verbose,
 	'h|help!'               => $usage
   )
   or $usage->();
 
-throw("No scaffold name given") unless ($scaffold_name);
+throw("No seq_region name given") unless ($seq_region_name);
 
 $output .= '/' if($output && $output !~ /\/$/);
 
@@ -104,15 +113,14 @@ my $slice_a              = $dba->get_SliceAdaptor();
 
 my $chromosome_cs = $cs_a->fetch_by_name( "chromosome" );;
 my $contig_cs = $cs_a->fetch_by_name("contig");
-my $scaffold_cs = $cs_a->fetch_by_name("scaffold");
+my $seq_region_cs = $cs_a->fetch_by_name($coord_sytem);
 
 
-my $scaffold_slice = $slice_a->fetch_by_region($scaffold_cs->name,$scaffold_name);
+my $slice = $slice_a->fetch_by_region($seq_region_cs->name,$seq_region_name,$start,$end);
 my $chromosome;
-my $contig_projection = $scaffold_slice->project($contig_cs->name);
+my $contig_projection = $slice->project($contig_cs->name);
 
 my $contigs = {};
-my $clones = {};
 
 foreach my $segment (@$contig_projection) {
       my $contig_slice = $segment->to_Slice();
@@ -122,55 +130,16 @@ foreach my $segment (@$contig_projection) {
       my $name = $contig_slice->seq_region_name();
       $contigs->{$name} = [$chr->start(),$chr->end()];
       print 'chr',$chr->seq_region_name(), ':', $chr->start(), '-',$chr->end(), ':', $chr->strand(),"\t(",$chr->length,")\t|\t",
-      		$scaffold_slice->seq_region_name(), ':', $segment->from_start(), '-',$segment->from_end(), "\t(",$segment->from_end()-$segment->from_start()+1,")\t|\t",
+      		$slice->seq_region_name(), ':', $segment->from_start(), '-',$segment->from_end(), "\t(",$segment->from_end()-$segment->from_start()+1,")\t|\t",
             $contig_slice->seq_region_name(), ':', $contig_slice->start(), '-',$contig_slice->end(), ':', $contig_slice->strand(), "\t(",$contig_slice->length,")\n" 
       if $verbose;
 }
 
-my $clone_length;
-my $clone_start;
-my $clone_end;
-my $gaps;
-my $i;
-
-foreach my $contig (sort { $a->[0] <=> $b->[0] } values %$contigs) {
-	my $contig_start   = $contig->[0];
-	my $contig_end     = $contig->[1];
-	my $contig_length  = $contig_end-$contig_start+1;
-	my $gap_start;
-	my $gap_end;
-	my $gap_length;
-	
-	# create clone if length >= 140Kb
-	if($clone_length && $clone_length>=140000) {
-		$i++;
-		my $clone_name = $scaffold_name.'_'.$i;
-		$clones->{$clone_name}->{coord} = [$clone_start,$clone_end,$clone_length];
-		$clones->{$clone_name}->{gaps} = $gaps;
-		$gaps = [];
-		$clone_length = 0;
-		$clone_start  = 0;
-		$clone_end    = 0;
-	}
-	
-	# save clone internal gaps
-	if($clone_end && ($clone_end+1) != $contig_start) {
-		$gap_length = $contig_start - $clone_end -1;
-		$gap_start = $clone_length+1;
-		$gap_end   = $gap_start + $gap_length -1;
-		
-		push @$gaps, [$gap_start,$gap_end];
-	}
-	
-	$clone_start  ||= $contig_start;
-	$clone_end      = $contig_end;
-	$clone_length  += $contig_length;
-	$clone_length  += $gap_length if $gap_length;
-}
+my $clones = &merge_contigs_into_clone_1($contigs);
 
 my $l;
 my $agp_out;
-open($agp_out, ">${output}${scaffold_name}.agp") or die "Can't create file $scaffold_name : $!";
+open($agp_out, ">${output}${seq_region_name}.agp") or die "Can't create file $seq_region_name : $!";
 
 foreach my $clone (sort {$clones->{$a}->{coord}->[0] <=> $clones->{$b}->{coord}->[0]} keys %$clones) {
 	my $asm_start = $clones->{$clone}->{coord}->[0];
@@ -203,6 +172,55 @@ foreach my $clone (sort {$clones->{$a}->{coord}->[0] <=> $clones->{$b}->{coord}-
 	}
 	
 	$gaps->dump($gap_out);
+}
+
+sub merge_contigs_into_clone_1 {
+	my ($contigs) = @_;
+	my $clones = {};
+	my $contigs_count = scalar (keys %$contigs);
+	my $count;
+	my $clone_length;
+	my $clone_start;
+	my $clone_end;
+	my $gaps;
+	my $i;
+	foreach my $contig (sort { $a->[0] <=> $b->[0] } values %$contigs) {
+		$count++;
+		my $contig_start   = $contig->[0];
+		my $contig_end     = $contig->[1];
+		my $contig_length  = $contig_end-$contig_start+1;
+		my $gap_start;
+		my $gap_end;
+		my $gap_length;
+
+		# save clone internal gaps
+		if($clone_end && ($clone_end+1) != $contig_start) {
+			$gap_length = $contig_start - $clone_end -1;
+			$gap_start = $clone_length+1;
+			$gap_end   = $gap_start + $gap_length -1;
+			
+			push @$gaps, [$gap_start,$gap_end];
+		}
+		
+		$clone_start  ||= $contig_start;
+		$clone_end      = $contig_end;
+		$clone_length  += $contig_length;
+		$clone_length  += $gap_length if $gap_length;
+		
+		# create clone if clone length >= 140Kb or last contig of the loop
+		if($clone_length && ($clone_length>=140000 || $count == $contigs_count) ) {
+			$i++;
+			my $clone_name = $seq_region_name.'_'.$i;
+			$clones->{$clone_name}->{coord} = [$clone_start,$clone_end,$clone_length];
+			$clones->{$clone_name}->{gaps} = $gaps;
+			$gaps = [];
+			$clone_length = 0;
+			$clone_start  = 0;
+			$clone_end    = 0;
+		}
+	}
+	
+	return $clones;
 }
 
 sub get_db_param {
