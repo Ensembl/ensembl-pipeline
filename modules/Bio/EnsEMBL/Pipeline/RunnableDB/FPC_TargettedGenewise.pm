bf995dd34341070411449d2ab853483120182a07
Bronwen Aken <ba1@sanger.ac.uk>
2007-05-29 16:06:32 +0000

    Now inherits from FPC_TargettedBase. Not tested.

--- modules/Bio/EnsEMBL/Pipeline/RunnableDB/FPC_TargettedGenewise.pm
+++ modules/Bio/EnsEMBL/Pipeline/RunnableDB/FPC_TargettedGenewise.pm
@@ -38,18 +38,13 @@ package Bio::EnsEMBL::Pipeline::RunnableDB::FPC_TargettedGenewise;
 use vars qw(@ISA);
 use strict;
 
-use Bio::EnsEMBL::Root;
-use Bio::EnsEMBL::Pipeline::RunnableDB;
-use Bio::EnsEMBL::Utils::Exception qw( throw ) ; 
-use Bio::EnsEMBL::Pipeline::RunnableDB::TargettedGenewise;
-use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Pipeline::Config::GeneBuild::Databases qw (
-							     GB_GW_DBNAME
-							     GB_GW_DBHOST
-							     GB_GW_DBUSER
-							     GB_GW_DBPASS
-							     GB_GW_DBPORT
-							    );
+                                                             GB_GW_DBNAME
+                                                             GB_GW_DBHOST
+                                                             GB_GW_DBUSER
+                                                             GB_GW_DBPASS
+                                                             GB_GW_DBPORT
+                                                            );
 
 use Bio::EnsEMBL::Pipeline::Config::GeneBuild::Sequences
   qw (
@@ -60,11 +55,6 @@ use Bio::EnsEMBL::Pipeline::Config::GeneBuild::Sequences
 use Bio::EnsEMBL::Pipeline::Config::GeneBuild::General
   qw (
       GB_INPUTID_REGEX
-     );
-
-use Bio::EnsEMBL::Pipeline::Config::GeneBuild::Scripts
-  qw (
-      GB_KILL_LIST
      );
 
 use Bio::EnsEMBL::Pipeline::Config::GeneBuild::Pmatch
@@ -78,91 +68,10 @@ use Bio::EnsEMBL::Pipeline::Config::GeneBuild::Targetted qw
    GB_TARGETTED_SOFTMASK
   );
 
-@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
+use Bio::EnsEMBL::Pipeline::RunnableDB::TargettedGenewise;
+use Bio::EnsEMBL::Pipeline::RunnableDB::FPC_TargettedBase;
 
-=head2 new
-
-    Title   :   new
-    Usage   :   $self->new(-DBOBJ       => $db,
-                           -INPUT_ID    => $id,
-			   -SEQFETCHER  => $sf,
-                           -ANALYSIS    => $analysis);
-
-    Function:   creates a Bio::EnsEMBL::Pipeline::RunnableDB::FPC_TargettedGeneWise object
-    Returns :   A Bio::EnsEMBL::Pipeline::RunnableDB::Gene_Builder object
-    Args    :   -db:      A Bio::EnsEMBL::DBSQL::DBAdaptor,
-                -input_id:   Contig input id (required), 
-                -seqfetcher: A Sequence Fetcher Object,
-                -analysis:   A Bio::EnsEMBL::Analysis (optional) 
-
-=cut
-
-sub new {
-  my ($class, @args) = @_;
-  #print STDERR "args @args\n";
-  my $self = $class->SUPER::new(@args);
-
-  # db, input_id, seqfetcher, and analysis objects are all set in
-  # in superclass constructor (RunnableDB.pm)
-
-  return $self;
-}
-
-=head2 make_seqfetcher
-
- Title   : make_seqfetcher
- Usage   :
- Function: if $index exists, 
-           returns a Bio::EnsEMBL::Pipeline::SeqFetcher::Getseqs, otherwise throws
- Example :
- Returns : Bio::DB::RandomAccessI
- Args    : $indexname - string
-
-
-=cut
-
-sub make_seqfetcher {
-  my ( $self, $index, $seqfetcher_class ) = @_;
-
-  my $seqfetcher;
-  
-  (my $class = $seqfetcher_class) =~ s/::/\//g;
-
-  throw ("Configuration-error !! There's no entry for GB_PROTEIN_SEQFETCHER in Sequences.pm\n") if (length($class)==0)  ;
-
-  require "$class.pm";
-
-  if(defined $index && $index ne ''){
-    my @db = ( $index );
-    
-    # make sure that your class is compatible with the index type
-    $seqfetcher = "$seqfetcher_class"->new('-db' => \@db, );
-  }
-  else{
-    $self->throw("can't make seqfetcher\n");
-  }
-
-  return $seqfetcher;
-}
-
-
-=head2 fetch_input
-
- Title   : fetch_input
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub fetch_input{
-  my ($self,@args) = @_;
-
-  $self->make_targetted_runnables;
-}
+@ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB::FPC_TargettedBase);
 
 =head2 make_targetted_runnables
 
@@ -195,7 +104,7 @@ sub make_targetted_runnables {
     ($self->query->seq_region_name, $self->query->start, 
      $self->query->end, $GB_FINAL_PMATCH_LOGICNAME );
  
-  my $genewise_db = new Bio::EnsEMBL::DBSQL::DBAdaptor
+  my $targetted_db = new Bio::EnsEMBL::DBSQL::DBAdaptor
     (
      '-host'   => $GB_GW_DBHOST,
      '-user'   => $GB_GW_DBUSER,
@@ -205,8 +114,8 @@ sub make_targetted_runnables {
     );
   
   
-  $genewise_db->dnadb($self->db);
-  $self->output_db($genewise_db);
+  $targetted_db->dnadb($self->db);
+  $self->output_db($targetted_db);
   my %kill_list = %{$self->fill_kill_list};
 
   foreach my $feat(@features){
@@ -245,172 +154,5 @@ sub make_targetted_runnables {
   }
 
 }
-
-=head2 targetted_runnable
-
- Title   : targetted_runnable
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub targetted_runnable{
-  my ($self,$arg) = @_;
-
-  if (!defined($self->{'_targetted_runnables'})) {
-      $self->{'_targetted_runnables'} = [];
-  }
-  
-  if (defined($arg)) {
-      if ($arg->isa("Bio::EnsEMBL::Pipeline::RunnableI")) {
-	  push(@{$self->{'_targetted_runnables'}},$arg);
-      } else {
-	  $self->throw("[$arg] is not a Bio::EnsEMBL::Pipeline::RunnableI");
-      }
-  }
-  
-  return @{$self->{'_targetted_runnables'}};
-}
-
-=head2 run
-
- Title   : run
- Usage   :
- Function: runs the various runnables controlling the whole gene build in the right order
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub run {
-  my ($self) = @_;
-  my $count = 0;
-  #print STDERR "***Running targetted build***\n\n";
- TGE:
-  foreach my $tge($self->targetted_runnable){
-
-    eval{
-      $tge->fetch_input;
-    };
-
-    if($@){
-      $self->warn("problems fetching input for TargettedGenewise: [$@]\n");
-      next TGE;
-    }
-
-    eval{
-      $tge->run;
-    };
-
-    if($@){
-      $self->warn("problems running TargettedGenewise: [$@]\n");
-      next TGE;
-    }
-
-  }
-
-
-}
-
-=head2 output
-
- Title   : output
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub output{
-  my ($self) = @_;
-  my @output;
-  foreach my $tge($self->targetted_runnable){
-    push(@output, $tge->output);
-  }
-  return @output;
-}
-
-=head2 write_output
-
-    Title   :   write_output
-    Usage   :   $self->write_output
-    Function:   Writes output data to db
-    Returns :   
-    Args    :   
-
-=cut
-
-
-sub write_output {
-  my ($self) = @_;
- TGE:
-  foreach my $tge($self->targetted_runnable){
-    eval{
-      my @genes = $tge->write_output;
-    };
-    
-    if($@){
-      $self->warn("problems writing output for TargettedGenewise: [$@]\n");
-      next TGE;
-    }
-  }
-  # data has already been written out, so no need to do anything here.
-}
-
-sub convert_output {
-  my ($self) = @_;
-  # nothing to do
-}
-
-
-sub output_db {
-    my( $self, $output_db ) = @_;
-
-    if ($output_db) 
-    {
-	$output_db->isa("Bio::EnsEMBL::DBSQL::DBAdaptor")
-	    || $self->throw("Input [$output_db] isn't a Bio::EnsEMBL::DBSQL::DBAdaptor");
-	$self->{_output_db} = $output_db;
-    }
-    return $self->{_output_db};
-}
-
-
-=head2 fill_kill_list
-
- Title   : fill_kill_list
- Usage   : 
- Function: 
- Returns : 
- Args    : 
-
-=cut
-
-sub fill_kill_list {
-  my ($self) = @_;
-  my %kill_list;
-  open (KILL_LIST, "< $GB_KILL_LIST") or die "can't open $GB_KILL_LIST";
-  while (<KILL_LIST>) {
-
-    chomp;
-    my @list = split;
-    next unless scalar(@list); 	# blank or empty line
-    $kill_list{$list[0]} = 1;
-  }
-
-  close KILL_LIST or die "error closing $GB_KILL_LIST\n";
-
-  return \%kill_list;
-}
-
 
 1;
