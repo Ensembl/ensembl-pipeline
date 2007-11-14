02c14762d100bbc613ff63394b171595e4c9a933
Mustapha Larbaoui <ml6@sanger.ac.uk>
2007-11-14 15:28:15 +0000

    update sql code with write_access and hidden

--- scripts/Finished/delete_sequence_set.pl
+++ scripts/Finished/delete_sequence_set.pl
@@ -1,36 +1,36 @@
 #!/usr/local/bin/perl -w
 
-=head1 NAME 
+=head1 NAME
 
 delete_sequence_set.pl
 
 =head1 SYNOPSIS
 
-delete_sequence_set.pl -p_host otterpipe2 -p_port 3303 -p_name pipe_zebrafish -p_user otta -p_pass **** -show
+delete_sequence_set.pl -host otterpipe2 -port 3303 -dbname pipe_zebrafish -user otta -pass **** -show
 
-delete_sequence_set.pl -p_host otterpipe2 -p_port 3303 -p_name pipe_zebrafish -p_user otta -p_pass **** -delete -set chr1
+delete_sequence_set.pl -host otterpipe2 -port 3303 -dbname pipe_zebrafish -user otta -pass **** -delete -set chr1
 
 =head1 DESCRIPTION
 
-This script is used to either B<delete> sequence sets or B<show> the list of loaded sequence sets from 
-a new B<pipeline> database. The Connection parameters (login, password and port) are 
+This script is used to either delete sequence sets or show the list of loaded sequence sets from
+a new schema database. The Connection parameters (login, password and port) are
 retrieved from the ~/.netrc file if it exists or provided as command line arguments.
 
 =head1 OPTIONS
 
     -help         - display this pod
-    
-    -p_host       - (default: otterpipe1) host name for database (gets put as host= in locator)
-    -p_port       - (check the ~/.netrc file) For RDBs, what port to connect to (port= in locator) 
-    -p_name       - For RDBs, what name to connect to (p_name= in locator)
-    -p_user       - (check the ~/.netrc file) For RDBs, what username to connect as (user= in locator)
-    -p_pass       - (check the ~/.netrc file) For RDBs, what password to use (pass= in locator)
-    
-    -delete       - delete the B<set>
+
+    -host       - (default: otterpipe1) host name for database (gets put as host= in locator)
+    -port       - (check the ~/.netrc file) For RDBs, what port to connect to (port= in locator)
+    -dbname       - For RDBs, what name to connect to (p_name= in locator)
+    -user       - (check the ~/.netrc file) For RDBs, what username to connect as (user= in locator)
+    -pass       - (check the ~/.netrc file) For RDBs, what password to use (pass= in locator)
+
+    -delete       - delete the set
      -set|name     - set the set name to delete (could be used several times)
      or
     -show         - show the list of sequence sets present in the pipeline database
-    
+
 =head1 AUTHOR
 
 Mustapha Larbaoui, B<email> ml6@sanger.ac.uk
@@ -47,18 +47,21 @@ my $host = 'otterpipe1';
 my $user;
 my $pass;
 my $port;
-my $pname;
+my $dbname;
 my $delete;
 my @sets;
 my $show;
 my $help;
 
+my $table_header = '+' . ( '-' x 4 ) . '+' . ( '-' x 7 ) . '+' . ( '-' x 20 ) . '+' . ( '-' x 90 ) . '+' .( '-' x 12 ) . '+' .( '-' x 6 ) . '+' . "\n";
+my $line_format  = "|%-4s|%-7s|%-20s|%-90s|%-12s|%-6s|\n";
+
 &GetOptions(
-	'p_host:s'   => \$host,
-	'p_port:n'   => \$port,
-	'p_user:s'   => \$user,
-	'p_pass:s'   => \$pass,
-	'p_name:s'   => \$pname,
+	'host:s'   => \$host,
+	'port:n'   => \$port,
+	'user:s'   => \$user,
+	'pass:s'   => \$pass,
+	'dbname:s'   => \$dbname,
 	'delete!'    => \$delete,
 	'show!'      => \$show,
 	'set|name:s' => \@sets,
@@ -72,8 +75,8 @@ if ($help) {
 if ( !$user || !$pass || !$port ) {
 	my $ref = Net::Netrc->lookup($host);
 	throw(
-		"~/.netrc file unavailable; 
-			need to provide missing parameter: 
+		"~/.netrc file unavailable;
+			need to provide missing parameter:
 			user [$user]; password [$pass]; port [$port]"
 	  )
 	  unless ($ref);
@@ -91,7 +94,7 @@ if ( !$user || !$pass || !$port ) {
 	  unless ( $user && $pass && $port );
 }
 
-if ( !$pname ) {
+if ( !$dbname ) {
 	throw("You must specify a database name (-p_name)");
 }
 
@@ -100,7 +103,7 @@ my $db = new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor(
 	-user   => $user,
 	-pass   => $pass,
 	-port   => $port,
-	-dbname => $pname
+	-dbname => $dbname
 );
 
 if ($show) {
@@ -127,19 +130,20 @@ if ( !$delete && !$show ) {
 
 sub show_sequence_sets {
 	my ( $db, $s_sets ) = @_;
-	my $line =
-	  '+' . ( '-' x 9 ) . '+' . ( '-' x 29 ) . '+' . ( '-' x 38 ) . '+' . "\n";
-	print STDOUT $line;
+	print STDOUT $table_header;
 	print STDOUT
-	  sprintf( "|%-9s|%-29s|%-38s|\n", 'chr', 'sequence_set', 'description' );
-	print STDOUT $line;
+	  sprintf( $line_format, 'chr', 'version', 'sequence_set', 'description','write_access','hidden' );
+	print STDOUT $table_header;
 	my $sets = $s_sets || get_sequence_sets($db);
 	foreach my $set ( sort keys %$sets ) {
-		my $chr  = $sets->{$set}->[0];
-		my $desc = $sets->{$set}->[1];
-		print STDOUT sprintf( "|%-9s|%-29s|%-38s|\n", $chr, $set, $desc );
+		my $version  = $sets->{$set}->[1];
+		my $chr  = $sets->{$set}->[2];
+		my $desc = $sets->{$set}->[3];
+		my $wa = defined($sets->{$set}->[4]) ? $sets->{$set}->[4] : 'null';
+		my $hidden= defined($sets->{$set}->[5]) ? $sets->{$set}->[5] : 'null';;
+		print STDOUT sprintf( $line_format, $chr, $version, $set, $desc, $wa , $hidden );
 	}
-	print STDOUT $line;
+	print STDOUT $table_header;
 }
 
 sub delete_sequence_sets {
@@ -148,57 +152,60 @@ sub delete_sequence_sets {
 	foreach my $set (@$del_sets) {
 		if ( !$stored_sets->{$set} ) {
 			show_sequence_sets( $db, $stored_sets );
-			throw("Sequence set [$set] is not in pipeline database");
+			throw("Sequence set [$set] is not in $dbname database");
 		}
 	}
-	my $del_attrib = "DELETE FROM seq_region_attrib
-					    WHERE seq_region_id IN (
-					  		SELECT seq_region_id 
-							FROM seq_region
-							WHERE name = ? )";
-	my $att_sth      = $db->prepare($del_attrib);
-	my $del_assembly = "DELETE FROM assembly
-						WHERE asm_seq_region_id IN (
-							SELECT seq_region_id 
-							FROM seq_region
-							WHERE name = ? )";
-	my $asm_sth     = $db->prepare($del_assembly);
-	my $del_seq_reg = "DELETE FROM seq_region
-				    	WHERE name = ?";
-	my $seq_sth = $db->prepare($del_seq_reg);
+
+	my $sql = qq{
+		DELETE a,s,at
+		FROM assembly a, seq_region s, seq_region_attrib at
+		WHERE s.name = ?
+		AND ( a.asm_seq_region_id = s.seq_region_id
+		OR a.cmp_seq_region_id = s.seq_region_id )
+		AND at.seq_region_id = s.seq_region_id
+	};
+	my $sth     = $db->prepare($sql);
 
 	foreach my $set (@$del_sets) {
-		$att_sth->execute($set);
-		$asm_sth->execute($set);
-		$seq_sth->execute($set);
-		print STDOUT "Set $set deleted from pipeline\n";
-		
+		$sth->execute($set);
+		print STDOUT "Set $set deleted from $dbname\n";
+
 	}
-	
+
 	show_sequence_sets($db);
 }
 
 sub get_sequence_sets {
 	my ($db) = @_;
 	my $hash;
-	my $query =
-	  "SELECT a1.value AS chromosome, s.name AS seq_set, a2.value AS description
-	 FROM seq_region_attrib a1, seq_region_attrib a2, 
-		  attrib_type t1, attrib_type t2, seq_region s, coord_system c
-	 WHERE c.name = 'chromosome'
-	 AND c.version = 'Otter'
-	 AND c.coord_system_id = s.coord_system_id
-	 AND t1.code = 'chr'
-	 AND t2.code = 'description'
-	 AND t1.attrib_type_id = a1.attrib_type_id 
-	 AND t2.attrib_type_id = a2.attrib_type_id 
-	 AND a1.seq_region_id =  s.seq_region_id
-	 AND a2.seq_region_id =  s.seq_region_id
-	 ORDER BY chromosome";
+	my $query = qq{
+				SELECT s.seq_region_id,c.version,
+				            a1.value AS chromosome,
+				            s.name AS seq_set,
+				            a2.value AS description,
+				            a3.value AS write_access,
+				            a4.value AS hidden
+				FROM attrib_type t1, attrib_type t2,attrib_type t3, attrib_type t4,coord_system c, seq_region s
+				LEFT JOIN seq_region_attrib a1 ON (a1.seq_region_id =  s.seq_region_id AND t1.attrib_type_id = a1.attrib_type_id)
+				LEFT JOIN seq_region_attrib a2 ON (a2.seq_region_id =  s.seq_region_id AND t2.attrib_type_id = a2.attrib_type_id)
+				LEFT JOIN seq_region_attrib a3 ON (a3.seq_region_id =  s.seq_region_id AND t3.attrib_type_id = a3.attrib_type_id)
+				LEFT JOIN seq_region_attrib a4 ON (a4.seq_region_id =  s.seq_region_id AND t4.attrib_type_id = a4.attrib_type_id)
+				WHERE c.name = 'chromosome'
+				AND c.coord_system_id = s.coord_system_id
+				AND t1.code = 'chr'
+				AND t2.code = 'description'
+				AND t3.code = 'write_access'
+				AND t4.code = 'hidden'
+				AND t1.attrib_type_id = a1.attrib_type_id
+				AND t2.attrib_type_id = a2.attrib_type_id
+				AND a1.seq_region_id =  s.seq_region_id
+				AND a2.seq_region_id =  s.seq_region_id
+				ORDER BY  s.seq_region_id DESC
+					};
 	my $sth = $db->prepare($query);
 	$sth->execute();
-	while ( my ( $chr, $set, $desc ) = $sth->fetchrow ) {
-		$hash->{$set} = [ $chr, $desc ];
+	while ( my ( $sr_id, $version, $chr, $set, $desc, $wa, $hidden ) = $sth->fetchrow ) {
+		$hash->{$set} = [ $sr_id, $version, $chr, $desc, $wa, $hidden ];
 	}
 
 	return $hash;
