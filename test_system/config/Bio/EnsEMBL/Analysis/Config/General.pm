5d8cd16c440855f8e7de70520fb940c170e2587f
Amy Tang <at6@sanger.ac.uk>
2008-12-02 17:09:59 +0000

    Updated directory path for FirstEF parser and parameters.

--- test_system/config/Bio/EnsEMBL/Analysis/Config/General.pm
+++ test_system/config/Bio/EnsEMBL/Analysis/Config/General.pm
@@ -1,3 +1,38 @@
+# Ensembl module for Bio::EnsEMBL::Analysis::Config::General
+#
+# Copyright (c) 2004 Ensembl
+#
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::Config::General
+
+=head1 SYNOPSIS
+    use Bio::EnsEMBL::Analysis::Config::General;
+    use Bio::EnsEMBL::Analysis::Config::General qw();
+
+=head1 DESCRIPTION
+
+    General analysis configuration.
+
+    It imports and sets a number of standard global variables into the
+    calling package. Without arguments all the standard variables are set,
+    and with a list, only those variables whose names are provided are set.
+    The module will die if a variable which doesn\'t appear in its
+    C<%Config> hash is asked to be set.
+
+   The variables can also be references to arrays or hashes.
+
+         Edit C<%Config> to add or alter variables.
+
+         All the variables are in capitals, so that they resemble environment
+         variables.
+
+=head1 CONTACT
+
+B<ensembl-dev@ebi.ac.uk>
+
+=cut
+         
 package Bio::EnsEMBL::Analysis::Config::General;
 
 use strict;
@@ -10,8 +45,22 @@ use vars qw(%Config);
            DATA_DIR => '/usr/local/ensembl/data',
            LIB_DIR  => '/usr/local/ensembl/lib',
 
+           # Path where the parser and parameter files for FirstEF program are allocated
+           PARAMETERS_DIR => '/vol/software/linux-i386/farm/lib/firstef/parameters/',
+           PARSE_SCRIPT => '/vol/software/linux-i386/farm/lib/firstef/FirstEF_parser.pl',
+
+           
+           # The default directory the Runnable runs its analysis in
            ANALYSIS_WORK_DIR => '/tmp',
-           ANALYSIS_REPEAT_MASKING => ['RepeatMask'],      
+           ANALYSIS_REPEAT_MASKING => ['RepeatMask'],
+  
+           CORE_VERBOSITY => 'WARNING',
+           LOGGER_VERBOSITY => 'OFF',
+           #the two versbosity values control when commands like warning or logger_info
+           #print to screen. The current settings give you most of what you want but
+           #look at Bio::EnsEMBL::Utils::Exception and
+           #Bio::EnsEMBL::Analysis::Tools::Logger for more info
+           
           );
 
 
