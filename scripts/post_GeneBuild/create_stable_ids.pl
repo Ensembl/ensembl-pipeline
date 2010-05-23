4d64a6e3e47619ee7683f7862bf2066d7fe9938b
Jan-hinnerk Vogel <jhv@sanger.ac.uk>
2010-05-23 11:45:13 +0000

    evil eval changed ( caused to not produce stable ids for genes without translations ); option -sql added to write sql statements

--- scripts/post_GeneBuild/create_stable_ids.pl
+++ scripts/post_GeneBuild/create_stable_ids.pl
@@ -3,19 +3,23 @@
 use strict;
 use Getopt::Long;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Utils::Exception qw(throw); 
 use Bio::SeqIO;
 use Getopt::Long;
+#use Bio::EnsEMBL::Pipeline::ESTConf;
 
 my $dbhost;
 my $dbuser    = 'ensro';
 my $dbname;
-my $label;
+my $label; 
+my $sql; 
 my $dbport = '3306' ;
 &GetOptions(
 	    'dbname:s'    => \$dbname,
 	    'dbhost:s'    => \$dbhost,
             'dbport:i'    => \$dbport,
 	    'label:s'     => \$label,
+            'sql!'        => \$sql, 
 
 	   );
 
@@ -39,6 +43,8 @@ my $transcript_file  = "transcript_stable_id";
 my $translation_file = "translation_stable_id";
 my $exon_file        = "exon_stable_id";
 
+print "Results will be written to $gene_file $transcript_file etc\n";  
+
 open (GENE,">$gene_file")               or die("unable to open file $gene_file");
 open (TRANSCRIPT,">$transcript_file")   or die("unable to open file $transcript_file");
 open (TRANSLATION,">$translation_file") or die("unable to open file $translation_file");
@@ -53,20 +59,23 @@ my %seen_exon;
 my %seen_transcript;
 
 my $gene_adaptor = $db->get_GeneAdaptor;
-foreach my $gene_id( @{$gene_adaptor->list_dbIDs} ) {
-  
-  #print STDERR "gene id $gene_id\n";
+foreach my $gene_id( @{$gene_adaptor->list_dbIDs} ) { 
+
+    my $gene = $db->get_GeneAdaptor->fetch_by_dbID($gene_id); 
   eval {
-    my $gene = $db->get_GeneAdaptor->fetch_by_dbID($gene_id);
-    
+
     ####################
     # create gene id 
     ####################
     $count_gene++;
     my $gene_id = $gene->dbID;
     my $gene_stable_id = &make_stable_id($label."G",$gene);
-    #  927491 | ENSestG00000000001 |       1 | 0000-00-00 00:00:00 | 0000-00-00 00:00:00 |
-    print GENE "$gene_id\t$gene_stable_id\t1\t0\t0\n";
+    #  927491 | ENSestG00000000001 |       1 | 0000-00-00 00:00:00 | 0000-00-00 00:00:00 |  
+    if ( $sql ) { 
+      make_sql($gene_id, $gene_stable_id, "gene") ;  
+    } else { 
+      print GENE "$gene_id\t$gene_stable_id\t1\t0\t0\n";
+    }
     
     #######################
     # create transcript id
@@ -76,30 +85,47 @@ foreach my $gene_id( @{$gene_adaptor->list_dbIDs} ) {
       my $transcript_id = $trans->dbID;
       my $transcript_stable_id = &make_stable_id($label."T",$trans);
       #  927491 | ENSestT00000000001 |       1 
+     if ( $sql ) { 
+      make_sql($transcript_id, $transcript_stable_id, "transcript") ;  
+     } else  { 
       print TRANSCRIPT "$transcript_id\t$transcript_stable_id\t1\n";
-      
+     } 
       #######################
       # create translation id
-      #######################
-      my $translation           = $trans->translation;
-      my $translation_id        = $translation->dbID;
-      my $translation_stable_id = &make_stable_id($label."P",$translation);
-      #  927491 | ENSestP00000000001 |       1 
-      print TRANSLATION "$translation_id\t$translation_stable_id\t1\n";
+      ####################### 
+      if ( defined  $trans->translation ) { 
+        my $translation           = $trans->translation;
+        my $translation_id        = $translation->dbID;
+        my $translation_stable_id = &make_stable_id($label."P",$translation);
+        #  927491 | ENSestP00000000001 |       1 
+        if ( $sql ) { 
+         make_sql($translation_id, $translation_stable_id, "translation") ;   
+        } else { 
+          print TRANSLATION "$translation_id\t$translation_stable_id\t1\n"; 
+        }
+      }
     }
 
     ####################
     # create exon ids 
     ####################
-    my @exons = @{$gene->get_all_Exons};
+    my @exons = @{$gene->get_all_Exons}; 
+    if  ( scalar(@exons) == 0 ) {  
+        throw( "gene " . $gene->dbID . " " . $gene->biotype . " has 0 exons \n") ;  
+    } 
     foreach my $exon ( @exons ){
       $count_exon++;
       my $exon_id = $exon->dbID;
       my $exon_stable_id = &make_stable_id($label."E",$exon);
       #  927491 | ENSestE00000000001 |       1 
-      print EXON "$exon_id\t$exon_stable_id\t1\t0\t0\n";
+        if ( $sql ) { 
+          make_sql($exon_id, $exon_stable_id, "exon") ;   
+        } else {  
+          print EXON "$exon_id\t$exon_stable_id\t1\t0\t0\n";
+        }
     }
-  }
+  }; 
+   warn $@ if $@;
 }
 
 close EXON;
@@ -108,7 +134,14 @@ close TRANSLATION;
 close GENE;
 
 
-print STDERR "Files gene_stable_id, transcript_stable_id, translation_stable_id , exon_stable_id written\n"; 
+print STDERR "Files gene_stable_id, transcript_stable_id, translation_stable_id , exon_stable_id written\n";  
+
+
+sub make_sql{ 
+  my ($id, $stable_id,$table) = @_ ;  
+
+  print "insert into $table\_stable_id values ( $id, \"$stable_id\",1,now(),now());\n";;
+}
  
 sub make_stable_id{
   my ($label,$object) = @_;
