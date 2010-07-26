3d6169060d8944932e03a770f72669b01537bcca
Amonida Zadissa <amonida@ebi.ac.uk>
2010-07-26 14:40:55 +0000

    Fixed the formatting.

--- scripts/DataConversion/mitochondria/MitConf.pm
+++ scripts/DataConversion/mitochondria/MitConf.pm
@@ -5,55 +5,63 @@ use vars qw( %MitConf );
 
 
 %MitConf = (
-	    #location of genbank file
-	    MIT_GENBANK_FILE => '/lustre/scratch103/ensembl/ba1/releases/v57/rat_MT_AC_000022.gb',
-	    # database to put sequnece and genes into
-	    MIT_DBNAME => 'ba1_rattus_norvegicus_core_57_34y',
-	    MIT_DBHOST => 'genebuild3',
-	    MIT_DBUSER => 'ensadmin',
-	    MIT_DBPASS => 'ensembl',
-	    MIT_DBPORT => '3306',
-	    # logic name of analysis object to be assigned to coding genes
-	    # Non coding genes are also assinged this logic_name 
-	    MIT_LOGIC_NAME => 'MT_genbank_import',
-            # name of sequence downloaded from genbank eg NC_001665
-            #  OR you can include the full http path if you like 
-            #  eg http://www.ncbi.nlm.nih.gov/nuccore/NC_001665
-            MIT_DB_FILE => 'http://www.ncbi.nlm.nih.gov/nuccore/NC_XXXX',
-	    # if want the verbose output and sequence output after the load
-	    MIT_DEBUG => '',
-	    # Name of the mitochondrial chromosome
-	    MIT_NAME => 'MT',
-	    # Names of the sequences for the coord systems
-	    # Will try and parse this information out of the 
-	    # Genbank file if left blank
-	    MIT_CONTIG_SEQNAME => '',
-	    MIT_CHROMOSOME_SEQNAME => '',
-	    MIT_SUPERCONTIG_SEQNAME => '',
-	    MIT_CLONE_SEQNAME => '',
-	    # Name of top level in coord system
-	    MIT_TOPLEVEL => 'chromosome',
-	    # Different oganisms use different mitochondial codons:
-	    # Vertebrate Mitochondrial (2)
-	    # Yeast Mitochondrial (3)
-	    # Mold, Protozoan, Coelenterate Mito. and Myco/Spiroplasma (4)
-	    # Invertebrate Mitochondrial (5)
-	    # Ciliate Nuclear, Dasycladacean Nuclear, Hexamita Nuclear (6)
-	    # Echinoderm Mitochondrial (9)
-	    # Euploid Nuclear (10)
-	    # Bacterial (11)
-	    # Alternative Yeast Nuclear (12)
-	    # Ascidian Mitochondrial (13)
-	    # Flatworm Mitochondrial (14)
-	    # Blepharisma Macronuclear (15)
-	    # Chlorophycean Mitochondrial (16)
-	    # Trematode Mitochondrial (21)
-	    MIT_CODON_TABLE  =>  '2',
-	    # Types
-	    MIT_GENE_TYPE => 'protein_coding',
-	    MIT_TRNA_TYPE => 'Mt_tRNA',
-	    MIT_RRNA_TYPE => 'Mt_rRNA',
-	   );
+  #location of genbank file
+  MIT_GENBANK_FILE => '/lustre/scratch103/ensembl/ba1/releases/v57/rat_MT_AC_000022.gb',
+
+  # database to put sequnece and genes into
+  MIT_DBNAME => 'ba1_rattus_norvegicus_core_57_34y',
+  MIT_DBHOST => 'genebuild3',
+  MIT_DBUSER => 'ensadmin',
+  MIT_DBPASS => 'ensembl',
+  MIT_DBPORT => '3306',
+
+  # logic name of analysis object to be assigned to coding genes
+  # Non coding genes are also assinged this logic_name
+  MIT_LOGIC_NAME => 'MT_genbank_import',
+
+  # name of sequence downloaded from genbank eg NC_001665
+  # OR you can include the full http path if you like
+  # eg http://www.ncbi.nlm.nih.gov/nuccore/NC_001665
+  MIT_DB_FILE => 'http://www.ncbi.nlm.nih.gov/nuccore/NC_XXXX',
+
+  # if want the verbose output and sequence output after the load
+  MIT_DEBUG => '',
+
+  # Name of the mitochondrial chromosome
+  MIT_NAME => 'MT',
+
+  # Names of the sequences for the coord systems
+  # Will try and parse this information out of the
+  # Genbank file if left blank
+  MIT_CONTIG_SEQNAME      => '',
+  MIT_CHROMOSOME_SEQNAME  => '',
+  MIT_SUPERCONTIG_SEQNAME => '',
+  MIT_CLONE_SEQNAME       => '',
+
+  # Name of top level in coord system
+  MIT_TOPLEVEL => 'chromosome',
+
+  # Different oganisms use different mitochondial codons:
+  # Vertebrate Mitochondrial (2)
+  # Yeast Mitochondrial (3)
+  # Mold, Protozoan, Coelenterate Mito. and Myco/Spiroplasma (4)
+  # Invertebrate Mitochondrial (5)
+  # Ciliate Nuclear, Dasycladacean Nuclear, Hexamita Nuclear (6)
+  # Echinoderm Mitochondrial (9)
+  # Euploid Nuclear (10)
+  # Bacterial (11)
+  # Alternative Yeast Nuclear (12)
+  # Ascidian Mitochondrial (13)
+  # Flatworm Mitochondrial (14)
+  # Blepharisma Macronuclear (15)
+  # Chlorophycean Mitochondrial (16)
+  # Trematode Mitochondrial (21)
+  MIT_CODON_TABLE => '2',
+
+  # Types
+  MIT_GENE_TYPE => 'protein_coding',
+  MIT_TRNA_TYPE => 'Mt_tRNA',
+  MIT_RRNA_TYPE => 'Mt_rRNA', );
 
 sub import {
     my ($callpack) = caller(0); # Name of the calling package
