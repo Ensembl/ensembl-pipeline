dfe2e2dd577865d393417ef9f73fb8107d98fd20
Matthew Astley <mca@sanger.ac.uk>
2011-01-07 11:38:42 +0000

    it's broken, nobble it until its extra feature is implemented in show_sequence_set or this is fixed.
    but before that, made some usability improvements for people who don't
    have -first and -last values to hand.

--- scripts/Finished/make_subregion_agp.pl
+++ scripts/Finished/make_subregion_agp.pl
@@ -27,8 +27,8 @@ recovered from the ~/.netrc file. See the Net::Netrc module for more details.
     -cs_name (default:chromosome) the name of the coordinate system being stored
     -cs_version (default:Otter) the version of the chromosome coordinate system being stored
     -set	 the sequence set name
-    -first	first accession in the subregion
-    -last	last accession in the subregion
+    -first	first accession in the subregion (regexp)
+    -last	last accession in the subregion (regexp)
     -chr	(optional) the chromosome name to be put in the agp
     -help|h      displays this documentation with PERLDOC
 
@@ -48,6 +48,23 @@ Here is a command line example:
 	-last	FP015984 \
 	-chr	1
 
+
+To find out whether the contigs from clones in the assembly are sorted
+or jumbled, try
+
+  make_subregion_agp.pl -host otterlive -port 3301 -user ottadmin -pass mumble \
+   -dbname loutre_pig -set chrX-05 -chr pigX5 -first FP236733 -last CU928280 \
+    |cut -f6 | cut -d. -f1 | uniq | sort | uniq -c | sort -rn
+  # Take the accession names and uniq them twice.
+  # When each clone is used in one place it has prefix "1" in output.
+
+To count the contigs per clone just remove the first uniq:
+
+  make_subregion_agp.pl -host otterlive -port 3301 -user ottadmin -pass mumble \
+   -dbname loutre_pig -set chrX-05 -chr pigX5 -first FP236733 -last CU928280 \
+    |cut -f6 | cut -d. -f1 | sort | uniq -c | sort -rn
+
+
 =head1 CONTACT
 
 Mustapha Larbaoui B<email> ml6@sanger.ac.uk
@@ -61,6 +78,28 @@ use Net::Netrc;
 use Bio::SeqIO;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
+
+
+die <<B0RK;
+Instead of this script, use
+  ensembl-otter/scripts/lace/show_sequence_set -agp -dataset pig -set chrX-05
+
+
+Problems with $0,
+
+  It cannot output gaps.
+
+  The "-last" clone will be truncated to one contig.
+
+  It assumes the contig seq_region.name is accession.version .
+
+ The feature this script has which show_sequence_set lacks is the
+ -first / -last limit.  If this is needed, to correctly output the
+ whole of the last clone it will be necessary either to peek the
+ assembly for (max(cmp_end) where clonename = ?) OR cut the tail after
+ reading the whole assembly.
+
+B0RK
 
 # loutre connexion parameters, default values.
 my $host = 'otterlive';
@@ -96,7 +135,9 @@ my $usage = sub { exec( 'perldoc', $0 ); };
 
 throw("No sequence set name given") unless ($set);
 
-throw("Must provide first and last accessions of the subregion") unless $first && $last;
+# If -first or -last are not provided, we should read the chromosome
+# anyway and tell what we saw
+foreach ($first, $last) { $_ = "^notprovided" unless defined $_ }
 
 throw("Can't connect to a database without a dbname") unless $name;
 
@@ -137,8 +178,13 @@ my $sth = $dba->dbc->prepare($agp_sql);
 $sth->execute($set);
 
 my ($region,$s_chr_end,$f,$l);
+my ($input_first, $input_last); # currently just for diagnostics
 
 while(my ($chr_start, $chr_end, $clone, $clone_start, $clone_end, $ori) = $sth->fetchrow) {
+
+	$input_first = $clone unless defined $input_first;
+	$input_last = $clone;
+
 	next unless $clone =~ /$first/ || $f;$f =1;
 
 	if($s_chr_end && (my $gap = $chr_start-$s_chr_end-1)) {
@@ -149,9 +195,10 @@ while(my ($chr_start, $chr_end, $clone, $clone_start, $clone_end, $ori) = $sth->
 	if($clone =~ /$last/) { $l = 1 ; last }
 }
 
-throw("First accession $first not found in chromosome $set") unless ($f);
-throw("Last accession $last not found in chromosome $set") unless ($l);
+throw("First accession $first not found in chromosome $set\n (input contained first=$input_first, last=$input_last)") unless ($f);
+throw("Last accession $last not found in chromosome $set\n (input contained first=$input_first, last=$input_last)") unless ($l);
 
+print $region; die;
 open my $agp, "| regions_to_agp ".($chromosome ? "-chr $chromosome " : "") or die "Can't open pipe to regions_to_agp; $!";
 print $agp $region;
 close $agp or die "Error running regions_to_agp; exit($?)";
