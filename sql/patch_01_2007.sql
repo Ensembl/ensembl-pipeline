1819a8646d327e88232e60bf563e25b3c92056ae
Rishi Nag <rn6@sanger.ac.uk>
2012-11-06 10:15:48 +0000

    Ch-ch-ch-CHANGEs not CHAGE

--- sql/patch_01_2007.sql
+++ sql/patch_01_2007.sql
@@ -3,6 +3,6 @@
 # checkout before January 2007 . 'condition' has become a reseved word in mysql 
 # ( changed in january 2007 ) 
  
-ALTER TABLE rule_conditions CHAGE condition rule_condition VARCHAR(40)  ; 
+ALTER TABLE rule_conditions CHANGE condition rule_condition VARCHAR(40)  ; 
 
 
