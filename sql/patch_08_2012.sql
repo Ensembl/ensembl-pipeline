8e80866d0c15f85463ee5915a7e9a578f717f1fd
Rishi Nag <rn6@sanger.ac.uk>
2012-11-06 10:38:43 +0000

    job_status not job update

--- sql/patch_08_2012.sql
+++ sql/patch_08_2012.sql
@@ -1,7 +1,7 @@
 # $Source: /tmp/ENSCOPY-ENSEMBL-PIPELINE/sql/patch_08_2012.sql,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # This patch needs to be applied to reference databases if they used an ensembl-pipeline 
 # checkout before August 2012. An info column has been added to the job table.
 
-ALTER TABLE job ADD info varchar(200) DEFAULT '' ;
+ALTER TABLE job_status ADD info varchar(200) DEFAULT '' ;
 
