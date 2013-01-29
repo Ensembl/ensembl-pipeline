050cb1a19582979a0ce1e1b438d94cb6c1c1a03b
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2013-01-29 14:59:48 +0000

    Bring into line with "rule_setup.pl".

--- scripts/analysis_setup.pl
+++ scripts/analysis_setup.pl
@@ -1,80 +1,73 @@
 #!/usr/bin/env perl
 # $Source: /tmp/ENSCOPY-ENSEMBL-PIPELINE/scripts/analysis_setup.pl,v $
-# $Revision: 1.12 $
-# $Id: analysis_setup.pl,v 1.12 2012-12-21 14:40:14 rn6 Exp $
-
-# POD documentation - main docs before the code
-
-=pod
+# $Revision: 1.13 $
+# $Id: analysis_setup.pl,v 1.13 2013-01-29 14:59:48 ak4 Exp $
 
 =head1 NAME
 
-Analysis setup
+analysis_setup.pl
 
 =head1 SYNOPSIS
- 
-Script for writing an analysis table and generate the config needed to
-generate an analysis table.
+
+Script for writing an analysis table given a configuration file, or the
+other way around.
 
 =head1 DESCRIPTION
 
-This script will both take a config file and write to an analysis table
-and take an analysis table and write a config file, it will work with
-both straight core dbs and core dbs with pipeline tables
-
+This script will take a configuration file and write to an analysis table
+or take an analysis table and write a configuration file.
 
 =head1 OPTIONS
 
-Database options  
+Database options:
 
-  -dbhost   host name for database (gets put as host= in locator)
-  -dbport   what port to connect to (port= in locator) (optional)
-  -dbname   what name to connect to (dbname= in locator)
-  -dbuser   what username to connect as (dbuser= in locator)
-  -dbpass   what password to use (dbpass= in locator)
+  -dbhost    Host name for database.
+  -dbport    Port to connect to (optional).
+  -dbname    Database to connect to.
+  -dbuser    Username to connect as.
+  -dbpass    Password to use.
 
-Other options
+Other options:
 
-  -read     Read a config file and write to the database.
+  -read     Read a configuration file and write to the database.
+  -write    Read the rule table and write a configuration file.
+  -file     File to read from or write too.
 
-  -write    Read the analysis table and write a config file.
 
-  -file     Path to the file you either want to read from or write too.
-
-  -pipeline_db  Indicates your database contains pipeline tables and the
-                pipeline dbadaptor should be used, this is on by default
+  -pipeline_db  Your database contains pipeline tables and the pipeline
+                database adaptor should be used, this is on by default
                 but can be turned off with the -nopipeline_db option.
 
-  -update   Update any existing analyses with the data in the config
-            file.
+  -update   Update any existing analyses with the data in the
+            configuration file.
 
-  -help     Display help.
+  -help     Displays help text.
 
 =head1 EXAMPLES
 
-This will generate a config file based on the analysis table of the 
-database it is pointed at
+To generate a configuration file based on the analysis table of the
+database it is pointed at:
 
   ./analysis setup -dbhost ecs1b -dbuser ensadmin -dbpass **** \
                    -dbname my_pipeline_db -dbport 3306 -write \
-                   -file analysis.conf 
+                   -file analysis.conf
 
-This will fill in an analysis table based on the config file
+To fill in an analysis table based on the configuration file:
 
   ./analysis setup -dbhost ecs1b -dbuser ensadmin -dbpass **** \
                    -dbname my_pipeline_db -dbport 3306 -read \
-                   -file analysis.conf 
+                   -file analysis.conf
 
-This will fill in an analysis table but leave the associated pipeline
-tables alone
+To fill in an analysis table but leave the associated pipeline tables
+alone:
 
   ./analysis setup -dbhost ecs1b -dbuser ensadmin -dbpass **** \
                    -dbname my_pipeline_db -dbport 3306 -read \
                    -file analysis.conf -nopipeline_db
 
 
-This is what a conf entry should like like.  The header should be the
-logic_name, the rest of the columns should lie in key value pairs.
+This is what a conf entry should like like.  The header for each section
+should be the analysis logic name, the rest is key-value pairs:
 
   [RepeatMask]
   db=repbase
@@ -83,24 +76,30 @@ logic_name, the rest of the columns should lie in key value pairs.
   program=RepeatMasker
   program_version=1
   program_file=RepeatMasker
-  parameters=-low, -lib, /path/to/file/briggsae.lib 
+  parameters=-low, -lib, /path/to/file/briggsae.lib
   module=RepeatMasker
   module_version=1
   gff_source=RepeatMasker
   gff_feature=Repeat
   input_id_type=CONTIG
 
-There is an example file in this directory called example_analysis.conf.
+There is an example file in this directory called "example_analysis.conf".
 
 =cut
 
 use strict;
 use warnings;
 
+use Getopt::Long;
+
 use AnalysisCreation;
 use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
-use Getopt::Long;
+
+sub usage {
+  exec( 'perldoc', $0 );
+  exit;
+}
 
 my $dbhost;
 my $dbuser;
@@ -114,21 +113,21 @@ my $help;
 my $pipeline = 1;
 my $update;
 
-GetOptions( 'dbhost=s'     => \$dbhost,
-            'dbname=s'     => \$dbname,
-            'dbuser=s'     => \$dbuser,
-            'dbpass=s'     => \$dbpass,
-            'dbport=s'     => \$dbport,
-            'read!'        => \$read,
-            'write!'       => \$write,
-            'pipeline_db!' => \$pipeline,
-            'update!'      => \$update,
-            'file=s'       => \$file,
-            'h|help!'      => \$help,
-  ) or
+if ( !GetOptions( 'dbhost=s'     => \$dbhost,
+                  'dbname=s'     => \$dbname,
+                  'dbuser=s'     => \$dbuser,
+                  'dbpass=s'     => \$dbpass,
+                  'dbport=s'     => \$dbport,
+                  'read!'        => \$read,
+                  'write!'       => \$write,
+                  'pipeline_db!' => \$pipeline,
+                  'update!'      => \$update,
+                  'file=s'       => \$file,
+                  'h|help!'      => \$help, ) ||
+     $help )
+{
   usage();
-
-
+}
 
 if ( !($dbhost) || !($dbuser) || !($dbname) ) {
   print STDERR
@@ -136,13 +135,13 @@ if ( !($dbhost) || !($dbuser) || !($dbname) ) {
   print STDERR
     "-dbhost $dbhost -dbuser $dbuser -dbpass $dbpass -dbname" .
     " $dbname -dbport $dbport\n";
-  $help = 1;
+  usage();
 }
 
 if ( ( !($read) && !($write) ) || ( $read && $write ) ) {
   print STDERR "you need to define either read or write on the " .
     "commandline but you shouldn't define both\n";
-  $help = 1;
+  usage();
 }
 
 if ( !$file ) {
@@ -150,46 +149,34 @@ if ( !$file ) {
     "You need to pass a file name which either represents a " .
     "analysis config file to be read and stored in the database or " .
     "written based on the analysis objects in the database\n";
-  $help = 1;
+  usage();
 }
-
-if ($help) { usage() }
 
 my $db;
-if($pipeline){
-  $db  = new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor
-    (-host => $dbhost,
-     -user => $dbuser,
-     -pass => $dbpass,
-     -dbname => $dbname,
-     -port => $dbport,
-    );
-}else{
-  $db  = new Bio::EnsEMBL::DBSQL::DBAdaptor
-    (-host => $dbhost,
-     -user => $dbuser,
-     -pass => $dbpass,
-     -dbname => $dbname,
-     -port => $dbport,
-    );
+
+if ($pipeline) {
+  $db =
+    new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor( -host   => $dbhost,
+                                                  -user   => $dbuser,
+                                                  -pass   => $dbpass,
+                                                  -dbname => $dbname,
+                                                  -port   => $dbport, );
+}
+else {
+  $db =
+    new Bio::EnsEMBL::DBSQL::DBAdaptor( -host   => $dbhost,
+                                        -user   => $dbuser,
+                                        -pass   => $dbpass,
+                                        -dbname => $dbname,
+                                        -port   => $dbport, );
 }
 
-
-
-if($read){
-  my @analyses = @{&parse_files($file)};
-  &write_into_db($db, \@analyses, $update);
+if ($read) {
+  my @analyses = @{ parse_files($file) };
+  write_into_db( $db, \@analyses, $update );
 }
-
-if($write){
-  my $analyses = &read_db($db);
-  $analyses = [sort {$a->dbID <=> $b->dbID} @$analyses];
-  &write_file($file, $analyses);
+else {
+  my $analyses = read_db($db);
+  $analyses = [ sort { $a->dbID() <=> $b->dbID() } @{$analyses} ];
+  write_file( $file, $analyses );
 }
-
-
-sub usage {
-  exec( 'perldoc', $0 );
-  exit;
-}
-
