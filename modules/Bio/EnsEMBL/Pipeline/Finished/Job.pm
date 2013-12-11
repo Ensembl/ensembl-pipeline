91ec03f5c30f72bf43f53442eb0438e34fe41f72
Rishi Nag <rn6@sanger.ac.uk>
2013-12-11 10:14:00 +0000

    Apache 2 licence

--- modules/Bio/EnsEMBL/Pipeline/Finished/Job.pm
+++ modules/Bio/EnsEMBL/Pipeline/Finished/Job.pm
@@ -1,3 +1,17 @@
+
+# Copyright [1999-2013] Genome Research Ltd. and the EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # Mar 9, 2006 10:20:44 AM
 #
 # Created by Mustapha Larbaoui <ml6@sanger.ac.uk>
--- modules/Bio/EnsEMBL/Pipeline/Finished/runner.pl
+++ modules/Bio/EnsEMBL/Pipeline/Finished/runner.pl
@@ -1,5 +1,21 @@
 #!/software/bin/perl
 
+
+# Copyright [1999-2013] Genome Research Ltd. and the EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # default Finished pipeline runner script
 # this script is passed as part of the batch submission request
 # and is therefore the first script to be executed on the
