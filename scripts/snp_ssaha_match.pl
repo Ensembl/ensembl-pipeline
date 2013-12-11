a83566d94fd17c2686756b607825ae6a148cb9e1
Rishi Nag <rn6@sanger.ac.uk>
2013-12-11 11:18:52 +0000

    Apache 2 licence added

--- scripts/snp_ssaha_match.pl
+++ scripts/snp_ssaha_match.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/env perl
+
+
+# Copyright [1999-2013] Genome Research Ltd. and the EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 #
 #This script run after snp_cross_match.pl. snp_cross_match.pl generate a list of refsnpid for which none of the clones #are on GP. snp_ssaha_match.pl by using ssaha method to locate the contigs on GP and then use cross_match to find
 #snp position
