ec1b17c771aab99b989afecda6a65fcec5c1200e
Rishi Nag <rn6@sanger.ac.uk>
2014-12-02 14:15:16 +0000

    TravisCI tests:compara added

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-external/modules:$PWD/ensembl-analysis/modules:$PWD/modules
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-external/modules:$PWD/ensembl-analysis/modules:$PWD/ensembl-compara/modules:$PWD/modules
 
 echo "Running test suite"
 echo "Using $PERL5LIB"
