55f854f2bba86f418b844714c2c66407e9c072a3
Thibaut Hourlier <thibaut@ebi.ac.uk>
2015-06-18 16:51:53 +0100

    Fixes to correct error reported by TravisCI

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-external/modules:$PWD/ensembl-analysis/modules:$PWD/ensembl-compara/modules:$PWD/modules
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-external/modules:$PWD/ensembl-analysis/scripts:$PWD/ensembl-analysis/modules:$PWD/ensembl-compara/modules:$PWD/modules:$PWD/ensembl-killlist/modules
 
 echo "Running test suite"
 echo "Using $PERL5LIB"
