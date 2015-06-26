3ccbb62a8440027371ccdde60d71265bbcf4d48c
Thibaut Hourlier <thibaut@ebi.ac.uk>
2015-06-26 15:01:09 +0100

    Yat 1

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-external/modules:$PWD/ensembl-analysis/scripts:$PWD/ensembl-analysis/modules:$PWD/ensembl-compara/modules:$PWD/modules:$PWD/ensembl-killlist/modules
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-external/modules:$PWD/ensembl-analysis/scripts:$PWD/ensembl-analysis/modules:$PWD/ensembl-compara/modules:$PWD/modules:$PWD/ensembl-killlist/modules:$PWD/bioperl-run/lib
 
 echo "Running test suite"
 echo "Using $PERL5LIB"
@@ -10,7 +10,19 @@ else
   find $PWD/ensembl-analysis/modules -type f -name '*.example' | while read f; do mv "$f" "${f%.example}"; done
   find $PWD/modules -type f -name '*.example' | while read f; do mv "$f" "${f%.example}"; done
   find $PWD/scripts -type f -name "*.pl" | xargs -i perl -c {}
-  find $PWD/modules -type f -name "*.pm" | xargs -i perl -c {} 
+  # Directory we don't want to check
+  printf "\e[31mWe will not test:\e[0m\n - \e[33m%s\e[0m\n" "Annacode modules"
+  RES="! -path \"*Finished*\""
+  for D in "GeneDuplication"; do
+      printf " \e[33m- %s\n\e[0m" "${D}"
+      RES="${RES} ! -path \"*${D}*\""
+  done
+# As long as EMBL parser has not been merge in ensembl-io master, we will avoid modules/Bio/EnsEMBL/Pipeline/SeqFetcher/UniProtKB.pm
+  for M in "modules/Bio/EnsEMBL/Pipeline/SeqFetcher/UniProtKB.pm"; do
+      printf " \e[33m- %s\n\e[0m" "${M}"
+      RES="${RES} ! -name \"*`basename ${M}`*\""
+  done
+  find $PWD/modules -type f -name "*.pm" "${RES}" | xargs -i perl -c {}
 fi
 rt=$?
 if [ $rt -eq 0 ]; then
